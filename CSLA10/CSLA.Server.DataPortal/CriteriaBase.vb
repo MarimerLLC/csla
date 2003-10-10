1250c715addf263f12b6e6c318428e5d2d29a6c4
Glamdring <Glamdring@fa5919ed-4f44-5740-a13a-40f41935fe31>
2003-10-10 19:17:50 +0000

    New class to support inheritance for code-gen purposes.

---
+++ CSLA10/CSLA.Server.DataPortal/CriteriaBase.vb
@@ -0,0 +1,17 @@
+''' <summary>
+''' Base type from which Criteria classes can be
+''' derived in a business class.
+''' </summary>
+<Serializable()> _
+Public MustInherit Class CriteriaBase
+  Public ObjectType As Type
+
+  ''' <summary>
+  ''' Initializes CriteriaBase with the type of
+  ''' business object to be created by the DataPortal.
+  ''' </summary>
+  Public Sub New(ByVal Type As Type)
+    ObjectType = Type
+  End Sub
+
+End Class

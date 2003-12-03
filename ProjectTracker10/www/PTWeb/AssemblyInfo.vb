2fd17f97e42ad7a97c725d42115742b41fde6c01
Glamdring <Glamdring@fa5919ed-4f44-5740-a13a-40f41935fe31>
2003-12-03 22:21:14 +0000

    Updated versions to 1.1

--- ProjectTracker10/www/PTWeb/AssemblyInfo.vb
+++ ProjectTracker10/www/PTWeb/AssemblyInfo.vb
@@ -29,4 +29,4 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.1.*")> 

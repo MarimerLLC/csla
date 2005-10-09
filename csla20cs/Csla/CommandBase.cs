4a4f7ee27ae07166e4b3e0525509e8f2b6cadcff
BrantE <BrantE@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-10-09 04:36:06 +0000

    *** empty log message ***

---
+++ csla20cs/Csla/CommandBase.cs
@@ -0,0 +1,126 @@
+using System;
+using System.ComponentModel;
+using Csla.Properties;
+
+namespace Csla
+{
+
+    /// <summary>
+    /// This is the base class from which command 
+    /// objects will be derived.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Command objects allow the execution of arbitrary server-side
+    /// functionality. Most often, this involves the invocation of
+    /// a stored procedure in the database, but can involve any other
+    /// type of stateless, atomic call to the server instead.
+    /// </para><para>
+    /// To implement a command object, inherit from CommandBase and
+    /// override the DataPortal_Update method. In this method you can
+    /// implement any server-side code as required.
+    /// </para><para>
+    /// To pass data to/from the server, use instance variables within
+    /// the command object itself. The command object is instantiated on
+    /// the client, and is passed by value to the server where the 
+    /// DataPortal_Update method is invoked. The command object is then
+    /// returned to the client by value.
+    /// </para>
+    /// </remarks>
+    [Serializable()]
+    public abstract class CommandBase
+    {
+
+        #region Constructors
+
+        protected CommandBase()
+        {
+
+        }
+
+        #endregion
+
+        #region Data Access
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "criteria")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        private void DataPortal_Create(object criteria)
+        {
+            throw new NotSupportedException(Resources.CreateNotSupportedException);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "criteria")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        private void DataPortal_Fetch(object criteria)
+        {
+            throw new NotSupportedException(Resources.FetchNotSupportedException);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        private void DataPortal_Update()
+        {
+            throw new NotSupportedException(Resources.UpdateNotSupportedException);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "criteria")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        private void DataPortal_Delete(object criteria)
+        {
+            throw new NotSupportedException(Resources.DeleteNotSupportedException);
+        }
+
+        /// <summary>
+        /// Override this method to implement any server-side code
+        /// that is to be run when the command is executed.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1707:IdentifiersShouldNotContainUnderscores", MessageId = "Member")]
+        protected virtual void DataPortal_Execute()
+        {
+            throw new NotSupportedException(Resources.ExecuteNotSupportedException);
+        }
+
+        /// <summary>
+        /// Called by the server-side DataPortal prior to calling the 
+        /// requested DataPortal_xyz method.
+        /// </summary>
+        /// <param name="e">The DataPortalContext object passed to the DataPortal.</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1707:IdentifiersShouldNotContainUnderscores", MessageId = "Member")]
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        protected virtual void DataPortal_OnDataPortalInvoke(DataPortalEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Called by the server-side DataPortal after calling the 
+        /// requested DataPortal_xyz method.
+        /// </summary>
+        /// <param name="e">The DataPortalContext object passed to the DataPortal.</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1707:IdentifiersShouldNotContainUnderscores", MessageId = "Member")]
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        protected virtual void DataPortal_OnDataPortalInvokeComplete(DataPortalEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Called by the server-side DataPortal if an exception
+        /// occurs during server-side processing.
+        /// </summary>
+        /// <param name="e">The DataPortalContext object passed to the DataPortal.</param>
+        /// <param name="ex">The Exception thrown during processing.</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1707:IdentifiersShouldNotContainUnderscores", MessageId = "Member")]
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        protected virtual void DataPortal_OnDataPortalException(DataPortalEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+    }
+}

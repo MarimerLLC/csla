09d5ded2b4cb00a9f8645136797ac9729ac33a8c
BrantE <BrantE@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-10-12 03:06:51 +0000

    *** empty log message ***

--- csla20cs/Csla/DataPortal/Client/IDataPortalProxy.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Csla.DataPortalClient
-{
-    public interface IDataPortalProxy : Server.IDataPortalServer
-    {
-        bool IsServerRemote { get;}
-    }
-}
--- csla20cs/Csla/EnterpriseServicesSettings.cs
+++
@@ -1,7 +0,0 @@
-using System.EnterpriseServices;
-
-// EnterpriseServices settings
-[assembly: ApplicationActivation(ActivationOption.Library)]
-[assembly: ApplicationName("CSLA .NET DataPortal")]
-[assembly: Description("CSLA .NET Serviced DataPortal")]
-[assembly: ApplicationAccessControl(false)]

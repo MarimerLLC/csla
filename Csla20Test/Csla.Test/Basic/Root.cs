6ffa4a49aa900da937df5f27f1705fcbca5fbd53
ScottSc <ScottSc@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-09 20:09:57 +0000

    *** empty log message ***

--- Csla20Test/Csla.Test/Basic/Root.cs
+++ Csla20Test/Csla.Test/Basic/Root.cs
@@ -4,7 +4,155 @@ using System.Text;
 
 namespace Csla.Test.Basic
 {
-    class Root
+    [Serializable()]
+    class Root : BusinessBase<Root>
     {
+        private string _data;
+
+        private Children _children = Csla.Test.Children.NewChildren();
+
+        protected override object GetIdValue()
+        {
+            return _data;
+        }
+
+        public string Data
+        {
+            get
+            {
+                if (CanReadProperty())
+                {
+                    return _data;
+                }
+                else
+                {
+                    throw new System.Security.SecurityException("Property get not allowed");
+                }
+            }
+            set
+            {
+                if (CanWriteProperty())
+                {
+                    if (_data != value)
+                    {
+                        _data = value;
+                        PropertyHasChanged();
+                    }
+                }
+                else
+                {
+                    throw new System.Security.SecurityException("Property set not allowed");
+                }
+            }
+        }
+
+        public Children Children
+        {
+            get { return _children; }
+        }
+
+        public override bool IsDirty
+        {
+            get { return base.IsDirty || _children.IsDirty(); }
+        }
+
+        [Serializable()]
+        private class Criteria
+        {
+            public string _data;
+
+            public Criteria()
+            {
+                _data = "<new>";
+            }
+
+            public Criteria(string data)
+            {
+                this._data = data;
+            }
+        }
+
+        public static Root NewRoot()
+        {
+            return DataPortal.Create<Root>(new Criteria());
+        }
+
+        public static Root GetRoot(string data)
+        {
+            return DataPortal.Fetch<Root>(new Criteria(data));
+        }
+
+        public static void DeleteRoot(string data)
+        {
+            DataPortal.Delete(new Criteria(data));
+        }
+
+        private Root()
+        {
+            //prevent direct creation
+        }
+
+        protected override void DataPortal_Create(object criteria)
+        {
+            Criteria crit = (Criteria)(criteria);
+            _data = crit._data;
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Created");
+        }
+
+        protected override void DataPortal_Fetch(object criteria)
+        {
+            Critera crit = (Criteria)(criteria);
+            _data = crit._data;
+            MarkOld();
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Fetched");
+        }
+
+        protected override void DataPortal_Insert()
+        {
+            Csla.ApplicationContext.GlobalContext.Add("clientcontext",
+                ApplicationContext.ClientContext.Item("clientcontext"));
+
+            Csla.ApplicationContext.GlobalContext.Add("globalcontext",
+                ApplicationContext.GlobalContext.Item("globalcontext"));
+
+            ApplicationContext.GlobalContext.Remove("globalcontext");
+            ApplicationContext.GlobalContext.Item("globalcontext") = "new global value";
+
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Inserted");
+        }
+
+        protected override void DataPortal_Update()
+        {
+            //we would update here
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Updated");
+        }
+
+        protected override void DataPortal_DeleteSelf()
+        {
+            //we would delete here
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Deleted self");
+        }
+
+        protected override void DataPortal_Delete(object criteria)
+        {
+            //we would delete here
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Deleted");
+        }
+
+        protected override void OnDeserialized(System.Runtime.Serialization.StreamingContext context)
+        {
+            base.OnDeserialized(context);
+            Csla.ApplicationContext.GlobalContext.Add("Deserialized", "root deserialized");
+        }
+
+        protected override void DataPortal_OnDataPortalInvoke(DataPortalEventArgs e)
+        {
+            Csla.ApplicationContext.GlobalContext("dpinvoke") = ApplicationContext.GlobalContext("global");
+        }
+
+        protected override void DataPortal_OnDataPortalInvokeComplete(DataPortalEventArgs e)
+        {
+            Csla.ApplicationContext.GlobalContext("dpinvokecomplete") = ApplicationContext.GlobalContext("global");
+        }    
     }
 }

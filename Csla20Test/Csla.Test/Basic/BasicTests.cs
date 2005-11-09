2122cc2fd92d78901104ffab73d962cd2148298b
ScottSc <ScottSc@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-09 22:29:32 +0000

    *** empty log message ***

--- Csla20Test/Csla.Test/Basic/BasicTests.cs
+++ Csla20Test/Csla.Test/Basic/BasicTests.cs
@@ -1,10 +1,65 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace Csla.Test.Basic
 {
-    class BasicTests
+    [TestFixture]
+    public class BasicTests
     {
+        [Test]
+        public void CreateGenRoot()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            GenRoot root;
+            root = GenRoot.NewRoot();
+            Assert.IsNotNull(root);
+            Assert.AreEqual("<new>", root.Data);
+            Assert.AreEqual("Created", Csla.ApplicationContext.GlobalContext["Root"]);
+            Assert.AreEqual(true, root.IsNew);
+            Assert.AreEqual(false, root.IsDeleted);
+            Assert.AreEqual(true, root.IsDirty);
+        }
+
+        [Test]
+        public void CreateRoot()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            Root root;
+            root = Csla.Test.Basic.Root.NewRoot();
+            Assert.IsNotNull(root);
+            Assert.AreEqual("<new>", root.Data);
+            Assert.AreEqual("Created", Csla.ApplicationContext.GlobalContext["Root"]);
+            Assert.AreEqual(true, root.IsNew);
+            Assert.AreEqual(false, root.IsDeleted);
+            Assert.AreEqual(true, root.IsDirty);
+        }
+
+        [Test]
+        public void AddChild()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            Root root = Csla.Test.Basic.Root.NewRoot();
+            root.Children.Add("1");
+            Assert.AreEqual(1, root.Children.Count);
+            Assert.AreEqual("1", root.Children[0]._data);
+        }
+
+        [Test]
+        public void AddRemoveChild()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            Root root = Csla.Test.Basic.Root.NewRoot();
+            root.Children.Add("1");
+            root.Children.Remove(root.Children[0]);
+            Assert.AreEqual(0, root.Children.Count);
+        }
+
+        [Test]
+        public void AddGrandChild()
+        { }
+
+
     }
 }
--- Csla20Test/Csla.Test/Basic/Children.cs
+++ Csla20Test/Csla.Test/Basic/Children.cs
@@ -1,10 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data;
 
 namespace Csla.Test.Basic
 {
-    class Children
+    [Serializable()]
+    public class Children : BusinessListBase<Children, Child>
     {
+        public void Add(string data)
+        {
+            this.Add(Child.NewChild(data));
+        }
+
+
+        internal static Children NewChildren()
+        {
+            return new Children();
+        }
+
+        internal static Children GetChildren(IDataReader dr)
+        {
+            //todo: load child data
+            return null;
+        }
+
+        internal void Update(IDbTransaction tr)
+        {
+            foreach (Child child in this)
+            {
+                child.Update(tr);
+            }
+        }
+
+        private Children()
+        {
+            //prevent direct creation
+            MarkAsChild();
+        }
     }
 }

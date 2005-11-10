0bbd0dbc34f99b01d8c5100fbd8f4c99852d27f0
ScottSc <ScottSc@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-10 01:05:32 +0000

    *** empty log message ***

--- Csla20Test/Csla.Test/RollBack/RollbackRoot.cs
+++ Csla20Test/Csla.Test/RollBack/RollbackRoot.cs
@@ -4,7 +4,127 @@ using System.Text;
 
 namespace Csla.Test.RollBack
 {
-    class RollbackRoot
+    [Serializable()]
+    public class RollbackRoot : BusinessBase<RollbackRoot>
     {
+        private string _data = "";
+        private bool _fail = false;
+
+        protected override object GetIdValue()
+        {
+            return _data;
+        }
+
+        public string Data
+        {
+            get { return _data; }
+            set
+            {
+                if (_data != value)
+                {
+                    _data = value;
+                    MarkDirty();
+                }
+            }
+        }
+
+        public bool Fail
+        {
+            get { return _fail; }
+            set
+            {
+                if (_fail != value)
+                {
+                    _fail = value;
+                    MarkDirty();
+                }
+            }
+        }
+
+        [Serializable()]
+        private class Criteria
+        {
+            public string _data;
+
+            public Criteria()
+            {
+                _data = "<new>";
+            }
+
+            public Criteria(string data)
+            {
+                this._data = data;
+            }
+        }
+
+        public static RollbackRoot NewRoot()
+        {
+            return ((RollbackRoot)(Csla.DataPortal.Create(new Criteria())));
+        }
+
+        public static RollbackRoot GetRoot(string data)
+        {
+            return ((RollbackRoot)(Csla.DataPortal.Fetch(new Criteria(data))));
+        }
+
+        public static void DeleteRoot(string data)
+        {
+            Csla.DataPortal.Delete(new Criteria(data));
+        }
+
+        private RollbackRoot()
+        {
+            //prevent direct creation
+        }
+
+        protected override void DataPortal_Create(object criteria)
+        {
+            Criteria crit = (Criteria)(criteria);
+            _data = crit._data;
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Created");
+        }
+
+        protected override void DataPortal_Fetch(object criteria)
+        {
+            Criteria crit = (Criteria)(criteria);
+            _data = crit._data;
+            MarkOld();
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Fetched");
+        }
+
+        protected override void DataPortal_Insert()
+        {
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Inserted");
+        }
+
+        protected override void DataPortal_Update()
+        {
+            //we would update here
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Updated");
+            
+            if (_fail)
+                throw new Exception("fail Update");
+        }
+
+        protected override void DataPortal_DeleteSelf()
+        {
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Deleted self");
+        }
+
+        protected override void DataPortal_Delete(object criteria)
+        {
+            //we would delete here
+            Csla.ApplicationContext.GlobalContext.Add("Root", "Deleted");
+        }
+
+        protected override void OnDeserialized(System.Runtime.Serialization.StreamingContext context)
+        {
+            base.OnDeserialized(context);
+            Csla.ApplicationContext.GlobalContext.Add("Deserialized", "root Deserialized");
+        }
+        
+
+
+
     }
 }

42e16ca3715759d71f709ec6201071536787870c
ScottSc <ScottSc@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-10 00:52:50 +0000

    *** empty log message ***

--- Csla20Test/Csla.Test/IO/IOTests.cs
+++ Csla20Test/Csla.Test/IO/IOTests.cs
@@ -1,10 +1,96 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-//
+using NUnit.Framework;
+
 namespace Csla.Test.IO
 {
-    class IOTests
+    [TestFixture]
+    public class IOTests
     {
+        [Test]
+        public void SaveNewRoot()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            Csla.Test.Basic.Root root = Csla.Test.Basic.Root.NewRoot();
+
+            root.Data = "saved";
+            Assert.AreEqual("saved", root.Data);
+            Assert.AreEqual(true, root.IsDirty);
+            Assert.AreEqual(true, root.IsValid);
+
+            Csla.ApplicationContext.GlobalContext.Clear();
+            root = root.Save();
+
+            Assert.IsNotNull(root);
+            Assert.AreEqual("updated", Csla.ApplicationContext.GlobalContext["Root"]);
+            Assert.AreEqual("saved", root.Data);
+            Assert.AreEqual(false, root.IsNew, "IsNew");
+            Assert.AreEqual(false, root.IsDeleted, "IsDeleted");
+            Assert.AreEqual(false, root.IsDirty, "IsDirty");
+        }
+
+        [Test]
+        public void LoadRoot()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            Csla.Test.Basic.Root root = Csla.Test.Basic.Root.GetRoot("loaded");
+            Assert.IsNotNull(root);
+            Assert.AreEqual("Fetched", Csla.ApplicationContext.GlobalContext["Root"]);
+            Assert.AreEqual("loaded", root.Data);
+            Assert.AreEqual(false, root.IsNew);
+            Assert.AreEqual(false, root.IsDeleted);
+            Assert.AreEqual(false, root.IsDirty);
+            Assert.AreEqual(false, root.IsValid);
+        }
+
+        [Test]
+        public void DeleteNewRoot()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            Csla.Test.Basic.Root root = Csla.Test.Basic.Root.NewRoot();
+
+            Csla.ApplicationContext.GlobalContext.Clear();
+            root.Delete();
+            Assert.AreEqual(true, root.IsNew);
+            Assert.AreEqual(true, root.IsDeleted);
+            Assert.AreEqual(true, root.IsDirty);
+
+            root = root.Save();
+            Assert.IsNotNull(root);
+            Assert.AreEqual(null, Csla.ApplicationContext.GlobalContext["Root"]);
+            Assert.AreEqual(true, root.IsNew);
+            Assert.AreEqual(false, root.IsDeleted);
+            Assert.AreEqual(true, root.IsDirty);
+        }
+
+        [Test]
+        public void DeleteOldRoot()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            Csla.Test.Basic.Root root = Csla.Test.Basic.Root.GetRoot("old");
+
+            Csla.ApplicationContext.GlobalContext.Clear();
+            root.Delete();
+            Assert.AreEqual(false, root.IsNew);
+            Assert.AreEqual(true, root.IsDeleted);
+            Assert.AreEqual(true, root.IsDirty);
+
+            root = root.Save();
+            Assert.IsNotNull(root);
+            Assert.AreEqual("Deleted self", Csla.ApplicationContext.GlobalContext["Root"]);
+            Assert.AreEqual(true, root.IsNew);
+            Assert.AreEqual(false, root.IsDeleted);
+            Assert.AreEqual(true, root.IsDirty);
+        }
+
+        [Test]
+        public void DeleteRootImmediate()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            Csla.Test.Basic.Root.DeleteRoot("test");
+            Assert.AreEqual("Deleted", Csla.ApplicationContext.GlobalContext["Root"]);
+        }
+
     }
 }

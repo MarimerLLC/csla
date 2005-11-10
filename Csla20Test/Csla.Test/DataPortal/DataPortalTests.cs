2ab478088a05c122f0e666045509483dccfbc172
JustinC <JustinC@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-10 16:07:06 +0000

    *** empty log message ***

--- Csla20Test/Csla.Test/DataPortal/DataPortalTests.cs
+++ Csla20Test/Csla.Test/DataPortal/DataPortalTests.cs
@@ -1,10 +1,63 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace Csla.Test.DataPortal
 {
+    [NUnit.Framework.TestFixture()]
     class DataPortalTests
     {
+        #if csla20vb
+        #warning The VB version is unable to access DataPortal.DataPortalInvokeEventHandler or InvokeCompleteHandler
+        public void DataPortalEvents()
+        {
+            Assert.Fail("The VB version is unable to access DataPortal.DataPortalInvokeEventHandler or InvokeCompleteHandler");
+        }
+        #elif csla20cs
+
+        private Csla.DataPortal.DataPortalInvokeEventHandler InvokeHandler;
+        private Csla.DataPortal.DataPortalInvokeCompleteEventHandler InvokeCompleteHandler;
+        [NUnit.Framework.Test()]
+        public void DataPortalEvents()
+        {
+            InvokeHandler = new Csla.DataPortal.DataPortalInvokeEventHandler(ClientPortal_DataPortalInvoke);
+            InvokeCompleteHandler = new Csla.DataPortal.DataPortalInvokeCompleteEventHandler(ClientPortal_DataPortalInvokeComplete);
+
+            Csla.DataPortal.DataPortalInvoke += InvokeHandler;
+            Csla.DataPortal.DataPortalInvokeComplete += InvokeCompleteHandler;
+
+            try
+            {
+                ApplicationContext.GlobalContext.Clear();
+                DpRoot root = DpRoot.NewRoot();
+
+                //root.Data = "saved";
+                //Csla.ApplicationContext.GlobalContext.Clear();
+                //root = root.Save();
+
+                Assert.IsTrue((bool)ApplicationContext.GlobalContext["dpinvoke"], "DataPortalInvoke not called");
+                Assert.IsTrue((bool)ApplicationContext.GlobalContext["dpinvokecomplete"], "DataPortalInvokeComplete not called");
+                Assert.IsTrue((bool)ApplicationContext.GlobalContext["serverinvoke"], "Server DataPortalInvoke not called");
+                Assert.IsTrue((bool)ApplicationContext.GlobalContext["serverinvokecomplete"], "Server DataPortalInvokeComplete not called");
+            }
+            finally
+            {
+                Csla.DataPortal.DataPortalInvoke -= InvokeHandler;
+                Csla.DataPortal.DataPortalInvokeComplete -= InvokeCompleteHandler;
+            }
+        }
+        #endif
+
+        private void ClientPortal_DataPortalInvoke(DataPortalEventArgs obj)
+        {
+            ApplicationContext.GlobalContext["dpinvoke"] = true;
+        }
+
+        private void ClientPortal_DataPortalInvokeComplete(DataPortalEventArgs obj)
+        {
+            ApplicationContext.GlobalContext["dpinvokecomplete"] = true;
+        }
+        
     }
 }

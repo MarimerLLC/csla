b5537ce0e459b5125bc482f70041f802054453f1
ScottSc <ScottSc@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-10 02:19:51 +0000

    *** empty log message ***

--- Csla20Test/Csla.Test/SmartDate/SmartDateTests.cs
+++ Csla20Test/Csla.Test/SmartDate/SmartDateTests.cs
@@ -1,10 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace Csla.Test.SmartDate
 {
-    class SmartDateTests
+    [TestFixture]
+    public class SmartDateTests
     {
+        [Test]
+        public void TestSmartDate()
+        {
+            DateTime now = DateTime.Now;
+            Csla.SmartDate d = new Csla.SmartDate(now, true);
+            Assert.AreEqual(now, d.Date);
+        }
+
+        //implement remaining
+
     }
 }
--- Csla20Test/Csla.Test/ValidationRules/ValidationTests.cs
+++ Csla20Test/Csla.Test/ValidationRules/ValidationTests.cs
@@ -1,10 +1,59 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace Csla.Test.ValidationRules
 {
-    class ValidationTests
+    [TestFixture]
+    public class ValidationTests
     {
+        [Test]
+        public void BreakRequiredRule()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            HasRulesManager root = HasRulesManager.NewHasRulesManager();
+            Assert.AreEqual(false, root.IsValid, "should not be valid");
+            Assert.AreEqual(1, root.BrokenRulesCollection.Count);
+            Assert.AreEqual("Name required", root.BrokenRulesCollection[0].Description);
+        }
+
+        [Test]
+        public void BreakLengthRule()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            HasRulesManager root = HasRulesManager.NewHasRulesManager();
+            root.Name = "12345678901";
+            Assert.AreEqual(false, root.IsValid, "should not be valid");
+            Assert.AreEqual(1, root.BrokenRulesCollection.Count);
+            //Assert.AreEqual("Name too long", root.GetBrokenRulesCollection[0].Description);
+            Assert.AreEqual("The value for Name is too long", root.BrokenRulesCollection[0].Description);
+
+            root.Name = "1234567890";
+            Assert.AreEqual(true, root.IsValid, "should be valid");
+            Assert.AreEqual(0, root.BrokenRulesCollection.Count);
+        }
+
+        [Test]
+        public void BreakLengthRuleAndClone()
+        {
+            Csla.ApplicationContext.GlobalContext.Clear();
+            HasRulesManager root = HasRulesManager.NewHasRulesManager();
+            root.Name = "12345678901";
+            Assert.AreEqual(false, root.IsValid, "should not be valid");
+            Assert.AreEqual(1, root.BrokenRulesCollection.Count);
+            //Assert.AreEqual("Name too long", root.GetBrokenRulesCollection[0].Description;
+            Assert.AreEqual("The value for Name is too long", root.BrokenRulesCollection[0].Description);
+
+            root = (HasRulesManager)(root.Clone());
+            Assert.AreEqual(false, root.IsValid, "should not be valid");
+            Assert.AreEqual(1, root.BrokenRulesCollection.Count);
+            //Assert.AreEqual("Name too long", root.GetBrokenRulesCollection[0].Description;
+            Assert.AreEqual("The value for Name is too long", root.BrokenRulesCollection[0].Description);
+
+            root.Name = "1234567890";
+            Assert.AreEqual(true, root.IsValid, "Should be valid");
+            Assert.AreEqual(0, root.BrokenRulesCollection.Count);
+        }
     }
 }

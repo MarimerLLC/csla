10de212a588bb68b9e975ec57451fd04e93d5a95
ScottSc <ScottSc@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-11 17:01:41 +0000

    *** empty log message ***

--- Csla20Test/Csla.Test/SmartDate/SmartDateTests.cs
+++ Csla20Test/Csla.Test/SmartDate/SmartDateTests.cs
@@ -23,7 +23,7 @@ namespace Csla.Test.SmartDate
             Csla.SmartDate d2 = new Csla.SmartDate();
             Csla.SmartDate d3;
 
-            d2.Date = new DateTime(1, 1, 2005);
+            d2.Date = new DateTime(2005, 1, 1);
             d3 = new Csla.SmartDate(d2.Add(new TimeSpan(30, 0, 0, 0)));
             Assert.AreEqual(DateAndTime.DateAdd(DateInterval.Day, 30, d2.Date), d3.Date, "Dates should be equal");
         }
@@ -34,7 +34,7 @@ namespace Csla.Test.SmartDate
             Csla.SmartDate d2 = new Csla.SmartDate();
             Csla.SmartDate d3;
 
-            d2.Date = new DateTime(1, 1, 2005);
+            d2.Date = new DateTime(2005, 1, 1);
             d3 = new Csla.SmartDate(d2.Subtract(new TimeSpan(30, 0, 0, 0)));
             Assert.AreEqual(DateAndTime.DateAdd(DateInterval.Day, -30, d2.Date), d3.Date, "Dates should be equal");
         }
@@ -49,17 +49,17 @@ namespace Csla.Test.SmartDate
             Assert.IsTrue(Csla.SmartDate.Equals(d2, d3), "Empty dates should be equal (shared)");
             Assert.IsTrue(d2.Equals(d3), "Empty dates should be equal (unary)");
 
-            d2.Date = new DateTime(1, 1, 2005);
+            d2.Date = new DateTime(2005, 1, 1);
             d3 = new Csla.SmartDate(d2.Date, d2.EmptyIsMin);
             Assert.AreEqual(d2, d3, "Assigned dates should be equal");
 
-            d3.Date = new DateTime(2, 2, 2005);
+            d3.Date = new DateTime(2005, 2, 2);
             Assert.AreEqual(1, d3.CompareTo(d2), "Should be greater than");
             Assert.AreEqual(-1, d2.CompareTo(d3), "Should be less than");
             Assert.IsFalse(d2.CompareTo(d3) == 0, "should not be equal");
 
             Assert.IsTrue(d3.Equals("2/2/2005"), "Should be equal to string date");
-            Assert.IsTrue(d3.Equals(new DateTime(2, 2, 2005)), "should be equal to DateTime");
+            Assert.IsTrue(d3.Equals(new DateTime(2005, 2, 2)), "should be equal to DateTime");
         }
 
         [Test()]
@@ -81,16 +81,16 @@ namespace Csla.Test.SmartDate
             Csla.SmartDate d1 = new Csla.SmartDate();
             Csla.SmartDate d2 = new Csla.SmartDate();
 
-            d1.Date = new DateTime(1, 1, 2005);
-            d2.Date = new DateTime(2, 1, 2005);
+            d1.Date = new DateTime(2005, 1, 1);
+            d2.Date = new DateTime(2005, 2, 1);
             Assert.IsTrue(d1 < d2, "d1 should be less than d2");
 
-            d1.Date = new DateTime(3, 1, 2005);
-            d2.Date = new DateTime(2, 1, 2005);
+            d1.Date = new DateTime(2005, 3, 1);
+            d2.Date = new DateTime(2005, 2, 1);
             Assert.IsTrue(d1 > d2, "d1 should be greater than d2");
 
-            d1.Date = new DateTime(2, 1, 2005);
-            d2.Date = new DateTime(2, 1, 2005);
+            d1.Date = new DateTime(2005, 2, 1);
+            d2.Date = new DateTime(2005, 2, 1);
             Assert.IsTrue(d1 == d2, "d1 should be equal to d2");
         }
     }

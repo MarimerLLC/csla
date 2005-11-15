85d76524eb7b008bfe745a35e040ab49c883cd94
ScottSc <ScottSc@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-15 18:00:05 +0000

    *** empty log message ***

---
+++ Csla20Test/Csla.Test/DataBinding/DataBindingApp/ListObject.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Csla.Core;
+using Csla;
+
+namespace DataBindingApp
+{
+    [Serializable()]
+    public class ListObject : BusinessListBase<ListObject, ListObject.DataObject>
+    {
+        [Serializable()]
+        public class DataObject : BusinessBase<DataObject>
+        {
+            private int _ID;
+            private string _data;
+            private int _number;
+
+            public int Number
+            {
+                get { return _number; }
+                set { _number = value; }
+            }
+
+            public string Data
+            {
+                get { return _data; }
+                set { _data = value; }
+            }
+
+            protected override object GetIdValue()
+            {
+                return _ID;
+            }
+
+            public DataObject(string data, int number)
+            {
+                this.MarkAsChild();
+                _data = data;
+                _number = number;
+            }
+        }
+
+        private ListObject()
+        { }
+
+        public static ListObject GetList()
+        {
+            ListObject list = new ListObject();
+
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(new DataObject("element" + i, i));
+            }
+
+            return list;
+        }
+
+
+    }
+}

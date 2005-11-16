2f835c5767fd572c6a862e590fe0123885b77812
ScottSc <ScottSc@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-16 16:18:20 +0000

    *** empty log message ***

---
+++ Csla20Test/Csla.Test/Nullable/NullableTests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#else
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+#endif
+
+namespace Csla.Test.Nullable
+{
+    [TestClass()]
+    public class NullableTests
+    {
+        [TestMethod()]
+        public void DoSomething()
+        { }
+
+    }
+}
+

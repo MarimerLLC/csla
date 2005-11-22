7669b775231d0606dcbf726549bcbc170a17e589
Glamdring <Glamdring@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-22 22:10:04 +0000

    Call methods in MethodCaller rather than using a local implementation.

--- csla20vb/Csla/DataPortal/Server/SimpleDataPortal.vb
+++ csla20vb/Csla/DataPortal/Server/SimpleDataPortal.vb
@@ -31,16 +31,16 @@ Namespace Server
         obj = Activator.CreateInstance(objectType, True)
 
         ' tell the business object we're about to make a DataPortal_xyz call
-        CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvoke", New DataPortalEventArgs(context))
+        MethodCaller.CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvoke", New DataPortalEventArgs(context))
 
         ' tell the business object to fetch its data
-        CallMethod(obj, "DataPortal_Create", criteria)
+        MethodCaller.CallMethod(obj, "DataPortal_Create", criteria)
 
         ' mark the object as new
-        CallMethodIfImplemented(obj, "MarkNew")
+        MethodCaller.CallMethodIfImplemented(obj, "MarkNew")
 
         ' tell the business object the DataPortal_xyz call is complete
-        CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvokeComplete", New DataPortalEventArgs(context))
+        MethodCaller.CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvokeComplete", New DataPortalEventArgs(context))
 
         ' return the populated business object as a result
         Return New DataPortalResult(obj)
@@ -48,7 +48,7 @@ Namespace Server
       Catch ex As Exception
         Try
           ' tell the business object there was an exception
-          CallMethodIfImplemented(obj, "DataPortal_OnDataPortalException", New DataPortalEventArgs(context), ex)
+          MethodCaller.CallMethodIfImplemented(obj, "DataPortal_OnDataPortalException", New DataPortalEventArgs(context), ex)
         Catch
           ' ignore exceptions from the exception handler
         End Try
@@ -75,16 +75,16 @@ Namespace Server
         obj = CreateBusinessObject(criteria)
 
         ' tell the business object we're about to make a DataPortal_xyz call
-        CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvoke", New DataPortalEventArgs(context))
+        MethodCaller.CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvoke", New DataPortalEventArgs(context))
 
         ' tell the business object to fetch its data
-        CallMethod(obj, "DataPortal_Fetch", criteria)
+        MethodCaller.CallMethod(obj, "DataPortal_Fetch", criteria)
 
         ' mark the object as old
-        CallMethodIfImplemented(obj, "MarkOld")
+        MethodCaller.CallMethodIfImplemented(obj, "MarkOld")
 
         ' tell the business object the DataPortal_xyz call is complete
-        CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvokeComplete", New DataPortalEventArgs(context))
+        MethodCaller.CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvokeComplete", New DataPortalEventArgs(context))
 
         ' return the populated business object as a result
         Return New DataPortalResult(obj)
@@ -92,7 +92,7 @@ Namespace Server
       Catch ex As Exception
         Try
           ' tell the business object there was an exception
-          CallMethodIfImplemented(obj, "DataPortal_OnDataPortalException", New DataPortalEventArgs(context), ex)
+          MethodCaller.CallMethodIfImplemented(obj, "DataPortal_OnDataPortalException", New DataPortalEventArgs(context), ex)
         Catch
           ' ignore exceptions from the exception handler
         End Try
@@ -114,7 +114,7 @@ Namespace Server
 
       Try
         ' tell the business object we're about to make a DataPortal_xyz call
-        CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvoke", New DataPortalEventArgs(context))
+        MethodCaller.CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvoke", New DataPortalEventArgs(context))
 
         ' tell the business object to update itself
         If TypeOf obj Is Core.BusinessBase Then
@@ -122,39 +122,39 @@ Namespace Server
           If busObj.IsDeleted Then
             If Not busObj.IsNew Then
               ' tell the object to delete itself
-              CallMethod(busObj, "DataPortal_DeleteSelf")
+              MethodCaller.CallMethod(busObj, "DataPortal_DeleteSelf")
             End If
             ' mark the object as new
-            CallMethodIfImplemented(busObj, "MarkNew")
+            MethodCaller.CallMethodIfImplemented(busObj, "MarkNew")
 
           Else
             If busObj.IsNew Then
               ' tell the object to insert itself
-              CallMethod(busObj, "DataPortal_Insert")
+              MethodCaller.CallMethod(busObj, "DataPortal_Insert")
 
             Else
               ' tell the object to update itself
-              CallMethod(busObj, "DataPortal_Update")
+              MethodCaller.CallMethod(busObj, "DataPortal_Update")
             End If
             ' mark the object as old
-            CallMethodIfImplemented(busObj, "MarkOld")
+            MethodCaller.CallMethodIfImplemented(busObj, "MarkOld")
           End If
 
         ElseIf TypeOf obj Is CommandBase Then
           ' tell the object to update itself
-          CallMethod(obj, "DataPortal_Execute")
+          MethodCaller.CallMethod(obj, "DataPortal_Execute")
 
         Else
           ' this is an updatable collection or some other
           ' non-BusinessBase type of object
           ' tell the object to update itself
-          CallMethod(obj, "DataPortal_Update")
+          MethodCaller.CallMethod(obj, "DataPortal_Update")
           ' mark the object as old
-          CallMethodIfImplemented(obj, "MarkOld")
+          MethodCaller.CallMethodIfImplemented(obj, "MarkOld")
         End If
 
         ' tell the business object the DataPortal_xyz call is complete
-        CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvokeComplete", New DataPortalEventArgs(context))
+        MethodCaller.CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvokeComplete", New DataPortalEventArgs(context))
 
         Return New DataPortalResult(obj)
 
@@ -180,13 +180,13 @@ Namespace Server
         obj = CreateBusinessObject(criteria)
 
         ' tell the business object we're about to make a DataPortal_xyz call
-        CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvoke", New DataPortalEventArgs(context))
+        MethodCaller.CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvoke", New DataPortalEventArgs(context))
 
         ' tell the business object to delete itself
-        CallMethod(obj, "DataPortal_Delete", criteria)
+        MethodCaller.CallMethod(obj, "DataPortal_Delete", criteria)
 
         ' tell the business object the DataPortal_xyz call is complete
-        CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvokeComplete", New DataPortalEventArgs(context))
+        MethodCaller.CallMethodIfImplemented(obj, "DataPortal_OnDataPortalInvokeComplete", New DataPortalEventArgs(context))
 
         Return New DataPortalResult
 
@@ -218,64 +218,6 @@ Namespace Server
 
       ' create an instance of the business object
       Return Activator.CreateInstance(businessType, True)
-
-    End Function
-
-#End Region
-
-#Region " Calling a method "
-
-    Private Shared Function CallMethodIfImplemented(ByVal obj As Object, _
-      ByVal method As String, ByVal ParamArray parameters() As Object) As Object
-
-      Dim info As MethodInfo = GetMethod(obj.GetType, method)
-      If info IsNot Nothing Then
-        Return CallMethod(obj, info, parameters)
-
-      Else
-        Return Nothing
-      End If
-
-    End Function
-
-    Private Shared Function CallMethod(ByVal obj As Object, _
-      ByVal method As String, ByVal ParamArray parameters() As Object) As Object
-
-      Dim info As MethodInfo = GetMethod(obj.GetType, method)
-      If info Is Nothing Then
-        Throw New NotImplementedException( _
-          method & " " & My.Resources.MethodNotImplemented)
-      End If
-
-      Return CallMethod(obj, info, parameters)
-
-    End Function
-
-    Private Shared Function CallMethod(ByVal obj As Object, _
-      ByVal info As MethodInfo, ByVal ParamArray parameters() As Object) As Object
-
-      ' call a private method on the object
-      Dim result As Object
-      Try
-        result = info.Invoke(obj, parameters)
-
-      Catch e As Exception
-        Throw New CallMethodException( _
-          info.Name & " " & My.Resources.MethodCallFailed, _
-          e.InnerException)
-      End Try
-      Return result
-
-    End Function
-
-    Private Shared Function GetMethod(ByVal objectType As Type, _
-      ByVal method As String) As MethodInfo
-
-      Return objectType.GetMethod(method, _
-        BindingFlags.FlattenHierarchy Or _
-        BindingFlags.Instance Or _
-        BindingFlags.Public Or _
-        BindingFlags.NonPublic)
 
     End Function
 

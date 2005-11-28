1da027a2016158822b50383f5eb5328f34378b5f
Glamdring <Glamdring@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-11-28 15:31:45 +0000

    Change the DataBase class to be a Module - effectively what is now a static class in C#.

--- ProjectTracker20vb/ProjectTracker.Library/Database.vb
+++ ProjectTracker20vb/ProjectTracker.Library/Database.vb
@@ -1,15 +1,15 @@
-Public Class DataBase
+Public Module DataBase
 
-  Public Shared ReadOnly Property DbConn() As String
+  Public ReadOnly Property DbConn() As String
     Get
       Return System.Configuration.ConfigurationManager.ConnectionStrings("PTracker").ConnectionString
     End Get
   End Property
 
-  Public Shared ReadOnly Property SecurityConn() As String
+  Public ReadOnly Property SecurityConn() As String
     Get
       Return System.Configuration.ConfigurationManager.ConnectionStrings("Security").ConnectionString
     End Get
   End Property
 
-End Class
+End Module

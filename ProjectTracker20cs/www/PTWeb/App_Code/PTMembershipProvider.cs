512ae89d980b1da4ea37e080c2fe37d4dfd025cd
BrantE <BrantE@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-12-03 06:07:24 +0000

    Implemented a basic Membership Provider for use with the login control.

---
+++ ProjectTracker20cs/www/PTWeb/App_Code/PTMembershipProvider.cs
@@ -0,0 +1,170 @@
+using System;
+using ProjectTracker.Library.Security;
+using System.Web.Security;
+
+public class PTMembershipProvider : MembershipProvider
+{
+
+  public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
+  {
+    base.Initialize(name, config);
+  }
+
+  public override bool ValidateUser(string username, string password)
+  {
+    PTPrincipal.Login(username, password);
+    System.Web.HttpContext.Current.User = System.Threading.Thread.CurrentPrincipal;
+    if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+    {
+      System.Web.HttpContext.Current.Session["CslaPrincipal"] = System.Threading.Thread.CurrentPrincipal;
+      return true;
+    }
+    return false;
+  }
+
+  #region Non-Implemented Members
+
+  // the following members must be implemented due to the abstract class MembershipProvider,
+  // but not required to be functional for using the Login control.
+  public override bool ChangePassword(string username, string oldPassword, string newPassword)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override bool DeleteUser(string username, bool deleteAllRelatedData)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override bool EnablePasswordReset
+  {
+    get { throw new Exception("The method or operation is not implemented."); }
+  }
+
+  public override bool EnablePasswordRetrieval
+  {
+    get { throw new Exception("The method or operation is not implemented."); }
+  }
+
+  public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override int GetNumberOfUsersOnline()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override string GetPassword(string username, string answer)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override MembershipUser GetUser(string username, bool userIsOnline)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override string GetUserNameByEmail(string email)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override int MaxInvalidPasswordAttempts
+  {
+    get { throw new Exception("The method or operation is not implemented."); }
+  }
+
+  public override int MinRequiredNonAlphanumericCharacters
+  {
+    get { throw new Exception("The method or operation is not implemented."); }
+  }
+
+  public override int MinRequiredPasswordLength
+  {
+    get { throw new Exception("The method or operation is not implemented."); }
+  }
+
+  public override int PasswordAttemptWindow
+  {
+    get { throw new Exception("The method or operation is not implemented."); }
+  }
+
+  public override MembershipPasswordFormat PasswordFormat
+  {
+    get { throw new Exception("The method or operation is not implemented."); }
+  }
+
+  public override string PasswordStrengthRegularExpression
+  {
+    get { throw new Exception("The method or operation is not implemented."); }
+  }
+
+  public override bool RequiresQuestionAndAnswer
+  {
+    get { throw new Exception("The method or operation is not implemented."); }
+  }
+
+  public override bool RequiresUniqueEmail
+  {
+    get { throw new Exception("The method or operation is not implemented."); }
+  }
+
+  public override string ResetPassword(string username, string answer)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override bool UnlockUser(string userName)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override void UpdateUser(MembershipUser user)
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  public override string ApplicationName
+  {
+    get
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+    set
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
+  #endregion
+
+  }
+
+}
+

bda85dc58aa8abced0de126703dacd9541ca22b8
BrantE <BrantE@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-12-04 01:56:52 +0000

    Moved DataMapper into Csla.Data

---
+++ csla20cs/Csla/Data/DataMapper.cs
@@ -0,0 +1,180 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Csla.Data
+{
+  public static class DataMapper
+  {
+
+    #region Map from IDictionary
+
+    /// <summary>
+    /// Copies values from the source into the
+    /// properties of the target.
+    /// </summary>
+    /// <param name="source">A name/value dictionary containing the source values.</param>
+    /// <param name="target">An object with properties to be set from the dictionary.</param>
+    /// <remarks>
+    /// The key names in the dictionary must match the property names on the target
+    /// object. Target properties may not be readonly or indexed.
+    /// </remarks>
+    public static void Map(System.Collections.IDictionary source, object target)
+    {
+      Map(source, target, false);
+    }
+
+    /// <summary>
+    /// Copies values from the source into the
+    /// properties of the target.
+    /// </summary>
+    /// <param name="source">A name/value dictionary containing the source values.</param>
+    /// <param name="target">An object with properties to be set from the dictionary.</param>
+    /// <param name="ignoreList">A list of property names to ignore. 
+    /// These properties will not be set on the target object.</param>
+    /// <remarks>
+    /// The key names in the dictionary must match the property names on the target
+    /// object. Target properties may not be readonly or indexed.
+    /// </remarks>
+    public static void Map(System.Collections.IDictionary source, object target, params string[] ignoreList)
+    {
+      Map(source, target, false, ignoreList);
+    }
+
+    /// <summary>
+    /// Copies values from the source into the
+    /// properties of the target.
+    /// </summary>
+    /// <param name="source">A name/value dictionary containing the source values.</param>
+    /// <param name="target">An object with properties to be set from the dictionary.</param>
+    /// <param name="ignoreList">A list of property names to ignore. 
+    /// These properties will not be set on the target object.</param>
+    /// <param name="supressExceptions">If True, any exceptions will be supressed.</param>
+    /// <remarks>
+    /// The key names in the dictionary must match the property names on the target
+    /// object. Target properties may not be readonly or indexed.
+    /// </remarks>
+    public static void Map(System.Collections.IDictionary source, object target, bool supressExceptions, params string[] ignoreList)
+    {
+      List<string> ignore = new List<string>(ignoreList);
+      Type targetType = target.GetType();
+      foreach (string propertyName in source.Keys)
+      {
+        if (!ignore.Contains(propertyName))
+        {
+          try
+          {
+            // get source value
+            object value = source[propertyName];
+            // set target value
+            PropertyInfo propertyInfo = targetType.GetProperty(propertyName);
+            Type pType = propertyInfo.PropertyType;
+            if (pType.Equals(value.GetType()))
+            {
+              // types match, just copy value
+              propertyInfo.SetValue(target, value, null);
+            }
+            else
+            {
+              // types don't match, try to coerce
+              if (pType.Equals(typeof(Guid)))
+                propertyInfo.SetValue(target, new Guid(value.ToString()), null);
+              else
+                propertyInfo.SetValue(target, Convert.ChangeType(value, pType), null);
+            }
+          }
+          catch
+          {
+            if (!supressExceptions)
+              throw;
+          }
+        }
+      }
+    }
+
+    #endregion
+
+    #region Map from Object
+
+    /// <summary>
+    /// Copies values from the source into the
+    /// properties of the target.
+    /// </summary>
+    /// <param name="source">An object containing the source values.</param>
+    /// <param name="target">An object with properties to be set from the dictionary.</param>
+    /// <remarks>
+    /// The property names and types of the source object must match the property names and types
+    /// on the target object. Source properties may not be indexed. 
+    /// Target properties may not be readonly or indexed.
+    /// </remarks>
+    public static void Map(object source, object target)
+    {
+      Map(source, target, false);
+    }
+
+    /// <summary>
+    /// Copies values from the source into the
+    /// properties of the target.
+    /// </summary>
+    /// <param name="source">An object containing the source values.</param>
+    /// <param name="target">An object with properties to be set from the dictionary.</param>
+    /// <param name="ignoreList">A list of property names to ignore. 
+    /// These properties will not be set on the target object.</param>
+    /// <remarks>
+    /// The property names and types of the source object must match the property names and types
+    /// on the target object. Source properties may not be indexed. 
+    /// Target properties may not be readonly or indexed.
+    /// </remarks>
+    public static void Map(object source, object target, params string[] ignoreList)
+    {
+      Map(source, target, false, ignoreList);
+    }
+
+    /// <summary>
+    /// Copies values from the source into the
+    /// properties of the target.
+    /// </summary>
+    /// <param name="source">An object containing the source values.</param>
+    /// <param name="target">An object with properties to be set from the dictionary.</param>
+    /// <param name="ignoreList">A list of property names to ignore. 
+    /// These properties will not be set on the target object.</param>
+    /// <param name="supressExceptions">If True, any exceptions will be supressed.</param>
+    /// <remarks>
+    /// The property names and types of the source object must match the property names and types
+    /// on the target object. Source properties may not be indexed. 
+    /// Target properties may not be readonly or indexed.
+    /// </remarks>
+    public static void Map(object source, object target, bool supressExceptions, params string[] ignoreList)
+    {
+      List<string> ignore = new List<string>(ignoreList);
+      Type sourceType = source.GetType();
+      PropertyInfo[] sourceProperties = sourceType.GetProperties();
+      Type targetType = target.GetType();
+      foreach (PropertyInfo sourceProperty in sourceProperties)
+      {
+        string propertyName = sourceProperty.Name;
+        if (!ignore.Contains(propertyName))
+        {
+          try
+          {
+            PropertyInfo propertyInfo;
+            // get source value
+            propertyInfo = sourceType.GetProperty(propertyName);
+            object value = propertyInfo.GetValue(source, null);
+            // set target value
+            propertyInfo = targetType.GetProperty(propertyName);
+            propertyInfo.SetValue(target, value, null);
+          }
+          catch
+          {
+            if (!supressExceptions)
+              throw;
+          }
+        }
+      }
+    }
+
+    #endregion
+
+  }
+}

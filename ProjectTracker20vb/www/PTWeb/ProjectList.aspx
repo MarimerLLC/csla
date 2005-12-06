0143f43466ca233371393d0c4962668d903a0f3a
Glamdring <Glamdring@fa5919ed-4f44-5740-a13a-40f41935fe31>
2005-12-06 17:29:29 +0000

    Add authorization checking.

--- ProjectTracker20vb/www/PTWeb/ProjectList.aspx
+++ ProjectTracker20vb/www/PTWeb/ProjectList.aspx
@@ -6,8 +6,9 @@
         <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AutoGenerateColumns="False" DataSourceID="ProjectListDataSource" PageSize="4" DataKeyNames="Id">
           <Columns>
             <asp:BoundField DataField="ID" HeaderText="ID" SortExpression="ID" Visible="False" />
-            <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
-            <asp:CommandField ShowDeleteButton="True" ShowSelectButton="True" SelectText="Edit" />
+            <asp:HyperLinkField DataNavigateUrlFields="Id" DataNavigateUrlFormatString="ProjectEdit.aspx?id={0}"
+              DataTextField="Name" HeaderText="Name" />
+            <asp:CommandField ShowDeleteButton="True" SelectText="Edit" />
           </Columns>
         </asp:GridView>
         <asp:LinkButton ID="NewProjectButton" runat="server">New project</asp:LinkButton>
--- ProjectTracker20vb/www/PTWeb/ProjectList.aspx.vb
+++ ProjectTracker20vb/www/PTWeb/ProjectList.aspx.vb
@@ -1,33 +1,42 @@
+Option Strict On
+
+Imports ProjectTracker.Library
 
 Partial Class ProjectList
   Inherits System.Web.UI.Page
 
-  Protected Sub GridView1_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles GridView1.SelectedIndexChanged
+  Protected Sub GridView1_SelectedIndexChanged(ByVal sender As Object, _
+    ByVal e As System.EventArgs) Handles GridView1.SelectedIndexChanged
 
     Dim idString As String = GridView1.SelectedDataKey.Value.ToString
     Response.Redirect("ProjectEdit.aspx?id=" & idString)
 
   End Sub
 
-  Protected Sub NewProjectButton_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles NewProjectButton.Click
+  Protected Sub NewProjectButton_Click(ByVal sender As Object, _
+    ByVal e As System.EventArgs) Handles NewProjectButton.Click
 
     Response.Redirect("ProjectEdit.aspx")
 
   End Sub
 
-  Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+  Protected Sub Page_Load(ByVal sender As Object, _
+    ByVal e As System.EventArgs) Handles Me.Load
 
-    NewProjectButton.Visible = ProjectTracker.Library.Project.CanAddObject
+    Me.GridView1.Columns(Me.GridView1.Columns.Count - 1).Visible = Project.CanDeleteObject
+    NewProjectButton.Visible = Project.CanAddObject
 
   End Sub
 
-  Protected Sub ProjectListDataSource_DeleteObject(ByVal sender As Object, ByVal e As Csla.Web.DeleteObjectArgs) Handles ProjectListDataSource.DeleteObject
+  Protected Sub ProjectListDataSource_DeleteObject(ByVal sender As Object, _
+    ByVal e As Csla.Web.DeleteObjectArgs) Handles ProjectListDataSource.DeleteObject
 
     ProjectTracker.Library.Project.DeleteProject(New Guid(e.Keys("Id").ToString))
 
   End Sub
 
-  Protected Sub ProjectListDataSource_SelectObject(ByVal sender As Object, ByVal e As Csla.Web.SelectObjectArgs) Handles ProjectListDataSource.SelectObject
+  Protected Sub ProjectListDataSource_SelectObject(ByVal sender As Object, _
+    ByVal e As Csla.Web.SelectObjectArgs) Handles ProjectListDataSource.SelectObject
 
     e.BusinessObject = ProjectTracker.Library.ProjectList.GetProjectList
 

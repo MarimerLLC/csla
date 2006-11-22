6b1d40d3a0d442ad5a6939650d8542e6930ff395
Glamdring <Glamdring@fa5919ed-4f44-5740-a13a-40f41935fe31>
2006-11-22 00:08:57 +0000

    Remove unused IFilterable interface.

--- csla20vb/Csla/IFilterable.vb
+++
@@ -1,4 +0,0 @@
-Friend Interface IFilterable
-  Function MatchesFilter(ByVal filter As String) As Boolean
-  Function MatchesFilter(ByVal filter As FilterProvider, ByVal state As Object) As Boolean
-End Interface

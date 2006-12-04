c96a2c3fedc9da73124321c29357e4bc18796fe3
Glamdring <Glamdring@fa5919ed-4f44-5740-a13a-40f41935fe31>
2006-12-04 15:31:31 +0000

    Add new template.

---
+++ csla20vb/Templates/DynamicRootList.vb
@@ -0,0 +1,54 @@
+Imports System.Data.SqlClient
+
+<Serializable()> _
+Public Class DynamicRootList
+  Inherits DynamicRootListBase(Of DynamicRootList, EditableRoot)
+
+#Region " Authorization Rules "
+
+  Public Shared Function CanGetObject() As Boolean
+    ' TODO: customize to check user role
+    Return ApplicationContext.User.IsInRole("")
+  End Function
+
+  Public Shared Function CanEditObject() As Boolean
+    ' TODO: customize to check user role
+    Return ApplicationContext.User.IsInRole("")
+  End Function
+
+#End Region
+
+#Region " Factory Methods "
+
+  Public Shared Function NewDynamicRootList() As DynamicRootList
+    Return New DynamicRootList()
+  End Function
+
+  Public Shared Function GetDynamicRootList() As DynamicRootList
+    Return DataPortal.Fetch(Of DynamicRootList)()
+  End Function
+
+  Private Sub New()
+    ' require use of factory methods
+  End Sub
+
+#End Region
+
+#Region " Data Access "
+
+  Private Overloads Sub DataPortal_Fetch()
+
+    ' TODO: load values
+    RaiseListChangedEvents = False
+    Using dr As SqlDataReader = Nothing
+      While dr.Read
+        Add(EditableRoot.GetEditableRoot(dr))
+      End While
+    End Using
+    RaiseListChangedEvents = True
+
+  End Sub
+
+#End Region
+
+End Class

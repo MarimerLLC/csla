69d4774cbb04e755344144073f7fe9e3fedc6f2f
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2007-02-27 22:29:45 +0000

    Update version number.

--- cslavb/Csla/My Project/AssemblyInfo.vb
+++ cslavb/Csla/My Project/AssemblyInfo.vb
@@ -36,4 +36,4 @@ Imports System.Runtime.InteropServices
 '<Assembly: AssemblyVersion("1.0.0.0")> 
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
 
-<Assembly: AssemblyVersionAttribute("2.1.4.0")> 
+<Assembly: AssemblyVersionAttribute("3.0.0.0")> 

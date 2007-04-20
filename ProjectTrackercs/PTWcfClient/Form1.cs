06cf645b4c5a57aae9923c7636502585c63f75d2
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2007-04-20 16:30:05 +0000

    Add client app for WCF service UI.

---
+++ ProjectTrackercs/PTWcfClient/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PTWcfClient
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      PTWcfService.PTServiceClient svc = new PTWcfClient.PTWcfService.PTServiceClient();
+      PTWcfService.ProjectData[] list = svc.GetProjectList();
+      this.projectDataBindingSource.DataSource = list;
+      svc.Close();
+    }
+  }
+}

21fd67bc0e39a7ae2a76406080377c39c3883eb7
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2007-05-04 17:02:31 +0000

    Update the file version to match the assembly version.

--- cslacs/Csla/Properties/AssemblyInfo.cs
+++ cslacs/Csla/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]

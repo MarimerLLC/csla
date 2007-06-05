85dcd90ade564517a34bb710dad8ec2b63e0b770
rockyl@magenic.com <rockyl@magenic.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2007-06-05 16:09:27 +0000

    Properly close the WCF service proxy object.

--- cslavb/Csla/DataPortal/Client/WcfProxy.vb
+++ cslavb/Csla/DataPortal/Client/WcfProxy.vb
@@ -41,16 +41,16 @@ Namespace DataPortalClient
     ''' <see cref="Server.DataPortalContext" /> object passed to the server.
     ''' </param>
     Public Function Create(ByVal objectType As Type, ByVal criteria As Object, ByVal context As DataPortalContext) As DataPortalResult Implements IDataPortalProxy.Create
-      Using cf As ChannelFactory(Of IWcfPortal) = New ChannelFactory(Of IWcfPortal)(_endPoint)
-        Dim svr As IWcfPortal = cf.CreateChannel()
-        Dim response As WcfResponse = svr.Create(New CreateRequest(objectType, criteria, context))
+      Dim cf As ChannelFactory(Of IWcfPortal) = New ChannelFactory(Of IWcfPortal)(_endPoint)
+      Dim svr As IWcfPortal = cf.CreateChannel()
+      Dim response As WcfResponse = svr.Create(New CreateRequest(objectType, criteria, context))
+      cf.Close()
 
-        Dim result As Object = response.Result
-        If TypeOf result Is Exception Then
-          Throw CType(result, Exception)
-        End If
-        Return CType(result, DataPortalResult)
-      End Using
+      Dim result As Object = response.Result
+      If TypeOf result Is Exception Then
+        Throw CType(result, Exception)
+      End If
+      Return CType(result, DataPortalResult)
     End Function
 
     ''' <summary>
@@ -63,16 +63,16 @@ Namespace DataPortalClient
     ''' <see cref="Server.DataPortalContext" /> object passed to the server.
     ''' </param>
     Public Function Fetch(ByVal objectType As Type, ByVal criteria As Object, ByVal context As DataPortalContext) As DataPortalResult Implements IDataPortalProxy.Fetch
-      Using cf As ChannelFactory(Of IWcfPortal) = New ChannelFactory(Of IWcfPortal)(_endPoint)
-        Dim svr As IWcfPortal = cf.CreateChannel()
-        Dim response As WcfResponse = svr.Fetch(New FetchRequest(objectType, criteria, context))
+      Dim cf As ChannelFactory(Of IWcfPortal) = New ChannelFactory(Of IWcfPortal)(_endPoint)
+      Dim svr As IWcfPortal = cf.CreateChannel()
+      Dim response As WcfResponse = svr.Fetch(New FetchRequest(objectType, criteria, context))
+      cf.Close()
 
-        Dim result As Object = response.Result
-        If TypeOf result Is Exception Then
-          Throw CType(result, Exception)
-        End If
-        Return CType(result, DataPortalResult)
-      End Using
+      Dim result As Object = response.Result
+      If TypeOf result Is Exception Then
+        Throw CType(result, Exception)
+      End If
+      Return CType(result, DataPortalResult)
     End Function
 
     ''' <summary>
@@ -84,16 +84,16 @@ Namespace DataPortalClient
     ''' <see cref="Server.DataPortalContext" /> object passed to the server.
     ''' </param>
     Public Function Update(ByVal obj As Object, ByVal context As DataPortalContext) As DataPortalResult Implements IDataPortalProxy.Update
-      Using cf As ChannelFactory(Of IWcfPortal) = New ChannelFactory(Of IWcfPortal)(_endPoint)
-        Dim svr As IWcfPortal = cf.CreateChannel()
-        Dim response As WcfResponse = svr.Update(New UpdateRequest(obj, context))
+      Dim cf As ChannelFactory(Of IWcfPortal) = New ChannelFactory(Of IWcfPortal)(_endPoint)
+      Dim svr As IWcfPortal = cf.CreateChannel()
+      Dim response As WcfResponse = svr.Update(New UpdateRequest(obj, context))
+      cf.Close()
 
-        Dim result As Object = response.Result
-        If TypeOf result Is Exception Then
-          Throw CType(result, Exception)
-        End If
-        Return CType(result, DataPortalResult)
-      End Using
+      Dim result As Object = response.Result
+      If TypeOf result Is Exception Then
+        Throw CType(result, Exception)
+      End If
+      Return CType(result, DataPortalResult)
     End Function
 
     ''' <summary>
@@ -105,19 +105,20 @@ Namespace DataPortalClient
     ''' <see cref="Server.DataPortalContext" /> object passed to the server.
     ''' </param>
     Public Function Delete(ByVal criteria As Object, ByVal context As DataPortalContext) As DataPortalResult Implements IDataPortalProxy.Delete
-      Using cf As ChannelFactory(Of IWcfPortal) = New ChannelFactory(Of IWcfPortal)(_endPoint)
-        Dim svr As IWcfPortal = cf.CreateChannel()
-        Dim response As WcfResponse = svr.Delete(New DeleteRequest(criteria, context))
+      Dim cf As ChannelFactory(Of IWcfPortal) = New ChannelFactory(Of IWcfPortal)(_endPoint)
+      Dim svr As IWcfPortal = cf.CreateChannel()
+      Dim response As WcfResponse = svr.Delete(New DeleteRequest(criteria, context))
+      cf.Close()
 
-        Dim result As Object = response.Result
-        If TypeOf result Is Exception Then
-          Throw CType(result, Exception)
-        End If
-        Return CType(result, DataPortalResult)
-      End Using
+      Dim result As Object = response.Result
+      If TypeOf result Is Exception Then
+        Throw CType(result, Exception)
+      End If
+      Return CType(result, DataPortalResult)
     End Function
 
 #End Region
+
   End Class
 
 End Namespace

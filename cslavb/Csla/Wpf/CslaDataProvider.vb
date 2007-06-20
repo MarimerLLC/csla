63070ead3506168eb40e0347b3a327c5af5a4ca1
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2007-06-20 03:20:55 +0000

    Add support for Save and Undo RoutedCommands in XAML. A command source (like a Button) can set its CommandTarget to the CommandManager property of the CslaDataProvider to execute these two commands on the CslaDataProvider control.

--- cslavb/Csla/Wpf/CslaDataProvider.vb
+++ cslavb/Csla/Wpf/CslaDataProvider.vb
@@ -14,22 +14,38 @@ Namespace Wpf
   Public Class CslaDataProvider
     Inherits DataSourceProvider
 
-    Private _objectType As Type = Nothing
-    Private _factoryMethod As String = String.Empty
-    Private _factoryParameters As ObservableCollection(Of Object)
-    Private _isAsynchronous As Boolean
-
     ''' <summary>
     ''' Creates an instance of the object.
     ''' </summary>
     Public Sub New()
-      _factoryParameters = New ObservableCollection(Of Object)()
-      AddHandler _factoryParameters.CollectionChanged, AddressOf _factoryParameters_CollectionChanged
+      mCommandManager = New CslaDataProviderCommandManager(Me)
+      mFactoryParameters = New ObservableCollection(Of Object)()
+      AddHandler mFactoryParameters.CollectionChanged, AddressOf _factoryParameters_CollectionChanged
     End Sub
 
     Private Sub _factoryParameters_CollectionChanged(ByVal sender As Object, ByVal e As System.Collections.Specialized.NotifyCollectionChangedEventArgs)
       BeginQuery()
     End Sub
+
+#Region " Properties "
+
+    Private mObjectType As Type = Nothing
+    Private mManageLifetime As Boolean
+    Private mFactoryMethod As String = String.Empty
+    Private mFactoryParameters As ObservableCollection(Of Object)
+    Private mIsAsynchronous As Boolean
+    Private mCommandManager As CslaDataProviderCommandManager
+
+    ''' <summary>
+    ''' Gets an object that can be used to execute
+    ''' Save and Undo commands on this CslaDataProvider 
+    ''' through XAML command bindings.
+    ''' </summary>
+    Public ReadOnly Property CommandManager() As CslaDataProviderCommandManager
+      Get
+        Return mCommandManager
+      End Get
+    End Property
 
     ''' <summary>
     ''' Gets or sets the type of object 
@@ -37,11 +53,27 @@ Namespace Wpf
     ''' </summary>
     Public Property ObjectType() As Type
       Get
-        Return _objectType
+        Return mObjectType
       End Get
       Set(ByVal value As Type)
-        _objectType = value
+        mObjectType = value
         OnPropertyChanged(New PropertyChangedEventArgs("TypeName"))
+      End Set
+    End Property
+
+    ''' <summary>
+    ''' Gets or sets a value indicating whether the
+    ''' data control should manage the lifetime of
+    ''' the business object, including using n-level
+    ''' undo.
+    ''' </summary>
+    Public Property ManageObjectLifetime() As Boolean
+      Get
+        Return mManageLifetime
+      End Get
+      Set(ByVal value As Boolean)
+        mManageLifetime = value
+        OnPropertyChanged(New PropertyChangedEventArgs("ManageObjectLifetime"))
       End Set
     End Property
 
@@ -53,10 +85,10 @@ Namespace Wpf
     ''' </summary>
     Public Property FactoryMethod() As String
       Get
-        Return _factoryMethod
+        Return mFactoryMethod
       End Get
       Set(ByVal value As String)
-        _factoryMethod = value
+        mFactoryMethod = value
         OnPropertyChanged(New PropertyChangedEventArgs("GetFactoryMethod"))
       End Set
     End Property
@@ -67,7 +99,7 @@ Namespace Wpf
     ''' </summary>
     Public ReadOnly Property FactoryParameters() As IList
       Get
-        Return _factoryParameters
+        Return mFactoryParameters
       End Get
     End Property
 
@@ -78,12 +110,16 @@ Namespace Wpf
     ''' </summary>
     Public Property IsAsynchronous() As Boolean
       Get
-        Return _isAsynchronous
+        Return mIsAsynchronous
       End Get
       Set(ByVal value As Boolean)
-        _isAsynchronous = value
+        mIsAsynchronous = value
       End Set
     End Property
+
+#End Region
+
+#Region " Query "
 
     ''' <summary>
     ''' Overridden. Starts to create the requested object, 
@@ -95,9 +131,9 @@ Namespace Wpf
         Return
       End If
       Dim request As QueryRequest = New QueryRequest()
-      request.ObjectType = _objectType
-      request.FactoryMethod = _factoryMethod
-      request.FactoryParameters = _factoryParameters
+      request.ObjectType = mObjectType
+      request.FactoryMethod = mFactoryMethod
+      request.FactoryParameters = mFactoryParameters
 
       If IsAsynchronous Then
         System.Threading.ThreadPool.QueueUserWorkItem(AddressOf DoQuery, request)
@@ -189,6 +225,92 @@ Namespace Wpf
         End Set
       End Property
     End Class
+
+#End Region
+
+#End Region
+
+#Region "Cancel/Update"
+
+    ''' <summary>
+    ''' Cancels changes to the business object, returning
+    ''' it to its previous state.
+    ''' </summary>
+    ''' <remarks>
+    ''' This metod does nothing unless ManageLifetime is
+    ''' set to true and the object supports n-level undo.
+    ''' </remarks>
+    Public Sub Cancel()
+
+      Dim undo As Csla.Core.ISupportUndo = TryCast(Me.Data, Csla.Core.ISupportUndo)
+      If Not undo Is Nothing AndAlso mManageLifetime Then
+        undo.CancelEdit()
+        undo.BeginEdit()
+      End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Accepts changes to the business object, and
+    ''' commits them by calling the object's Save()
+    ''' method.
+    ''' </summary>
+    ''' <remarks>
+    ''' <para>
+    ''' This method does nothing unless the object
+    ''' implements Csla.Core.ISavable.
+    ''' </para><para>
+    ''' If the object implements IClonable, it
+    ''' will be cloned, and the clone will be
+    ''' saved.
+    ''' </para><para>
+    ''' If the object supports n-level undo and
+    ''' ManageLifetime is true, then this method
+    ''' will automatically call ApplyEdit() and
+    ''' BeginEdit() appropriately.
+    ''' </para>
+    ''' </remarks>
+    Public Sub Save()
+
+      ' only do something if the object implements
+      ' ISavable
+      Dim savable As Csla.Core.ISavable = TryCast(Me.Data, Csla.Core.ISavable)
+      If Not savable Is Nothing Then
+        Dim result As Object = savable
+        Dim exceptionResult As Exception = Nothing
+        Try
+          ' apply edits in memory
+          Dim undo As Csla.Core.ISupportUndo = TryCast(savable, Csla.Core.ISupportUndo)
+          If Not undo Is Nothing AndAlso mManageLifetime Then
+            undo.ApplyEdit()
+          End If
+
+          ' clone the object if possible
+          Dim clonable As ICloneable = TryCast(savable, ICloneable)
+          If Not clonable Is Nothing Then
+            savable = CType(clonable.Clone(), Csla.Core.ISavable)
+          End If
+
+          ' save the clone
+          result = savable.Save()
+
+          ' start editing the resulting object
+          undo = TryCast(result, Csla.Core.ISupportUndo)
+          If Not undo Is Nothing AndAlso mManageLifetime Then
+            undo.BeginEdit()
+          End If
+
+        Catch ex As Exception
+          exceptionResult = ex
+        End Try
+
+        ' clear previous object
+        MyBase.OnQueryFinished(Nothing, Nothing, Nothing, Nothing)
+        ' return result to base class
+        MyBase.OnQueryFinished(result, exceptionResult, Nothing, Nothing)
+      End If
+
+    End Sub
 
 #End Region
 
---
+++ cslavb/Csla/Wpf/CslaDataProviderCommandManager.vb
@@ -0,0 +1,83 @@
+﻿#If Not NET20 Then
+Imports System.Windows.Input
+Imports System.Windows
+
+Namespace Wpf
+
+  ''' <summary>
+  ''' Implements support for RoutedCommands that can
+  ''' be executed by the CslaDataProvider control.
+  ''' </summary>
+  ''' <remarks>
+  ''' Use this object as the CommandTarget for command
+  ''' source objects when you want the CslaDataProvider
+  ''' to execute the command.
+  ''' </remarks>
+  Public Class CslaDataProviderCommandManager
+    Inherits System.Windows.UIElement
+
+    Private mProvider As CslaDataProvider
+
+    Private ReadOnly Property Provider() As CslaDataProvider
+      Get
+        Return mProvider
+      End Get
+    End Property
+
+    Friend Sub New(ByVal provider As CslaDataProvider)
+
+      mProvider = provider
+
+    End Sub
+
+    Shared Sub New()
+
+      Dim binding As CommandBinding = New CommandBinding(ApplicationCommands.Save, AddressOf SaveCommand, AddressOf CanExecute)
+      CommandManager.RegisterClassCommandBinding(GetType(CslaDataProviderCommandManager), binding)
+
+      binding = New CommandBinding(ApplicationCommands.Undo, AddressOf UndoCommand, AddressOf CanExecute)
+      CommandManager.RegisterClassCommandBinding(GetType(CslaDataProviderCommandManager), binding)
+
+    End Sub
+
+    Friend Shared Sub CanExecute(ByVal target As Object, ByVal e As CanExecuteRoutedEventArgs)
+
+      Dim result As Boolean = False
+      Dim ctl As CslaDataProviderCommandManager = TryCast(target, CslaDataProviderCommandManager)
+      If Not ctl Is Nothing AndAlso Not ctl.Provider Is Nothing Then
+        Dim ibiz As Csla.Core.IEditableBusinessObject = TryCast(ctl.Provider.Data, Csla.Core.IEditableBusinessObject)
+        If Not ibiz Is Nothing Then
+          result = ibiz.IsSavable
+        Else
+          Dim icol As Csla.Core.IEditableCollection = TryCast(ctl.Provider.Data, Csla.Core.IEditableCollection)
+          If Not icol Is Nothing Then
+            result = icol.IsSavable
+          End If
+        End If
+      End If
+      e.CanExecute = result
+
+    End Sub
+
+    Friend Shared Sub SaveCommand(ByVal target As Object, ByVal e As ExecutedRoutedEventArgs)
+
+      Dim ctl As CslaDataProviderCommandManager = TryCast(target, CslaDataProviderCommandManager)
+      If Not ctl Is Nothing AndAlso Not ctl.Provider Is Nothing Then
+        ctl.Provider.Save()
+      End If
+
+    End Sub
+
+    Friend Shared Sub UndoCommand(ByVal target As Object, ByVal e As ExecutedRoutedEventArgs)
+
+      Dim ctl As CslaDataProviderCommandManager = TryCast(target, CslaDataProviderCommandManager)
+      If Not ctl Is Nothing AndAlso Not ctl.Provider Is Nothing Then
+        ctl.Provider.Cancel()
+      End If
+
+    End Sub
+
+  End Class
+
+End Namespace
+#End If

988c57e4a2d17af22ab41fd37e9495875763e6af
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2007-06-22 21:27:23 +0000

    Add PTWcfService project.

---
+++ ProjectTrackervb/www/PTWcfService/App_Code/PTService.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Collections.Generic
+Imports ProjectTracker.Library
+
+Public Class PTService
+  Implements IPTService
+
+#Region "IPTService Members"
+
+  Public Function GetProjectList() As ProjectData() Implements IPTService.GetProjectList
+
+    ' anonymous access allowed
+    Security.UseAnonymous()
+
+    Try
+      Dim list As ProjectList = ProjectList.GetProjectList()
+      Dim result As List(Of ProjectData) = New List(Of ProjectData)()
+      For Each item As ProjectInfo In list
+        Dim info As ProjectData = New ProjectData()
+        Csla.Data.DataMapper.Map(item, info)
+        result.Add(info)
+      Next item
+      Return result.ToArray()
+
+    Catch ex As Csla.DataPortalException
+      Throw ex.BusinessException
+
+    Catch ex As Exception
+      Throw New Exception(ex.Message)
+    End Try
+
+  End Function
+
+#End Region
+
+End Class

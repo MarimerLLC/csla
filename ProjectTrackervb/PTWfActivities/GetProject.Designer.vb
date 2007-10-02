fca8cb30767b3bb2babc00c2f84bcc42d6c0f207
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2007-10-02 18:41:38 +0000

    Add PTWfActivities project and use its activities.

---
+++ ProjectTrackervb/PTWfActivities/GetProject.Designer.vb
@@ -0,0 +1,13 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial class GetProject
+
+    'NOTE: The following procedure is required by the Workflow Designer
+    'It can be modified using the Workflow Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Private Sub InitializeComponent()
+        Name = "GetProject"
+    End Sub
+
+End Class
+

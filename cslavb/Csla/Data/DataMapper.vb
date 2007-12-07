78d9fb0e2153ee1794378070d0054fb4c71a42b8
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2007-12-07 21:53:08 +0000

    Add and enhance tests for DataMapper around the new type coercion capabilities and the ability to set fields as well as properties. Add TypeInfoCache to minimize use of reflection in key areas.
    Add DataMap class, and change DataMapper to support the concept of an explicit data map.
    
    Change how DataMapper coerces values by implementing a CoerceValue method that does a more robust job than the previous implementation (and handles both properties and fields).
    
    Refine type coercion so string.Empty and null convert to valid primitive values as a 0/false/etc.
    
    Also do a better job of handling Nullable<T> properties by determining the underlying type (T) for coercion.

--- cslavb/Csla/Data/DataMapper.vb
+++ cslavb/Csla/Data/DataMapper.vb
@@ -86,6 +86,54 @@ Namespace Data
 
 #End Region
 
+#Region "Map to Dictionary"
+
+    ''' <summary>
+    ''' Copies values from the source into the target.
+    ''' </summary>
+    ''' <param name="source">An object with properties to be loaded into the dictionary.</param>
+    ''' <param name="target">A name/value dictionary containing the source values.</param>
+    Public Sub Map(ByVal source As Object, ByVal target As Dictionary(Of String, Object))
+      Map(source, target, False)
+    End Sub
+
+    ''' <summary>
+    ''' Copies values from the source into the target.
+    ''' </summary>
+    ''' <param name="source">An object with properties to be loaded into the dictionary.</param>
+    ''' <param name="target">A name/value dictionary containing the source values.</param>
+    ''' <param name="ignoreList">A list of property names to ignore. 
+    ''' These properties will not be set on the target object.</param>
+    Public Sub Map(ByVal source As Object, ByVal target As Dictionary(Of String, Object), ByVal ParamArray ignoreList() As String)
+      Map(source, target, False, ignoreList)
+    End Sub
+
+    ''' <summary>
+    ''' Copies values from the source into the target.
+    ''' </summary>
+    ''' <param name="source">An object with properties to be loaded into the dictionary.</param>
+    ''' <param name="target">A name/value dictionary containing the source values.</param>
+    ''' <param name="ignoreList">A list of property names to ignore. 
+    ''' These properties will not be set on the target object.</param>
+    ''' <param name="suppressExceptions">If <see langword="true" />, any exceptions will be supressed.</param>
+    Public Sub Map(ByVal source As Object, ByVal target As Dictionary(Of String, Object), ByVal suppressExceptions As Boolean, ByVal ParamArray ignoreList() As String)
+      Dim ignore As List(Of String) = New List(Of String)(ignoreList)
+      For Each prop As PropertyInfo In Reflection.TypeInfoCache.GetPropertyInfo(source.GetType())
+        Dim propertyName As String = prop.Name
+        If (Not ignore.Contains(propertyName)) Then
+          Try
+            target.Add(propertyName, prop.GetValue(source, Nothing))
+          Catch ex As Exception
+            If (Not suppressExceptions) Then
+              Throw New ArgumentException(String.Format("{0} ({1})", My.Resources.PropertyCopyFailed, propertyName), ex)
+            End If
+          End Try
+        End If
+      Next prop
+    End Sub
+
+#End Region
+
 #Region " Map from Object "
 
     ''' <summary>
@@ -190,6 +238,20 @@ Namespace Data
 
 #End Region
 
+#Region "GetValue"
+
+    Private Function GetValue(ByVal member As MemberInfo, ByVal source As Object) As Object
+      If member.MemberType = MemberTypes.Property Then
+        Return (CType(member, PropertyInfo)).GetValue(source, Nothing)
+      Else
+        Return (CType(member, FieldInfo)).GetValue(source)
+      End If
+    End Function
+
+#End Region
+
+#Region "SetValue"
+
     ''' <summary>
     ''' Sets an object's property with the specified value,
     ''' coercing that value to the appropriate type if possible.
@@ -197,39 +259,86 @@ Namespace Data
     ''' <param name="target">Object containing the property to set.</param>
     ''' <param name="propertyName">Name of the property to set.</param>
     ''' <param name="value">Value to set into the property.</param>
-    Public Sub SetPropertyValue( _
-      ByVal target As Object, ByVal propertyName As String, _
-      ByVal value As Object)
-
-      Dim propertyInfo As PropertyInfo = _
-        target.GetType.GetProperty(propertyName)
-      If value Is Nothing Then
-        propertyInfo.SetValue(target, value, Nothing)
-
-      Else
-        Dim pType As Type = Utilities.GetPropertyType(propertyInfo.PropertyType)
-        Dim vType As Type = Utilities.GetPropertyType(value.GetType)
-        If pType.Equals(vType) Then
-          ' types match, just copy value
-          propertyInfo.SetValue(target, value, Nothing)
-
-        Else
-          ' types don't match, try to coerce types
-          If pType.Equals(GetType(Guid)) Then
-            propertyInfo.SetValue(target, New Guid(value.ToString), Nothing)
-
-          ElseIf pType.IsEnum AndAlso vType.Equals(GetType(String)) Then
-            propertyInfo.SetValue( _
-              target, System.Enum.Parse(pType, value.ToString), Nothing)
-
-          Else
-            propertyInfo.SetValue(target, _
-              Convert.ChangeType(value, pType), Nothing)
-          End If
-        End If
-      End If
-
+    Public Sub SetPropertyValue(ByVal target As Object, ByVal propertyName As String, ByVal value As Object)
+      Dim propertyInfo As PropertyInfo = target.GetType().GetProperty(propertyName, BindingFlags.Public Or BindingFlags.Instance Or BindingFlags.FlattenHierarchy)
+      SetValue(target, propertyInfo, value)
     End Sub
+
+    ''' <summary>
+    ''' Sets an object's field with the specified value,
+    ''' coercing that value to the appropriate type if possible.
+    ''' </summary>
+    ''' <param name="target">Object containing the field to set.</param>
+    ''' <param name="fieldName">Name of the field (public or non-public) to set.</param>
+    ''' <param name="value">Value to set into the field.</param>
+    Public Sub SetFieldValue(ByVal target As Object, ByVal fieldName As String, ByVal value As Object)
+      Dim fieldInfo As FieldInfo = target.GetType().GetField(fieldName, BindingFlags.Public Or BindingFlags.NonPublic Or BindingFlags.Instance)
+      SetValue(target, fieldInfo, value)
+    End Sub
+
+    ''' <summary>
+    ''' Sets an object's property or field with the specified value,
+    ''' coercing that value to the appropriate type if possible.
+    ''' </summary>
+    ''' <param name="target">Object containing the member to set.</param>
+    ''' <param name="memberInfo">MemberInfo object for the member to set.</param>
+    ''' <param name="value">Value to set into the member.</param>
+    Public Sub SetValue(ByVal target As Object, ByVal memberInfo As MemberInfo, ByVal value As Object)
+      If value IsNot Nothing Then
+        Dim pType As Type
+        If memberInfo.MemberType = MemberTypes.Property Then
+          pType = (CType(memberInfo, PropertyInfo)).PropertyType
+        Else
+          pType = (CType(memberInfo, FieldInfo)).FieldType
+        End If
+        Dim vType As Type = Utilities.GetPropertyType(value.GetType())
+        value = CoerceValue(pType, vType, value)
+      End If
+      If memberInfo.MemberType = MemberTypes.Property Then
+        CType(memberInfo, PropertyInfo).SetValue(target, value, Nothing)
+      Else
+        CType(memberInfo, FieldInfo).SetValue(target, value)
+      End If
+    End Sub
+
+    Private Function CoerceValue(ByVal propertyType As Type, ByVal valueType As Type, ByVal value As Object) As Object
+      If propertyType.Equals(valueType) Then
+        ' types match, just return value
+        Return value
+      Else
+        If propertyType.IsGenericType Then
+          If propertyType.GetGenericTypeDefinition() Is GetType(Nullable(Of )) Then
+            If value Is Nothing Then
+              Return Nothing
+            ElseIf valueType.Equals(GetType(String)) AndAlso CStr(value) = String.Empty Then
+              Return Nothing
+            End If
+          End If
+          propertyType = Utilities.GetPropertyType(propertyType)
+        End If
+
+        If propertyType.IsEnum AndAlso valueType.Equals(GetType(String)) Then
+          Return System.Enum.Parse(propertyType, value.ToString())
+        End If
+
+        If propertyType.IsPrimitive AndAlso valueType.Equals(GetType(String)) AndAlso String.IsNullOrEmpty(CStr(value)) Then
+          value = 0
+        End If
+
+        Try
+          Return Convert.ChangeType(value, Utilities.GetPropertyType(propertyType))
+        Catch
+          Dim cnv As TypeConverter = TypeDescriptor.GetConverter(Utilities.GetPropertyType(propertyType))
+          If cnv IsNot Nothing AndAlso cnv.CanConvertFrom(value.GetType()) Then
+            Return cnv.ConvertFrom(value)
+          Else
+            Throw
+          End If
+        End Try
+      End If
+    End Function
+
+#End Region
 
   End Module
 
---
+++ cslavb/Csla/Reflection/TypeInfoCache.vb
@@ -0,0 +1,183 @@
+﻿Imports System.Reflection
+Imports System.ComponentModel
+
+Namespace Reflection
+
+  ''' <summary>
+  ''' Maintains a cache of property and
+  ''' field info about types in the AppDomain.
+  ''' </summary>
+  Friend Class TypeInfoCache
+#Region "PropertyInfo"
+
+    Private Shared _propertyInfoCache As Dictionary(Of Type, List(Of PropertyInfo)) = New Dictionary(Of Type, List(Of PropertyInfo))()
+
+    ''' <summary>
+    ''' Gets a list of PropertyInfo objects for
+    ''' all properties on the specified type.
+    ''' </summary>
+    ''' <param name="type">Type of object for which to get properties.</param>
+    ''' <returns>
+    ''' Value is returned from the cache if possible, otherwise the
+    ''' information is loaded using reflection and placed in the
+    ''' cache for future use.
+    ''' </returns>
+    ''' <remarks>
+    ''' Only public instance properties are returned by this method, and
+    ''' the inheritance hierarchy is flattened.
+    ''' </remarks>
+    Public Shared Function GetPropertyInfo(ByVal type As Type) As List(Of PropertyInfo)
+      If (Not _propertyInfoCache.ContainsKey(type)) Then
+        SyncLock _propertyInfoCache
+          If (Not _propertyInfoCache.ContainsKey(type)) Then
+            _propertyInfoCache.Add(type, New List(Of PropertyInfo)(type.GetProperties(BindingFlags.Public Or BindingFlags.Instance Or BindingFlags.FlattenHierarchy)))
+          End If
+        End SyncLock
+      End If
+      Return _propertyInfoCache(type)
+    End Function
+
+#End Region
+
+#Region "BrowsablePropertyInfo"
+
+    Private Shared _browsablePropertyInfoCache As Dictionary(Of Type, List(Of PropertyInfo)) = New Dictionary(Of Type, List(Of PropertyInfo))()
+
+    ''' <summary>
+    ''' Gets a list of Browsable PropertyInfo objects for
+    ''' all properties on the specified type.
+    ''' </summary>
+    ''' <param name="type">Type of object for which to get properties.</param>
+    ''' <returns>
+    ''' Value is returned from the cache if possible, otherwise the
+    ''' information is loaded using reflection and placed in the
+    ''' cache for future use.
+    ''' </returns>
+    ''' <remarks>
+    ''' The Browsable attribute indicates whether a property can
+    ''' be used by data binding. Browsable properties can be
+    ''' data bound, non-browsable properties can not. This method
+    ''' only returns a list of Browsable PropertyDescriptor objects
+    ''' for the specified type, so all items returned can
+    ''' be used by data binding.
+    ''' </remarks>
+    Public Shared Function GetBrowsablePropertyInfo(ByVal type As Type) As List(Of PropertyInfo)
+      If (Not _browsablePropertyInfoCache.ContainsKey(type)) Then
+        SyncLock _browsablePropertyInfoCache
+          If (Not _browsablePropertyInfoCache.ContainsKey(type)) Then
+            Dim tmp As List(Of PropertyInfo) = New List(Of PropertyInfo)()
+            Dim list As PropertyDescriptorCollection = TypeDescriptor.GetProperties(type)
+            For Each item As PropertyDescriptor In list
+              If item.IsBrowsable Then
+                tmp.Add(type.GetProperty(item.Name))
+              End If
+            Next item
+            _browsablePropertyInfoCache.Add(type, tmp)
+          End If
+        End SyncLock
+      End If
+      Return _browsablePropertyInfoCache(type)
+    End Function
+
+#End Region
+
+#Region "PropertyDescriptor"
+
+    Private Shared _propertyDescriptorCache As Dictionary(Of Type, PropertyDescriptorCollection) = New Dictionary(Of Type, PropertyDescriptorCollection)()
+
+    ''' <summary>
+    ''' Gets a list of PropertyDescriptor objects for
+    ''' all properties on the specified type.
+    ''' </summary>
+    ''' <param name="type">Type of object for which to get properties.</param>
+    ''' <returns>
+    ''' Value is returned from the cache if possible, otherwise the
+    ''' information is loaded using reflection and placed in the
+    ''' cache for future use.
+    ''' </returns>
+    Public Shared Function GetPropertyDescriptors(ByVal type As Type) As PropertyDescriptorCollection
+      If (Not _propertyDescriptorCache.ContainsKey(type)) Then
+        SyncLock _propertyDescriptorCache
+          If (Not _propertyDescriptorCache.ContainsKey(type)) Then
+            _propertyDescriptorCache.Add(type, TypeDescriptor.GetProperties(type))
+          End If
+        End SyncLock
+      End If
+      Return _propertyDescriptorCache(type)
+    End Function
+
+#End Region
+
+#Region "BrowsablePropertyDescriptor"
+
+    Private Shared _browsablePropertyDescriptorCache As Dictionary(Of Type, List(Of PropertyDescriptor)) = New Dictionary(Of Type, List(Of PropertyDescriptor))()
+
+    ''' <summary>
+    ''' Gets a list of Browsable PropertyDescriptor 
+    ''' objects for all properties on the specified type.
+    ''' </summary>
+    ''' <param name="type">Type of object for which to get properties.</param>
+    ''' <returns>
+    ''' Value is returned from the cache if possible, otherwise the
+    ''' information is loaded using reflection and placed in the
+    ''' cache for future use.
+    ''' </returns>
+    ''' <remarks>
+    ''' The Browsable attribute indicates whether a property can
+    ''' be used by data binding. Browsable properties can be
+    ''' data bound, non-browsable properties can not. This method
+    ''' only returns a list of Browsable PropertyDescriptor objects
+    ''' for the specified type, so all items returned can
+    ''' be used by data binding.
+    ''' </remarks>
+    Public Shared Function GetBrowsablePropertyDescriptors(ByVal type As Type) As List(Of PropertyDescriptor)
+      If (Not _browsablePropertyDescriptorCache.ContainsKey(type)) Then
+        SyncLock _browsablePropertyDescriptorCache
+          If (Not _browsablePropertyDescriptorCache.ContainsKey(type)) Then
+            Dim tmp As List(Of PropertyDescriptor) = New List(Of PropertyDescriptor)()
+            Dim list As PropertyDescriptorCollection = TypeDescriptor.GetProperties(type)
+            For Each item As PropertyDescriptor In list
+              If item.IsBrowsable Then
+                tmp.Add(item)
+              End If
+            Next item
+            _browsablePropertyDescriptorCache.Add(type, tmp)
+          End If
+        End SyncLock
+      End If
+      Return _browsablePropertyDescriptorCache(type)
+    End Function
+
+#End Region
+
+#Region "FieldInfo"
+
+    Private Shared _fieldInfoCache As Dictionary(Of Type, List(Of FieldInfo)) = New Dictionary(Of Type, List(Of FieldInfo))()
+
+    ''' <summary>
+    ''' Gets a list of FieldInfo objects for
+    ''' all instance fields (public and non-public) on 
+    ''' the specified type.
+    ''' </summary>
+    ''' <param name="type">Type of object for which to get fields.</param>
+    ''' <returns>
+    ''' Value is returned from the cache if possible, otherwise the
+    ''' information is loaded using reflection and placed in the
+    ''' cache for future use.
+    ''' </returns>
+    Public Shared Function GetFieldInfo(ByVal type As Type) As List(Of FieldInfo)
+      If (Not _fieldInfoCache.ContainsKey(type)) Then
+        SyncLock _fieldInfoCache
+          If (Not _fieldInfoCache.ContainsKey(type)) Then
+            _fieldInfoCache.Add(type, New List(Of FieldInfo)(type.GetFields(BindingFlags.NonPublic Or BindingFlags.Instance Or BindingFlags.Public)))
+          End If
+        End SyncLock
+      End If
+      Return _fieldInfoCache(type)
+    End Function
+
+#End Region
+
+  End Class
+
+End Namespace

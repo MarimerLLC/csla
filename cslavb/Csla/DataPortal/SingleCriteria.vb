69e712fc9ccfd046b650ba1c96f7dcdfc574e633
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2007-12-18 19:22:26 +0000

    Add SingleCriteria class, which is a generic implementation of a Criteria object that contains a single value.

---
+++ cslavb/Csla/DataPortal/SingleCriteria.vb
@@ -0,0 +1,29 @@
+﻿''' <summary>
+''' A single-value criteria used to retrieve business
+''' objects that only require one criteria value.
+''' </summary>
+''' <typeparam name="B">
+''' Type of business object to retrieve.
+''' </typeparam>
+''' <typeparam name="C">
+''' Type of the criteria value.
+''' </typeparam>
+''' <remarks></remarks>
+<Serializable()> _
+Public Class SingleCriteria(Of B, C)
+  Inherits CriteriaBase
+
+  Private mValue As C
+
+  Public ReadOnly Property Value() As C
+    Get
+      Return mValue
+    End Get
+  End Property
+
+  Public Sub New(ByVal value As C)
+    MyBase.New(GetType(B))
+    mValue = value
+  End Sub
+
+End Class

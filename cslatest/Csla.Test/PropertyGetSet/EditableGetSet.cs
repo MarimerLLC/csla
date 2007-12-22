1a5e8165fb8ed865edf38aed6e42fc5be03780d2
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2007-12-22 00:07:15 +0000

    Add tests for the new GetProperty and SetProperty functionality.

---
+++ cslatest/Csla.Test/PropertyGetSet/EditableGetSet.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Csla;
+
+#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#else
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+#endif
+
+namespace Csla.Test.PropertyGetSet
+{
+  public class EditableGetSet : Csla.BusinessBase<EditableGetSet>
+  {
+    private static Csla.PropertyInfo<string> F01Property = new Csla.PropertyInfo<string>("F01");
+    private string _f01 = F01Property.DefaultValue;
+    public string F01
+    {
+      get { return GetProperty<string>(F01Property, _f01); }
+      set { SetProperty<string>(F01Property, ref _f01, value); }
+    }
+
+    private static Csla.PropertyInfo<int> F02Property = new Csla.PropertyInfo<int>("F02");
+    private int _f02 = F02Property.DefaultValue;
+    public int F02
+    {
+      get { return GetProperty<int>(F02Property, _f02); }
+      set { SetProperty<int>(F02Property, ref _f02, value); }
+    }
+
+    private static Csla.PropertyInfo<string> F03Property = new Csla.PropertyInfo<string>("F03", "field 3", "n/a");
+    private string _f03 = F03Property.DefaultValue;
+    public string F03
+    {
+      get { return GetProperty<string>(F03Property, _f03); }
+      set { SetProperty<string>(F03Property, ref _f03, value); }
+    }
+
+    private static Csla.PropertyInfo<string> M01Property = new Csla.PropertyInfo<string>("M01");
+    public string M01
+    {
+      get { return GetProperty<string>(M01Property); }
+      set { SetProperty<string>(M01Property, value); }
+    }
+
+    private static Csla.PropertyInfo<int> M02Property = new Csla.PropertyInfo<int>("M02");
+    public int M02
+    {
+      get { return GetProperty<int>(M02Property); }
+      set { SetProperty<int>(M02Property, value); }
+    }
+
+    private static Csla.PropertyInfo<string> M03Property = new Csla.PropertyInfo<string>("M03", "field 3", "n/a");
+    public string M03
+    {
+      get { return GetProperty<string>(M03Property); }
+      set { SetProperty<string>(M03Property, value); }
+    }
+
+    private static Csla.PropertyInfo<Csla.SmartDate> F04Property = new Csla.PropertyInfo<Csla.SmartDate>("F04");
+    private Csla.SmartDate _F04 = F04Property.DefaultValue;
+    public string F04
+    {
+      get { return GetProperty<Csla.SmartDate, string>(F04Property, _F04); }
+      set { SetProperty<Csla.SmartDate, string>(F04Property, ref _F04, value); }
+    }
+
+    private static Csla.PropertyInfo<Csla.SmartDate> M04Property = new Csla.PropertyInfo<Csla.SmartDate>("M04");
+    public string M04
+    {
+      get { return GetProperty<Csla.SmartDate, string>(M04Property); }
+      set { SetProperty<Csla.SmartDate, string>(M04Property, value); }
+    }
+
+    private static Csla.PropertyInfo<EditableGetSet> C01Property = new Csla.PropertyInfo<EditableGetSet>("C01");
+    public EditableGetSet C01
+    {
+      get 
+      { 
+        if (!FieldManager.FieldExists(C01Property))
+          SetProperty<EditableGetSet>(C01Property, new EditableGetSet());
+        return GetProperty<EditableGetSet>(C01Property); 
+      }
+    }
+  }
+}
---
+++ cslatest/Csla.Test/PropertyGetSet/PropertyGetSetTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#else
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+#endif
+
+namespace Csla.Test.PropertyGetSet
+{
+  [TestClass()]
+  public class PropertyGetSetTests
+  {
+    [TestMethod]
+    public void ExplicitFieldProperties()
+    {
+      EditableGetSet root = new EditableGetSet();
+      root.PropertyChanging += new System.ComponentModel.PropertyChangingEventHandler(root_PropertyChanging);
+      root.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(root_PropertyChanged);
+      Assert.AreEqual("n/a", root.F03, "Default value should have been set");
+      Assert.AreEqual("", root.F01, "String should default to string.Empty");
+      Assert.AreEqual(0, root.F02, "Numeric should default to 0");
+
+      root.F01 = "hi there";
+      Assert.AreEqual("hi there", root.F01, "String should have been set");
+      Assert.AreEqual("F01", _changingName, "F01 should have been changing");
+      Assert.AreEqual("F01", _changedName, "F01 should have changed");
+
+      root.F02 = 123;
+      Assert.AreEqual(123, root.F02, "Numeric should have been set");
+
+      root.PropertyChanging -= new System.ComponentModel.PropertyChangingEventHandler(root_PropertyChanging);
+      root.PropertyChanged -= new System.ComponentModel.PropertyChangedEventHandler(root_PropertyChanged);
+    }
+
+    [TestMethod]
+    public void ManagedFieldProperties()
+    {
+      EditableGetSet root = new EditableGetSet();
+      root.PropertyChanging += new System.ComponentModel.PropertyChangingEventHandler(root_PropertyChanging);
+      root.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(root_PropertyChanged);
+      Assert.AreEqual("n/a", root.M03, "Default value should have been set");
+      Assert.AreEqual("", root.M01, "String should default to string.Empty");
+      Assert.AreEqual(0, root.M02, "Numeric should default to 0");
+
+      root.M01 = "hi there";
+      Assert.AreEqual("hi there", root.M01, "String should have been set");
+      Assert.AreEqual("M01", _changingName, "M01 should have been changing");
+      Assert.AreEqual("M01", _changedName, "M01 should have changed");
+
+      root.M02 = 123;
+      Assert.AreEqual(123, root.M02, "Numeric should have been set");
+
+      root.PropertyChanging -= new System.ComponentModel.PropertyChangingEventHandler(root_PropertyChanging);
+      root.PropertyChanged -= new System.ComponentModel.PropertyChangedEventHandler(root_PropertyChanged);
+    }
+
+    [TestMethod]
+    public void SmartDateProperties()
+    {
+      EditableGetSet root = new EditableGetSet();
+      Assert.AreEqual("", root.F04, "Field should default to string.Empty");
+      Assert.AreEqual("", root.M04, "Should default to string.Empty");
+
+      root.F04 = new DateTime(1998, 12, 21).ToShortDateString();
+      Assert.AreEqual(new DateTime(1998, 12, 21).ToShortDateString(), root.F04, "Field SmartDate should have been set");
+
+      root.M04 = new DateTime(1998,12,21).ToShortDateString();
+      Assert.AreEqual(new DateTime(1998, 12, 21).ToShortDateString(), root.M04, "SmartDate should have been set");
+    }
+
+    [TestMethod]
+    public void SimpleChildProperties()
+    {
+      EditableGetSet root = new EditableGetSet();
+      root.PropertyChanging += new System.ComponentModel.PropertyChangingEventHandler(root_PropertyChanging);
+      root.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(root_PropertyChanged);
+      
+      EditableGetSet child = root.C01;
+      Assert.IsNotNull(child, "Child should not be null");
+      Assert.AreEqual("C01", _changingName, "C01 should have been changing");
+      Assert.AreEqual("C01", _changedName, "C01 should have changed");
+      _changingName = "";
+      _changedName = "";
+
+      child.F01 = "hi there";
+      Assert.AreEqual("", _changingName, "C01 should NOT have been changing");
+      Assert.AreEqual("C01", _changedName, "C01 should have changed");
+    }
+
+    private string _changingName;
+    private string _changedName;
+    void root_PropertyChanging(object sender, System.ComponentModel.PropertyChangingEventArgs e)
+    {
+      _changingName = e.PropertyName;
+    }
+
+    void root_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+      _changedName = e.PropertyName;
+    }
+
+  }
+}

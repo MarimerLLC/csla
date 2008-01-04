632efa41de7104fd1e541a58894c30787269939a
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-01-04 22:06:48 +0000

    Remove unused file.

--- cslavb/Csla/Core/FieldManager/FieldData.vb
+++ cslavb/Csla/Core/FieldManager/FieldData.vb
@@ -1,5 +1,9 @@
 ﻿Namespace Core.FieldManager
 
+  ''' <summary>
+  ''' Contains a field value and related metadata.
+  ''' </summary>
+  ''' <typeparam name="T">Type of field value contained.</typeparam>
   <Serializable()> _
   Public Class FieldData(Of T)
     Implements IFieldData
@@ -8,16 +12,28 @@
     Private mData As T
     Private mIsDirty As Boolean
 
+    ''' <summary>
+    ''' Creates a new instance of the object.
+    ''' </summary>
+    ''' <param name="name">
+    ''' Name of the field.
+    ''' </param>
     Public Sub New(ByVal name As String)
       mName = name
     End Sub
 
+    ''' <summary>
+    ''' Gets the name of the field.
+    ''' </summary>
     Public ReadOnly Property Name() As String Implements IFieldData.Name
       Get
         Return mName
       End Get
     End Property
 
+    ''' <summary>
+    ''' Gets or sets the value of the field.
+    ''' </summary>
     Public Property Value() As T
       Get
         Return mData
@@ -49,6 +65,10 @@
       End Get
     End Property
 
+    ''' <summary>
+    ''' Gets a value indicating whether the field
+    ''' has been changed.
+    ''' </summary>
     Public ReadOnly Property IsDirty() As Boolean Implements ITrackStatus.IsDirty
       Get
         Dim child As ITrackStatus = TryCast(mData, ITrackStatus)
@@ -61,6 +81,9 @@
       End Get
     End Property
 
+    ''' <summary>
+    ''' Marks the field as unchanged.
+    ''' </summary>
     Public Sub MarkClean() Implements IFieldData.MarkClean
 
       mIsDirty = False

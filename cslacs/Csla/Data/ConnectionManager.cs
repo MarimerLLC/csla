4913c2b65bbb5265822f7f595e4d1b2bce43d2c4
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-01-08 16:42:28 +0000

    Add ConnectionManager.

---
+++ cslacs/Csla/Data/ConnectionManager.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Csla.Data
+{
+  /// <summary>
+  /// Provides an automated way to reuse open
+  /// database connections within the context
+  /// of a single data portal operation.
+  /// </summary>
+  /// <typeparam name="C">
+  /// Type of database connection object to use.
+  /// </typeparam>
+  /// <remarks>
+  /// This type stores the open database connection
+  /// in <see cref="Csla.ApplicationContext.LocalContext" />
+  /// and uses reference counting through
+  /// <see cref="IDisposable" /> to keep the connection
+  /// open for reuse by child objects, and to automatically
+  /// dispose the connection when the last consumer
+  /// has called Dispose."
+  /// </remarks>
+  public class ConnectionManager<C> : IDisposable where C : IDbConnection, new()
+  {
+    private static object _lock = new object();
+    private C _connection;
+    private string _connectionString;
+
+    /// <summary>
+    /// Gets the ConnectionManager object for the specified
+    /// connectionString.
+    /// </summary>
+    /// <param name="connectionString">
+    /// The database connection string.
+    /// </param>
+    /// <returns>ConnectionManager object for the connection.</returns>
+    public static ConnectionManager<C> GetManager(string connectionString)
+    {
+
+      lock (_lock)
+      {
+        ConnectionManager<C> mgr = null;
+        if (ApplicationContext.LocalContext.Contains("__db:" + connectionString))
+        {
+          mgr = (ConnectionManager<C>)(ApplicationContext.LocalContext["__db:" + connectionString]);
+
+        }
+        else
+        {
+          mgr = new ConnectionManager<C>(connectionString);
+          ApplicationContext.LocalContext["__db:" + connectionString] = mgr;
+        }
+        mgr.AddRef();
+        return mgr;
+      }
+
+    }
+
+    private ConnectionManager(string connectionString)
+    {
+
+      _connectionString = connectionString;
+
+      // open connection
+      _connection = new C();
+      _connection.ConnectionString = connectionString;
+      _connection.Open();
+
+    }
+
+    /// <summary>
+    /// Gets the open database connection object.
+    /// </summary>
+    public C Connection
+    {
+      get
+      {
+        return _connection;
+      }
+    }
+
+    #region  Reference counting
+
+    private int mRefCount;
+
+    private void AddRef()
+    {
+      mRefCount += 1;
+    }
+
+    private void DeRef()
+    {
+
+      lock (_lock)
+      {
+        mRefCount -= 1;
+        if (mRefCount == 0)
+        {
+          _connection.Dispose();
+          ApplicationContext.LocalContext.Remove("__db:" + _connectionString);
+        }
+      }
+
+    }
+
+    #endregion
+
+    #region  IDisposable
+
+    public void Dispose()
+    {
+      DeRef();
+    }
+
+    #endregion
+
+  }
+}

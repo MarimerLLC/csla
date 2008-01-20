11e081d59a01573ea983680b4a77aee1dc7dc0d0
aarone@magenic.com <aarone@magenic.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-01-20 20:26:50 +0000

    Added tests for indexing - somewhat of a hybrid performance/unit test (since indexing, by definition, is supposed to help indexing)

--- cslatest/Csla.Test/Basic/CollectionTests.cs
+++ cslatest/Csla.Test/Basic/CollectionTests.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Diagnostics;
 
 #if !NUNIT
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -39,6 +41,151 @@ namespace Csla.Test.Basic
       RootListChild child = list.AddNew();
       string[] rules = child.GetRuleDescriptions();
     }
+
+    [TestMethod]
+    public void IndexOnReadOnlyWorks()
+    {
+      var sampleSize = 100000;
+      Console.WriteLine("Creating " + sampleSize + " element collection...");
+      var readOnlyCollection = new TestReadOnlyCollection(sampleSize);
+      Console.WriteLine("Collection established.");
+      
+      //first query establishes the index
+      var controlSet = readOnlyCollection.ToList();
+
+      var primeQuery = from i in readOnlyCollection where i.IndexedString == "42" select i;
+      var forcedPrimeITeration = primeQuery.ToArray();
+
+      Stopwatch watch = new Stopwatch();
+
+      watch.Start();
+      var indexedQuery = from i in readOnlyCollection where i.IndexedString == "42" select i;
+      var forcedIterationIndexed = indexedQuery.ToArray();
+      watch.Stop();
+
+      var indexedRead = watch.ElapsedMilliseconds;
+
+      watch.Reset();
+      
+      watch.Start();
+      var nonIndexedQuery = from i in readOnlyCollection where i.NonIndexedString == "42" select i;
+      var forcedIterationNonIndexed = nonIndexedQuery.ToArray();
+      watch.Stop();
+      
+      var nonIndexedRead = watch.ElapsedMilliseconds;
+
+      watch.Reset();
+
+      watch.Start();
+      var controlQuery = from i in controlSet where i.IndexedString == "42" select i;
+      var forcedControlIteration = controlQuery.ToArray();
+      watch.Stop();
+
+      var controlRead = watch.ElapsedMilliseconds;
+
+      
+      Console.WriteLine("Sample size = " + sampleSize);
+      Console.WriteLine("Indexed Read = " + indexedRead + "ms");
+      Console.WriteLine("Non-Indexed Read = " + nonIndexedRead + "ms");
+      Console.WriteLine("Standard Linq-to-objects Read = " + controlRead + "ms");
+      Assert.IsTrue(indexedRead < nonIndexedRead);
+      Assert.IsTrue(forcedIterationIndexed.Count() == forcedIterationNonIndexed.Count());
+    }
+
+    [TestMethod]
+    public void IndexOnBusinessListBaseWorks()
+    {
+      var sampleSize = 100000;
+      Console.WriteLine("Creating " + sampleSize + " element collection...");
+      var blbCollection = new TestBusinessListBaseCollection(sampleSize);
+      Console.WriteLine("Collection established.");
+
+      //first query establishes the index
+      var controlSet = blbCollection.ToList();
+
+      var primeQuery = from i in blbCollection where i.IndexedString == "42" select i;
+      var forcedPrimeITeration = primeQuery.ToArray();
+
+      Stopwatch watch = new Stopwatch();
+
+      watch.Start();
+      var indexedQuery = from i in blbCollection where i.IndexedString == "42" select i;
+      var forcedIterationIndexed = indexedQuery.ToArray();
+      watch.Stop();
+
+      var indexedRead = watch.ElapsedMilliseconds;
+
+      watch.Reset();
+
+      watch.Start();
+      var nonIndexedQuery = from i in blbCollection where i.NonIndexedString == "42" select i;
+      var forcedIterationNonIndexed = nonIndexedQuery.ToArray();
+      watch.Stop();
+
+      var nonIndexedRead = watch.ElapsedMilliseconds;
+
+      watch.Reset();
+
+      watch.Start();
+      var controlQuery = from i in controlSet where i.IndexedString == "42" select i;
+      var forcedControlIteration = controlQuery.ToArray();
+      watch.Stop();
+
+      var controlRead = watch.ElapsedMilliseconds;
+
+
+      Console.WriteLine("Sample size = " + sampleSize);
+      Console.WriteLine("Indexed Read = " + indexedRead + "ms");
+      Console.WriteLine("Non-Indexed Read = " + nonIndexedRead + "ms");
+      Console.WriteLine("Standard Linq-to-objects Read = " + controlRead + "ms");
+      Assert.IsTrue(indexedRead < nonIndexedRead);
+      Assert.IsTrue(forcedIterationIndexed.Count() == forcedIterationNonIndexed.Count());
+    }
+  }
+
+  [Serializable]
+  public class TestReadOnlyCollection : ReadOnlyListBase<TestReadOnlyCollection, TestIndexableItem>
+  {
+    public TestReadOnlyCollection(int sampleSize)
+    {
+      //allow adds
+      IsReadOnly = false;
+      Random rnd = new Random();
+      for(int i = 0; i < sampleSize; i++)
+      {
+        int nextRnd = rnd.Next(sampleSize / 100);
+        Add(
+          new TestIndexableItem 
+          { 
+            IndexedString = nextRnd.ToString(), 
+            IndexedInt = nextRnd, 
+            NonIndexedString = nextRnd.ToString() 
+          }
+          );
+      }
+      IsReadOnly = true;
+    }
+  }
+
+  [Serializable]
+  public class TestBusinessListBaseCollection : BusinessListBase<TestBusinessListBaseCollection, TestIndexableItem>
+  {
+    public TestBusinessListBaseCollection(int sampleSize)
+    {
+      Random rnd = new Random();
+      for (int i = 0; i < sampleSize; i++)
+      {
+        int nextRnd = rnd.Next(sampleSize / 100);
+        Add(
+          new TestIndexableItem
+          {
+            IndexedString = nextRnd.ToString(),
+            IndexedInt = nextRnd,
+            NonIndexedString = nextRnd.ToString()
+          }
+          );
+      }
+    }
   }
 
   [Serializable]
@@ -58,5 +205,16 @@ namespace Csla.Test.Basic
     {
       MarkAsChild();
     }
+  }
+
+  [Serializable]
+  public class TestIndexableItem : BusinessBase<TestIndexableItem>
+  {
+    [Indexable]
+    public string IndexedString{get; set;}
+    [Indexable]
+    public int IndexedInt{get; set;}
+    public string NonIndexedString{get; set;}
+
   }
 }

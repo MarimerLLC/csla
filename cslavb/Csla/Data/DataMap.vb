7f51127e6e24b09e4be35900f8055fed951c8913
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-01-31 17:36:18 +0000

    Add comments.

--- cslavb/Csla/Data/DataMap.vb
+++ cslavb/Csla/Data/DataMap.vb
@@ -2,6 +2,11 @@
 
 Namespace Data
 
+  ''' <summary>
+  ''' Defines a mapping between two sets of
+  ''' properties/fields for use by
+  ''' DataMapper.
+  ''' </summary>
   Public Class DataMap
 
 #Region "MapElement"
@@ -35,6 +40,15 @@ Namespace Data
     Private _fieldFlags As BindingFlags = BindingFlags.Public Or BindingFlags.NonPublic Or BindingFlags.Instance
     Private _propertyFlags As BindingFlags = BindingFlags.Public Or BindingFlags.Instance Or BindingFlags.FlattenHierarchy
 
+    ''' <summary>
+    ''' Initializes an instance of the type.
+    ''' </summary>
+    ''' <param name="sourceType">
+    ''' Type of source object.
+    ''' </param>
+    ''' <param name="targetType">
+    ''' Type of target object.
+    ''' </param>
     Public Sub New(ByVal sourceType As Type, ByVal targetType As Type)
       _sourceType = sourceType
       _targetType = targetType
@@ -50,18 +64,55 @@ Namespace Data
       Return _map
     End Function
 
+    ''' <summary>
+    ''' Adds a property to property
+    ''' mapping.
+    ''' </summary>
+    ''' <param name="sourceProperty">
+    ''' Name of source property.
+    ''' </param>
+    ''' <param name="targetProperty">
+    ''' Name of target property.
+    ''' </param>
     Public Sub AddPropertyMapping(ByVal sourceProperty As String, ByVal targetProperty As String)
       _map.Add(New MemberMapping(_sourceType.GetProperty(sourceProperty, _propertyFlags), _targetType.GetProperty(targetProperty, _propertyFlags)))
     End Sub
 
+    ''' <summary>
+    ''' Adds a field to field mapping.
+    ''' </summary>
+    ''' <param name="sourceField">
+    ''' Name of source field.
+    ''' </param>
+    ''' <param name="targetField">
+    ''' Name of target field.
+    ''' </param>
     Public Sub AddFieldMapping(ByVal sourceField As String, ByVal targetField As String)
       _map.Add(New MemberMapping(_sourceType.GetField(sourceField, _fieldFlags), _targetType.GetField(targetField, _fieldFlags)))
     End Sub
 
+    ''' <summary>
+    ''' Adds a field to property mapping.
+    ''' </summary>
+    ''' <param name="sourceField">
+    ''' Name of source field.
+    ''' </param>
+    ''' <param name="targetProperty">
+    ''' Name of target property.
+    ''' </param>
     Public Sub AddFieldToPropertyMapping(ByVal sourceField As String, ByVal targetProperty As String)
       _map.Add(New MemberMapping(_sourceType.GetField(sourceField, _fieldFlags), _targetType.GetProperty(targetProperty, _propertyFlags)))
     End Sub
 
+    ''' <summary>
+    ''' Adds a property to field mapping.
+    ''' </summary>
+    ''' <param name="sourceProperty">
+    ''' Name of source property.
+    ''' </param>
+    ''' <param name="targetField">
+    ''' Name of target field.
+    ''' </param>
     Public Sub AddPropertyToFieldMapping(ByVal sourceProperty As String, ByVal targetField As String)
       _map.Add(New MemberMapping(_sourceType.GetProperty(sourceProperty, _propertyFlags), _targetType.GetField(targetField, _fieldFlags)))
     End Sub

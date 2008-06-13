783e4e00d7e36a4dce4b4a226f13bfacf373378d
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-06-13 19:21:29 +0000

    Restore TypeExtensions.cs.

---
+++ cslalightcs/Csla/Serialization/TypeExtensions.cs
@@ -0,0 +1,13 @@
+﻿﻿using System;
+
+namespace Csla.Serialization
+{
+  public static class TypeExtensions
+  {
+    public static bool IsSerializable(this Type type)
+    {
+      var result = type.GetCustomAttributes(typeof(SerializableAttribute), false);
+      return (result != null && result.Length > 0);
+    }
+  }
+}

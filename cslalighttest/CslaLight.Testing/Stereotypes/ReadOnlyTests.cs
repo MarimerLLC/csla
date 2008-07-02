56693b4c761c2b6b089d25448f586a90de2158f1
sergeyb@magenic.com <sergeyb@magenic.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-07-02 03:17:34 +0000

    Readonly Business Base data portal tests have been implemented.  The same test for read only list base are in progress.

---
+++ cslalighttest/CslaLight.Testing/Stereotypes/ReadOnlyTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Csla.Testing.Business.EditableRootTests;
+using cslalighttest.Engine;
+using System.Diagnostics;
+using Csla.Testing.Business.ReadOnlyTest;
+
+namespace cslalighttest.Stereotypes
+{
+  [TestClass]
+  public class ReadOnlyTests
+  {
+    [TestSetup]
+    public void Setup()
+    {
+      Csla.DataPortal.ProxyTypeName = "Csla.DataPortalClient.WcfProxy, Csla";
+      Csla.DataPortalClient.WcfProxy.DefaultUrl = ReadOnlyPerson.DataPortalUrl;
+    }
+
+    [TestMethod]
+    public void TestReadOnlyFetch(AsyncTestContext context)
+    {
+      Guid id = Guid.NewGuid();
+      ReadOnlyPerson.Fetch(
+        id,
+        (actual, error) =>
+        {
+          context.Assert.AreEqual(id, actual.Id);
+          context.Assert.AreEqual(id.ToString(), actual.Name);
+          context.Assert.AreEqual(new DateTime(1980, 1, 1), actual.Birthdate);
+          context.Assert.Success();
+        });
+    }
+  }
+}

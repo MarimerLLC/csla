0e975160fc68749c54d324f487d2285ed540f684
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-07-23 15:08:18 +0000

    Have BusinessBase raise a ChildChanged event when a child changes, rather than a PropertyChanged event. This matches the new CSLA .NET behavior and avoids an issue with data binding. bugid: 40

---
+++ cslalightcs/Csla/Core/ChildChangedEventArgs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Csla.Core
+{
+  /// <summary>
+  /// Contains event data about the changed child object.
+  /// </summary>
+  public class ChildChangedEventArgs : EventArgs
+  {
+    /// <summary>
+    /// Gets a reference to the changed child object.
+    /// </summary>
+    public object ChildObject { get; private set; }
+    /// <summary>
+    /// Gets the PropertyChangedEventArgs object from the
+    /// child's PropertyChanged event, if the child is
+    /// not a collection or list.
+    /// </summary>
+    public PropertyChangedEventArgs PropertyChangedArgs { get; private set; }
+    /// <summary>
+    /// Gets the NotifyCollectionChangedEventArgs object from the
+    /// child's ListChanged event, if the child is a
+    /// collection or list.
+    /// </summary>
+    public NotifyCollectionChangedEventArgs ListChangedArgs { get; private set; }
+
+    /// <summary>
+    /// Creates an instance of the object.
+    /// </summary>
+    /// <param name="source">
+    /// Reference to the object that was changed.
+    /// </param>
+    /// <param name="listArgs">
+    /// NotifyCollectionChangedEventArgs object or null.
+    /// </param>
+    /// <param name="propertyArgs">
+    /// PropertyChangedEventArgs object or null.
+    /// </param>
+    public ChildChangedEventArgs(object source, PropertyChangedEventArgs propertyArgs,  NotifyCollectionChangedEventArgs listArgs)
+    {
+      this.ChildObject = source;
+      this.PropertyChangedArgs = propertyArgs;
+      this.ListChangedArgs = listArgs;
+    }
+  }
+}

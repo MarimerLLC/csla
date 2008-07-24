b3449b4109dab0955bc8a92ffc119acad86e666f
grantb@magenic.com <grantb@magenic.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-07-24 18:59:32 +0000

    bugid: 47

---
+++ cslalighttest/SecurityTest/CustomAuthentication/CARemoteWithAppServer/ClassLibrary/SLIdentity.partial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Csla.Core;
+using System.ServiceModel;
+
+namespace ClassLibrary
+{
+  public partial class SLIdentity
+  {
+    public void DataPortal_Fetch(ClassLibrary.SLIdentity.CredentialsCriteria criteria)
+    {
+      BasicHttpBinding bind = new BasicHttpBinding();
+      EndpointAddress endpoint = new EndpointAddress("http://localhost:4769/Authentication.svc");
+
+      ClassLibrary.AuthenticationService.AuthenticationClient client = 
+        new ClassLibrary.AuthenticationService.AuthenticationClient(bind, endpoint);
+      string response = client.Authenticate(criteria.Username, criteria.Password);
+
+      string[] results = response.Split(';');
+
+      if (Boolean.Parse(results[0]))
+      {
+        SetCslaIdentity(new MobileList<string>(results[1].Split(',')), true, criteria.Username);
+      }
+      else
+      {
+        SetCslaIdentity(null, false, "");
+      }
+    }
+  }
+}
---
+++ cslalighttest/SecurityTest/CustomAuthentication/CARemoteWithAppServer/SilverlightApplication/Page.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using ClassLibrary;
+
+namespace SilverlightApplication
+{
+  public partial class Page : UserControl
+  {
+    public Page()
+    {
+      InitializeComponent();
+      Csla.DataPortal.ProxyTypeName = "Csla.DataPortalClient.WcfProxy, Csla";
+      Csla.DataPortalClient.WcfProxy.DefaultUrl = "http://localhost:4769/WcfPortal.svc";
+    }
+
+    private void btnRemoteSuccessfulLogin_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        txtRemoteSuccessfulLogin.Text = String.Empty;
+
+        SLPrincipal.Logout();
+        SLPrincipal.Login("TestUser", "1234", (o, e2) =>
+        {
+          if (Csla.ApplicationContext.User.Identity.Name == "TestUser"
+            && Csla.ApplicationContext.User.Identity.IsAuthenticated
+            && ((SLPrincipal.LoginEventArgs)e2).LoginSucceded)
+          {
+            txtRemoteSuccessfulLogin.Text = "Pass";
+          }
+          else
+          {
+            txtRemoteSuccessfulLogin.Text = "Fail";
+          }
+        });
+      }
+      catch
+      {
+        txtRemoteSuccessfulLogin.Text = "Fail";
+      }
+    }
+
+    private void btnRemoteUnsuccessfulLogin_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        txtRemoteUnsuccessfulLogin.Text = String.Empty;
+
+        SLPrincipal.Logout();
+        SLPrincipal.Login("invaliduser", "invalidpassword", (o, e2) =>
+        {
+          if (Csla.ApplicationContext.User.Identity.GetType() == typeof(Csla.Security.UnauthenticatedIdentity)
+             && !((SLPrincipal.LoginEventArgs)e2).LoginSucceded)
+          {
+            txtRemoteUnsuccessfulLogin.Text = "Pass";
+          }
+          else
+          {
+            txtRemoteUnsuccessfulLogin.Text = "Fail";
+          }
+        });
+      }
+      catch
+      {
+        txtRemoteUnsuccessfulLogin.Text = "Fail";
+      }
+    }
+
+    private void btnRemoteRoles_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        txtRemoteRoles.Text = String.Empty;
+
+        SLPrincipal.Logout();
+        SLPrincipal.Login("TestUser", "1234", (o, e2) =>
+        {
+          if (Csla.ApplicationContext.User.IsInRole("User")
+            && Csla.ApplicationContext.User.IsInRole("Admin")
+            && !Csla.ApplicationContext.User.IsInRole("invalidrole"))
+          {
+            txtRemoteRoles.Text = "Pass";
+          }
+          else
+          {
+            txtRemoteRoles.Text = "Fail";
+          }
+        });
+      }
+      catch
+      {
+        txtRemoteRoles.Text = "Fail";
+      }
+    }
+  }
+}

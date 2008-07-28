1bedd20ba090f903d2e32ebd9d6f28de32e21c08
sergeyb@magenic.com <sergeyb@magenic.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-07-28 19:02:30 +0000

    Implement ability to override authentication type in CslaIdentity and MembershipIdentity classes.  Also, re-factor the code in both classes in order to remove SilverLight compiler directives. bugid: 46

---
+++ cslalightcs/Csla/Security/CslaIdentity.partial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Principal;
+using Csla.Serialization;
+using System.Collections.Generic;
+using Csla.Core.FieldManager;
+using System.Runtime.Serialization;
+using Csla.Core;
+
+namespace Csla.Security
+{
+  public abstract partial class CslaIdentity : ReadOnlyBase<CslaIdentity>, IIdentity
+  {
+    public static void GetCslaIdentity<T>(EventHandler<DataPortalResult<T>> completed, object criteria) where T : CslaIdentity
+    {
+      DataPortal<T> dp = new DataPortal<T>();
+      dp.FetchCompleted += completed;
+      dp.BeginFetch(criteria);
+    }
+  }
+}
---
+++ cslalightcs/Csla/Security/MembershipIdentity.partial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Principal;
+using Csla.Serialization;
+using System.Collections.Generic;
+using Csla.Core.FieldManager;
+using System.Runtime.Serialization;
+using Csla.DataPortalClient;
+using Csla.Silverlight;
+using Csla.Core;
+
+namespace Csla.Security
+{
+  public abstract partial class MembershipIdentity : ReadOnlyBase<MembershipIdentity>, IIdentity
+  {
+    public static void GetMembershipIdentity<T>(EventHandler<DataPortalResult<T>> completed, string userName, string password, bool isRunOnWebServer) where T : MembershipIdentity
+    {
+      DataPortal<T> dp = new DataPortal<T>();
+      dp.FetchCompleted += completed;
+      dp.BeginFetch(new Criteria(userName, password, typeof(T), isRunOnWebServer));
+    }
+  }
+}

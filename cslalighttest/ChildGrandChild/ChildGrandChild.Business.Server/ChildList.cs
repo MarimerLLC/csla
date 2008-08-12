9f5dfc36aa8add075f3ba76f3b3087bfa0f56e12
markst@magenic.com <markst@magenic.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-08-12 18:07:54 +0000

    ADD New Test bugid: 001

---
+++ cslalighttest/ChildGrandChild/ChildGrandChild.Business.Server/ChildList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Csla;
+using Csla.Serialization;
+
+
+
+namespace DataBinding.Business
+{
+    [Serializable]
+    public class ChildList : BusinessListBase<ChildList, Child>
+    {
+      //  protected override object AddNewCore()
+      //  {
+      //      Child item = new Child();
+      //      Add(item);
+      //      return item;
+      //  }
+
+        public ChildList()
+        {
+            AllowNew = true;
+        }
+
+        public void DumpEditLevels(StringBuilder sb)
+        {
+            sb.AppendFormat("  {0} {1}: {2}\r", this.GetType().Name, "n/a", this.EditLevel);
+            foreach (Child item in DeletedList)
+                item.DumpEditLevels(sb);
+            foreach (Child item in this)
+                item.DumpEditLevels(sb);
+        }
+    }
+}

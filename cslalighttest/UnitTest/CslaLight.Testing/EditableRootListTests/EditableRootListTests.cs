f00d64a8c54673c52928a2bf83cf8b1e1469cb97
sergeyb@magenic.com <sergeyb@magenic.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-08-12 14:46:00 +0000

    Finish unit tests for EditableRootListBase, address an issue found during unit testing process. bugid: 54

--- cslalighttest/UnitTest/CslaLight.Testing/EditableRootListTests/EditableRootListTests.cs
+++ cslalighttest/UnitTest/CslaLight.Testing/EditableRootListTests/EditableRootListTests.cs
@@ -29,17 +29,17 @@ namespace cslalighttest.EditableRootListTests
       UnitTestContext context = GetContext();
       DataPortal.ProxyTypeName = "Csla.DataPortalClient.WcfProxy, Csla";
       WcfProxy.DefaultUrl = cslalighttest.Properties.Resources.RemotePortalUrl;
+      ApplicationContext.GlobalContext.Clear();
 
       RootSingleItemsList list;
       RootSingleItemsList.GetRootSingleItemsList(1, 2, (o, e) =>
         {
-          ApplicationContext.GlobalContext.Clear();
           context.Assert.IsNotNull(e.Object);
           list = e.Object;
           SingleItem item = SingleItem.GetSingleItem();
           list.Add(item);
-          Assert.AreEqual(3, list.Count, "Count should be 3");
-          Assert.IsTrue(list[2].IsNew, "Object should be new");
+          context.Assert.AreEqual(3, list.Count, "Count should be 3");
+          context.Assert.IsTrue(list[2].IsNew, "Object should be new");
           context.Assert.Success();
         }
         );
@@ -52,29 +52,136 @@ namespace cslalighttest.EditableRootListTests
       UnitTestContext context = GetContext();
       DataPortal.ProxyTypeName = "Csla.DataPortalClient.WcfProxy, Csla";
       WcfProxy.DefaultUrl = cslalighttest.Properties.Resources.RemotePortalUrl;
+      ApplicationContext.GlobalContext.Clear();
 
       RootSingleItemsList list;
       RootSingleItemsList.GetRootSingleItemsList(1, 2, (o, e) =>
       {
-        ApplicationContext.GlobalContext.Clear();
         context.Assert.IsNotNull(e.Object);
         list = e.Object;
         SingleItem item = SingleItem.GetSingleItem();
         list.Add(item);
-        Assert.AreEqual(3, list.Count, "Count should be 3");
-        Assert.IsTrue(list[2].IsNew, "Object should be new");
+        context.Assert.AreEqual(3, list.Count, "Count should be 3");
+        context.Assert.IsTrue(list[2].IsNew, "Object should be new");
 
         list.RemoveAt(2);
         context.Assert.AreEqual(2, list.Count, "Incorrect count after remove");
-        context.Assert.IsNull(ApplicationContext.GlobalContext["ERLBDeleteSelf"]);
-        context.Assert.IsTrue(item.IsNew,"Object should be new after delete");
+        context.Assert.AreEqual(false, ApplicationContext.GlobalContext.ContainsKey("ERLBDeleteSelf"), "Should not have deleted new item");
+        context.Assert.IsTrue(item.IsNew, "Object should be new after delete");
 
         context.Assert.Success();
       }
         );
       context.Complete();
 
-     
+
+    }
+
+    [TestMethod]
+    public void RemoveOldItem()
+    {
+      UnitTestContext context = GetContext();
+      DataPortal.ProxyTypeName = "Csla.DataPortalClient.WcfProxy, Csla";
+      WcfProxy.DefaultUrl = cslalighttest.Properties.Resources.RemotePortalUrl;
+      ApplicationContext.GlobalContext.Clear();
+
+      RootSingleItemsList list;
+      RootSingleItemsList.GetRootSingleItemsList(1, 2, (o, e) =>
+      {
+        context.Assert.IsNotNull(e.Object);
+        list = e.Object;
+        SingleItem item = SingleItem.GetSingleItem();
+        list.Add(item);
+        context.Assert.AreEqual(3, list.Count, "Count should be 3");
+        context.Assert.IsTrue(list[2].IsNew, "Object should be new");
+        list[0].Saved += (o1, e1) =>
+          {
+            context.Assert.IsNull(e1.Error);
+            context.Assert.AreEqual(2, list.Count, "Incorrect count after remove");
+            context.Assert.AreEqual("DataPortal_DeleteSelf",
+                  ApplicationContext.GlobalContext["ERLBDeleteSelf"]);
+            context.Assert.Success();
+          };
+        list.RemoveAt(0);
+       
+      }
+        );
+      context.Complete();
+    }
+
+    [TestMethod]
+    public void InsertItem()
+    {
+
+      UnitTestContext context = GetContext();
+      DataPortal.ProxyTypeName = "Csla.DataPortalClient.WcfProxy, Csla";
+      WcfProxy.DefaultUrl = cslalighttest.Properties.Resources.RemotePortalUrl;
+      ApplicationContext.GlobalContext.Clear();
+
+      RootSingleItemsList list;
+      RootSingleItemsList.GetRootSingleItemsList(1, 2, (o, e) =>
+      {
+        context.Assert.IsNotNull(e.Object);
+        list = e.Object;
+        SingleItem item = SingleItem.GetSingleItem();
+        list.Add(item);
+        context.Assert.AreEqual(3, list.Count, "Count should be 3");
+        context.Assert.IsTrue(list[2].IsNew, "Object should be new");
+        list[2].Saved += (o1, e1) =>
+        {
+          context.Assert.IsNull(e1.Error);
+          context.Assert.AreEqual(3, list.Count, "Incorrect count after remove");
+          context.Assert.AreEqual("DataPortal_Insert", ApplicationContext.GlobalContext["ERLBInsert"].ToString(), "Object should have been inserted");
+          context.Assert.IsFalse(list[2].IsNew, "Object should not be new");
+          context.Assert.Success();
+        };
+
+        // simulate grid edit
+        item = list[2];
+        System.Windows.Controls.IEditableObject obj = (System.Windows.Controls.IEditableObject)item;
+        obj.BeginEdit();
+        item.Name = "test";
+        obj.EndEdit();
+      }
+        );
+      context.Complete();
+
+    }
+
+    [TestMethod]
+    public void UpdateItem()
+    {
+
+      UnitTestContext context = GetContext();
+      DataPortal.ProxyTypeName = "Csla.DataPortalClient.WcfProxy, Csla";
+      WcfProxy.DefaultUrl = cslalighttest.Properties.Resources.RemotePortalUrl;
+      ApplicationContext.GlobalContext.Clear();
+
+      RootSingleItemsList list;
+      RootSingleItemsList.GetRootSingleItemsList(1, 2, (o, e) =>
+      {
+        context.Assert.IsNotNull(e.Object);
+        list = e.Object;
+        context.Assert.AreEqual(2, list.Count, "Count should be 2");
+        list[0].Saved += (o1, e1) =>
+        {
+          context.Assert.IsNull(e1.Error);
+          context.Assert.AreEqual(2, list.Count, "Incorrect count after remove");
+          context.Assert.AreEqual("DataPortal_Update", ApplicationContext.GlobalContext["ERLBUpdate"].ToString(), "Object should have been updated");
+          context.Assert.IsFalse(list[0].IsDirty, "Object should not be dirty");
+          context.Assert.Success();
+        };
+
+        // simulate grid edit
+        SingleItem item = list[0];
+        System.Windows.Controls.IEditableObject obj = (System.Windows.Controls.IEditableObject)item;
+        obj.BeginEdit();
+        item.Name = "test";
+        obj.EndEdit();
+      }
+        );
+      context.Complete();
+
     }
 
   }

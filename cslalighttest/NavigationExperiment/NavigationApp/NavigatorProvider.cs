9607a8303571676261d718e0961ff8b9c7ef05aa
sergeyb@magenic.com <sergeyb@magenic.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-09-26 21:31:40 +0000

    Navigation work in progress bugid: 142

---
+++ cslalighttest/NavigationExperiment/NavigationApp/NavigatorProvider.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace NavigationApp
+{
+  public class NavigatorProvider
+  {
+    public static readonly DependencyProperty NavigatorProviderProperty =
+      DependencyProperty.RegisterAttached("NavigatorProvider",
+      typeof(NavigatorProvider),
+      typeof(NavigatorProvider),
+      null);
+
+    public static void SetNavigatorProvider(UIElement element, object value)
+    {
+      element.SetValue(NavigatorProviderProperty, value);
+    }
+
+    public static NavigatorProvider GetNavigatorProvider(UIElement element)
+    {
+      return (NavigatorProvider)element.GetValue(NavigatorProviderProperty);
+    }
+
+    public static readonly DependencyProperty ControlTypeNameProperty =
+      DependencyProperty.RegisterAttached("ControlTypeName",
+      typeof(string),
+      typeof(NavigatorProvider),
+      null);
+
+    public static void SetControlTypeName(UIElement element, string value)
+    {
+      element.SetValue(ControlTypeNameProperty, value);
+    }
+
+    public static string GetControlTypeName(UIElement element)
+    {
+      return (string)element.GetValue(ControlTypeNameProperty);
+    }
+
+    public static readonly DependencyProperty TriggerEventProperty =
+      DependencyProperty.RegisterAttached("TriggerEvent",
+      typeof(string),
+      typeof(NavigatorProvider),
+      null);
+
+    public static void SetTriggerEvent(UIElement element, string value)
+    {
+      element.SetValue(TriggerEventProperty, value);
+      NavigatorProvider target = (NavigatorProvider)element.GetValue(NavigatorProviderProperty);
+      if (target == null)
+      {
+        //TODO: move to resource.
+        throw new ArgumentException("Please set NavigatorProvider prior to TriggerEvent property.");
+      }
+      else
+      {
+        target.RegisterEvent(element, value);
+      }
+    }
+    public static string GetTriggerEvent(UIElement element)
+    {
+      return (string)element.GetValue(TriggerEventProperty);
+    }
+
+    private void RegisterEvent(UIElement element, string triggerEvent)
+    {
+      var eventRef = element.GetType().GetEvent(triggerEvent);
+      if (eventRef != null)
+      {
+        var invokeMethod = eventRef.EventHandlerType.GetMethod("Invoke");
+        var parameters = invokeMethod.GetParameters();
+        if (parameters.Length == 2)
+        {
+          if (typeof(RoutedEventArgs).IsAssignableFrom(parameters[1].ParameterType))
+          {
+            eventRef.AddEventHandler(element, new RoutedEventHandler(InvokeProvider));
+          }
+          else if (typeof(EventArgs).IsAssignableFrom(parameters[1].ParameterType))
+          {
+            eventRef.AddEventHandler(element, new EventHandler(InvokeProvider));
+          }
+        }
+      }
+    }
+
+    private void InvokeProvider(object sender, EventArgs e)
+    {
+      UIElement source = sender as UIElement;
+      if (source != null)
+      {
+        string controlTypeName = (string)source.GetValue(ControlTypeNameProperty);
+        if (!string.IsNullOrEmpty(controlTypeName))
+        {
+          Navigator.Current.Navigate(controlTypeName);
+        }
+      }
+    }
+  }
+}

aa5cd7b202d2a2c2b7e995eb4a23a6c70011ff63
sergeyb@magenic.com <sergeyb@magenic.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-10-11 01:32:45 +0000

    Make constructor public on DataPortalResult.

--- cslalightcs/Csla/DataPortalResult.cs
+++ cslalightcs/Csla/DataPortalResult.cs
@@ -2,19 +2,55 @@
 
 namespace Csla
 {
+  /// <summary>
+  /// IDataPortalResult defines the results of DataPortal operation
+  /// </summary>
   public interface IDataPortalResult 
   {
     object Object { get; }
     Exception Error { get; }
   }
 
+  /// <summary>
+  /// DataPortalResult defines the results of DataPortal operation.
+  /// It contains object that was received from the server, 
+  /// an error (if occurred) and userState - user defined information
+  /// that was passed into data portal on initial request
+  /// </summary>
+  /// <typeparam name="T">Type of object that DataPortal received</typeparam>
   public class DataPortalResult<T> : EventArgs, IDataPortalResult
   {
+    /// <summary>
+    /// Object that DataPortal received as a result of current operation
+    /// </summary>
     public T Object { get; private set; }
+    /// <summary>
+    /// Error that occurred during the DataPotal call.
+    /// This will be null if no errors occurred.
+    /// </summary>
     public Exception Error { get; private set; }
+
+    /// <summary>
+    /// User defined information
+    /// that was passed into data portal on initial request
+    /// </summary>
     public object UserState { get; private set; }
 
-    internal DataPortalResult(T obj, Exception ex, object userState)
+    /// <summary>
+    /// Create new instance of data portal result
+    /// </summary>
+    /// <param name="obj">
+    /// Object that DataPortal received as a result of current operation
+    /// </param>
+    /// <param name="ex">
+    /// Error that occurred during the DataPotal call.
+    /// This will be null if no errors occurred.
+    /// </param>
+    /// <param name="userState">
+    /// User defined information
+    /// that was passed into data portal on initial request
+    /// </param>
+    public DataPortalResult(T obj, Exception ex, object userState)
     {
       this.Object = obj;
       this.Error = ex;

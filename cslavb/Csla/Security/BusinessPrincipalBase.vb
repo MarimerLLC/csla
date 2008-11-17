6f52f65969c35064884a66fb954168b947ba0173
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-11-17 00:48:53 +0000

    Translation - Think it's perfect but I'm sure I missed something and I have some questions about SharedAuthorizationRules.vb and PrincipleCache static/shared conversion bugid: 165

--- cslavb/Csla/Security/BusinessPrincipalBase.vb
+++ cslavb/Csla/Security/BusinessPrincipalBase.vb
@@ -1,4 +1,7 @@
+Imports System
 Imports System.Security.Principal
+Imports Csla.Serialization
+Imports Csla.Serialization.Mobile
 
 Namespace Security
 
@@ -7,44 +10,71 @@ Namespace Security
   ''' objects should inherit to operate
   ''' properly with the data portal.
   ''' </summary>
-  <Serializable()> _
-  Public Class BusinessPrincipalBase
-    Implements IPrincipal
+    <Serializable()> _
+    Public Class BusinessPrincipalBase
+        Inherits Core.MobileObject
+        Implements IPrincipal
 
-    Private _identity As IIdentity
+        Private _identity As IIdentity
 
-    ''' <summary>
-    ''' Returns the user's identity object.
-    ''' </summary>
-    Public Overridable ReadOnly Property Identity() As IIdentity _
-      Implements IPrincipal.Identity
-      Get
-        Return _identity
-      End Get
-    End Property
+        Protected Sub New(ByVal _identity As UnauthenticatedIdentity())
 
-    ''' <summary>
-    ''' Returns a value indicating whether the
-    ''' user is in a given role.
-    ''' </summary>
-    ''' <param name="role">Name of the role.</param>
-    Public Overridable Function IsInRole(ByVal role As String) As Boolean _
-      Implements IPrincipal.IsInRole
+        End Sub
 
-      Return False
+        ''' <summary>
+        ''' Returns the user's identity object.
+        ''' </summary>
+        Public Overridable ReadOnly Property Identity() As IIdentity _
+            Implements IPrincipal.Identity
+            Get
+                Return _identity
+            End Get
+        End Property
 
-    End Function
+        ''' <summary>
+        ''' Returns a value indicating whether the
+        ''' user is in a given role.
+        ''' </summary>
+        ''' <param name="role">Name of the role.</param>
+        Public Overridable Function IsInRole(ByVal role As String) As Boolean _
+          Implements IPrincipal.IsInRole
 
-    ''' <summary>
-    ''' Creates an instance of the object.
-    ''' </summary>
-    ''' <param name="identity">Identity object for the user.</param>
-    Protected Sub New(ByVal identity As IIdentity)
+            Dim check = CType(_identity, ICheckRoles)
 
-      _identity = identity
+            If check IsNot Nothing Then
+                Return check.IsInRole(role)
+            Else
+                Return False
+            End If
 
-    End Sub
+        End Function
 
-  End Class
+        ''' <summary>
+        ''' Creates an instance of the object.
+        ''' </summary>
+        ''' <param name="identity">Identity object for the user.</param>
+        Protected Sub New(ByVal identity As IIdentity)
+
+            _identity = identity
+
+        End Sub
+
+        ''' <summary>
+        ''' Override this method to get custom field values
+        ''' from the serialization stream.
+        ''' </summary>
+        ''' <param name="info">Serialization info.</param>
+        ''' <param name="mode">Serialization mode.</param>
+        Protected Overrides Sub OnGetState(ByVal info As Serialization.Mobile.SerializationInfo, ByVal mode As Core.StateMode)
+            info.AddValue("BusinessPrincipalBase.Identity", MobileFormatter.Serialize(_identity))
+            MyBase.OnGetState(info, mode)
+        End Sub
+
+        Protected Overrides Sub OnSetState(ByVal info As Serialization.Mobile.SerializationInfo, ByVal mode As Core.StateMode)
+            MyBase.OnSetState(info, mode)
+            _identity = CType(MobileFormatter.Deserialize(info.GetValue(Of Byte())("BusinessPrincipalBase.Identity")), IIdentity)
+        End Sub
+
+    End Class
 
 End Namespace
---
+++ cslavb/Csla/Security/CslaIdentity.partial.vb
@@ -0,0 +1,55 @@
+﻿Imports System
+Imports System.Security.Principal
+Imports Csla.Serialization
+Imports System.Collections.Generic
+Imports Csla.Core.FieldManager
+Imports System.Runtime.Serialization
+Imports Csla.Core
+
+Namespace Security
+
+    ''' <summary>
+    ''' Provides a base class to simplify creation of
+    ''' a .NET identity object for use with BusinessPrincipalBase.
+    ''' </summary>
+    Partial Public MustInherit Class CslaIdentity
+        Inherits ReadOnlyBase(Of CslaIdentity)
+        Implements IIdentity
+
+        Private Shared _forceInit As Integer
+
+#Region "Constructor"
+
+        ''' <summary>
+        ''' Creates an instance of the object.
+        ''' </summary>
+        Protected Sub New()
+            _forceInit = _forceInit + 0
+        End Sub
+
+#End Region
+
+        ''' <summary>
+        ''' Invokes the data portal to get an instance of
+        ''' the identity object.
+        ''' </summary>
+        ''' <typeparam name="T">
+        ''' Type of the CslaIdentity subclass to retrieve.
+        ''' </typeparam>
+        ''' <param name="criteria">
+        ''' Object containing the user's credentials.
+        ''' </param>
+        ''' <returns></returns>
+        Public Shadows Function GetCslaIdentity(Of T As CslaIdentity)(ByVal criteria As Object) As T
+            Return DataPortal.Fetch(Of T)(criteria)
+        End Function
+
+        Protected Overrides Sub OnDeserialized(ByVal context As System.Runtime.Serialization.StreamingContext)
+            _forceInit = 0
+            MyBase.OnDeserialized(context)
+        End Sub
+        
+    End Class
+
+End Namespace
+

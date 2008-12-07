5e15d618613e3d074897c237d62644ec2fe30326
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2008-12-07 05:53:22 +0000

    Added/Updated from current c# equivalent bugid: 256

--- cslavb/Csla/Server/Hosts/Silverlight/IWcfPortal.vb
+++ cslavb/Csla/Server/Hosts/Silverlight/IWcfPortal.vb
@@ -7,34 +7,34 @@ Namespace Server.Hosts.Silverlight
     ''' Defines the service contract for the WCF data
     ''' portal.
     ''' </summary>
-    <ServiceContract(Namespace = "http://ws.lhotka.net/WcfDataPortal")> _
-    Public Interface IWcfPortal
+  <ServiceContract(Namespace:="http://ws.lhotka.net/WcfDataPortal")> _
+  Public Interface IWcfPortal
 
-        ''' <summary>
-        ''' Create a new business object.
-        ''' </summary>
-        ''' <param name="request"></param>
-        <OperationContract()> _
-        Function Create(ByVal request As CriteriaRequest) As WcfResponse
-        ''' <summary>
-        ''' Get an existing business object.
-        ''' </summary>
-        ''' <param name="request">The request parameter object.</param>
-        <OperationContract()> _
-        Function Fetch(ByVal request As CriteriaRequest) As WcfResponse
-        ''' <summary>
-        ''' Update a business object.
-        ''' </summary>
-        ''' <param name="request">The request parameter object.</param>
-        <OperationContract()> _
-        Function Update(ByVal request As UpdateRequest) As WcfResponse
-        ''' <summary>
-        ''' Delete a business object.
-        ''' </summary>
-        ''' <param name="request">The request parameter object.</param>
-        <OperationContract()> _
-        Function Delete(ByVal request As CriteriaRequest) As WcfResponse
+    ''' <summary>
+    ''' Create a new business object.
+    ''' </summary>
+    ''' <param name="request">The request parameter object.</param>
+    <OperationContract()> _
+    Function Create(ByVal request As CriteriaRequest) As WcfResponse
+    ''' <summary>
+    ''' Get an existing business object.
+    ''' </summary>
+    ''' <param name="request">The request parameter object.</param>
+    <OperationContract()> _
+    Function Fetch(ByVal request As CriteriaRequest) As WcfResponse
+    ''' <summary>
+    ''' Update a business object.
+    ''' </summary>
+    ''' <param name="request">The request parameter object.</param>
+    <OperationContract()> _
+    Function Update(ByVal request As UpdateRequest) As WcfResponse
+    ''' <summary>
+    ''' Delete a business object.
+    ''' </summary>
+    ''' <param name="request">The request parameter object.</param>
+    <OperationContract()> _
+    Function Delete(ByVal request As CriteriaRequest) As WcfResponse
 
-    End Interface
+  End Interface
 End Namespace
 

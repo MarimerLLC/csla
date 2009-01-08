da1d7a8e1b3d7b00b5093cbb7ab1d3c98b633df2
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-01-08 00:35:41 +0000

    Fixed indentation bugid: 280

--- cslavb/Csla/Serialization/Mobile/MobileFormatter.vb
+++ cslavb/Csla/Serialization/Mobile/MobileFormatter.vb
@@ -10,247 +10,247 @@ Imports Csla.Properties
 
 Namespace Serialization.Mobile
 
-    ''' <summary>
-    ''' Serializes and deserializes objects
-    ''' at the field level. A Silverlight-
-    ''' compatible facsimile of the
-    ''' BinaryFormatter or NetDataContractSerializer.
-    ''' </summary>
+  ''' <summary>
+  ''' Serializes and deserializes objects
+  ''' at the field level. A Silverlight-
+  ''' compatible facsimile of the
+  ''' BinaryFormatter or NetDataContractSerializer.
+  ''' </summary>
 #If TESTING Then
   [System.Diagnostics.DebuggerStepThrough]
 #End If
-    Public NotInheritable Class MobileFormatter
-        Implements ISerializationFormatter
+  Public NotInheritable Class MobileFormatter
+    Implements ISerializationFormatter
 
 #Region "Serialize"
 
-        ''' <summary>
-        ''' Serialize an object graph into XML.
-        ''' </summary>
-        ''' <param name="serializationStream">
-        ''' Stream to which the serialized data
-        ''' will be written.
-        ''' </param>
-        ''' <param name="graph">
-        ''' Root object of the object graph
-        ''' to serialize.
-        ''' </param>
-        Public Sub Serialize(ByVal serializationStream As System.IO.Stream, ByVal graph As Object) Implements ISerializationFormatter.Serialize
-            Dim writer As XmlWriter = XmlWriter.Create(serializationStream)
-            Serialize(writer, graph)
-            writer.Flush()
-        End Sub
+    ''' <summary>
+    ''' Serialize an object graph into XML.
+    ''' </summary>
+    ''' <param name="serializationStream">
+    ''' Stream to which the serialized data
+    ''' will be written.
+    ''' </param>
+    ''' <param name="graph">
+    ''' Root object of the object graph
+    ''' to serialize.
+    ''' </param>
+    Public Sub Serialize(ByVal serializationStream As System.IO.Stream, ByVal graph As Object) Implements ISerializationFormatter.Serialize
+      Dim writer As XmlWriter = XmlWriter.Create(serializationStream)
+      Serialize(writer, graph)
+      writer.Flush()
+    End Sub
 
-        ''' <summary>
-        ''' Serialize an object graph into XML.
-        ''' </summary>
-        ''' <param name="textWriter">
-        ''' TextWriter to which the serialized data
-        ''' will be written.
-        ''' </param>
-        ''' <param name="graph">
-        ''' Root object of the object graph
-        ''' to serialize.
-        ''' </param>
-        Public Sub Serialize(ByVal textWriter As TextWriter, ByVal graph As Object)
-            Dim writer As XmlWriter = XmlWriter.Create(textWriter)
-            Serialize(writer, graph)
-            writer.Flush()
-        End Sub
+    ''' <summary>
+    ''' Serialize an object graph into XML.
+    ''' </summary>
+    ''' <param name="textWriter">
+    ''' TextWriter to which the serialized data
+    ''' will be written.
+    ''' </param>
+    ''' <param name="graph">
+    ''' Root object of the object graph
+    ''' to serialize.
+    ''' </param>
+    Public Sub Serialize(ByVal textWriter As TextWriter, ByVal graph As Object)
+      Dim writer As XmlWriter = XmlWriter.Create(textWriter)
+      Serialize(writer, graph)
+      writer.Flush()
+    End Sub
 
-        ''' <summary>
-        ''' Serialize an object graph into XML.
-        ''' </summary>
-        ''' <param name="writer">
-        ''' XmlWriter to which the serialized data
-        ''' will be written.
-        ''' </param>
-        ''' <param name="graph">
-        ''' Root object of the object graph
-        ''' to serialize.
-        ''' </param>
-        Public Sub Serialize(ByVal writer As XmlWriter, ByVal graph As Object)
-            _serializationReferences.Clear()
-            SerializeObject(graph)
-            Dim serialized As List(Of SerializationInfo) = _serializationReferences.Values.ToList()
-            Dim dc As DataContractSerializer = New DataContractSerializer(GetType(List(Of SerializationInfo)), New Type() {GetType(List(Of Integer)), GetType(Byte())})
-            dc.WriteObject(writer, serialized)
-        End Sub
+    ''' <summary>
+    ''' Serialize an object graph into XML.
+    ''' </summary>
+    ''' <param name="writer">
+    ''' XmlWriter to which the serialized data
+    ''' will be written.
+    ''' </param>
+    ''' <param name="graph">
+    ''' Root object of the object graph
+    ''' to serialize.
+    ''' </param>
+    Public Sub Serialize(ByVal writer As XmlWriter, ByVal graph As Object)
+      _serializationReferences.Clear()
+      SerializeObject(graph)
+      Dim serialized As List(Of SerializationInfo) = _serializationReferences.Values.ToList()
+      Dim dc As DataContractSerializer = New DataContractSerializer(GetType(List(Of SerializationInfo)), New Type() {GetType(List(Of Integer)), GetType(Byte())})
+      dc.WriteObject(writer, serialized)
+    End Sub
 
-        ''' <summary>
-        ''' Serializes an object into a SerializationInfo object.
-        ''' </summary>
-        ''' <param name="obj">Object to be serialized.</param>
-        ''' <returns></returns>
-        Public Function SerializeObject(ByVal obj As Object) As SerializationInfo
-            Dim info As SerializationInfo
+    ''' <summary>
+    ''' Serializes an object into a SerializationInfo object.
+    ''' </summary>
+    ''' <param name="obj">Object to be serialized.</param>
+    ''' <returns></returns>
+    Public Function SerializeObject(ByVal obj As Object) As SerializationInfo
+      Dim info As SerializationInfo
 
-            If obj Is Nothing Then
-                info = New SerializationInfo(_serializationReferences.Count + 1)
+      If obj Is Nothing Then
+        info = New SerializationInfo(_serializationReferences.Count + 1)
 
-            Else
-                Dim thisType = obj.GetType()
+      Else
+        Dim thisType = obj.GetType()
 
-                If IsSerializable(thisType) = False Then
-                    Throw New InvalidOperationException(String.Format(Resources.ObjectNotSerializableFormatted, thisType.FullName))
-                End If
+        If IsSerializable(thisType) = False Then
+          Throw New InvalidOperationException(String.Format(Resources.ObjectNotSerializableFormatted, thisType.FullName))
+        End If
 
-                Dim mobile = CType(obj, IMobileObject)
+        Dim mobile = CType(obj, IMobileObject)
 
-                If mobile Is Nothing Then
-                    Throw New InvalidOperationException(String.Format(Resources.MustImplementIMobileObject, thisType.Name))
-                End If
+        If mobile Is Nothing Then
+          Throw New InvalidOperationException(String.Format(Resources.MustImplementIMobileObject, thisType.Name))
+        End If
 
-                If _serializationReferences.TryGetValue(mobile, info) Then
-                    info = New SerializationInfo(_serializationReferences.Count + 1)
-                    _serializationReferences.Add(mobile, info)
-                    info.TypeName = thisType.AssemblyQualifiedName
-                    mobile.GetChildren(info, Me)
-                    mobile.GetState(info)
-                End If
-            End If
+        If _serializationReferences.TryGetValue(mobile, info) Then
+          info = New SerializationInfo(_serializationReferences.Count + 1)
+          _serializationReferences.Add(mobile, info)
+          info.TypeName = thisType.AssemblyQualifiedName
+          mobile.GetChildren(info, Me)
+          mobile.GetState(info)
+        End If
+      End If
 
-            Return info
+      Return info
 
-        End Function
+    End Function
 
-        Private _serializationReferences As Dictionary(Of IMobileObject, SerializationInfo) = New Dictionary(Of IMobileObject, SerializationInfo)
+    Private _serializationReferences As Dictionary(Of IMobileObject, SerializationInfo) = New Dictionary(Of IMobileObject, SerializationInfo)
 
-        Private Shared Function IsSerializable(ByVal objectType As Type) As Boolean
+    Private Shared Function IsSerializable(ByVal objectType As Type) As Boolean
 #If SILVERLIGHT Then
  Return objectType.IsSerializable()
 #Else
-            Return objectType.IsSerializable
+      Return objectType.IsSerializable
 #End If
-        End Function
+    End Function
 
 #End Region
 
 #Region "Deserialize"
 
-        Private _deserializationReferences As Dictionary(Of Integer, IMobileObject) = New Dictionary(Of Integer, IMobileObject)
+    Private _deserializationReferences As Dictionary(Of Integer, IMobileObject) = New Dictionary(Of Integer, IMobileObject)
 
-        ''' <summary>
-        ''' Deserialize an object from XML.
-        ''' </summary>
-        ''' <param name="serializationStream">
-        ''' Stream containing the serialized XML
-        ''' data.
-        ''' </param>
-        ''' <returns></returns>
-        Public Function Deserialize(ByVal serializationStream As System.IO.Stream) As Object Implements ISerializationFormatter.Deserialize
-            Dim reader As XmlReader = XmlReader.Create(serializationStream)
-            Return Deserialize(reader)
-        End Function
+    ''' <summary>
+    ''' Deserialize an object from XML.
+    ''' </summary>
+    ''' <param name="serializationStream">
+    ''' Stream containing the serialized XML
+    ''' data.
+    ''' </param>
+    ''' <returns></returns>
+    Public Function Deserialize(ByVal serializationStream As System.IO.Stream) As Object Implements ISerializationFormatter.Deserialize
+      Dim reader As XmlReader = XmlReader.Create(serializationStream)
+      Return Deserialize(reader)
+    End Function
 
-        ''' <summary>
-        ''' Deserialize an object from XML.
-        ''' </summary>
-        ''' <param name="textReader">
-        ''' TextReader containing the serialized XML
-        ''' data.
-        ''' </param>
-        ''' <returns></returns>
-        Public Function Deserialize(ByVal textReader As TextReader) As Object
-            Dim reader As XmlReader = XmlReader.Create(textReader)
-            Return Deserialize(reader)
-        End Function
+    ''' <summary>
+    ''' Deserialize an object from XML.
+    ''' </summary>
+    ''' <param name="textReader">
+    ''' TextReader containing the serialized XML
+    ''' data.
+    ''' </param>
+    ''' <returns></returns>
+    Public Function Deserialize(ByVal textReader As TextReader) As Object
+      Dim reader As XmlReader = XmlReader.Create(textReader)
+      Return Deserialize(reader)
+    End Function
 
-        ''' <summary>
-        ''' Deserialize an object from XML.
-        ''' </summary>
-        ''' <param name="reader">
-        ''' XmlReader containing the serialized XML
-        ''' data.
-        ''' </param>
-        ''' <returns></returns>
-        Public Function Deserialize(ByVal reader As XmlReader) As Object
-            Dim dc As DataContractSerializer = New DataContractSerializer(GetType(List(Of SerializationInfo)), New Type() {GetType(List(Of Integer)), GetType(Byte())})
-            Dim deserialized As List(Of SerializationInfo) = CType(dc.ReadObject(reader), List(Of SerializationInfo))
+    ''' <summary>
+    ''' Deserialize an object from XML.
+    ''' </summary>
+    ''' <param name="reader">
+    ''' XmlReader containing the serialized XML
+    ''' data.
+    ''' </param>
+    ''' <returns></returns>
+    Public Function Deserialize(ByVal reader As XmlReader) As Object
+      Dim dc As DataContractSerializer = New DataContractSerializer(GetType(List(Of SerializationInfo)), New Type() {GetType(List(Of Integer)), GetType(Byte())})
+      Dim deserialized As List(Of SerializationInfo) = CType(dc.ReadObject(reader), List(Of SerializationInfo))
 
-            _deserializationReferences = New Dictionary(Of Integer, IMobileObject)
+      _deserializationReferences = New Dictionary(Of Integer, IMobileObject)
 
-            For Each info In deserialized
-                Dim type As Type = System.Type.GetType(info.TypeName)
+      For Each info In deserialized
+        Dim type As Type = System.Type.GetType(info.TypeName)
 
-                If type Is Nothing Then
-                    Throw New SerializationException(String.Format(Resources.MobileFormatterUnableToDeserialize, info.TypeName))
-                End If
+        If type Is Nothing Then
+          Throw New SerializationException(String.Format(Resources.MobileFormatterUnableToDeserialize, info.TypeName))
+        End If
 
 #If SILVERLIGHT Then
         Dim mobile As IMobileObject  = CType(Activator.CreateInstance(type), IMobileObject)
 #Else
-                Dim mobile As IMobileObject = CType(Activator.CreateInstance(type, True), IMobileObject)
+        Dim mobile As IMobileObject = CType(Activator.CreateInstance(type, True), IMobileObject)
 #End If
-                _deserializationReferences.Add(info.ReferenceId, mobile)
+        _deserializationReferences.Add(info.ReferenceId, mobile)
 
-                mobile.SetState(info)
+        mobile.SetState(info)
 
-            Next
+      Next
 
-            For Each info In deserialized
-                Dim mobile As IMobileObject = _deserializationReferences(info.ReferenceId)
-                Mobile.SetChildren(info, Me)
-            Next
+      For Each info In deserialized
+        Dim mobile As IMobileObject = _deserializationReferences(info.ReferenceId)
+        mobile.SetChildren(info, Me)
+      Next
 
-            For Each info In deserialized
-                Dim notifiable As ISerializationNotification = CType(_deserializationReferences(info.ReferenceId), ISerializationNotification)
+      For Each info In deserialized
+        Dim notifiable As ISerializationNotification = CType(_deserializationReferences(info.ReferenceId), ISerializationNotification)
 
-                If notifiable IsNot Nothing Then
-                    notifiable.Deserialized()
-                End If
-            Next
+        If notifiable IsNot Nothing Then
+          notifiable.Deserialized()
+        End If
+      Next
 
-            Return IIf(_deserializationReferences.Count > 0, _deserializationReferences(1), Nothing)
+      Return IIf(_deserializationReferences.Count > 0, _deserializationReferences(1), Nothing)
 
-        End Function
+    End Function
 
-        Public Function GetObject(ByVal referenceId As Integer) As IMobileObject
-            Return _deserializationReferences(referenceId)
-        End Function
+    Public Function GetObject(ByVal referenceId As Integer) As IMobileObject
+      Return _deserializationReferences(referenceId)
+    End Function
 
 #End Region
 
 #Region "Static Helpers"
 
-        ''' <summary>
-        ''' Serializes the object into a byte array.
-        ''' </summary>
-        ''' <param name="obj">
-        ''' The object to be serialized, which must implement
-        ''' IMobileObject.
-        ''' </param>
-        ''' <returns></returns>
-        Public Shared Function Serialize(ByVal obj As Object) As Byte()
-            Using buffer = New System.IO.MemoryStream()
-                Dim formatter = New MobileFormatter()
-                formatter.Serialize(buffer, obj)
-                Return buffer.ToArray()
-            End Using
-        End Function
+    ''' <summary>
+    ''' Serializes the object into a byte array.
+    ''' </summary>
+    ''' <param name="obj">
+    ''' The object to be serialized, which must implement
+    ''' IMobileObject.
+    ''' </param>
+    ''' <returns></returns>
+    Public Shared Function Serialize(ByVal obj As Object) As Byte()
+      Using buffer = New System.IO.MemoryStream()
+        Dim formatter = New MobileFormatter()
+        formatter.Serialize(buffer, obj)
+        Return buffer.ToArray()
+      End Using
+    End Function
 
-        ''' <summary>
-        ''' Deserializes a byte stream into an object.
-        ''' </summary>
-        ''' <param name="data">
-        ''' Byte array containing the object's serialized
-        ''' data.
-        ''' </param>
-        ''' <returns>
-        ''' An object containing the data from the
-        ''' byte stream. The object must implement
-        ''' IMobileObject to be deserialized.
-        ''' </returns>
-        Public Shared Function Deserialize(ByVal data() As Byte) As Object
-            Using buffer = New System.IO.MemoryStream(data)
-                Dim formatter = New MobileFormatter()
-                return formatter.Deserialize(buffer)
-            End Using
-        End Function
+    ''' <summary>
+    ''' Deserializes a byte stream into an object.
+    ''' </summary>
+    ''' <param name="data">
+    ''' Byte array containing the object's serialized
+    ''' data.
+    ''' </param>
+    ''' <returns>
+    ''' An object containing the data from the
+    ''' byte stream. The object must implement
+    ''' IMobileObject to be deserialized.
+    ''' </returns>
+    Public Shared Function Deserialize(ByVal data() As Byte) As Object
+      Using buffer = New System.IO.MemoryStream(data)
+        Dim formatter = New MobileFormatter()
+        Return formatter.Deserialize(buffer)
+      End Using
+    End Function
 
 #End Region
 
-    End Class
+  End Class
 
 End Namespace
 

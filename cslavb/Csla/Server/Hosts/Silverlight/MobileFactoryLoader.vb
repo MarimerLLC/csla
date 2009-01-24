8db4e28f6b81eaf3318c43c80fe4af3f4e042222
vonbvillafuerte <vonbvillafuerte@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-01-24 18:10:24 +0000

    Implement IMobileFactoryLoader bugid: 300

--- cslavb/Csla/Server/Hosts/Silverlight/MobileFactoryLoader.vb
+++ cslavb/Csla/Server/Hosts/Silverlight/MobileFactoryLoader.vb
@@ -2,7 +2,7 @@
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Text
-Imports Csla.Properties
+
 
 Namespace Server.Hosts.Silverlight
 
@@ -12,6 +12,8 @@ Namespace Server.Hosts.Silverlight
   ''' Silverlight data portal host.
   ''' </summary>
   Public Class MobileFactoryLoader
+    Implements IMobileFactoryLoader
+
 
     ''' <summary>
     ''' Creates an instance of a mobile factory
@@ -27,16 +29,15 @@ Namespace Server.Hosts.Silverlight
     ''' An instance of the type specified by the
     ''' type name parameter.
     ''' </returns>
-    Public Function GetFactory(ByVal factoryName As String) As Object
+    Public Function GetFactory(ByVal factoryName As String) As Object Implements IMobileFactoryLoader.GetFactory
       Dim ft = Type.GetType(factoryName)
 
       If ft Is Nothing Then
-        Throw New InvalidOperationException(String.Format(Resources.FactoryTypeNotFoundException, factoryName))
+        Throw New InvalidOperationException(String.Format(My.Resources.FactoryTypeNotFoundException, factoryName))
       End If
 
       Return Activator.CreateInstance(ft)
     End Function
-
 
   End Class
 

b55580c99a36ee9119417ee3ec4dac5eda6c1d08
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-01-28 20:02:32 +0000

    Make GetProxy() virtual in WcfProxy. bugid: 315

--- cslalightcs/Csla/DataPortalClient/WcfProxy.cs
+++ cslalightcs/Csla/DataPortalClient/WcfProxy.cs
@@ -6,10 +6,20 @@ using Csla.Core;
 
 namespace Csla.DataPortalClient
 {
+  /// <summary>
+  /// Data portal client-side proxy used to communicate
+  /// with a CSLA .NET for Windows server-side
+  /// data portal.
+  /// </summary>
   public static class WcfProxy
   {
     private static System.ServiceModel.Channels.Binding _defaultBinding;
     private const int TimeoutInMinutes = 10;
+
+    /// <summary>
+    /// Gets or sets the default binding used to initialize
+    /// future instances of WcfProxy.
+    /// </summary>
     public static System.ServiceModel.Channels.Binding DefaultBinding
     {
       get 
@@ -30,6 +40,11 @@ namespace Csla.DataPortalClient
     }
 
     private static string _defaultUrl;
+
+    /// <summary>
+    /// Gets or sets the default URL address
+    /// for the data portal server.
+    /// </summary>
     public static string DefaultUrl
     {
       get { return _defaultUrl; }
@@ -37,21 +52,50 @@ namespace Csla.DataPortalClient
     }
   }
 
+  /// <summary>
+  /// Data portal client-side proxy used to communicate
+  /// with a CSLA .NET for Windows server-side
+  /// data portal.
+  /// </summary>
 #if TESTING
   [System.Diagnostics.DebuggerNonUserCode]
 #endif
   public class WcfProxy<T> : IDataPortalProxy<T> where T : IMobileObject
   {
+    /// <summary>
+    /// Creates an instance of the object, initializing
+    /// it to use the DefaultUrl and DefaultBinding
+    /// values.
+    /// </summary>
     public WcfProxy()
     {
       this.DataPortalUrl = WcfProxy.DefaultUrl;
       this.Binding = WcfProxy.DefaultBinding;
     }
 
+    /// <summary>
+    /// Gets the binding object used by this proxy instance.
+    /// </summary>
     public System.ServiceModel.Channels.Binding Binding { get; protected set; }
+
+    /// <summary>
+    /// Gets the URL address for the data portal server
+    /// used by this proxy instance.
+    /// </summary>
     public string DataPortalUrl { get; protected set; }
 
-    private WcfPortal.WcfPortalClient GetProxy()
+    /// <summary>
+    /// Gets an instance of the WcfPortalClient object
+    /// for use in communicating with the server.
+    /// </summary>
+    /// <remarks>
+    /// If both DataPortalUrl and Binding are non-null then
+    /// those values are used to initialize the proxy, otherwise
+    /// the proxy is initialized using values from the
+    /// system.serviceModel element in the Silverlight
+    /// client-side config file.
+    /// </remarks>
+    protected virtual WcfPortal.WcfPortalClient GetProxy()
     {
       if (!string.IsNullOrEmpty(this.DataPortalUrl) && this.Binding != null)
       {
@@ -66,6 +110,10 @@ namespace Csla.DataPortalClient
 
     private Csla.Core.ContextDictionary _globalContext;
 
+    /// <summary>
+    /// Gets the GlobalContext dictionary returned from the
+    /// server upon completion of the data portal call.
+    /// </summary>
     public Csla.Core.ContextDictionary GlobalContext
     {
       get { return _globalContext; }
@@ -73,7 +121,8 @@ namespace Csla.DataPortalClient
 
     #endregion
 
-    #region Cirteria
+    #region Criteria
+
     private WcfPortal.CriteriaRequest GetBaseCriteriaRequest()
     {
       var request = new WcfPortal.CriteriaRequest();
@@ -83,6 +132,7 @@ namespace Csla.DataPortalClient
       request.Principal = MobileFormatter.Serialize(ApplicationContext.User);
       return request;
     }
+
     private WcfPortal.UpdateRequest GetBaseUpdateCriteriaRequest()
     {
       var request = new WcfPortal.UpdateRequest();
@@ -92,18 +142,31 @@ namespace Csla.DataPortalClient
       request.Principal = MobileFormatter.Serialize(ApplicationContext.User);
       return request;
     }
+
     #endregion
 
     #region Create
 
+    /// <summary>
+    /// Event raised when the asynchronous BeginCreate() call
+    /// is complete.
+    /// </summary>
     public event EventHandler<DataPortalResult<T>> CreateCompleted;
 
+    /// <summary>
+    /// Raises the CreateCompleted event.
+    /// </summary>
+    /// <param name="e">Event arguments.</param>
     protected virtual void OnCreateCompleted(DataPortalResult<T> e)
     {
       if (CreateCompleted != null)
         CreateCompleted(this, e);
     }
 
+    /// <summary>
+    /// Begins an asynchronous server call to create an
+    /// instance of a business object.
+    /// </summary>
     public void BeginCreate()
     {
       var request = GetBaseCriteriaRequest();
@@ -115,10 +178,22 @@ namespace Csla.DataPortalClient
       proxy.CreateAsync(request);
     }
 
+    /// <summary>
+    /// Begins an asynchronous server call to create an
+    /// instance of a business object.
+    /// </summary>
+    /// <param name="criteria">Criteria object.</param>
     public void BeginCreate(object criteria)
     {
       BeginCreate(criteria, null);
     }
+
+    /// <summary>
+    /// Begins an asynchronous server call to create an
+    /// instance of a business object.
+    /// </summary>
+    /// <param name="criteria">Criteria object.</param>
+    /// <param name="userState">User state object.</param>
     public void BeginCreate(object criteria, object userState)
     {
       var request = GetBaseCriteriaRequest();
@@ -167,14 +242,26 @@ namespace Csla.DataPortalClient
 
     #region Fetch
 
+    /// <summary>
+    /// Event raised when the asynchronous BeginFetch() call
+    /// is complete.
+    /// </summary>
     public event EventHandler<DataPortalResult<T>> FetchCompleted;
 
+    /// <summary>
+    /// Raises the FetchCompleted event.
+    /// </summary>
+    /// <param name="e">Event arguments.</param>
     protected virtual void OnFetchCompleted(DataPortalResult<T> e)
     {
       if (FetchCompleted != null)
         FetchCompleted(this, e);
     }
 
+    /// <summary>
+    /// Begins an asynchronous server call to retrieve an
+    /// instance of a business object.
+    /// </summary>
     public void BeginFetch()
     {
       var request = GetBaseCriteriaRequest();
@@ -186,10 +273,22 @@ namespace Csla.DataPortalClient
       proxy.FetchAsync(request);
     }
 
+    /// <summary>
+    /// Begins an asynchronous server call to retrieve an
+    /// instance of a business object.
+    /// </summary>
+    /// <param name="criteria">Criteria object.</param>
     public void BeginFetch(object criteria)
     {
       BeginFetch(criteria, null);
     }
+
+    /// <summary>
+    /// Begins an asynchronous server call to retrieve an
+    /// instance of a business object.
+    /// </summary>
+    /// <param name="criteria">Criteria object.</param>
+    /// <param name="userState">User state object.</param>
     public void BeginFetch(object criteria, object userState)
     {
       var request = GetBaseCriteriaRequest();
@@ -238,23 +337,42 @@ namespace Csla.DataPortalClient
 
     #region Update
 
+    /// <summary>
+    /// Event raised when the asynchronous BeginUpdate() call
+    /// is complete.
+    /// </summary>
     public event EventHandler<DataPortalResult<T>> UpdateCompleted;
 
+    /// <summary>
+    /// Raises the UpdateCompleted event.
+    /// </summary>
+    /// <param name="e">Event arguments.</param>
     protected virtual void OnUpdateCompleted(DataPortalResult<T> e)
     {
       if (UpdateCompleted != null)
         UpdateCompleted(this, e);
     }
 
-    public void BeginUpdate(object criteria)
+    /// <summary>
+    /// Begins an asynchronous server call to update an
+    /// instance of a business object.
+    /// </summary>
+    /// <param name="obj">Object to update.</param>
+    public void BeginUpdate(object obj)
     {
-      BeginUpdate(criteria, null);
+      BeginUpdate(obj, null);
     }
 
-    public void BeginUpdate(object criteria, object userState)
+    /// <summary>
+    /// Begins an asynchronous server call to update an
+    /// instance of a business object.
+    /// </summary>
+    /// <param name="obj">Object to update.</param>
+    /// <param name="userState">User state object.</param>
+    public void BeginUpdate(object obj, object userState)
     {
       var request = GetBaseUpdateCriteriaRequest();
-      request.ObjectData = MobileFormatter.Serialize(criteria);
+      request.ObjectData = MobileFormatter.Serialize(obj);
       request = ConvertRequest(request);
       var proxy = GetProxy();
       proxy.UpdateCompleted += new EventHandler<Csla.WcfPortal.UpdateCompletedEventArgs>(proxy_UpdateCompleted);
@@ -302,17 +420,38 @@ namespace Csla.DataPortalClient
 
     #region Delete
 
+    /// <summary>
+    /// Event raised when the asynchronous BeginDelete() call
+    /// is complete.
+    /// </summary>
     public event EventHandler<DataPortalResult<T>> DeleteCompleted;
 
+    /// <summary>
+    /// Raises the DeleteCompleted event.
+    /// </summary>
+    /// <param name="e">Event arguments.</param>
     protected virtual void OnDeleteCompleted(DataPortalResult<T> e)
     {
       if (DeleteCompleted != null)
         DeleteCompleted(this, e);
     }
+
+    /// <summary>
+    /// Begins an asynchronous server call to delete an
+    /// instance of a business object.
+    /// </summary>
+    /// <param name="criteria">Criteria object.</param>
     public void BeginDelete(object criteria)
     {
       BeginDelete(criteria, null);
     }
+
+    /// <summary>
+    /// Begins an asynchronous server call to delete an
+    /// instance of a business object.
+    /// </summary>
+    /// <param name="criteria">Criteria object.</param>
+    /// <param name="userState">User state object.</param>
     public void BeginDelete(object criteria, object userState)
     {
       var request = GetBaseCriteriaRequest();
@@ -357,19 +496,38 @@ namespace Csla.DataPortalClient
 
     #region Execute
 
+    /// <summary>
+    /// Event raised when the asynchronous BeginExecute() call
+    /// is complete.
+    /// </summary>
     public event EventHandler<DataPortalResult<T>> ExecuteCompleted;
 
+    /// <summary>
+    /// Raises the ExecuteCompleted event.
+    /// </summary>
+    /// <param name="e">Event arguments.</param>
     protected virtual void OnExecuteCompleted(DataPortalResult<T> e)
     {
       if (ExecuteCompleted != null)
         ExecuteCompleted(this, e);
     }
 
+    /// <summary>
+    /// Begins an asynchronous server call to execute a
+    /// command object.
+    /// </summary>
+    /// <param name="command">Object to execute.</param>
     public void BeginExecute(T command)
     {
       BeginExecute(command, null);
     }
 
+    /// <summary>
+    /// Begins an asynchronous server call to execute a
+    /// command object.
+    /// </summary>
+    /// <param name="command">Object to execute.</param>
+    /// <param name="userState">User state object.</param>
     public void BeginExecute(T command, object userState)
     {
       var request = GetBaseUpdateCriteriaRequest();
@@ -421,16 +579,31 @@ namespace Csla.DataPortalClient
 
     #region Extention Method for Requests
 
+    /// <summary>
+    /// Override this method to manipulate the message
+    /// request data sent to the server.
+    /// </summary>
+    /// <param name="request">Update request data.</param>
     protected virtual WcfPortal.UpdateRequest ConvertRequest(WcfPortal.UpdateRequest request)
     {
       return request;
     }
 
+    /// <summary>
+    /// Override this method to manipulate the message
+    /// request data sent to the server.
+    /// </summary>
+    /// <param name="request">Criteria request data.</param>
     protected virtual WcfPortal.CriteriaRequest ConvertRequest(WcfPortal.CriteriaRequest request)
     {
       return request;
     }
 
+    /// <summary>
+    /// Override this method to manipulate the message
+    /// request data returned from the server.
+    /// </summary>
+    /// <param name="request">Response data.</param>
     protected virtual WcfPortal.WcfResponse ConvertResponse(WcfPortal.WcfResponse response)
     {
       return response;

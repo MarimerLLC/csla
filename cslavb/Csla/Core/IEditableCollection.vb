24adfb1f7a68a1ec08b3eeed6cad3497f42f0f1e
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-02-07 17:33:34 +0000

    sync bugid: 300

--- cslavb/Csla/Core/IEditableCollection.vb
+++ cslavb/Csla/Core/IEditableCollection.vb
@@ -10,8 +10,6 @@ Namespace Core
   ''' so as to not clutter up the native interface of
   ''' the collection objects.
   ''' </remarks>
-  <System.Diagnostics.CodeAnalysis.SuppressMessage( _
-   "Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix")> _
   Public Interface IEditableCollection
     Inherits IBusinessObject
     Inherits ISupportUndo
@@ -24,12 +22,22 @@ Namespace Core
     ''' <param name="child">Child object to be removed.</param>
     Sub RemoveChild(ByVal child As Core.IEditableBusinessObject)
     ''' <summary>
+    ''' Returns <see langword="true" /> if this object is both dirty and valid.
+    ''' </summary>
+    ''' <returns>A value indicating if this object is both dirty and valid.</returns>
+    Overloads ReadOnly Property IsSavable() As Boolean
+    ''' <summary>
     ''' Used by BusinessListBase as a child object is 
     ''' created to tell the child object about its
     ''' parent.
     ''' </summary>
     ''' <param name="parent">A reference to the parent collection object.</param>
     Sub SetParent(ByVal parent As IParent)
+    ''' <summary>
+    ''' Used by ObjectFactory to gain access to the
+    ''' list of deleted items contained in the collection.
+    ''' </summary>
+    Function GetDeletedList() As Object
 
   End Interface
 

cfafca6da5aa5dc3e0cc7592d7f76b1f2c43c1f4
vonbvillafuerte <vonbvillafuerte@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-02-07 00:51:02 +0000

    Synch with C# version. bugid: 300

--- cslavb/Csla/CriteriaBase.vb
+++ cslavb/Csla/CriteriaBase.vb
@@ -1,13 +1,43 @@
+Imports System
+Imports Csla.Serialization.Mobile
+Imports Csla.Core
+Imports System.ComponentModel
+
 ''' <summary>
 ''' Base type from which Criteria classes can be
 ''' derived in a business class. 
 ''' </summary>
 <Serializable()> _
 Public MustInherit Class CriteriaBase
-
+  Inherits Core.ManagedObjectBase
   Implements ICriteria
 
+  Private Shared _forceInit As Boolean = True
+
+  ''' <summary>
+  ''' Defines the TypeName property.
+  ''' </summary>
+  <EditorBrowsable(EditorBrowsableState.Never)> _
+  Public Shared ReadOnly TypeNameProperty As PropertyInfo(Of String) = RegisterProperty( _
+    GetType(CriteriaBase), New PropertyInfo(Of String)("TypeName"))
+
+  <NonSerialized()> _
+  <Csla.NotUndoable()> _
   Private _objectType As Type
+
+  ''' <summary>
+  ''' Assembly qualified type name of the business 
+  ''' object to be instantiated by
+  ''' the server-side DataPortal. 
+  ''' </summary>
+  Public Property TypeName() As String
+    Get
+      Return ReadProperty(TypeNameProperty)
+    End Get
+    Protected Set(ByVal value As String)
+      LoadProperty(TypeNameProperty, value)
+    End Set
+  End Property
 
   ''' <summary>
   ''' Type of the business object to be instantiated by
@@ -18,6 +48,38 @@ Public MustInherit Class CriteriaBase
       Return _objectType
     End Get
   End Property
+
+#If SILVERLIGHT Then
+' Do NOTHING fow now
+#Else
+  ''' <summary>
+  ''' Initializes empty CriteriaBase. The type of
+  ''' business object to be created by the DataPortal
+  ''' MUST be supplied by the subclass.
+  ''' </summary>
+  Protected Sub New()
+
+  End Sub
+
+  <System.Runtime.Serialization.OnDeserialized()> _
+  Private Sub OnDeserializedHandler(ByVal context As System.Runtime.Serialization.StreamingContext)
+    OnDeserialized(context)
+  End Sub
+
+  ''' <summary>
+  ''' This method is called on a newly deserialized object
+  ''' after deserialization is complete.
+  ''' </summary>
+  ''' <param name="context">Serialization context object.</param>
+  <EditorBrowsable(EditorBrowsableState.Advanced)> _
+  Protected Overridable Sub OnDeserialized(ByVal context As System.Runtime.Serialization.StreamingContext)
+
+    _forceInit = _forceInit And False
+    If FieldManager IsNot Nothing Then
+      FieldManager.SetPropertyList(Me.GetType())
+    End If
+  End Sub
+#End If
 
   ''' <summary>
   ''' Initializes CriteriaBase with the type of

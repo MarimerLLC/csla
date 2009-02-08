b7908b14ed94a5f8e6ff03b63416d09bdc6ef165
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-02-08 01:21:26 +0000

    Sync bugid: 300

--- cslavb/Csla/DataPortalResult.vb
+++ cslavb/Csla/DataPortalResult.vb
@@ -8,62 +8,65 @@
 ''' Type of business object.
 ''' </typeparam>
 Public Class DataPortalResult(Of T)
-    Inherits EventArgs
+  Inherits EventArgs
 
-    Private _object As T
-    Private _error As Exception
-    Private _userState As Object
+  Private _object As T
+  Private _error As Exception
+  Private _userState As Object
 
-    ''' <summary>
-    ''' The business object returned from
-    ''' the data portal.
-    ''' </summary>
-    Public Property [Object]() As T
-        Get
-            Return _object
-        End Get
-        Private Set(ByVal value As T)
-            _object = value
-        End Set
-    End Property
+  ''' <summary>
+  ''' The business object returned from
+  ''' the data portal.
+  ''' </summary>
+  Public Property [Object]() As T
+    Get
+      Return _object
+    End Get
+    Private Set(ByVal value As T)
+      _object = value
+    End Set
+  End Property
 
-    ''' <summary>
-    ''' Any Exception object returned from
-    ''' the data portal. If this is not null,
-    ''' then an exception occurred and should
-    ''' be processed.
-    ''' </summary>
-    Public Property [Error]() As Exception
-        Get
-            Return _error
-        End Get
-        Private Set(ByVal value As Exception)
-            _error = value
-        End Set
-    End Property
+  ''' <summary>
+  ''' Any Exception object returned from
+  ''' the data portal. If this is not null,
+  ''' then an exception occurred and should
+  ''' be processed.
+  ''' </summary>
+  Public Property [Error]() As Exception
+    Get
+      Return _error
+    End Get
+    Private Set(ByVal value As Exception)
+      _error = value
+    End Set
+  End Property
 
-    Public Property UserState() As Object
-        Get
-            Return _userState
-        End Get
-        Private Set(ByVal value As Object)
-            _userState = value
-        End Set
-    End Property
+  ''' <summary>
+  ''' Gets the user state value.
+  ''' </summary>
+  Public Property UserState() As Object
+    Get
+      Return _userState
+    End Get
+    Private Set(ByVal value As Object)
+      _userState = value
+    End Set
+  End Property
 
-    ''' <summary>
-    ''' Creates and populates an instance of 
-    ''' the object.
-    ''' </summary>
-    ''' <param name="obj">
-    ''' The business object to return.
-    ''' </param>
-    ''' <param name="ex">
-    ''' The Exception (if any) to return.
-    ''' </param>
-    Public Sub New(ByVal obj As T, ByVal ex As Exception, ByVal userState As Object)
-        Me.Object = obj
-        Me.Error = ex
-        Me.UserState = userState
-    End Sub
+  ''' <summary>
+  ''' Creates and populates an instance of 
+  ''' the object.
+  ''' </summary>
+  ''' <param name="obj">
+  ''' The business object to return.
+  ''' </param>
+  ''' <param name="ex">
+  ''' The Exception (if any) to return.
+  ''' </param>
+  Public Sub New(ByVal obj As T, ByVal ex As Exception, ByVal userState As Object)
+    Me.Object = obj
+    Me.Error = ex
+    Me.UserState = userState
+  End Sub
 End Class

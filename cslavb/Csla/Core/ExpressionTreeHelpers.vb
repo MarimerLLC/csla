3c8b0450b46a332c263cd52b90bba9123159f240
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-02-11 02:40:09 +0000

    bugid: 300

--- cslavb/Csla/Core/ExpressionTreeHelpers.vb
+++ cslavb/Csla/Core/ExpressionTreeHelpers.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Linq.Expressions
 
 Namespace Core
+
   Friend Class ExpressionTreeHelpers
     Friend Shared Function IsMemberEqualsValueExpression(ByVal exp As Expression, ByVal declaringType As Type, ByVal memberName As String) As Boolean
       If exp.NodeType <> ExpressionType.Equal Then
@@ -10,20 +11,24 @@ Namespace Core
       Dim be As BinaryExpression = CType(exp, BinaryExpression)
 
       ' Assert.
-      If ExpressionTreeHelpers.IsSpecificMemberExpression(be.Left, declaringType, memberName) AndAlso ExpressionTreeHelpers.IsSpecificMemberExpression(be.Right, declaringType, memberName) Then
-        Throw New Exception("Cannot have 'member' == 'member' in an expression!")
+      If ExpressionTreeHelpers.IsSpecificMemberExpression(be.Left, declaringType, memberName) AndAlso _
+         ExpressionTreeHelpers.IsSpecificMemberExpression(be.Right, declaringType, memberName) Then
+        Throw New Exception(My.Resources.CannotHaveMemberEqualsMemberInAnExpression)
       End If
 
-      Return (ExpressionTreeHelpers.IsSpecificMemberExpression(be.Left, declaringType, memberName) OrElse ExpressionTreeHelpers.IsSpecificMemberExpression(be.Right, declaringType, memberName))
+      Return (ExpressionTreeHelpers.IsSpecificMemberExpression(be.Left, declaringType, memberName) OrElse _
+              ExpressionTreeHelpers.IsSpecificMemberExpression(be.Right, declaringType, memberName))
     End Function
 
     Friend Shared Function IsSpecificMemberExpression(ByVal exp As Expression, ByVal declaringType As Type, ByVal memberName As String) As Boolean
-      Return ((TypeOf exp Is MemberExpression) AndAlso ((CType(exp, MemberExpression)).Member.DeclaringType Is declaringType) AndAlso ((CType(exp, MemberExpression)).Member.Name = memberName))
+      Return ((TypeOf exp Is MemberExpression) AndAlso _
+              ((CType(exp, MemberExpression)).Member.DeclaringType Is declaringType) AndAlso _
+              ((CType(exp, MemberExpression)).Member.Name = memberName))
     End Function
 
     Friend Shared Function GetValueFromEqualsExpression(ByVal be As BinaryExpression, ByVal memberDeclaringType As Type, ByVal memberName As String) As String
       If be.NodeType <> ExpressionType.Equal Then
-        Throw New Exception("There is a bug in this program.")
+        Throw New InvalidProgramException()
       End If
 
       If be.Left.NodeType = ExpressionType.MemberAccess Then
@@ -41,14 +46,14 @@ Namespace Core
       End If
 
       ' We should have returned by now.
-      Throw New Exception("There is a bug in this program.")
+      Throw New InvalidProgramException()
     End Function
 
     Friend Shared Function GetValueFromExpression(ByVal expression As Expression) As String
       If expression.NodeType = ExpressionType.Constant Then
         Return CStr((CType(expression, ConstantExpression)).Value)
       Else
-        Throw New InvalidQueryException(String.Format("The expression type {0} is not supported to obtain a value.", expression.NodeType))
+        Throw New InvalidQueryException(String.Format(My.Resources.ExpressionTypeNotSupportedToObtainValue, expression.NodeType))
       End If
     End Function
   End Class

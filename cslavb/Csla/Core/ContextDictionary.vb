9176f74e6ee9ea804f76bae3e3190641a3d88e3a
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-02-11 02:32:49 +0000

    Sync bugid: 300

--- cslavb/Csla/Core/ContextDictionary.vb
+++ cslavb/Csla/Core/ContextDictionary.vb
@@ -13,49 +13,50 @@ Imports Csla.Serialization.Mobile
 Imports System.Collections.Specialized
 
 Namespace Core
-    ''' <summary> 
-    ''' Dictionary type that is serializable 
-    ''' with the MobileFormatter. 
-    ''' </summary> 
-    <Serializable()> _
-    Public Class ContextDictionary
-        Inherits HybridDictionary
-        '    Implements IMobileObject
-        '#Region "IMobileObject Members"
+  ''' <summary> 
+  ''' Dictionary type that is serializable 
+  ''' with the MobileFormatter. 
+  ''' </summary> 
+  <Serializable()> _
+  Public Class ContextDictionary
+    Inherits HybridDictionary
+    Implements IMobileObject
 
-        '    Private Sub GetState(ByVal info As SerializationInfo) Implements IMobileObject.GetState
-        '      For Each key As String In Me.Keys
-        '        Dim value As Object = Me(key)
-        '        If Not (TypeOf value Is IMobileObject) Then
-        '          info.AddValue(key, value)
-        '        End If
-        '      Next
-        '    End Sub
+#Region "IMobileObject Members"
 
-        '    Private Sub GetChildren(ByVal info As SerializationInfo, ByVal formatter As MobileFormatter) Implements IMobileObject.GetChildren
-        '      For Each key As String In Me.Keys
-        '        Dim value As Object = Me(key)
-        '        Dim mobile As IMobileObject = TryCast(value, IMobileObject)
-        '        If mobile IsNot Nothing Then
-        '          Dim si As SerializationInfo = formatter.SerializeObject(mobile)
-        '          info.AddChild(key, si.ReferenceId)
-        '        End If
-        '      Next
-        '    End Sub
+    Private Sub GetState(ByVal info As SerializationInfo) Implements IMobileObject.GetState
+      For Each key As String In Me.Keys
+        Dim value As Object = Me(key)
+        If Not (TypeOf value Is IMobileObject) Then
+          info.AddValue(key, value)
+        End If
+      Next
+    End Sub
 
-        '    Private Sub SetState(ByVal info As SerializationInfo) Implements IMobileObject.SetState
-        '      For Each key As String In info.Values.Keys
-        '        Add(key, info.Values(key).Value)
-        '      Next
-        '    End Sub
+    Private Sub GetChildren(ByVal info As SerializationInfo, ByVal formatter As MobileFormatter) Implements IMobileObject.GetChildren
+      For Each key As String In Me.Keys
+        Dim value As Object = Me(key)
+        Dim mobile As IMobileObject = TryCast(value, IMobileObject)
+        If mobile IsNot Nothing Then
+          Dim si As SerializationInfo = formatter.SerializeObject(mobile)
+          info.AddChild(key, si.ReferenceId)
+        End If
+      Next
+    End Sub
 
-        '    Private Sub SetChildren(ByVal info As SerializationInfo, ByVal formatter As MobileFormatter) Implements IMobileObject.SetChildren
-        '      For Each key As String In info.Children.Keys
-        '        Dim referenceId As Integer = info.Children(key).ReferenceId
-        '        Me.Add(key, formatter.GetObject(referenceId))
-        '      Next
-        '    End Sub
+    Private Sub SetState(ByVal info As SerializationInfo) Implements IMobileObject.SetState
+      For Each key As String In info.Values.Keys
+        Add(key, info.Values(key).Value)
+      Next
+    End Sub
 
-        '#End Region
-    End Class
+    Private Sub SetChildren(ByVal info As SerializationInfo, ByVal formatter As MobileFormatter) Implements IMobileObject.SetChildren
+      For Each key As String In info.Children.Keys
+        Dim referenceId As Integer = info.Children(key).ReferenceId
+        Me.Add(key, formatter.GetObject(referenceId))
+      Next
+    End Sub
+
+#End Region
+  End Class
 End Namespace

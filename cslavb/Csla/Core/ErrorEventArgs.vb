3c6a23670232258e1afe1c8885bcac3c76e990d8
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-02-11 02:35:22 +0000

    Sync bugid: 300

--- cslavb/Csla/Core/ErrorEventArgs.vb
+++ cslavb/Csla/Core/ErrorEventArgs.vb
@@ -5,10 +5,18 @@ Imports System.Text
 
 Namespace Core
 
+  ''' <summary>
+  ''' Event arguments for an unhandled async
+  ''' exception.
+  ''' </summary>
+  ''' <remarks></remarks>
   Public Class ErrorEventArgs
     Inherits EventArgs
 
     Private _originalSender As Object
+    ''' <summary>
+    ''' Reference to the original sender of the event.
+    ''' </summary>
     Public Property OriginalSender() As Object
       Get
         Return _originalSender
@@ -19,6 +27,9 @@ Namespace Core
     End Property
 
     Private _error As Exception
+    ''' <summary>
+    ''' Reference to the unhandled async exception object.
+    ''' </summary>
     Public Property [Error]() As Exception
       Get
         Return _error
@@ -28,6 +39,15 @@ Namespace Core
       End Set
     End Property
 
+    ''' <summary>
+    ''' Creates an instance of the object.
+    ''' </summary>
+    ''' <param name="originalSender">
+    ''' Reference to the original sender of the event.
+    ''' </param>
+    ''' <param name="error">
+    ''' Reference to the unhandled async exception object.
+    ''' </param>
     Public Sub New(ByVal originalSender As Object, ByVal [error] As Exception)
       originalSender = originalSender
       [error] = [error]

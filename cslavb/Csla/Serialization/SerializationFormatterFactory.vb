888e35a0610fc8e2d7cd2c887a7a6e04197884bb
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-02-27 02:17:55 +0000

    Sync with c# bugid: 300

--- cslavb/Csla/Serialization/SerializationFormatterFactory.vb
+++ cslavb/Csla/Serialization/SerializationFormatterFactory.vb
@@ -11,13 +11,16 @@ Namespace Serialization
     ''' Creates a serialization formatter object.
     ''' </summary>
     Public Function GetFormatter() As ISerializationFormatter
-
+#If SILVERLIGHT Then
+      Return New Csla.Serialization.Mobile.MobileFormatter()
+#Else
       If ApplicationContext.SerializationFormatter = ApplicationContext.SerializationFormatters.BinaryFormatter Then
         Return New BinaryFormatterWrapper()
 
       Else
         Return New NetDataContractSerializerWrapper()
       End If
+#End If
 
     End Function
 

aa9c88c67e291afdbba15a3c4afa6e1a00b5ae1f
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-03-18 00:28:05 +0000

    Make sure RegisterProperty() with lambda expressions works right in CslaIdentity subclasses bugid: 365

--- cslacs/Csla/Security/CslaIdentity.cs
+++ cslacs/Csla/Security/CslaIdentity.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Security.Principal;
 using Csla.Serialization;
 using System.Collections.Generic;
 using Csla.Core.FieldManager;
 using System.Runtime.Serialization;
+using System.Reflection;
 using Csla.Core;
+using Csla.Reflection;
 
 namespace Csla.Security
 {
@@ -16,6 +19,58 @@ namespace Csla.Security
   public abstract partial class CslaIdentity : ReadOnlyBase<CslaIdentity>, IIdentity, ICheckRoles
   {
     private static bool _forceInit;
+
+    #region RegisterProperty
+
+    /// <summary>
+    /// Indicates that the specified property belongs
+    /// to the business object type.
+    /// </summary>
+    /// <typeparam name="T">Type of object to which the property belongs.</typeparam>
+    /// <typeparam name="P">Type of property</typeparam>
+    /// <param name="propertyLambdaExpression">Property Expression</param>
+    /// <returns>The provided IPropertyInfo object.</returns>
+    protected static PropertyInfo<P> RegisterProperty<T, P>(Expression<Func<T, object>> propertyLambdaExpression)
+    {
+      PropertyInfo reflectedPropertyInfo = Reflect<T>.GetProperty(propertyLambdaExpression);
+
+      return RegisterProperty(typeof(T), new PropertyInfo<P>(reflectedPropertyInfo.Name));
+    }
+
+    /// <summary>
+    /// Indicates that the specified property belongs
+    /// to the business object type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="P">Type of property</typeparam>
+    /// <param name="propertyLambdaExpression">Property Expression</param>
+    /// <param name="friendlyName">Friendly description for a property to be used in databinding</param>
+    /// <returns>The provided IPropertyInfo object.</returns>
+    protected static PropertyInfo<P> RegisterProperty<T, P>(Expression<Func<T, object>> propertyLambdaExpression, string friendlyName)
+    {
+      PropertyInfo reflectedPropertyInfo = Reflect<T>.GetProperty(propertyLambdaExpression);
+
+      return RegisterProperty(typeof(T), new PropertyInfo<P>(reflectedPropertyInfo.Name, friendlyName));
+    }
+
+    /// <summary>
+    /// Indicates that the specified property belongs
+    /// to the business object type.
+    /// </summary>
+    /// <typeparam name="T">Type of Target</typeparam>
+    /// <typeparam name="P">Type of property</typeparam>
+    /// <param name="propertyLambdaExpression">Property Expression</param>
+    /// <param name="friendlyName">Friendly description for a property to be used in databinding</param>
+    /// <param name="defaultValue">Default Value for the property</param>
+    /// <returns></returns>
+    protected static PropertyInfo<P> RegisterProperty<T, P>(Expression<Func<T, object>> propertyLambdaExpression, string friendlyName, P defaultValue)
+    {
+      PropertyInfo reflectedPropertyInfo = Reflect<T>.GetProperty(propertyLambdaExpression);
+
+      return RegisterProperty(typeof(T), new PropertyInfo<P>(reflectedPropertyInfo.Name, friendlyName, defaultValue));
+    }
+
+    #endregion
 
     #region UnauthenticatedIdentity
 

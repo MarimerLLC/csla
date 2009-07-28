7fce2978d2f63150b521adaa207ac11fa122ad69
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-07-28 16:49:42 +0000

    Put IDataPortalResult into its own file. bugid: 456

--- cslacs/Csla/DataPortalResult.cs
+++ cslacs/Csla/DataPortalResult.cs
@@ -3,21 +3,6 @@
 namespace Csla
 {
   /// <summary>
-  /// IDataPortalResult defines the results of DataPortal operation
-  /// </summary>
-  public interface IDataPortalResult
-  {
-    /// <summary>
-    /// Gets the resulting object.
-    /// </summary>
-    object Object { get; }
-    /// <summary>
-    /// Gets any resulting error information.
-    /// </summary>
-    Exception Error { get; }
-  }
-
-  /// <summary>
   /// DataPortalResult defines the results of DataPortal operation.
   /// It contains object that was received from the server, 
   /// an error (if occurred) and userState - user defined information
---
+++ cslacs/Csla/IDataPortalResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Csla
+{
+  /// <summary>
+  /// IDataPortalResult defines the results of DataPortal operation
+  /// </summary>
+  public interface IDataPortalResult
+  {
+    /// <summary>
+    /// Gets the resulting object.
+    /// </summary>
+    object Object { get; }
+    /// <summary>
+    /// Gets any resulting error information.
+    /// </summary>
+    Exception Error { get; }
+  }
+}

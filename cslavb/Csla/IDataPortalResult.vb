a7943adee34d12355f32599edb4c0fe1c0147267
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-07-28 01:24:57 +0000

    Added new IDataPortalResult.vb bugid: 457

---
+++ cslavb/Csla/IDataPortalResult.vb
@@ -0,0 +1,13 @@
+﻿Public Interface IDataPortalResult
+
+  ''' <summary>
+  ''' Gets the resulting object.
+  ''' </summary>
+  ReadOnly Property [Object]() As Object
+
+  ''' <summary>
+  ''' Gets any resulting error information.
+  ''' </summary>
+  ReadOnly Property [Error]() As Exception
+
+End Interface

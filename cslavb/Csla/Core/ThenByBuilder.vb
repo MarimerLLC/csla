4ae200badd5cc6a3a7d1b622941c60515f037d82
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-07-30 00:21:44 +0000

    Initial creation bugid: 463

---
+++ cslavb/Csla/Core/ThenByBuilder.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Linq.Expressions
+
+Namespace Core
+
+  Friend Class ThenByBuilder
+    Inherits ExpressionVisitor
+
+    Private exprList As New List(Of Expression)
+
+    Public Function GetThenByExpressions(ByVal expression As Expression) As List(Of Expression)
+      Visit(expression)
+      Return exprList
+    End Function
+
+    Protected Overrides Function VisitMethodCall(ByVal expression As MethodCallExpression) As Expression
+
+      If expression.Method.Name.StartsWith("ThenBy") Then
+        exprList.Insert(0, expression)
+        Visit(expression.Arguments(0))
+      End If
+
+      Return expression
+    End Function
+
+  End Class
+
+End Namespace
+

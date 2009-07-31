4696c20afa5b504bb33e4db66c059c05cdcecf2f
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-07-31 01:18:15 +0000

    Sync with 3.6.3 bugid: 469

--- cslavb/Csla/Utilities.vb
+++ cslavb/Csla/Utilities.vb
@@ -136,7 +136,7 @@ Public Module Utilities
   ''' </remarks>
   Public Function CoerceValue(ByVal desiredType As Type, ByVal valueType As Type, ByVal oldValue As Object, ByVal value As Object) As Object
 
-    If desiredType.Equals(valueType) Then
+    If desiredType.IsAssignableFrom(valueType) Then
       ' types match, just return value
       Return value
 
@@ -160,8 +160,10 @@ Public Module Utilities
       End If
 
       If desiredType.Equals(GetType(SmartDate)) AndAlso oldValue IsNot Nothing Then
-        If value Is Nothing Then _
+        If value Is Nothing Then
           value = String.Empty
+        End If
+
         Dim tmp = DirectCast(oldValue, SmartDate)
         If valueType.Equals(GetType(DateTime)) Then
           tmp.Date = CType(value, DateTime)
@@ -171,28 +173,28 @@ Public Module Utilities
         Return tmp
       End If
 
-        If (desiredType.IsPrimitive OrElse desiredType.Equals(GetType(Decimal))) _
-            AndAlso valueType.Equals(GetType(String)) _
-            AndAlso String.IsNullOrEmpty(CStr(value)) Then
-          value = 0
-        End If
-
-        Try
-          If desiredType.Equals(GetType(String)) AndAlso value IsNot Nothing Then
-            Return value.ToString()
-          Else
-            Return Convert.ChangeType(value, desiredType)
-          End If
-        Catch
-          Dim cnv As TypeConverter = TypeDescriptor.GetConverter(desiredType)
-          If cnv IsNot Nothing AndAlso cnv.CanConvertFrom(valueType) Then
-            Return cnv.ConvertFrom(value)
-
-          Else
-            Throw
-          End If
-        End Try
+      If (desiredType.IsPrimitive OrElse desiredType.Equals(GetType(Decimal))) _
+          AndAlso valueType.Equals(GetType(String)) _
+          AndAlso String.IsNullOrEmpty(CStr(value)) Then
+        value = 0
       End If
+
+      Try
+        If desiredType.Equals(GetType(String)) AndAlso value IsNot Nothing Then
+          Return value.ToString()
+        Else
+          Return Convert.ChangeType(value, desiredType)
+        End If
+      Catch
+        Dim cnv As TypeConverter = TypeDescriptor.GetConverter(desiredType)
+        If cnv IsNot Nothing AndAlso cnv.CanConvertFrom(valueType) Then
+          Return cnv.ConvertFrom(value)
+
+        Else
+          Throw
+        End If
+      End Try
+    End If
 
   End Function
 

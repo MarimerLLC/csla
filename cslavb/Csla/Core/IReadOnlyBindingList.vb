36bfe6463173f68e078da01581de12a057094281
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-08-01 21:33:29 +0000

    Initial creatation bugid: 478

---
+++ cslavb/Csla/Core/IReadOnlyBindingList.vb
@@ -0,0 +1,10 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+
+Public Interface IReadOnlyBindingList
+
+  Property IsReadOnly() As Boolean
+
+End Interface

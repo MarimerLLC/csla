f0dd0a3df84fc4fbe9ff3ad5178410ee84aff7c2
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-08-09 16:09:18 +0000

    Initial implementation bugid: 502

---
+++ cslavb/Csla/Serialization/Mobile/NullPlaceholder.vb
@@ -0,0 +1,44 @@
+﻿
+Namespace Serialization.Mobile
+
+  ''' <summary>
+  ''' Placeholder for null child objects.
+  ''' </summary>
+  ''' <remarks></remarks>
+  <Serializable()> _
+  Public NotInheritable Class NullPlaceholder
+    Implements IMobileObject
+
+
+#Region " Constructors "
+
+    Public Sub New()
+      'Nothing
+    End Sub
+
+#End Region
+
+#Region " IMobileObject Members "
+
+    Public Sub GetChildren(ByVal info As SerializationInfo, ByVal formatter As MobileFormatter) Implements IMobileObject.GetChildren
+      'Nothing
+    End Sub
+
+    Public Sub GetState(ByVal info As SerializationInfo) Implements IMobileObject.GetState
+      'Nothing
+    End Sub
+
+    Public Sub SetChildren(ByVal info As SerializationInfo, ByVal formatter As MobileFormatter) Implements IMobileObject.SetChildren
+      'Nothing
+    End Sub
+
+    Public Sub SetState(ByVal info As SerializationInfo) Implements IMobileObject.SetState
+      'Nothing
+    End Sub
+
+#End Region
+    
+  End Class
+
+End Namespace
+

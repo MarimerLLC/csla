212e63b617763bb2bc4e838810dfd562f748d8e5
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-08-09 16:56:54 +0000

    Initial creation bugid: 505

---
+++ cslavb/Csla/Serialization/Mobile/ReferenceComparer.vb
@@ -0,0 +1,30 @@
+﻿Imports System
+Imports System.Collections.Generic
+
+Namespace Serialization.Mobile
+
+  ''' <summary>
+  ''' Implements an equality comparer for <see cref="IMobileObject" /> that compares
+  ''' the objects only on the basis is the reference value.
+  ''' </summary>
+  Public NotInheritable Class ReferenceComparer(Of T)
+    Implements IEqualityComparer(Of T)
+
+#Region " IEqualityComparer<T> Members "
+
+    ''' <summary>
+    ''' Determines if the two objects are reference-equal.
+    ''' </summary>
+    Public Shadows Function Equals(ByVal x As T, ByVal y As T) As Boolean Implements System.Collections.Generic.IEqualityComparer(Of T).Equals
+      Return Object.ReferenceEquals(x, y)
+    End Function
+
+    Public Shadows Function GetHashCode(ByVal obj As T) As Integer Implements System.Collections.Generic.IEqualityComparer(Of T).GetHashCode
+      Return obj.GetHashCode()
+    End Function
+
+#End Region
+
+  End Class
+End Namespace
+

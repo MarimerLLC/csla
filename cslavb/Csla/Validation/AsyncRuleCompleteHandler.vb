f04988a6bf4254226f9581c17b11b4f3f412d692
Sean.Rhone@lendingtree.com <Sean.Rhone@lendingtree.com@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-09-27 14:34:16 +0000

    Change method declaration to allow use in Lambda expression in AsyncRuleMethod.Invoke bugid: 574

--- cslavb/Csla/Validation/AsyncRuleCompleteHandler.vb
+++ cslavb/Csla/Validation/AsyncRuleCompleteHandler.vb
@@ -5,8 +5,9 @@
   ''' </summary>
   ''' <param name="sender">Object calling the handler.</param>
   ''' <param name="result">Result arguments from the validation rule.</param>
-  ''' <remarks></remarks>
-  Public Delegate Sub AsyncRuleCompleteHandler(ByVal sender As Object, ByVal result As AsyncRuleResult)
+  ''' <remarks>Note there is a difference in the method declaration between c# and VB.net. In AsyncRuleMethod.Invoke we
+  ''' use a Lambda expression with this and in VB 9.0 the Lambda expression requires a single return value where c# does not.</remarks>
+  Public Delegate Function AsyncRuleCompleteHandler(ByVal sender As Object, ByVal result As AsyncRuleResult) As Boolean
 
 End Namespace
 

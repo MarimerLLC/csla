8f785f251a409ea401967d5f4ca4e1794fe2de02
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-10-20 16:48:43 +0000

    Update cslatest project so it builds under VS10 Beta 2. At this time it won't actually run, because there doesn't appear to be a version of nunit for .NET 4.0. bugid: 602

--- cslatest/Csla.Test/DataAnnotations/DataAnnotationsTests.cs
+++ cslatest/Csla.Test/DataAnnotations/DataAnnotationsTests.cs
@@ -3,47 +3,102 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Csla;
+using Csla.Serialization;
+using UnitDriven;
 using System.ComponentModel.DataAnnotations;
 
-#if !NUNIT
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-#else
+#if NUNIT
 using NUnit.Framework;
 using TestClass = NUnit.Framework.TestFixtureAttribute;
 using TestInitialize = NUnit.Framework.SetUpAttribute;
 using TestCleanup = NUnit.Framework.TearDownAttribute;
 using TestMethod = NUnit.Framework.TestAttribute;
+using TestSetup = NUnit.Framework.SetUpAttribute;
+#elif MSTEST
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 #endif
 
 namespace Csla.Test.DataAnnotations
 {
+#if TESTING
+  [System.Diagnostics.DebuggerNonUserCode]
+#endif
   [TestClass]
-  public class DataAnnotationsTests
+  public class DataAnnotationsTests : TestBase
   {
+#if SILVERLIGHT
+    [TestInitialize]
+    public void Setup()
+    {
+      Csla.DataPortal.ProxyTypeName = "Local";
+    }
+#endif
+
     [TestMethod]
     public void SingleAttribute()
     {
-      var root = Csla.DataPortal.Create<Single>();
-      var rules = root.GetRules();
+      var context = GetContext();
 
-      Assert.AreEqual(1, rules.Length, "Should be 1 rule");
-      Assert.IsFalse(root.IsValid, "Obj shouldn't be valid");
-      Assert.AreEqual(1, root.BrokenRulesCollection.Count, "Should be 1 broken rule");
-      Assert.AreEqual("Name value required", root.BrokenRulesCollection[0].Description, "Desc should match");
+      var dp = new Csla.DataPortal<Single>();
+      dp.CreateCompleted += (o, e) =>
+        {
+          var root = e.Object;
+          var rules = root.GetRules();
+
+          Assert.AreEqual(1, rules.Length, "Should be 1 rule");
+          Assert.IsFalse(root.IsValid, "Obj shouldn't be valid");
+          Assert.AreEqual(1, root.BrokenRulesCollection.Count, "Should be 1 broken rule");
+          Assert.AreEqual("Name value required", root.BrokenRulesCollection[0].Description, "Desc should match");
+          context.Assert.Success();
+        };
+      dp.BeginCreate();
+
+      context.Complete();
     }
 
     [TestMethod]
     public void MultipleAttributes()
     {
-      var root = Csla.DataPortal.Create<Multiple>();
-      var rules = root.GetRules();
+      var context = GetContext();
 
-      Assert.AreEqual(3, rules.Length, "Should be 3 rules");
-      Assert.IsFalse(root.IsValid, "Obj shouldn't be valid");
-      Assert.AreEqual(1, root.BrokenRulesCollection.Count, "Should be 1 broken rule");
-      root.Name = "xyz";
-      Assert.AreEqual(2, root.BrokenRulesCollection.Count, "Should be 2 broken rules after edit");
+      var dp = new Csla.DataPortal<Multiple>();
+      dp.CreateCompleted += (o, e) =>
+        {
+          var root = e.Object;
+          var rules = root.GetRules();
+
+          Assert.AreEqual(3, rules.Length, "Should be 3 rules");
+          Assert.IsFalse(root.IsValid, "Obj shouldn't be valid");
+          Assert.AreEqual(1, root.BrokenRulesCollection.Count, "Should be 1 broken rule");
+          root.Name = "xyz";
+          Assert.AreEqual(2, root.BrokenRulesCollection.Count, "Should be 2 broken rules after edit");
+          context.Assert.Success();
+        };
+      dp.BeginCreate();
+
+      context.Complete();
+    }
+
+    [TestMethod]
+    public void CustomAttribute()
+    {
+      var context = GetContext();
+
+      var dp = new Csla.DataPortal<Custom>();
+      dp.CreateCompleted += (o, e) =>
+      {
+        var root = e.Object;
+        var rules = root.GetRules();
+
+        Assert.AreEqual(1, rules.Length, "Should be 1 rule");
+        Assert.IsFalse(root.IsValid, "Obj shouldn't be valid");
+        Assert.AreEqual(1, root.BrokenRulesCollection.Count, "Should be 1 broken rule");
+        Assert.AreEqual("Name must be abc", root.BrokenRulesCollection[0].Description, "Desc should match");
+        context.Assert.Success();
+      };
+      dp.BeginCreate();
+
+      context.Complete();
     }
   }
 
@@ -80,6 +135,38 @@ namespace Csla.Test.DataAnnotations
     public string[] GetRules()
     {
       return ValidationRules.GetRuleDescriptions();
+    }
+  }
+
+  [Serializable]
+  public class Custom : BusinessBase<Custom>
+  {
+    private static PropertyInfo<string> NameProperty = RegisterProperty<string>(c => c.Name);
+    [TestRule]
+    public string Name
+    {
+      get { return GetProperty(NameProperty); }
+      set { SetProperty(NameProperty, value); }
+    }
+
+    public string[] GetRules()
+    {
+      return ValidationRules.GetRuleDescriptions();
+    }
+  }
+
+  public class TestRuleAttribute : ValidationAttribute
+  {
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+      if (validationContext.ObjectInstance == null)
+        return new ValidationResult("ObjectInstance is null");
+      var obj = validationContext.ObjectInstance as Custom;
+      if (obj == null)
+        return new ValidationResult("ObjectInstance is not the Custom type");
+      if (string.IsNullOrEmpty(obj.Name) || obj.Name != "abc")
+        return new ValidationResult("Name must be abc");
+      return null;
     }
   }
 }
--- cslatest/Csla.Test/Properties/AssemblyInfo.cs
+++ cslatest/Csla.Test/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Marimer LLC")]
-[assembly: AssemblyProduct("Csla.Test")]
+[assembly: AssemblyProduct("CSLA .NET")]
 [assembly: AssemblyCopyright("Copyright ©  2009 Marimer LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.8.0.0")]
-[assembly: AssemblyFileVersion("3.8.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]

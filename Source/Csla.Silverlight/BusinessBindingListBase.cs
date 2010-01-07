68d976581391ee19c6ca5d956afbb9c14509df3a
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2010-01-07 02:30:30 +0000

    Add BusinessBindingListBase to Silverlight. bugid: 336

---
+++ Source/Csla.Silverlight/BusinessBindingListBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Csla.Serialization;
+
+namespace Csla
+{
+  /// <summary>
+  /// This is the base class from which most business collections
+  /// or lists will be derived.
+  /// </summary>
+  /// <typeparam name="T">Type of the business object being defined.</typeparam>
+  /// <typeparam name="C">Type of the child objects contained in the list.</typeparam>
+#if TESTING
+  [System.Diagnostics.DebuggerStepThrough]
+#endif
+  [Serializable]
+  public abstract class BusinessBindingListBase<T, C> : BusinessListBase<T, C>
+    where T : BusinessBindingListBase<T, C>
+    where C : Csla.Core.IEditableBusinessObject
+  {
+
+  }
+}

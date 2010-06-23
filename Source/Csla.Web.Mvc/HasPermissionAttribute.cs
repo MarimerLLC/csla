6d71a3545ae2a66403c97034e85645abb7717ad5
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2010-06-23 20:11:19 +0000

    Add MVC authorization support. bugid: 778

---
+++ Source/Csla.Web.Mvc/HasPermissionAttribute.cs
@@ -0,0 +1,45 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="HasPermissionAttribute.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: http://www.lhotka.net/cslanet/
+// </copyright>
+// <summary>Restricts callers to an action method.</summary>
+//-----------------------------------------------------------------------
+using System;
+using System.Web;
+using System.Web.Mvc;
+using Csla.Rules;
+
+namespace Csla.Web.Mvc
+{
+  /// <summary>
+  /// Restricts callers to an action method.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+  public class HasPermissionAttribute : AuthorizeAttribute
+  {
+    private AuthorizationActions _action;
+    private Type _objectType;
+
+    /// <summary>
+    /// Creates an instance of the type.
+    /// </summary>
+    /// <param name="action">AuthorizationActions for which the authorization is required.</param>
+    /// <param name="objectType">CSLA object type for which the action is applied.</param>
+    public HasPermissionAttribute(AuthorizationActions action, Type objectType)
+    {
+      _action = action;
+      _objectType = objectType;
+    }
+
+    /// <summary>
+    /// Determines whether access to the core framework is authorized.
+    /// </summary>
+    /// <param name="httpContext">The HTTP context.</param>
+    /// <returns>True if access is authorized; otherwise, false.</returns>
+    protected override bool AuthorizeCore(HttpContextBase httpContext)
+    {
+      return BusinessRules.HasPermission(_action, _objectType);
+    }
+  }
+}
---
+++ Source/Csla.Web.Mvc/HtmlExtensions.cs
@@ -0,0 +1,89 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="HtmlExtensions.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: http://www.lhotka.net/cslanet/
+// </copyright>
+// <summary>Html extension methods providing support for HTML rendering based on security permissions in an application.</summary>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace Csla.Web.Mvc
+{
+  /// <summary>
+  /// Html extension methods providing support for HTML rendering based on security permissions in an application.
+  /// </summary>
+  public static class HtmlExtensions
+  {
+    /// <summary>
+    /// Conditionally render HTML output according to the provided authorization action and underlyiong object type.
+    /// </summary>
+    /// <param name="htmlHelper">The HTML helper instance that this method extends.</param>
+    /// <param name="action">AuthorizationActions for which the authorization is required.</param>
+    /// <param name="objectType">CSLA object type for which the action is applied.</param>
+    /// <param name="granted">The rendered HTML output for granted users.</param>
+    /// <param name="denied">The rendered HTML output for denied users.</param>
+    /// <returns>The appropriate HTML rendered output.</returns>
+    public static MvcHtmlString HasPermission(
+                                    this HtmlHelper htmlHelper,
+                                    Csla.Rules.AuthorizationActions action,
+                                    Type objectType,
+                                    MvcHtmlString granted,
+                                    MvcHtmlString denied)
+    {
+      if (Csla.Rules.BusinessRules.HasPermission(action, objectType))
+        return granted;
+      else
+        return denied;
+    }
+
+    /// <summary>
+    /// Conditionally render HTML output according to the provided authorization action and underlyiong object type.
+    /// </summary>
+    /// <param name="htmlHelper">The HTML helper instance that this method extends.</param>
+    /// <param name="action">AuthorizationActions for which the authorization is required.</param>
+    /// <param name="objectType">CSLA object type for which the action is applied.</param>
+    /// <param name="granted">The rendered HTML output for granted users.</param>
+    /// <param name="denied">The rendered HTML output for denied users.</param>
+    /// <returns>The appropriate HTML rendered output.</returns>
+    public static MvcHtmlString HasPermission(
+                            this HtmlHelper htmlHelper,
+                            Csla.Rules.AuthorizationActions action,
+                            Type objectType,
+                            MvcHtmlString granted,
+                            string denied)
+    {
+      if (Csla.Rules.BusinessRules.HasPermission(action, objectType))
+        return granted;
+      else
+        return MvcHtmlString.Create(denied);
+    }
+
+    /// <summary>
+    /// Conditionally render HTML output according to the provided authorization action and underlyiong object type.
+    /// </summary>
+    /// <param name="htmlHelper">The HTML helper instance that this method extends.</param>
+    /// <param name="action">AuthorizationActions for which the authorization is required.</param>
+    /// <param name="objectType">CSLA object type for which the action is applied.</param>
+    /// <param name="granted">The rendered HTML output for granted users.</param>
+    /// <param name="denied">The rendered HTML output for denied users.</param>
+    /// <returns>The appropriate HTML rendered output.</returns>
+    public static MvcHtmlString HasPermission(
+                            this HtmlHelper htmlHelper,
+                            Csla.Rules.AuthorizationActions action,
+                            Type objectType,
+                            string granted,
+                            string denied)
+    {
+      if (Csla.Rules.BusinessRules.HasPermission(action, objectType))
+        return MvcHtmlString.Create(granted);
+      else
+        return MvcHtmlString.Create(denied);
+    }
+  }
+}

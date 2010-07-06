74e1c0f5c36b589152b724eaa992b7de124bea57
jonnybee <jonnybee@fa5919ed-4f44-5740-a13a-40f41935fe31>
2010-07-06 21:17:40 +0000

    Simplified code in Datalist bugid: 748

--- Samples/CslaNet/cs/BLBTest/BLBTest/DataList.cs
+++ Samples/CslaNet/cs/BLBTest/BLBTest/DataList.cs
@@ -8,18 +8,6 @@ namespace BLBTest
   [Serializable]
   public class DataList : BusinessBindingListBase<DataList, DataEdit>
   {
-    public DataList()
-    {
-      AllowEdit = true;
-      AllowNew = true;
-      AllowRemove = true;
-    }
 
-    protected override object AddNewCore()
-    {
-      DataEdit item = new DataEdit();
-      Add(item);
-      return item;
-    }
   }
 }

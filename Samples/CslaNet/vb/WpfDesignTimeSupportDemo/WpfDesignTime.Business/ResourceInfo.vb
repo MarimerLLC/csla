667efdd836f02155e5d10a33abafdf0a920c6af3
JonnyBee <JonnyBee@fa5919ed-4f44-5740-a13a-40f41935fe31>
2010-07-13 10:48:22 +0000

    Updated WpfDesigntimeSupport sample in VB.NET for Csla4 bugid: 748

--- Samples/CslaNet/vb/WpfDesignTimeSupportDemo/WpfDesignTime.Business/ResourceInfo.vb
+++ Samples/CslaNet/vb/WpfDesignTimeSupportDemo/WpfDesignTime.Business/ResourceInfo.vb
@@ -5,28 +5,28 @@ Imports System
 Namespace WpfDesignTime.Business
   <Serializable()> _
   Public Class ResourceInfo
-	  Inherits ReadOnlyBase(Of ResourceInfo)
-	Private _id As Integer
-	Public ReadOnly Property Id() As Integer
-	  Get
-		  Return _id
-	  End Get
-	End Property
+    Inherits ReadOnlyBase(Of ResourceInfo)
+    Private _id As Integer
+    Public ReadOnly Property Id() As Integer
+      Get
+        Return _id
+      End Get
+    End Property
 
-	Private _name As String
-	Public ReadOnly Property Name() As String
-	  Get
-		  Return _name
-	  End Get
-	End Property
+    Private _name As String
+    Public ReadOnly Property Name() As String
+      Get
+        Return _name
+      End Get
+    End Property
 
-	Public Overrides Function ToString() As String
-	  Return _name
-	End Function
+    Public Overrides Function ToString() As String
+      Return _name
+    End Function
 
-	Friend Sub New(ByVal id As Integer, ByVal lastname As String, ByVal firstname As String)
-	  _id = id
-	  _name = String.Format("{0}, {1}", lastname, firstname)
-	End Sub
+    Friend Sub New(ByVal id As Integer, ByVal lastname As String, ByVal firstname As String)
+      _id = id
+      _name = String.Format("{0}, {1}", lastname, firstname)
+    End Sub
   End Class
 End Namespace
--- Samples/CslaNet/vb/WpfDesignTimeSupportDemo/WpfDesignTime.Business/ResourceList.vb
+++ Samples/CslaNet/vb/WpfDesignTimeSupportDemo/WpfDesignTime.Business/ResourceList.vb
@@ -1,55 +1,53 @@
 Imports Microsoft.VisualBasic
 Imports Csla
-Imports Csla.Data
 Imports System
-Imports System.Linq
 
 Namespace WpfDesignTime.Business
   <Serializable()> _
   Public Class ResourceList
-	  Inherits ReadOnlyListBase(Of ResourceList, ResourceInfo)
-	#Region " Factory Methods"
+    Inherits ReadOnlyListBase(Of ResourceList, ResourceInfo)
+#Region " Factory Methods"
 
-	Public Shared Function EmptyList() As ResourceList
-	  Return New ResourceList()
-	End Function
+    Public Shared Function EmptyList() As ResourceList
+      Return New ResourceList()
+    End Function
 
-	Public Shared Function GetResourceList() As ResourceList
-	  Return DataPortal.Fetch(Of ResourceList)()
-	End Function
+    Public Shared Function GetResourceList() As ResourceList
+      Return DataPortal.Fetch(Of ResourceList)()
+    End Function
 
-	Private Sub New()
-'INSTANT VB NOTE: Embedded comments are not maintained by Instant VB
-'ORIGINAL LINE: { /* require use of factory methods */ }
-	End Sub
+    Private Sub New()
+      'INSTANT VB NOTE: Embedded comments are not maintained by Instant VB
+      'ORIGINAL LINE: { /* require use of factory methods */ }
+    End Sub
 
-	#End Region
+#End Region
 
-	#Region " Data Access"
+#Region " Data Access"
 
-	Private Overloads Sub DataPortal_Fetch()
-	  RaiseListChangedEvents = False
-	  IsReadOnly = False
-	  ' Simulated Database access
-	  For i As Integer = 1 To 9
-		Me.Add(New ResourceInfo(i, "Last Name # " & i.ToString(), "First Name # " & i.ToString()))
-	  Next i
+    Private Overloads Sub DataPortal_Fetch()
+      RaiseListChangedEvents = False
+      IsReadOnly = False
+      ' Simulated Database access
+      For i As Integer = 1 To 9
+        Me.Add(New ResourceInfo(i, "Last Name # " & i.ToString(), "First Name # " & i.ToString()))
+      Next i
 
-	  IsReadOnly = True
-	  RaiseListChangedEvents = True
-	End Sub
+      IsReadOnly = True
+      RaiseListChangedEvents = True
+    End Sub
 
-	#End Region
+#End Region
 
-	#Region " Design Time Support"
-	Private Function DesignTime_Create() As ResourceList
-	  IsReadOnly = False
-	  Me.Add(New ResourceInfo(1, "Doe", "John"))
-	  Me.Add(New ResourceInfo(2, "Doe", "Jane"))
-	  IsReadOnly = True
-	  Return Me
-	End Function
-	#End Region
+#Region " Design Time Support"
+    Private Function DesignTime_Create() As ResourceList
+      IsReadOnly = False
+      Me.Add(New ResourceInfo(1, "Doe", "John"))
+      Me.Add(New ResourceInfo(2, "Doe", "Jane"))
+      IsReadOnly = True
+      Return Me
+    End Function
+#End Region
 
   End Class
 End Namespace

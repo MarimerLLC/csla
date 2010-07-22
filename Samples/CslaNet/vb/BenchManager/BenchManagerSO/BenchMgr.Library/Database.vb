4f0e76663a1f2c86dbc5d474499f65e13e0c7fb5
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2010-07-22 23:20:09 +0000

    Remove non-working sample. bugid: 748

--- Samples/CslaNet/vb/BenchManager/BenchManagerSO/BenchMgr.Library/Database.vb
+++
@@ -1,9 +0,0 @@
-﻿Module Database
-
-  Public ReadOnly Property BenchMgrConnectionString() As String
-    Get
-      Return System.Configuration.ConfigurationManager.ConnectionStrings("BenchMgr").ConnectionString
-    End Get
-  End Property
-
-End Module
--- Samples/CslaNet/vb/BenchManager/BenchManagerSO/Sales.Library/Database.vb
+++
@@ -1,9 +0,0 @@
-﻿Module Database
-
-  Public ReadOnly Property SalesConnectionString() As String
-    Get
-      Return System.Configuration.ConfigurationManager.ConnectionStrings("Sales").ConnectionString
-    End Get
-  End Property
-
-End Module

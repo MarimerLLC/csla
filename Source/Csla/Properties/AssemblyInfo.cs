cdb401bcc78fa939db60cb9c7a65aea409cd438c
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2010-07-27 20:47:22 +0000

    Update version numbers.

--- Source/Csla/Properties/AssemblyInfo.cs
+++ Source/Csla/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.0.1.0")]
+[assembly: AssemblyFileVersion("4.0.1.0")]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.SourceAssembly, ResourceDictionaryLocation.SourceAssembly)]

a069a0c7618accdc7fcff2e2d7922aca2428e75a
garybr <garybr@fa5919ed-4f44-5740-a13a-40f41935fe31>
2010-08-31 03:44:00 +0000

    updated MVC authorization attribute and helper bugid: 837

--- Source/Csla.Web.Mvc/HtmlExtensions.cs
+++ Source/Csla.Web.Mvc/HtmlExtensions.cs
@@ -52,11 +52,11 @@ namespace Csla.Web.Mvc
     /// <param name="denied">The rendered HTML output for denied users.</param>
     /// <returns>The appropriate HTML rendered output.</returns>
     public static MvcHtmlString HasPermission(
-                            this HtmlHelper htmlHelper,
-                            Csla.Rules.AuthorizationActions action,
-                            Type objectType,
-                            MvcHtmlString granted,
-                            string denied)
+                                    this HtmlHelper htmlHelper,
+                                    Csla.Rules.AuthorizationActions action,
+                                    Type objectType,
+                                    MvcHtmlString granted,
+                                    string denied)
     {
       if (Csla.Rules.BusinessRules.HasPermission(action, objectType))
         return granted;
@@ -74,16 +74,156 @@ namespace Csla.Web.Mvc
     /// <param name="denied">The rendered HTML output for denied users.</param>
     /// <returns>The appropriate HTML rendered output.</returns>
     public static MvcHtmlString HasPermission(
-                            this HtmlHelper htmlHelper,
-                            Csla.Rules.AuthorizationActions action,
-                            Type objectType,
-                            string granted,
-                            string denied)
+                                    this HtmlHelper htmlHelper,
+                                    Csla.Rules.AuthorizationActions action,
+                                    Type objectType,
+                                    string granted,
+                                    string denied)
     {
       if (Csla.Rules.BusinessRules.HasPermission(action, objectType))
         return MvcHtmlString.Create(granted);
       else
         return MvcHtmlString.Create(denied);
+    }
+
+    /// <summary>
+    /// Conditionally render HTML output according to the provided authorization member action and underlyiong object type.
+    /// </summary>
+    /// <typeparam name="T">CSLA object type for which the member action is applied.</typeparam>
+    /// <param name="htmlHelper">The HTML helper instance that this method extends.</param>
+    /// <param name="action">AuthorizationActions for which the authorization is required.</param>
+    /// <param name="target">CSLA object instance.</param>
+    /// <param name="member">CSLA object member.</param>
+    /// <param name="granted">The rendered HTML output for granted users.</param>
+    /// <param name="denied">The rendered HTML output for denied users.</param>
+    /// <returns>The appropriate HTML rendered output.</returns>
+    public static MvcHtmlString HasPermission(
+                                    this HtmlHelper htmlHelper,
+                                    Csla.Rules.AuthorizationActions action,
+                                    object target,
+                                    Csla.Core.IMemberInfo member,
+                                    MvcHtmlString granted,
+                                    MvcHtmlString denied)
+    {
+      var instance = target as Csla.Security.IAuthorizeReadWrite;
+      if (instance == null) return denied;
+
+      if ((action == Rules.AuthorizationActions.ReadProperty && instance.CanReadProperty(member.Name)) ||
+          (action == Rules.AuthorizationActions.WriteProperty && instance.CanWriteProperty(member.Name)) ||
+          (action == Rules.AuthorizationActions.ExecuteMethod && instance.CanExecuteMethod(member.Name)))
+        return granted;
+      else
+        return denied;
+    }
+
+    /// <summary>
+    /// Conditionally render HTML output according to the provided authorization member action and underlyiong object type.
+    /// </summary>
+    /// <typeparam name="T">CSLA object type for which the member action is applied.</typeparam>
+    /// <param name="htmlHelper">The HTML helper instance that this method extends.</param>
+    /// <param name="action">AuthorizationActions for which the authorization is required.</param>
+    /// <param name="target">CSLA object instance.</param>
+    /// <param name="member">CSLA object member.</param>
+    /// <param name="granted">The rendered HTML output for granted users.</param>
+    /// <param name="denied">The rendered HTML output for denied users.</param>
+    /// <returns>The appropriate HTML rendered output.</returns>
+    public static MvcHtmlString HasPermission(
+                                    this HtmlHelper htmlHelper,
+                                    Csla.Rules.AuthorizationActions action,
+                                    object target,
+                                    Csla.Core.IMemberInfo member,
+                                    MvcHtmlString granted,
+                                    string denied)
+    {
+      var instance = target as Csla.Security.IAuthorizeReadWrite;
+      if (instance == null) return MvcHtmlString.Create(denied);
+
+      if ((action == Rules.AuthorizationActions.ReadProperty && instance.CanReadProperty(member.Name)) ||
+          (action == Rules.AuthorizationActions.WriteProperty && instance.CanWriteProperty(member.Name)) ||
+          (action == Rules.AuthorizationActions.ExecuteMethod && instance.CanExecuteMethod(member.Name)))
+        return granted;
+      else
+        return MvcHtmlString.Create(denied);
+    }
+
+    /// <summary>
+    /// Conditionally render HTML output according to the provided authorization member action and underlyiong object type.
+    /// </summary>
+    /// <typeparam name="T">CSLA object type for which the member action is applied.</typeparam>
+    /// <param name="htmlHelper">The HTML helper instance that this method extends.</param>
+    /// <param name="action">AuthorizationActions for which the authorization is required.</param>
+    /// <param name="target">CSLA object instance.</param>
+    /// <param name="member">CSLA object member.</param>
+    /// <param name="granted">The rendered HTML output for granted users.</param>
+    /// <param name="denied">The rendered HTML output for denied users.</param>
+    /// <returns>The appropriate HTML rendered output.</returns>
+    public static MvcHtmlString HasPermission(
+                                    this HtmlHelper htmlHelper,
+                                    Csla.Rules.AuthorizationActions action,
+                                    object target,
+                                    Csla.Core.IMemberInfo member,
+                                    string granted,
+                                    string denied)
+    {
+      var instance = target as Csla.Security.IAuthorizeReadWrite;
+      if (instance == null) return MvcHtmlString.Create(denied);
+
+      if ((action == Rules.AuthorizationActions.ReadProperty && instance.CanReadProperty(member.Name)) ||
+          (action == Rules.AuthorizationActions.WriteProperty && instance.CanWriteProperty(member.Name)) ||
+          (action == Rules.AuthorizationActions.ExecuteMethod && instance.CanExecuteMethod(member.Name)))
+        return MvcHtmlString.Create(granted);
+      else
+        return MvcHtmlString.Create(denied);
+    }
+
+    /// <summary>
+    /// Conditionally render HTML output according to the provided authorization action and underlyiong object type.
+    /// </summary>
+    /// <param name="htmlHelper">The HTML helper instance that this method extends.</param>
+    /// <param name="action">AuthorizationActions for which the authorization is required.</param>
+    /// <param name="objectType">CSLA object type for which the action is applied.</param>
+    /// <param name="grantedAction">The rendered HTML helper action for granted users.</param>
+    /// <returns>The appropriate HTML rendered output.</returns>
+    public static MvcHtmlString HasPermission(
+                                    this HtmlHelper htmlHelper,
+                                    Csla.Rules.AuthorizationActions action,
+                                    Type objectType,
+                                    Func<HtmlHelper, MvcHtmlString> grantedAction)
+    {
+      if (Csla.Rules.BusinessRules.HasPermission(action, objectType))
+        return grantedAction.Invoke(htmlHelper);
+      else
+        return MvcHtmlString.Empty;
+    }
+
+    /// <summary>
+    /// Conditionally render HTML output according to the provided authorization member action and underlyiong object type.
+    /// </summary>
+    /// <typeparam name="T">CSLA object type for which the member action is applied.</typeparam>
+    /// <param name="htmlHelper">The HTML helper instance that this method extends.</param>
+    /// <param name="action">AuthorizationActions for which the authorization is required.</param>
+    /// <param name="target">CSLA object instance.</param>
+    /// <param name="member">CSLA object member.</param>
+    /// <param name="grantedAction">The rendered HTML helper action for granted users.</param>
+    /// <param name="denieddAction">The rendered HTML helper action for denied users.</param>
+    /// <returns>The appropriate HTML rendered output.</returns>
+    public static MvcHtmlString HasPermission(
+                                    this HtmlHelper htmlHelper,
+                                    Csla.Rules.AuthorizationActions action,
+                                    object target,
+                                    Csla.Core.IMemberInfo member,
+                                    Func<HtmlHelper, MvcHtmlString> grantedAction,
+                                    Func<HtmlHelper, MvcHtmlString> denieddAction)
+    {
+      var instance = target as Csla.Security.IAuthorizeReadWrite;
+      if (instance == null) return denieddAction.Invoke(htmlHelper);
+
+      if ((action == Rules.AuthorizationActions.ReadProperty && instance.CanReadProperty(member.Name)) ||
+          (action == Rules.AuthorizationActions.WriteProperty && instance.CanWriteProperty(member.Name)) ||
+          (action == Rules.AuthorizationActions.ExecuteMethod && instance.CanExecuteMethod(member.Name)))
+        return grantedAction.Invoke(htmlHelper);
+      else
+        return denieddAction.Invoke(htmlHelper);
     }
   }
 }

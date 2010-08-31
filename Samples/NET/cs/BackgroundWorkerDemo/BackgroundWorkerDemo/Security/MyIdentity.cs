9d80cc572376f8063c696d1f7e3dcf65e5bcc6d4
JonnyBee <JonnyBee@fa5919ed-4f44-5740-a13a-40f41935fe31>
2010-08-31 15:32:28 +0000

    Added sample for Csla.Threading.BackgroundWorker in WindowsForms bugid: 839

---
+++ Samples/NET/cs/BackgroundWorkerDemo/BackgroundWorkerDemo/Security/MyIdentity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackgroundWorkerDemo.Security
+{
+  [Serializable]
+  public class MyIdentity : Csla.Security.CslaIdentity
+  {
+    public MyIdentity(string name)
+    {
+      this.Name = name;
+    }
+  }
+}

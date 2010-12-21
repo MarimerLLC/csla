033b97e934413c5b5b41456a67f56ce320a036f9
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2010-12-21 20:50:05 +0000

    Add base DAL project.

---
+++ Samples/NET/cs/ProjectTrackercs/ProjectTracker.Dal/DalFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectTracker.Dal
+{
+  public static class DalFactory
+  {
+    private static IDalFactory _factory;
+    public static IDalFactory FactoryInstance
+    {
+      get
+      {
+        if (_factory == null)
+        {
+          var dalTypeName = System.Configuration.ConfigurationManager.AppSettings["DalTypeName"];
+          var dalType = Type.GetType(dalTypeName);
+          _factory = (IDalFactory)Activator.CreateInstance(dalType);
+        }
+        return _factory;
+      }
+      set
+      {
+        _factory = value;
+      }
+    }
+
+    public static T GetDal<T>()
+    {
+      return FactoryInstance.GetDal<T>();
+    }
+  }
+}

572b52dc0238fb3b4f2f39cc4fdc02b4d06dc93f
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2011-03-25 17:24:38 +0000

    Rename folder. bugid: 912

--- Samples/NET/cs/SimpleNTier/SilverlightUI/App.xaml.cs
+++ Samples/NET/cs/SimpleNTier/SilverlightUI/App.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.ServiceModel;
 
 namespace SilverlightUI
 {

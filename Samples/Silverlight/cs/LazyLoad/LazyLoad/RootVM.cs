81e73644417274d536cf42c72546168600257cbe
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2011-07-22 21:15:24 +0000

    Add LazyLoad sample.

---
+++ Samples/Silverlight/cs/LazyLoad/LazyLoad/RootVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Csla.Xaml;
+
+namespace SilverlightApplication9
+{
+  public class RootVM : ViewModel<Root>
+  {
+    public RootVM()
+    {
+      if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+        BeginRefresh("GetRoot");
+    }
+
+    protected override void OnError(Exception error)
+    {
+      base.OnError(error);
+    }
+
+    protected override void OnRefreshed()
+    {
+      base.OnRefreshed();
+    }
+  }
+}

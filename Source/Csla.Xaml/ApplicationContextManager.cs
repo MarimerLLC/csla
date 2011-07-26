ae435659523472a6acc949040b765f8f94632548
JonnyBee <JonnyBee@fa5919ed-4f44-5740-a13a-40f41935fe31>
2011-07-26 08:10:08 +0000

    Added documentation to public properties bugid: 944

--- Source/Csla.Xaml/ApplicationContextManager.cs
+++ Source/Csla.Xaml/ApplicationContextManager.cs
@@ -19,11 +19,20 @@ namespace Csla.Xaml
 
     private static IPrincipal _principal;
 
+    /// <summary>
+    /// Gets a value indicating whether this
+    /// context manager is valid for use in
+    /// the current environment.
+    /// </summary>
     public bool IsValid
     {
       get { return true; }
     }
 
+    /// <summary>
+    /// Gets the current principal.
+    /// </summary>
+    /// <returns></returns>
     public IPrincipal GetUser()
     {
       IPrincipal current;
@@ -43,6 +52,10 @@ namespace Csla.Xaml
       return current;
     }
 
+    /// <summary>
+    /// Sets the current principal.
+    /// </summary>
+    /// <param name="principal">Principal object.</param>
     public void SetUser(IPrincipal principal)
     {
       if (System.Windows.Application.Current != null)
@@ -50,18 +63,30 @@ namespace Csla.Xaml
       Thread.CurrentPrincipal = principal;
     }
 
+    /// <summary>
+    /// Gets the local context.
+    /// </summary>
+    /// <returns></returns>
     public ContextDictionary GetLocalContext()
     {
       LocalDataStoreSlot slot = Thread.GetNamedDataSlot(_localContextName);
       return (ContextDictionary)Thread.GetData(slot);
     }
 
+    /// <summary>
+    /// Sets the local context.
+    /// </summary>
+    /// <param name="localContext">Local context.</param>
     public void SetLocalContext(ContextDictionary localContext)
     {
       LocalDataStoreSlot slot = Thread.GetNamedDataSlot(_localContextName);
       Thread.SetData(slot, localContext);
     }
 
+    /// <summary>
+    /// Gets the client context.
+    /// </summary>
+    /// <returns></returns>
     public ContextDictionary GetClientContext()
     {
       if (ApplicationContext.ExecutionLocation == ApplicationContext.ExecutionLocations.Client)
@@ -75,6 +100,10 @@ namespace Csla.Xaml
       }
     }
 
+    /// <summary>
+    /// Sets the client context.
+    /// </summary>
+    /// <param name="clientContext">Client context.</param>
     public void SetClientContext(ContextDictionary clientContext)
     {
       if (ApplicationContext.ExecutionLocation == ApplicationContext.ExecutionLocations.Client)
@@ -88,12 +117,20 @@ namespace Csla.Xaml
       }
     }
 
+    /// <summary>
+    /// Gets the global context.
+    /// </summary>
+    /// <returns></returns>
     public ContextDictionary GetGlobalContext()
     {
       LocalDataStoreSlot slot = Thread.GetNamedDataSlot(_globalContextName);
       return (ContextDictionary)Thread.GetData(slot);
     }
 
+    /// <summary>
+    /// Sets the global context.
+    /// </summary>
+    /// <param name="globalContext">Global context.</param>
     public void SetGlobalContext(ContextDictionary globalContext)
     {
       LocalDataStoreSlot slot = Thread.GetNamedDataSlot(_globalContextName);

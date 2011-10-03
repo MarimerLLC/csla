d820a8439aa59d006349536ee07daca642deedb0
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2011-10-03 03:38:40 +0000

    Mark SL and WP7 CSLA projects as CLS compliant. bugid: 903

--- Source/Csla.Silverlight/Properties/AssemblyInfo.cs
+++ Source/Csla.Silverlight/Properties/AssemblyInfo.cs
@@ -22,6 +22,9 @@ using System.Diagnostics;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Mark the assembly as CLS compliant
+[assembly: System.CLSCompliant(true)]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.

14b9ecc6fb2cd72a430feb26c831aabb6a9bd30a
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2011-10-15 23:27:47 +0000

    Further work to complete ProjectDetails functionality. bugid: 912

---
+++ Samples/NET/cs/ProjectTracker/WpUI/ViewModels/ProjectEdit.cs
@@ -0,0 +1,18 @@
+﻿namespace WpUI.ViewModels
+{
+  public class ProjectEdit : ViewModel<ProjectTracker.Library.ProjectGetter>
+  {
+    public ProjectEdit(string queryString)
+    {
+      var p = queryString.Split('=');
+      var projectId = int.Parse(p[1]);
+      ManageObjectLifetime = false;
+      BeginRefresh(callback => ProjectTracker.Library.ProjectGetter.GetExistingProject(projectId, callback));
+    }
+
+    internal void Close()
+    {
+      Bxf.Shell.Instance.ShowView(null, "Dialog");
+    }
+  }
+}

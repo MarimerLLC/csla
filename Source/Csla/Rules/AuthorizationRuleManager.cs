16bf0a2c9067737dd21f1de0aed811666c5d710b
jonnybee <jonnybee@fa5919ed-4f44-5740-a13a-40f41935fe31>
2011-12-27 07:42:27 +0000

    Removed BindingFlags.FlattenHierarchy so that AddObjectAuthorizationRules must always exist on the actual implemented class. AddObjectAuthorizationrules is a static method that must exist on actual class. This may be a breaking change in existing code. bugid: 952

--- Source/Csla/Rules/AuthorizationRuleManager.cs
+++ Source/Csla/Rules/AuthorizationRuleManager.cs
@@ -77,8 +77,7 @@ namespace Csla.Rules
             try
             {
               // invoke method to add auth roles
-              var flags = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic |
-                          BindingFlags.FlattenHierarchy;
+              var flags = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
               System.Reflection.MethodInfo method = type.GetMethod("AddObjectAuthorizationRules", flags);
               if (method != null)
                 method.Invoke(null, null);

3b251b316635f7f5b6c81630756b8883caf6e6c3
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2012-01-23 20:43:56 +0000

    Update to match ProjectList implementation. bugid: 912

--- Samples/NET/cs/ProjectTracker/WpUI/ViewModels/ResourceList.cs
+++ Samples/NET/cs/ProjectTracker/WpUI/ViewModels/ResourceList.cs
@@ -29,7 +29,7 @@ namespace WpUI.ViewModels
           return null;
         else
           return new ObservableCollection<ResourceInfo>(
-            Model.Select(r => new ResourceInfo(r, this)));
+            Model.Select(r => new ResourceInfo(r)));
       }
     }
 
@@ -52,50 +52,14 @@ namespace WpUI.ViewModels
 
     public class ResourceInfo : ViewModelLocal<ProjectTracker.Library.ResourceInfo>
     {
-      public ResourceList Parent { get; private set; }
-
-      public ResourceInfo(ProjectTracker.Library.ResourceInfo info, ResourceList parent)
+      public ResourceInfo(ProjectTracker.Library.ResourceInfo info)
       {
-        Parent = parent;
         Model = info;
-      }
-
-      public bool CanEdit
-      {
-        get { return Csla.Rules.BusinessRules.HasPermission(Csla.Rules.AuthorizationActions.EditObject, typeof(ProjectTracker.Library.ResourceEdit)); }
-      }
-
-      public new bool CanRemove
-      {
-        get { return Csla.Rules.BusinessRules.HasPermission(Csla.Rules.AuthorizationActions.DeleteObject, typeof(ProjectTracker.Library.ResourceEdit)); }
       }
 
       public void DisplayItem()
       {
         Bxf.Shell.Instance.ShowView("/ResourceDisplay.xaml?id=" + Model.Id, null, null, null);
-      }
-
-      public void EditItem()
-      {
-        Bxf.Shell.Instance.ShowView("/ResourceEdit.xaml?id=" + Model.Id, null, null, null);
-      }
-
-      public void RemoveItem()
-      {
-        Bxf.Shell.Instance.ShowStatus(new Bxf.Status { IsBusy = true, Text = "Deleting item..." });
-        ProjectTracker.Library.ResourceEdit.DeleteResource(Model.Id, (o, e) =>
-        {
-          if (e.Error != null)
-          {
-            Bxf.Shell.Instance.ShowError(e.Error.Message, "Failed to delete item");
-            Bxf.Shell.Instance.ShowStatus(new Bxf.Status { IsOk = false, Text = "Item NOT deleted" });
-          }
-          else
-          {
-            Parent.Model.RemoveChild(Model.Id);
-            Bxf.Shell.Instance.ShowStatus(new Bxf.Status { Text = "Item deleted" });
-          }
-        });
       }
     }
   }

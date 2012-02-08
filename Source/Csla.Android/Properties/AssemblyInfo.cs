ed710cccdd230a5c95a132e61c6690d5cd457e06
kevinf <kevinf@fa5919ed-4f44-5740-a13a-40f41935fe31>
2012-02-08 14:08:53 +0000

    Changes to support remote data portal. bugid: 1017

--- Source/Csla.Android/Properties/AssemblyInfo.cs
+++ Source/Csla.Android/Properties/AssemblyInfo.cs
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.3.0.0")]
 [assembly: AssemblyFileVersion("4.3.0.0")]
+[assembly: AssemblyKeyFileAttribute("CslaKey.snk")]
+

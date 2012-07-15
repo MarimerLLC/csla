3af40b9d71a2cbd62c54e014b3b341cd09d44ea5
Jaans <Jaans@fa5919ed-4f44-5740-a13a-40f41935fe31>
2012-07-15 11:54:15 +0000

    Created Csla-Templates NuGet package for purposes of installing snippets and templates. bugid: 1076

---
+++ NuGet/Definition/tools/uninstall.ps1
@@ -0,0 +1,24 @@
+param($installPath, $toolsPath, $package, $project)
+
+	$vsVersions = @("2010", "2012")
+	$cslaFolder = "Csla"
+	$destinationDocumentsRoot = [System.Environment]::GetFolderPath( "MyDocuments" );
+
+	# Uninstall for all versions of VS defined
+	foreach ($vsVersion in $vsVersions)
+	{
+		# VS Version test - Test if User's Visual Studio destination path exists and skip if it doesn't
+		$destinationUserVisualStudio = Join-Path $destinationDocumentsRoot "\Visual Studio $vsVersion"
+		if ((Test-Path $destinationUserVisualStudio) -ne $true) { break }
+		
+		$destinationSnippetsFolderCS = Join-Path $destinationUserVisualStudio "\Code Snippets\Visual C#\My Code Snippets\$cslaFolder"
+		$destinationSnippetsFolderVB = Join-Path $destinationUserVisualStudio "\Code Snippets\Visual Basic\My Code Snippets\$cslaFolder"
+		$destinationItemsFolderCS = Join-Path $destinationUserVisualStudio "\Templates\ItemTemplates\Visual C#\$cslaFolder"
+		$destinationItemsFolderVB = Join-Path $destinationUserVisualStudio "\Templates\ItemTemplates\Visual Basic\$cslaFolder"
+
+		Write-Host "Uninstalling templates and snippets for $cslaFolder from Visual Studio $vsVersion"
+		Remove-Item -Recurse -Force $destinationSnippetsFolderCS
+		Remove-Item -Recurse -Force $destinationSnippetsFolderVB
+		Remove-Item -Recurse -Force $destinationItemsFolderCS
+		Remove-Item -Recurse -Force $destinationItemsFolderVB
+	}

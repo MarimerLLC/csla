10b0ad1595ea1b6f245a1541c91106c2d7b935c9
jonnybee <jonnybee@fa5919ed-4f44-5740-a13a-40f41935fe31>
2012-10-08 16:17:10 +0000

    Added missing files in solution. bugid: 1018

---
+++ Samples/NET/cs/RuleTutorial/RuleBaseClasses/AuthorizationRuleNoCacheResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Csla.Rules;
+
+namespace RuleBaseClasses
+{
+  public abstract class AuthorizationRuleNoCacheResult : AuthorizationRule
+  {
+    public AuthorizationRuleNoCacheResult(AuthorizationActions action, Csla.Core.IMemberInfo element) : base(action, element)
+    {
+      CacheResult = false;
+    }
+  }
+}

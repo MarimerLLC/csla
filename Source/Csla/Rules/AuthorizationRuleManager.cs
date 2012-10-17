0ff1b6e7521a21eac4a06b1b59c358ba6fc9ac13
jonnybee <jonnybee@fa5919ed-4f44-5740-a13a-40f41935fe31>
2012-10-17 06:03:18 +0000

    Rolled back changes to Authz as this causes StackOverflow Exception. bugid: 1107

--- Source/Csla/Rules/AuthorizationRuleManager.cs
+++ Source/Csla/Rules/AuthorizationRuleManager.cs
@@ -77,10 +77,10 @@ namespace Csla.Rules
         lock (mgr)
           if (!mgr.InitializedPerType)
           {
+            mgr.InitializedPerType = true;
             // Only call AddObjectAuthorizationRules when there are no rules for this type
             if (RulesExistForType(type))
             {
-              mgr.InitializedPerType = true;
               return;
             }
 
@@ -91,7 +91,7 @@ namespace Csla.Rules
               System.Reflection.MethodInfo method = type.GetMethod("AddObjectAuthorizationRules", flags);
               if (method != null)
                 method.Invoke(null, null);
-              mgr.InitializedPerType = true;
+
             }
             catch (Exception)
             {

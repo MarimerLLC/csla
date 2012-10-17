bc07c2d69f0be11c8933b6d72af4f2c5e032d4ef
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2012-10-17 22:23:15 +0000

    Version 4.5.4 release candidate build. bugid: 1113

--- Source/Csla.test/Properties/AssemblyInfo.cs
+++ Source/Csla.test/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.3.1.0")]
-[assembly: AssemblyFileVersion("4.3.1.0")]
+[assembly: AssemblyVersion("4.5.4.0")]
+[assembly: AssemblyFileVersion("4.5.4.0")]

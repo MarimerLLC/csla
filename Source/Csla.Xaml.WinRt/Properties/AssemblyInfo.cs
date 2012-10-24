be4f56ce635e469de895555975b70ed09a8cb6d7
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2012-10-24 20:58:49 +0000

    Version 4.5.10 release. bugid: 1119

--- Source/Csla.Xaml.WinRt/Properties/AssemblyInfo.cs
+++ Source/Csla.Xaml.WinRt/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.5.4.0")]
-[assembly: AssemblyFileVersion("4.5.4.0")]
+[assembly: AssemblyVersion("4.5.10.0")]
+[assembly: AssemblyFileVersion("4.5.10.0")]
 
 [assembly: ComVisible(false)]

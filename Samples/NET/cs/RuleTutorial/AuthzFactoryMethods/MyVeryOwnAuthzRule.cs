3506773be6dd83b44768284f6359f6f867879c95
jonnybee <jonnybee@fa5919ed-4f44-5740-a13a-40f41935fe31>
2012-11-13 07:23:49 +0000

    Added missing files to RuleTutorial bugid: 1018

---
+++ Samples/NET/cs/RuleTutorial/AuthzFactoryMethods/MyVeryOwnAuthzRule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csla;
+using Csla.Core;
+using Csla.Rules;
+using Csla.Rules.CommonRules;
+
+namespace AuthzFactoryMethods
+{
+  public class MyVeryOwnObjectAuthzRule : AuthorizationRule
+  {
+    private readonly IPropertyInfo _prop1;
+    private readonly IPropertyInfo _prop2;
+
+    public MyVeryOwnObjectAuthzRule(AuthorizationActions action, PropertyInfo<string> prop1, PropertyInfo<string> prop2)
+      : base(action)
+    {
+      _prop1 = prop1;
+      _prop2 = prop2;
+    }
+
+    protected override void Execute(AuthorizationContext context)
+    {
+      if (context.Target != null)
+      {
+        var val1 = (string)ReadProperty(context.Target, _prop1);
+        var val2 = (string)ReadProperty(context.Target, _prop2);
+
+        // add my own if tests here 
+      }
+    }
+  }
+}

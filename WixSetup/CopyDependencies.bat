bda041d4f2b0098ee3d6a7dd4ed602bf65d89af5
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2013-02-05 23:58:46 +0000

    Add initial Wix setup project. bugid: 1138

---
+++ WixSetup/CopyDependencies.bat
@@ -0,0 +1,22 @@
+copy ..\bin\release\net\Csla.dll ..\Samples\Dependencies\Net
+copy ..\bin\release\net\Csla.Web.dll ..\Samples\Dependencies\Net
+copy ..\bin\release\net\Csla.Web.Mvc.dll ..\Samples\Dependencies\Net
+copy ..\bin\release\net\Csla.Windows.dll ..\Samples\Dependencies\Net
+copy ..\bin\release\net\Csla.Xaml.dll ..\Samples\Dependencies\Net
+copy ..\bin\release\net\Csla*.xml ..\Samples\Dependencies\Net
+
+copy ..\bin\release\net4\Csla.dll ..\Samples\Dependencies\Net4
+copy ..\bin\release\net4\Csla.Web.dll ..\Samples\Dependencies\Net4
+copy ..\bin\release\net4\Csla.Web.Mvc.dll ..\Samples\Dependencies\Net4
+copy ..\bin\release\net4\Csla.Windows.dll ..\Samples\Dependencies\Net4
+copy ..\bin\release\net4\Csla.Xaml.dll ..\Samples\Dependencies\Net4
+copy ..\bin\release\net4\Csla*.xml ..\Samples\Dependencies\Net4
+
+copy ..\bin\release\Silverlight\Csla.dll ..\Samples\Dependencies\sl
+copy ..\bin\release\Silverlight\Csla.Xaml.dll ..\Samples\Dependencies\sl
+copy ..\bin\release\Silverlight\Csla*.xml ..\Samples\Dependencies\sl
+
+copy ..\bin\release\WinRT\Csla.dll ..\Samples\Dependencies\WinRT
+copy ..\bin\release\WinRT\Csla.Xaml.dll ..\Samples\Dependencies\WinRT
+copy ..\bin\release\WinRT\Csla*.xml ..\Samples\Dependencies\WinRT
+copy ..\bin\release\WinRT\Csla*.pri ..\Samples\Dependencies\WinRT

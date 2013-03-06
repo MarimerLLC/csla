878a656f4025fc63c7c3a2de10d46c9687fb4cde
Rockford Lhotka <rocky@lhotka.net>
2013-03-06 00:23:17 -0600

    #59 Fix version numbers of WinPRT projects

--- Source/Csla.Xaml.WinPRT/Properties/AssemblyInfo.cs
+++ Source/Csla.Xaml.WinPRT/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.5.4.0")]
-[assembly: AssemblyFileVersion("4.5.4.0")]
+[assembly: AssemblyVersion("4.5.13.0")]
+[assembly: AssemblyFileVersion("4.5.13.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]

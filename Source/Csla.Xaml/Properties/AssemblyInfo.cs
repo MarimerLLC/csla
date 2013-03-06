bade7fc4cb605c8f9d53a8c3c168ed18ca6b90a1
Rockford Lhotka <rocky@lhotka.net>
2013-03-06 09:05:42 -0600

    #59 Update version numbers, dependencies, and nuget definitions

--- Source/Csla.Xaml/Properties/AssemblyInfo.cs
+++ Source/Csla.Xaml/Properties/AssemblyInfo.cs
@@ -44,9 +44,8 @@ using System.Windows.Markup;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.5.13.0")]
-[assembly: AssemblyFileVersion("4.5.13.0")]
+[assembly: AssemblyVersion("4.5.14.0")]
+[assembly: AssemblyFileVersion("4.5.14.0")]
 
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.SourceAssembly, ResourceDictionaryLocation.SourceAssembly)]

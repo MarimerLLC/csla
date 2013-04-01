1716780fca6c9a85e76019915e78d42d9875ca10
Nicolay Velizhanin <inickvel@live.com>
2013-04-01 09:06:06 +0600

    Added standard header to the CancelableViewModel.cs file

--- Source/Csla.Xaml/CancelableViewModel.cs
+++ Source/Csla.Xaml/CancelableViewModel.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="CancelableViewModel.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: http://www.lhotka.net/cslanet/
+// </copyright>
+// <summary>Base class used to create CancelableViewModel objects,</summary>
+//-----------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 
 using Csla.Reflection;

029f2e5c84b5470d405707562d39f363880354f3
Rockford Lhotka <rocky@lhotka.net>
2013-05-09 23:03:49 -0500

    #107 Update version and copyright information.

--- Source/Csla.Xaml/Properties/AssemblyInfo.cs
+++ Source/Csla.Xaml/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Windows.Markup;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Marimer LLC")]
 [assembly: AssemblyProduct("CSLA .NET")]
-[assembly: AssemblyCopyright("Copyright © 2010-12 Marimer LLC")]
+[assembly: AssemblyCopyright("Copyright © 2010-13 Marimer LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -45,8 +45,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.3.13.0")]
-[assembly: AssemblyFileVersion("4.3.13.0")]
+[assembly: AssemblyVersion("4.3.14.0")]
+[assembly: AssemblyFileVersion("4.3.14.0")]
 
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.SourceAssembly, ResourceDictionaryLocation.SourceAssembly)]

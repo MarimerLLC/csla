5eaf649c429fe94e5bd7be07d55ff10333d83fc8
Kevin E. Ford <Bowman74@hotmail.com>
2013-05-11 15:47:48 -0400

    #10 and #132.  Update to CSLA 4.5 with async and fixes for WCF access.  Currently need Mono for Android beta to work with .Net 4.5 features.

--- Source/Csla.Android/Properties/AssemblyInfo.cs
+++ Source/Csla.Android/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Marimer LLC")]
 [assembly: AssemblyProduct("CSLA .NET")]
-[assembly: AssemblyCopyright("Copyright © 2010-11 Marimer LLC")]
+[assembly: AssemblyCopyright("Copyright © 2010-13 Marimer LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.3.10.0")]
-[assembly: AssemblyFileVersion("4.3.10.0")]
+[assembly: AssemblyVersion("4.5.24.0")]
+[assembly: AssemblyFileVersion("4.5.24.0")]
 [assembly: AssemblyKeyFileAttribute("CslaKey.snk")]
 

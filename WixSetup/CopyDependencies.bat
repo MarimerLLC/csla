9358312ce1e31e70d7be5303682c1de8d6f85150
Rockford Lhotka <rocky@lhotka.net>
2013-11-01 15:51:03 -0500

    #212 Version 4.5.491 prerelease.

--- WixSetup/CopyDependencies.bat
+++ WixSetup/CopyDependencies.bat
@@ -1,31 +1,43 @@
 copy ..\bin\release\net\Csla.dll ..\Samples\Dependencies\Net
 copy ..\bin\release\net\Csla.Web.dll ..\Samples\Dependencies\Net
-copy ..\bin\release\net\Csla.Web.Mvc3.dll ..\Samples\Dependencies\Net
 copy ..\bin\release\net\Csla.Web.Mvc4.dll ..\Samples\Dependencies\Net
+copy ..\bin\release\net\Csla.Web.Mvc5.dll ..\Samples\Dependencies\Net
 copy ..\bin\release\net\Csla.Data.EF4.dll ..\Samples\Dependencies\Net
 copy ..\bin\release\net\Csla.Data.EF5.dll ..\Samples\Dependencies\Net
+copy ..\bin\release\net\Csla.Data.EF6.dll ..\Samples\Dependencies\Net
 copy ..\bin\release\net\Csla.Windows.dll ..\Samples\Dependencies\Net
 copy ..\bin\release\net\Csla.Xaml.dll ..\Samples\Dependencies\Net
+copy ..\bin\release\net\Csla.Validation.dll ..\Samples\Dependencies\Net
 copy ..\bin\release\net\Csla*.xml ..\Samples\Dependencies\Net
 
 copy ..\bin\release\net4\Csla.dll ..\Samples\Dependencies\Net4
 copy ..\bin\release\net4\Csla.Web.dll ..\Samples\Dependencies\Net4
-copy ..\bin\release\net4\Csla.Web.Mvc3.dll ..\Samples\Dependencies\Net4
 copy ..\bin\release\net4\Csla.Web.Mvc4.dll ..\Samples\Dependencies\Net4
 copy ..\bin\release\net4\Csla.Data.EF4.dll ..\Samples\Dependencies\Net
+copy ..\bin\release\net4\Csla.Data.EF5.dll ..\Samples\Dependencies\Net
+copy ..\bin\release\net4\Csla.Data.EF6.dll ..\Samples\Dependencies\Net
 copy ..\bin\release\net4\Csla.Windows.dll ..\Samples\Dependencies\Net4
 copy ..\bin\release\net4\Csla.Xaml.dll ..\Samples\Dependencies\Net4
+copy ..\bin\release\net4\Csla.Validation.dll ..\Samples\Dependencies\Net4
 copy ..\bin\release\net4\Csla*.xml ..\Samples\Dependencies\Net4
 
 copy ..\bin\release\Silverlight\Csla.dll ..\Samples\Dependencies\sl
 copy ..\bin\release\Silverlight\Csla.Xaml.dll ..\Samples\Dependencies\sl
+copy ..\bin\release\Silverlight\Csla.Validation.dll ..\Samples\Dependencies\sl
 copy ..\bin\release\Silverlight\Csla*.xml ..\Samples\Dependencies\sl
 
 copy ..\bin\release\WinRT\Csla.dll ..\Samples\Dependencies\WinRT
 copy ..\bin\release\WinRT\Csla.Xaml.dll ..\Samples\Dependencies\WinRT
+copy ..\bin\release\WinRT\Csla.Validation.dll ..\Samples\Dependencies\WinRT
 copy ..\bin\release\WinRT\Csla*.xml ..\Samples\Dependencies\WinRT
 copy ..\bin\release\WinRT\Csla*.pri ..\Samples\Dependencies\WinRT
 
 copy ..\bin\release\WinPRT\Csla.dll ..\Samples\Dependencies\WinPRT
 copy ..\bin\release\WinPRT\Csla.Xaml.dll ..\Samples\Dependencies\WinPRT
+copy ..\bin\release\WinPRT\Csla.Validation.dll ..\Samples\Dependencies\WinPRT
 copy ..\bin\release\WinPRT\Csla*.xml ..\Samples\Dependencies\WinPRT
+
+copy ..\bin\release\Android\Csla.dll ..\Samples\Dependencies\Android
+copy ..\bin\release\Android\Csla.Axml.dll ..\Samples\Dependencies\Android
+copy ..\bin\release\Android\Csla.Validation.dll ..\Samples\Dependencies\Android
+copy ..\bin\release\Android\Csla*.xml ..\Samples\Dependencies\Android

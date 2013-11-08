3009db23e4cf61f506371c52a7dbbd180d1534a4
Rockford Lhotka <rocky@lhotka.net>
2013-11-08 00:12:46 -0600

    Add non-functional MVC 5 UI experimental code.

--- Samples/NET/cs/ProjectTracker/MvcUI/Controllers/HomeController.cs
+++ Samples/NET/cs/ProjectTracker/MvcUI/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace MvcUI.Controllers
 {
+  [CustomAuthentication]
   public class HomeController : Controller
   {
     public ActionResult Index()
--- Samples/NET/cs/ProjectTracker/MvcUI/Global.asax.cs
+++ Samples/NET/cs/ProjectTracker/MvcUI/Global.asax.cs
@@ -8,14 +8,14 @@ using System.Web.Routing;
 
 namespace MvcUI
 {
-    public class MvcApplication : System.Web.HttpApplication
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    protected void Application_Start()
     {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
+      AreaRegistration.RegisterAllAreas();
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
     }
+  }
 }

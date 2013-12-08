90c1b09a82b1f4883fd3dbfe9f21b413b8a05462
Rockford Lhotka <rocky@lhotka.net>
2013-12-07 22:30:47 -0600

    #238 Start updating the SimpleApp.sln for iOS

--- Samples/Ios/cs/SimpleApp/CustomerEdit.cs
+++ Samples/Ios/cs/SimpleApp/CustomerEdit.cs
@@ -4,229 +4,201 @@ using System.Linq;
 using System.Text;
 using Csla;
 using Csla.Serialization;
-using MonoTouch.Dialog;
+using System.Threading.Tasks;
 
-namespace Library
+namespace SimpleApp
 {
   [Serializable]
   public class CustomerEdit : BusinessBase<CustomerEdit>
   {
-    #region Business Methods
-
-    private static PropertyInfo<int> IdProperty = RegisterProperty<int> (c => c.Id);
-    public int Id {
-      get { return GetProperty (IdProperty); }
-      set { SetProperty (IdProperty, value); }
-    }
-
-    private static PropertyInfo<string> NameProperty = RegisterProperty<string> (c => c.Name);
-    [Entry("Customer name")]
-    public string Name {
-      get { return GetProperty (NameProperty); }
-      set { SetProperty (NameProperty, value); }
-    }
-
-    private static PropertyInfo<DateTime> BirthDateProperty = RegisterProperty<DateTime> (c => c.BirthDate);
-    [Caption("Date of Birth")]
-    [Date]
-    public DateTime BirthDate {
-      get { return GetProperty (BirthDateProperty); }
-      set { SetProperty (BirthDateProperty, value); }
-    }
-
-    private static PropertyInfo<string> StatusProperty = RegisterProperty<string> (c => c.Status);
-    public string Status {
-      get { return GetProperty (StatusProperty); }
-      set { SetProperty (StatusProperty, value); }
-    }
-
-    #endregion
-
-    #region Business Rules
-
-    protected override void AddBusinessRules ()
+    private static PropertyInfo<int> IdProperty = RegisterProperty<int>(c => c.Id);
+    public int Id
     {
-      base.AddBusinessRules ();
-      BusinessRules.AddRule (new Csla.Rules.CommonRules.Required (NameProperty));
-      BusinessRules.AddRule (new StringOnlyLetters { PrimaryProperty = NameProperty });
-      BusinessRules.AddRule (new ToUpper (NameProperty));
-      BusinessRules.AddRule (new CheckBirthDate (StatusProperty, BirthDateProperty));
+      get { return GetProperty(IdProperty); }
+      set { SetProperty(IdProperty, value); }
+    }
+
+    private static PropertyInfo<string> NameProperty = RegisterProperty<string>(c => c.Name);
+    public string Name
+    {
+      get { return GetProperty(NameProperty); }
+      set { SetProperty(NameProperty, value); }
+    }
+
+    private static PropertyInfo<DateTime> BirthDateProperty = RegisterProperty<DateTime>(c => c.BirthDate);
+    public DateTime BirthDate
+    {
+      get { return GetProperty(BirthDateProperty); }
+      set { SetProperty(BirthDateProperty, value); }
+    }
+
+    private static PropertyInfo<string> StatusProperty = RegisterProperty<string>(c => c.Status);
+    public string Status
+    {
+      get { return GetProperty(StatusProperty); }
+      set { SetProperty(StatusProperty, value); }
+    }
+
+    protected override void AddBusinessRules()
+    {
+      base.AddBusinessRules();
+      BusinessRules.AddRule(new Csla.Rules.CommonRules.Required(NameProperty));
+      BusinessRules.AddRule(new StringOnlyLetters { PrimaryProperty = NameProperty });
+      BusinessRules.AddRule(new ToUpper(NameProperty));
+      BusinessRules.AddRule(new CheckBirthDate(StatusProperty, BirthDateProperty));
       //BusinessRules.AddRule(new StringOnlyLettersAsync(NameProperty));
-      
-      BusinessRules.AddRule (new Csla.Rules.CommonRules.IsInRole (Csla.Rules.AuthorizationActions.WriteProperty, IdProperty, "None"));
-    }
 
-    public override void BeginSave (bool forceUpdate, EventHandler<Csla.Core.SavedEventArgs> handler, object userState)
-    {
-      base.BeginSave (forceUpdate, handler, userState);
+      BusinessRules.AddRule(new Csla.Rules.CommonRules.IsInRole(Csla.Rules.AuthorizationActions.WriteProperty, IdProperty, "None"));
     }
 
     private class StringOnlyLetters : Csla.Rules.BusinessRule
     {
-      protected override void Execute (Csla.Rules.RuleContext context)
+      protected override void Execute(Csla.Rules.RuleContext context)
       {
         var ce = (CustomerEdit)context.Target;
-        bool result = string.IsNullOrEmpty (ce.Name) || !(from c in ce.Name.ToCharArray ()
-          where char.IsDigit (c)
-          select c).Any ();
+        bool result = string.IsNullOrEmpty(ce.Name) || !(from c in ce.Name.ToCharArray()
+                                                         where char.IsDigit(c)
+                                                         select c).Any();
         if (!result)
-          context.AddErrorResult ("Name must consist of only letters.");
+          context.AddErrorResult("Name must consist of only letters.");
       }
     }
 
     public class ToUpper : Csla.Rules.BusinessRule
     {
-      public ToUpper (Csla.Core.IPropertyInfo primaryProperty) : base(primaryProperty)
+      public ToUpper(Csla.Core.IPropertyInfo primaryProperty)
+        : base(primaryProperty)
       {
         InputProperties = new List<Csla.Core.IPropertyInfo> { PrimaryProperty };
       }
-      protected override void Execute (Csla.Rules.RuleContext context)
+      protected override void Execute(Csla.Rules.RuleContext context)
       {
-        var input = context.InputPropertyValues[PrimaryProperty].ToString ();
-        if (!string.IsNullOrEmpty (input)) {
-          context.AddOutValue (PrimaryProperty, input.ToUpper ());
+        var input = context.InputPropertyValues[PrimaryProperty].ToString();
+        if (!string.IsNullOrEmpty(input))
+        {
+          context.AddOutValue(PrimaryProperty, input.ToUpper());
         }
       }
     }
 
     public class CheckBirthDate : Csla.Rules.BusinessRule
     {
-      public CheckBirthDate (Csla.Core.IPropertyInfo primaryProperty, Csla.Core.IPropertyInfo birthDateProperty) : base(primaryProperty)
+      public CheckBirthDate(Csla.Core.IPropertyInfo primaryProperty, Csla.Core.IPropertyInfo birthDateProperty)
+        : base(primaryProperty)
       {
-        InputProperties = new List<Csla.Core.IPropertyInfo> ();
-        InputProperties.Add (birthDateProperty);
+        InputProperties = new List<Csla.Core.IPropertyInfo>();
+        InputProperties.Add(birthDateProperty);
       }
 
-      protected override void Execute (Csla.Rules.RuleContext context)
+      protected override void Execute(Csla.Rules.RuleContext context)
       {
-        var birthDateProperty = context.InputPropertyValues.Single (p => p.Key.Name == "BirthDateProperty");
-        
+        var birthDateProperty = context.InputPropertyValues.Single(p => p.Key.Name == "BirthDateProperty");
+
         var status = "Unknown Date of Birth";
-        if (birthDateProperty.Value != null) {
+        if (birthDateProperty.Value != null)
+        {
           var birthDate = (DateTime)birthDateProperty.Value;
-          if (birthDate.CompareTo (DateTime.Today) == 0) {
+          if (birthDate.CompareTo(DateTime.Today) == 0)
+          {
             status = "Happy Birthday";
           }
-          else {
+          else
+          {
             status = "It's not your birthday";
           }
         }
-        
-        context.AddOutValue (PrimaryProperty, status);
-        
+
+        context.AddOutValue(PrimaryProperty, status);
+
       }
     }
-    #endregion
-    #region Factory Methods
 
-    #if SILVERLIGHT
-    public static void BeginNewCustomer (DataPortal.ProxyModes proxyMode, EventHandler<DataPortalResult<CustomerEdit>> callback)
+    public static void NewCustomerEdit(EventHandler<DataPortalResult<CustomerEdit>> callback)
     {
-      var dp = new DataPortal<CustomerEdit> (DataPortal.ProxyModes.LocalOnly);
-      dp.CreateCompleted += callback;
-      dp.BeginCreate ();
+      DataPortal.BeginCreate<CustomerEdit>(callback);
     }
 
-    public static void BeginGetCustomer (DataPortal.ProxyModes proxyMode, EventHandler<DataPortalResult<CustomerEdit>> callback)
+    public static void GetCustomerEdit(int id, EventHandler<DataPortalResult<CustomerEdit>> callback)
     {
-      var dp = new DataPortal<CustomerEdit> ();
-      dp.FetchCompleted += callback;
-      dp.BeginFetch ();
+      DataPortal.BeginFetch<CustomerEdit>(id, callback);
     }
 
-    [Obsolete("For use by MobileFormatter")]
-    public CustomerEdit ()
+    public static async Task<CustomerEdit> NewCustomerEditAsync()
     {
-      // required by MobileFormatter
+      return await DataPortal.CreateAsync<CustomerEdit>();
     }
 
-    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
-    public override void DataPortal_Create (Csla.DataPortalClient.LocalProxy<CustomerEdit>.CompletedHandler handler)
+    public static async Task<CustomerEdit> GetCustomerEditAsync(int id)
     {
-      var bw = new System.ComponentModel.BackgroundWorker ();
-      bw.DoWork += (s, e) => { e.Result = e.Argument; };
-      bw.RunWorkerCompleted += (s, e) =>
+      return await DataPortal.FetchAsync<CustomerEdit>(id);
+    }
+
+#if !SILVERLIGHT && !WINDOWS_PHONE && !NETFX_CORE
+
+    public static CustomerEdit NewCustomerEdit()
+    {
+      return DataPortal.Create<CustomerEdit>();
+    }
+
+    public static CustomerEdit GetCustomerEdit(int id)
+    {
+      return DataPortal.Fetch<CustomerEdit>(id);
+    }
+
+    public static void DeleteCustomerEdit(int id)
+    {
+      DataPortal.Delete<CustomerEdit>(id);
+    }
+
+#endif
+
+    protected override void DataPortal_Create()
+    {
+      using (BypassPropertyChecks)
       {
-        try {
-          using (BypassPropertyChecks) {
-            BirthDate = DateTime.Today;
-            Status = "Created " + ApplicationContext.ExecutionLocation.ToString ();
-          }
-          CreateComplete (handler);
-        }
-        catch (Exception ex) {
-          handler (this, ex);
-        }
-      };
-      bw.RunWorkerAsync ();
-    }
-
-    private void CreateComplete (Csla.DataPortalClient.LocalProxy<CustomerEdit>.CompletedHandler handler)
-    {
-      base.DataPortal_Create (handler);
-    }
-    #else
-    public static CustomerEdit GetCustomer (int id)
-    {
-      return DataPortal.Fetch<CustomerEdit> (new SingleCriteria<CustomerEdit, int> (id));
-    }
-
-    private CustomerEdit ()
-    {
-    }
-    /* require use of factory methods */
-    #endif
-
-    #endregion
-
-    #region Data Access
-
-    #if !SILVERLIGHT
-    protected override void DataPortal_Create ()
-    {
-      using (BypassPropertyChecks) {
-        Status = "Created " + ApplicationContext.ExecutionLocation.ToString ();
+        Status = "Created " + ApplicationContext.ExecutionLocation.ToString();
       }
-      base.DataPortal_Create ();
+      base.DataPortal_Create();
     }
 
-    private void DataPortal_Fetch (SingleCriteria<CustomerEdit, int> criteria)
+    private void DataPortal_Fetch(SingleCriteria<CustomerEdit, int> criteria)
     {
-      System.Threading.Thread.Sleep (1500);
-      using (BypassPropertyChecks) {
+      System.Threading.Thread.Sleep(1500);
+      using (BypassPropertyChecks)
+      {
         Id = criteria.Value;
         Name = "Test " + criteria.Value;
-        Status = "Retrieved " + ApplicationContext.ExecutionLocation.ToString ();
+        Status = "Retrieved " + ApplicationContext.ExecutionLocation.ToString();
       }
     }
 
-    protected override void DataPortal_Insert ()
+    protected override void DataPortal_Insert()
     {
-      System.Threading.Thread.Sleep (1500);
-      
-      using (BypassPropertyChecks) {
+      System.Threading.Thread.Sleep(1500);
+
+      using (BypassPropertyChecks)
+      {
         Id = 987;
-        Status = "Inserted " + ApplicationContext.ExecutionLocation.ToString ();
+        Status = "Inserted " + ApplicationContext.ExecutionLocation.ToString();
       }
     }
 
-    protected override void DataPortal_Update ()
+    protected override void DataPortal_Update()
     {
-      System.Threading.Thread.Sleep (1500);
-      using (BypassPropertyChecks) {
-        Status = "Updated " + ApplicationContext.ExecutionLocation.ToString ();
+      System.Threading.Thread.Sleep(1500);
+      using (BypassPropertyChecks)
+      {
+        Status = "Updated " + ApplicationContext.ExecutionLocation.ToString();
       }
     }
 
-    protected override void DataPortal_DeleteSelf ()
+    protected override void DataPortal_DeleteSelf()
     {
-      System.Threading.Thread.Sleep (1500);
-      using (BypassPropertyChecks) {
-        Status = "Deleted " + ApplicationContext.ExecutionLocation.ToString ();
+      System.Threading.Thread.Sleep(1500);
+      using (BypassPropertyChecks)
+      {
+        Status = "Deleted " + ApplicationContext.ExecutionLocation.ToString();
       }
     }
-    #endif
-    
-    #endregion
   }
 }
--- Samples/Ios/cs/SimpleApp/Main.cs
+++ Samples/Ios/cs/SimpleApp/Main.cs
@@ -1,9 +1,7 @@
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Csla;
-using MonoTouch.Dialog;
+
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
@@ -11,67 +9,12 @@ namespace SimpleApp
 {
   public class Application
   {
-    static void Main (string[] args)
+    // This is the main entry point of the application.
+    static void Main(string[] args)
     {
-      UIApplication.Main (args);
-      Csla.DataPortal.ProxyTypeName = "Local";
+      // if you want to use a different Application Delegate class from "AppDelegate"
+      // you can specify it here.
+      UIApplication.Main(args, null, "AppDelegate");
     }
   }
-
-  // The name AppDelegate is referenced in the MainWindow.xib file.
-  public partial class AppDelegate : UIApplicationDelegate
-  {
-    private DialogViewController customerViewController;
-    // This method is invoked when the application has loaded its UI and its ready to run
-    public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-    {
-
-      window.AddSubview (navigationController.View);
-      
-      Library.CustomerEdit.BeginNewCustomer (Csla.DataPortal.ProxyModes.LocalOnly, (o, e) =>
-      {
-        if (e.Error == null) {
-          var customer = e.Object;
-          // Now make sure we invoke on the main thread the updates
-          navigationController.InvokeOnMainThread (delegate {
-            var context = new BindingContext (this, customer, "Simple App");
-            customerViewController = new DialogViewController (context.Root, true);
-            // When the view goes out of screen, we fetch the data.
-            customerViewController.ViewDissapearing += delegate {
-              // This reflects the data back to the object instance
-              context.Fetch ();
-              Console.WriteLine ("Customer Name: {0}", customer.Name);
-              Console.WriteLine ("Date of Birth: {0}", customer.BirthDate);
-              Console.WriteLine ("Status: {0}", customer.Status);
-            };
-            navigationController.PushViewController (customerViewController, true);
-          });
-          
-        }
-
-        
-        else {
-          throw e.Error;
-        }
-        
-      });
-      
-      window.MakeKeyAndVisible ();
-      
-      return true;
-    }
-
-    // This method is required in iPhoneOS 3.0
-    public override void OnActivated (UIApplication application)
-    {
-    }
-    
-  }
-  /*
-    public override void WillTerminate (UIApplication application)
-    {
-      //Save data here
-    }
-    */  
 }
-

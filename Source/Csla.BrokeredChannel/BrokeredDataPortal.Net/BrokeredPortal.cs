8137cb41aae1d2f2c56104cc27be74783130a884
Rockford Lhotka <rocky@lhotka.net>
2014-04-30 12:56:37 -0500

    #269 Initial implementation of brokered data portal channel.

---
+++ Source/Csla.BrokeredChannel/BrokeredDataPortal.Net/BrokeredPortal.cs
@@ -0,0 +1,91 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BrokeredDataPortal.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: http://www.lhotka.net/cslanet/
+// </copyright>
+// <summary>Implements a data portal host</summary>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csla.Server;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace BrokeredDataPortal
+{
+  /// <summary>
+  /// Data portal host implemented as a brokered assembly
+  /// for use by side-loaded WinRT client apps.
+  /// </summary>
+  public sealed class BrokeredPortal
+  {
+    /// <summary>
+    /// Create and initialize a business object.
+    /// </summary>
+    /// <param name="objectType">Type of business object to create.</param>
+    /// <param name="criteriaData">Criteria object describing business object.</param>
+    /// <param name="contextData">
+    /// Server.DataPortalContext object passed to the server.
+    /// </param>
+    public byte[] Create(Type objectType, [ReadOnlyArray] byte[] criteriaData, [ReadOnlyArray] byte[] contextData)
+    {
+      var criteria = Csla.Serialization.Mobile.MobileFormatter.Deserialize(criteriaData);
+      var context = (DataPortalContext)Csla.Serialization.Mobile.MobileFormatter.Deserialize(contextData);
+      var portal = new DataPortal();
+      var result = portal.Create(objectType, criteria, context, true).Result;
+      return Csla.Serialization.Mobile.MobileFormatter.Serialize(result);
+    }
+
+    /// <summary>
+    /// Get an existing business object.
+    /// </summary>
+    /// <param name="objectType">Type of business object to retrieve.</param>
+    /// <param name="criteriaData">Criteria object describing business object.</param>
+    /// <param name="contextData">
+    /// Server.DataPortalContext object passed to the server.
+    /// </param>
+    public byte[] Fetch(Type objectType, [ReadOnlyArray] byte[] criteriaData, [ReadOnlyArray] byte[] contextData)
+    {
+      var criteria = Csla.Serialization.Mobile.MobileFormatter.Deserialize(criteriaData);
+      var context = (DataPortalContext)Csla.Serialization.Mobile.MobileFormatter.Deserialize(contextData);
+      var portal = new DataPortal();
+      var result = portal.Fetch(objectType, criteria, context, true).Result;
+      return Csla.Serialization.Mobile.MobileFormatter.Serialize(result);
+    }
+
+    /// <summary>
+    /// Update a business object.
+    /// </summary>
+    /// <param name="objectData">Business object to update.</param>
+    /// <param name="contextData">
+    /// Server.DataPortalContext object passed to the server.
+    /// </param>
+    public byte[] Update([ReadOnlyArray] byte[] objectData, [ReadOnlyArray] byte[] contextData)
+    {
+      var obj = Csla.Serialization.Mobile.MobileFormatter.Deserialize(objectData);
+      var context = (DataPortalContext)Csla.Serialization.Mobile.MobileFormatter.Deserialize(contextData);
+      var portal = new DataPortal();
+      var result = portal.Update(obj, context, true).Result;
+      return Csla.Serialization.Mobile.MobileFormatter.Serialize(result);
+    }
+
+    /// <summary>
+    /// Delete a business object.
+    /// </summary>
+    /// <param name="objectType">Type of business object to create.</param>
+    /// <param name="criteriaData">Criteria object describing business object.</param>
+    /// <param name="contextData">
+    /// Server.DataPortalContext object passed to the server.
+    /// </param>
+    public byte[] Delete(Type objectType, [ReadOnlyArray] byte[] criteriaData, [ReadOnlyArray] byte[] contextData)
+    {
+      var criteria = Csla.Serialization.Mobile.MobileFormatter.Deserialize(criteriaData);
+      var context = (DataPortalContext)Csla.Serialization.Mobile.MobileFormatter.Deserialize(contextData);
+      var portal = new DataPortal();
+      var result = portal.Delete(objectType, criteria, context, true).Result;
+      return Csla.Serialization.Mobile.MobileFormatter.Serialize(result);
+    }
+  }
+}
---
+++ Source/Csla.BrokeredChannel/BrokeredProxy/BrokeredProxy.cs
@@ -0,0 +1,133 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BrokeredProxy.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: http://www.lhotka.net/cslanet/
+// </copyright>
+// <summary>Implements a data portal proxy to relay data portal</summary>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csla.Server;
+
+namespace BrokeredProxy
+{
+  /// <summary>
+  /// Implements a data portal proxy to relay data portal
+  /// calls to an application server hosted locally 
+  /// in the client process and AppDomain.
+  /// </summary>
+  public class BrokeredProxy : Csla.DataPortalClient.IDataPortalProxy
+  {
+    /// <summary>
+    /// Get a value indicating whether this proxy will invoke
+    /// a remote data portal server, or run the "server-side"
+    /// data portal in the caller's process and AppDomain.
+    /// </summary>
+    public bool IsServerRemote
+    {
+      get { return true; }
+    }
+
+    /// <summary>
+    /// Called by the client-side DataPortal to create a
+    /// new business object.
+    /// </summary>
+    /// <param name="objectType">Type of business object to create.</param>
+    /// <param name="criteria">Criteria object describing business object.</param>
+    /// <param name="context">Server.DataPortalContext object passed to the server.</param>
+    /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
+    public async Task<DataPortalResult> Create(Type objectType, object criteria, DataPortalContext context, bool isSync)
+    {
+      var task = Task.Run<DataPortalResult>(() =>
+        {
+          var contextData = Csla.Serialization.Mobile.MobileFormatter.Serialize(context);
+          var criteriaData = Csla.Serialization.Mobile.MobileFormatter.Serialize(criteria);
+          var portal = new BrokeredDataPortal.BrokeredPortal();
+          var resultData = portal.Create(objectType, criteriaData, contextData);
+          var result = (DataPortalResult)Csla.Serialization.Mobile.MobileFormatter.Deserialize(resultData);
+          return result;
+        });
+      if (isSync)
+        return task.Result;
+      else
+        return await task;
+    }
+
+    /// <summary>
+    /// Called by the client-side DataPortal to fetch a
+    /// business object.
+    /// </summary>
+    /// <param name="objectType">Type of business object to fetch.</param>
+    /// <param name="criteria">Criteria object describing business object.</param>
+    /// <param name="context">Server.DataPortalContext object passed to the server.</param>
+    /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
+    public async Task<DataPortalResult> Fetch(Type objectType, object criteria, DataPortalContext context, bool isSync)
+    {
+      var task = Task.Run<DataPortalResult>(() =>
+      {
+        var contextData = Csla.Serialization.Mobile.MobileFormatter.Serialize(context);
+        var criteriaData = Csla.Serialization.Mobile.MobileFormatter.Serialize(criteria);
+        var portal = new BrokeredDataPortal.BrokeredPortal();
+        var resultData = portal.Fetch(objectType, criteriaData, contextData);
+        var result = (DataPortalResult)Csla.Serialization.Mobile.MobileFormatter.Deserialize(resultData);
+        return result;
+      });
+      if (isSync)
+        return task.Result;
+      else
+        return await task;
+    }
+
+    /// <summary>
+    /// Called by the client-side DataPortal to update a
+    /// business object.
+    /// </summary>
+    /// <param name="obj">Business object to update.</param>
+    /// <param name="context">Server.DataPortalContext object passed to the server.</param>
+    /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
+    public async Task<DataPortalResult> Update(object obj, DataPortalContext context, bool isSync)
+    {
+      var task = Task.Run<DataPortalResult>(() =>
+      {
+        var contextData = Csla.Serialization.Mobile.MobileFormatter.Serialize(context);
+        var objectData = Csla.Serialization.Mobile.MobileFormatter.Serialize(obj);
+        var portal = new BrokeredDataPortal.BrokeredPortal();
+        var resultData = portal.Update(objectData, contextData);
+        var result = (DataPortalResult)Csla.Serialization.Mobile.MobileFormatter.Deserialize(resultData);
+        return result;
+      });
+      if (isSync)
+        return task.Result;
+      else
+        return await task;
+    }
+
+    /// <summary>
+    /// Called by the client-side DataPortal to delete a
+    /// business object.
+    /// </summary>
+    /// <param name="objectType">Type of business object to delete.</param>
+    /// <param name="criteria">Criteria object describing business object.</param>
+    /// <param name="context">Server.DataPortalContext object passed to the server.</param>
+    /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
+    public async Task<DataPortalResult> Delete(Type objectType, object criteria, DataPortalContext context, bool isSync)
+    {
+      var task = Task.Run<DataPortalResult>(() =>
+      {
+        var contextData = Csla.Serialization.Mobile.MobileFormatter.Serialize(context);
+        var criteriaData = Csla.Serialization.Mobile.MobileFormatter.Serialize(criteria);
+        var portal = new BrokeredDataPortal.BrokeredPortal();
+        var resultData = portal.Delete(objectType, criteriaData, contextData);
+        var result = (DataPortalResult)Csla.Serialization.Mobile.MobileFormatter.Deserialize(resultData);
+        return result;
+      });
+      if (isSync)
+        return task.Result;
+      else
+        return await task;
+    }
+  }
+}

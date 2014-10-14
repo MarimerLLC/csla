323b0fd411756f0ca65a02d1238f3f2a6c19af40
Miguel Bernard <mbernard@sybersphere.com>
2014-10-14 19:52:55 -0400

    Fix tab space from 4 to 2

--- Source/Csla/DataPortalClient/LocalProxy.cs
+++ Source/Csla/DataPortalClient/LocalProxy.cs
@@ -12,113 +12,113 @@ using Csla.Threading;
 
 namespace Csla.DataPortalClient
 {
+  /// <summary>
+  /// Implements a data portal proxy to relay data portal
+  /// calls to an application server hosted locally 
+  /// in the client process and AppDomain.
+  /// </summary>
+  public class LocalProxy : DataPortalClient.IDataPortalProxy
+  {
+    private Server.IDataPortalServer _portal =
+      new Server.DataPortal();
+
     /// <summary>
-    /// Implements a data portal proxy to relay data portal
-    /// calls to an application server hosted locally 
-    /// in the client process and AppDomain.
+    /// Called by <see cref="DataPortal" /> to create a
+    /// new business object.
     /// </summary>
-    public class LocalProxy : DataPortalClient.IDataPortalProxy
+    /// <param name="objectType">Type of business object to create.</param>
+    /// <param name="criteria">Criteria object describing business object.</param>
+    /// <param name="context">
+    /// <see cref="Server.DataPortalContext" /> object passed to the server.
+    /// </param>
+    /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
+    public async Task<DataPortalResult> Create(
+      Type objectType, object criteria, DataPortalContext context, bool isSync)
     {
-        private Server.IDataPortalServer _portal =
-          new Server.DataPortal();
-
-        /// <summary>
-        /// Called by <see cref="DataPortal" /> to create a
-        /// new business object.
-        /// </summary>
-        /// <param name="objectType">Type of business object to create.</param>
-        /// <param name="criteria">Criteria object describing business object.</param>
-        /// <param name="context">
-        /// <see cref="Server.DataPortalContext" /> object passed to the server.
-        /// </param>
-        /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
-        public async Task<DataPortalResult> Create(
-          Type objectType, object criteria, DataPortalContext context, bool isSync)
-        {
-            if (isSync || Csla.ApplicationContext.LogicalExecutionLocation == ApplicationContext.LogicalExecutionLocations.Server)
-            {
-                return await _portal.Create(objectType, criteria, context, isSync);
-            }
-            else
-            {
-                return await _portal.Create(objectType, criteria, context, isSync).ConfigureScheduler(new CslaTaskScheduler());
-            }
-        }
-
-        /// <summary>
-        /// Called by <see cref="DataPortal" /> to load an
-        /// existing business object.
-        /// </summary>
-        /// <param name="objectType">Type of business object to retrieve.</param>
-        /// <param name="criteria">Criteria object describing business object.</param>
-        /// <param name="context">
-        /// <see cref="Server.DataPortalContext" /> object passed to the server.
-        /// </param>
-        /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
-        public async Task<DataPortalResult> Fetch(Type objectType, object criteria, DataPortalContext context, bool isSync)
-        {
-            if (isSync || Csla.ApplicationContext.LogicalExecutionLocation == ApplicationContext.LogicalExecutionLocations.Server)
-            {
-                return await _portal.Fetch(objectType, criteria, context, isSync);
-            }
-            else
-            {
-                return await _portal.Fetch(objectType, criteria, context, isSync).ConfigureScheduler(new CslaTaskScheduler());
-            }
-        }
-
-        /// <summary>
-        /// Called by <see cref="DataPortal" /> to update a
-        /// business object.
-        /// </summary>
-        /// <param name="obj">The business object to update.</param>
-        /// <param name="context">
-        /// <see cref="Server.DataPortalContext" /> object passed to the server.
-        /// </param>
-        /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
-        public async Task<DataPortalResult> Update(object obj, DataPortalContext context, bool isSync)
-        {
-            if (isSync || Csla.ApplicationContext.LogicalExecutionLocation == ApplicationContext.LogicalExecutionLocations.Server)
-            {
-                return await _portal.Update(obj, context, isSync);
-            }
-            else
-            {
-                return await _portal.Update(obj, context, isSync).ConfigureScheduler(new CslaTaskScheduler());
-            }
-        }
-
-        /// <summary>
-        /// Called by <see cref="DataPortal" /> to delete a
-        /// business object.
-        /// </summary>
-        /// <param name="objectType">Type of business object to create.</param>
-        /// <param name="criteria">Criteria object describing business object.</param>
-        /// <param name="context">
-        /// <see cref="Server.DataPortalContext" /> object passed to the server.
-        /// </param>
-        /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
-        public async Task<DataPortalResult> Delete(Type objectType, object criteria, DataPortalContext context, bool isSync)
-        {
-            if (isSync || Csla.ApplicationContext.LogicalExecutionLocation == ApplicationContext.LogicalExecutionLocations.Server)
-            {
-                return await _portal.Delete(objectType, criteria, context, isSync);
-            }
-            else
-            {
-                return await _portal.Delete(objectType, criteria, context, isSync).ConfigureScheduler(new CslaTaskScheduler());
-            }
-        }
-
-        /// <summary>
-        /// Get a value indicating whether this proxy will invoke
-        /// a remote data portal server, or run the "server-side"
-        /// data portal in the caller's process and AppDomain.
-        /// </summary>
-        public bool IsServerRemote
-        {
-            get { return false; }
-        }
-
+      if (isSync || Csla.ApplicationContext.LogicalExecutionLocation == ApplicationContext.LogicalExecutionLocations.Server)
+      {
+        return await _portal.Create(objectType, criteria, context, isSync);
+      }
+      else
+      {
+        return await _portal.Create(objectType, criteria, context, isSync).ConfigureScheduler(new CslaTaskScheduler());
+      }
     }
+
+    /// <summary>
+    /// Called by <see cref="DataPortal" /> to load an
+    /// existing business object.
+    /// </summary>
+    /// <param name="objectType">Type of business object to retrieve.</param>
+    /// <param name="criteria">Criteria object describing business object.</param>
+    /// <param name="context">
+    /// <see cref="Server.DataPortalContext" /> object passed to the server.
+    /// </param>
+    /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
+    public async Task<DataPortalResult> Fetch(Type objectType, object criteria, DataPortalContext context, bool isSync)
+    {
+      if (isSync || Csla.ApplicationContext.LogicalExecutionLocation == ApplicationContext.LogicalExecutionLocations.Server)
+      {
+        return await _portal.Fetch(objectType, criteria, context, isSync);
+      }
+      else
+      {
+        return await _portal.Fetch(objectType, criteria, context, isSync).ConfigureScheduler(new CslaTaskScheduler());
+      }
+    }
+
+    /// <summary>
+    /// Called by <see cref="DataPortal" /> to update a
+    /// business object.
+    /// </summary>
+    /// <param name="obj">The business object to update.</param>
+    /// <param name="context">
+    /// <see cref="Server.DataPortalContext" /> object passed to the server.
+    /// </param>
+    /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
+    public async Task<DataPortalResult> Update(object obj, DataPortalContext context, bool isSync)
+    {
+      if (isSync || Csla.ApplicationContext.LogicalExecutionLocation == ApplicationContext.LogicalExecutionLocations.Server)
+      {
+        return await _portal.Update(obj, context, isSync);
+      }
+      else
+      {
+        return await _portal.Update(obj, context, isSync).ConfigureScheduler(new CslaTaskScheduler());
+      }
+    }
+
+    /// <summary>
+    /// Called by <see cref="DataPortal" /> to delete a
+    /// business object.
+    /// </summary>
+    /// <param name="objectType">Type of business object to create.</param>
+    /// <param name="criteria">Criteria object describing business object.</param>
+    /// <param name="context">
+    /// <see cref="Server.DataPortalContext" /> object passed to the server.
+    /// </param>
+    /// <param name="isSync">True if the client-side proxy should synchronously invoke the server.</param>
+    public async Task<DataPortalResult> Delete(Type objectType, object criteria, DataPortalContext context, bool isSync)
+    {
+      if (isSync || Csla.ApplicationContext.LogicalExecutionLocation == ApplicationContext.LogicalExecutionLocations.Server)
+      {
+        return await _portal.Delete(objectType, criteria, context, isSync);
+      }
+      else
+      {
+        return await _portal.Delete(objectType, criteria, context, isSync).ConfigureScheduler(new CslaTaskScheduler());
+      }
+    }
+
+    /// <summary>
+    /// Get a value indicating whether this proxy will invoke
+    /// a remote data portal server, or run the "server-side"
+    /// data portal in the caller's process and AppDomain.
+    /// </summary>
+    public bool IsServerRemote
+    {
+      get { return false; }
+    }
+
+  }
 }
--- Source/Csla/Threading/TasksExtensions.cs
+++ Source/Csla/Threading/TasksExtensions.cs
@@ -6,136 +6,136 @@ using System.Threading.Tasks;
 
 namespace Csla.Threading
 {
+  /// <summary>
+  /// Represents extensions methods for Tasks.
+  /// </summary>
+  public static class TaskExtensions
+  {
+    #region Public Methods and Operators
+
     /// <summary>
-    /// Represents extensions methods for Tasks.
+    /// Configures the task with a task scheduler.
     /// </summary>
-    public static class TaskExtensions
+    /// <typeparam name="T">Type of the result</typeparam>
+    /// <param name="task">The task.</param>
+    /// <param name="scheduler">The scheduler.</param>
+    /// <returns>
+    /// Returns a new awaitable task with a specified task scheduler.
+    /// </returns>
+    public static TaskSchedulerAwaitable<T> ConfigureScheduler<T>(this Task<T> task, TaskScheduler scheduler)
     {
-        #region Public Methods and Operators
+      return new TaskSchedulerAwaitable<T>(task, scheduler);
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Represents a task scheduler awaitable.
+    /// </summary>
+    /// <typeparam name="T">Type of the result</typeparam>
+    public struct TaskSchedulerAwaitable<T>
+    {
+      #region Fields
+
+      private readonly TaskSchedulerAwaiter<T> _awaiter;
+
+      #endregion
+
+      #region Constructors and Destructors
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="TaskSchedulerAwaitable{T}"/> struct.
+      /// </summary>
+      /// <param name="task">The task.</param>
+      /// <param name="scheduler">The scheduler.</param>
+      public TaskSchedulerAwaitable(Task<T> task, TaskScheduler scheduler)
+      {
+        this._awaiter = new TaskSchedulerAwaiter<T>(task, scheduler);
+      }
+
+      #endregion
+
+      #region Public Methods and Operators
+
+      /// <summary>
+      /// Gets the awaiter.
+      /// </summary>
+      /// <returns>
+      /// Returns the awaiter.
+      /// </returns>
+      public TaskSchedulerAwaiter<T> GetAwaiter()
+      {
+        return this._awaiter;
+      }
+
+      #endregion
+
+      /// <summary>
+      /// Represents a task scheduler awaiter
+      /// </summary>
+      /// <typeparam name="TResult">Type of the result</typeparam>
+      public struct TaskSchedulerAwaiter<TResult> : INotifyCompletion
+      {
+        #region Fields
+
+        private readonly Task<TResult> _task;
+        private readonly TaskScheduler _scheduler;
+
+        #endregion
+
+        #region Constructors and Destructors
 
         /// <summary>
-        /// Configures the scheduler.
+        /// Initializes a new instance of the <see cref="TaskSchedulerAwaiter{TResult}"/> struct.
         /// </summary>
-        /// <typeparam name="T">Type of the result</typeparam>
         /// <param name="task">The task.</param>
         /// <param name="scheduler">The scheduler.</param>
-        /// <returns>
-        /// Returns a new awaitable task with specified scheduler.
-        /// </returns>
-        public static CustomTaskAwaitable<T> ConfigureScheduler<T>(this Task<T> task, TaskScheduler scheduler)
+        public TaskSchedulerAwaiter(Task<TResult> task, TaskScheduler scheduler)
         {
-            return new CustomTaskAwaitable<T>(task, scheduler);
+          this._task = task;
+          this._scheduler = scheduler;
         }
 
         #endregion
 
+        #region Public Properties
+
         /// <summary>
-        /// Represents a custom task awaitable.
+        /// Gets a value indicating whether this instance is completed.
         /// </summary>
-        /// <typeparam name="T">Type of the result</typeparam>
-        public struct CustomTaskAwaitable<T>
+        public bool IsCompleted
         {
-            #region Fields
-
-            private readonly CustomTaskAwaiter<T> _awaitable;
-
-            #endregion
-
-            #region Constructors and Destructors
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="CustomTaskAwaitable{T}"/> struct.
-            /// </summary>
-            /// <param name="task">The task.</param>
-            /// <param name="scheduler">The scheduler.</param>
-            public CustomTaskAwaitable(Task<T> task, TaskScheduler scheduler)
-            {
-                this._awaitable = new CustomTaskAwaiter<T>(task, scheduler);
-            }
-
-            #endregion
-
-            #region Public Methods and Operators
-
-            /// <summary>
-            /// Gets the awaiter.
-            /// </summary>
-            /// <returns>
-            /// Returns the awaiter.
-            /// </returns>
-            public CustomTaskAwaiter<T> GetAwaiter()
-            {
-                return this._awaitable;
-            }
-
-            #endregion
-
-            /// <summary>
-            /// Represents a custom task awaiter
-            /// </summary>
-            /// <typeparam name="TResult">Type of the result</typeparam>
-            public struct CustomTaskAwaiter<TResult> : INotifyCompletion
-            {
-                #region Fields
-
-                private readonly Task<TResult> _task;
-                private readonly TaskScheduler _scheduler;
-
-                #endregion
-
-                #region Constructors and Destructors
-
-                /// <summary>
-                /// Initializes a new instance of the <see cref="CustomTaskAwaiter`1"/> struct.
-                /// </summary>
-                /// <param name="task">The task.</param>
-                /// <param name="scheduler">The scheduler.</param>
-                public CustomTaskAwaiter(Task<TResult> task, TaskScheduler scheduler)
-                {
-                    this._task = task;
-                    this._scheduler = scheduler;
-                }
-
-                #endregion
-
-                #region Public Properties
-
-                /// <summary>
-                /// Gets a value indicating whether this instance is completed.
-                /// </summary>
-                public bool IsCompleted
-                {
-                    get
-                    {
-                        return this._task.IsCompleted;
-                    }
-                }
-
-                #endregion
-
-                #region Public Methods and Operators
-
-                /// <summary>
-                /// Schedules the continuation action that's invoked when the instance completes.
-                /// </summary>
-                /// <param name="continuation">The action to invoke when the operation completes.</param>
-                public void OnCompleted(Action continuation)
-                {
-                    // ContinueWith sets the scheduler to use for the continuation action
-                    this._task.ContinueWith(x => continuation(), this._scheduler);
-                }
-
-                /// <summary>
-                /// Gets the result.
-                /// </summary>
-                /// <returns></returns>
-                public TResult GetResult()
-                {
-                    return this._task.Result;
-                }
-
-                #endregion
-            }
+          get
+          {
+            return this._task.IsCompleted;
+          }
         }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Schedules the continuation action that's invoked when the instance completes.
+        /// </summary>
+        /// <param name="continuation">The action to invoke when the operation completes.</param>
+        public void OnCompleted(Action continuation)
+        {
+          // ContinueWith sets the scheduler to use for the continuation action
+          this._task.ContinueWith(x => continuation(), this._scheduler);
+        }
+
+        /// <summary>
+        /// Gets the result.
+        /// </summary>
+        /// <returns>Returns the result.</returns>
+        public TResult GetResult()
+        {
+          return this._task.Result;
+        }
+
+        #endregion
+      }
     }
+  }
 }

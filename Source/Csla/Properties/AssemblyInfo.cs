7adf63d8ff25cecd4c42d6ad9e8ee4322a878d83
Rockford Lhotka <rocky@lhotka.net>
2015-01-06 11:45:56 -0600

    #300 and #298 - add new files to Android and iOS core CSLA projects.

--- Source/Csla/Properties/AssemblyInfo.cs
+++ Source/Csla/Properties/AssemblyInfo.cs
@@ -42,5 +42,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.5.601.0")]
-[assembly: AssemblyFileVersion("4.5.601.0")]
+[assembly: AssemblyVersion("4.5.680.0")]
+[assembly: AssemblyFileVersion("4.5.680.0")]

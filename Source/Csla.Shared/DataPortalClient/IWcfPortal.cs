1d6f5162cb20923fff142f91a67defd96e773793
Rockford Lhotka <rocky@lhotka.net>
2015-05-27 17:25:25 -0500

    #350 Use shared code project

---
+++ Source/Csla.Shared/DataPortalClient/IWcfPortal.cs
@@ -0,0 +1,48 @@
+﻿#if !NETFX_CORE
+#pragma warning disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csla.Server.Hosts.WcfChannel;
+using System.ServiceModel;
+
+namespace Csla.DataPortalClient
+{
+#if NET40
+#else
+  [System.ServiceModel.ServiceContractAttribute(Namespace = "http://ws.lhotka.net/WcfDataPortal", ConfigurationName = "WcfPortal.IWcfPortal")]
+  public interface IWcfPortal
+  {
+    [System.ServiceModel.OperationContractAttribute(Action = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/Create", ReplyAction = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/CreateResponse")]
+    [UseNetDataContract]
+    WcfResponse Create(CreateRequest request);
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/Create", ReplyAction = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/CreateResponse")]
+    System.Threading.Tasks.Task<WcfResponse> CreateAsync(CreateRequest request);
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/Fetch", ReplyAction = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/FetchResponse")]
+    [UseNetDataContract]
+    WcfResponse Fetch(FetchRequest request);
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/Fetch", ReplyAction = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/FetchResponse")]
+    System.Threading.Tasks.Task<WcfResponse> FetchAsync(FetchRequest request);
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/Update", ReplyAction = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/UpdateResponse")]
+    [UseNetDataContract]
+    WcfResponse Update(UpdateRequest request);
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/Update", ReplyAction = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/UpdateResponse")]
+    System.Threading.Tasks.Task<WcfResponse> UpdateAsync(UpdateRequest request);
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/Delete", ReplyAction = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/DeleteResponse")]
+    [UseNetDataContract]
+    WcfResponse Delete(DeleteRequest request);
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/Delete", ReplyAction = "http://ws.lhotka.net/WcfDataPortal/IWcfPortal/DeleteResponse")]
+    System.Threading.Tasks.Task<WcfResponse> DeleteAsync(DeleteRequest request);
+  }
+#endif
+}
+#endif

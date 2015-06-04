56834b53fee6dc28f595c0c4d72e56fc8f758ee1
Rockford Lhotka <rocky@lhotka.net>
2015-06-03 22:36:36 -0500

    #354 Add .NET 4.6 project for Web Forms

--- Source/Csla.Web.Net4.6/Properties/AssemblyInfo.cs
+++ Source/Csla.Web.Net4.6/Properties/AssemblyInfo.cs
@@ -14,16 +14,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Mark the assembly as CLS compliant
-[assembly: System.CLSCompliant(true)]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("80442eec-03d2-4041-a8c3-45ebfdc1e656")]
+[assembly: Guid("963430c1-39f4-4bb8-961d-185c5e813dab")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.6.001.0")]
 [assembly: AssemblyFileVersion("4.6.001.0")]

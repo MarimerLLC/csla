3153297246ce0277476b5064981d14622d3d8f79
Rockford Lhotka <rocky@lhotka.net>
2015-07-10 12:45:18 -0500

    #375 Update version numbers for release.

--- Source/Csla.Xaml.WinRt/Properties/AssemblyInfo.cs
+++ Source/Csla.Xaml.WinRt/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.6.001.0")]
-[assembly: AssemblyFileVersion("4.6.001.0")]
+[assembly: AssemblyVersion("4.6.002.0")]
+[assembly: AssemblyFileVersion("4.6.002.0")]
 
 [assembly: ComVisible(false)]

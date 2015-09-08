523d023238982c659f909c3dfc4d0ac76d6e1715
Rockford Lhotka <rocky@lhotka.net>
2015-09-08 15:38:06 -0500

    #418 Update version numbers.

--- Source/Csla.Shared/Serialization/SerializationAttribute.cs
+++ Source/Csla.Shared/Serialization/SerializationAttribute.cs
@@ -1,4 +1,4 @@
-﻿#if (!ANDROID && !IOS) && NETFX_CORE
+﻿#if NETFX_CORE
 //-----------------------------------------------------------------------
 // <copyright file="SerializationAttribute.cs" company="Marimer LLC">
 //     Copyright (c) Marimer LLC. All rights reserved.

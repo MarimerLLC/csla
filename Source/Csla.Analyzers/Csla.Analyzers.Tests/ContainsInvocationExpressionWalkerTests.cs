2065309c278a88db34d248e2309bc2e9167aef4d
Jason Bock <jason@jasonbock.net>
2015-09-13 10:24:02 -0500

    added tests for other walker

--- Source/Csla.Analyzers/Csla.Analyzers.Tests/ContainsInvocationExpressionWalkerTests.cs
+++ Source/Csla.Analyzers/Csla.Analyzers.Tests/ContainsInvocationExpressionWalkerTests.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Csla.Analyzers.Tests
 {
-  class ContainsInvocationExpressionWalkerTests
+  [TestClass]
+  public sealed class ContainsInvocationExpressionWalkerTests
   {
+    private static async Task<ContainsInvocationExpressionWalker> GetWalker(string path)
+    {
+      var code = File.ReadAllText(path);
+      var document = TestHelpers.Create(code);
+      var root = await document.GetSyntaxRootAsync();
+
+      return new ContainsInvocationExpressionWalker(root);
+    }
+
+    [TestMethod]
+    public async Task WalkWhenNodeHasNoInvocations()
+    {
+      var walker = await ContainsInvocationExpressionWalkerTests.GetWalker(
+        $@"Targets\{nameof(ContainsInvocationExpressionWalkerTests)}\{(nameof(this.WalkWhenNodeHasNoInvocations))}.cs");
+      Assert.IsFalse(walker.HasIssue);
+    }
+
+    [TestMethod]
+    public async Task WalkWhenNodeHasInvocation()
+    {
+      var walker = await ContainsInvocationExpressionWalkerTests.GetWalker(
+        $@"Targets\{nameof(ContainsInvocationExpressionWalkerTests)}\{(nameof(this.WalkWhenNodeHasInvocation))}.cs");
+      Assert.IsTrue(walker.HasIssue);
+    }
   }
 }

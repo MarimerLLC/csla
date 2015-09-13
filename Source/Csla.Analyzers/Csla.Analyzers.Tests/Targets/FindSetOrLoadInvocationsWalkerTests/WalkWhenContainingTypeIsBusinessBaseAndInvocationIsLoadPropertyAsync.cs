4e2ecb7de6d3f3b0fff700a38fa68d654d091098
Jason Bock <jason@jasonbock.net>
2015-09-13 09:13:11 -0500

    added tests for walkers

---
+++ Source/Csla.Analyzers/Csla.Analyzers.Tests/Targets/FindSetOrLoadInvocationsWalkerTests/WalkWhenContainingTypeIsBusinessBaseAndInvocationIsLoadPropertyAsync.cs
@@ -0,0 +1,11 @@
+﻿namespace Csla.Analyzers.Tests.Targets.FindSetOrLoadInvocationsWalkerTests
+{
+  public class WalkWhenContainingTypeIsBusinessBaseAndInvocationIsLoadPropertyAsync
+    : BusinessBase<WalkWhenContainingTypeIsBusinessBaseAndInvocationIsLoadPropertyAsync>
+  {
+    public void Go()
+    {
+      this.LoadPropertyAsync<int>(null, null);
+    }
+  }
+}
---
+++ Source/Csla.Analyzers/Csla.Analyzers.Tests/Targets/FindSetOrLoadInvocationsWalkerTests/WalkWhenContainingTypeIsBusinessBaseAndInvocationIsSetProperty.cs
@@ -0,0 +1,11 @@
+﻿namespace Csla.Analyzers.Tests.Targets.FindSetOrLoadInvocationsWalkerTests
+{
+  public class WalkWhenContainingTypeIsBusinessBaseAndInvocationIsSetProperty
+    : BusinessBase<WalkWhenContainingTypeIsBusinessBaseAndInvocationIsSetProperty>
+  {
+    public void Go()
+    {
+      this.SetProperty(null, null);
+    }
+  }
+}

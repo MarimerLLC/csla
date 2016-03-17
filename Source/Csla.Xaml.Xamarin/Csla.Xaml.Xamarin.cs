847941e6b6ae5243eaa6b03a9334e261c81182c3
Rockford Lhotka <rocky@lhotka.net>
2016-03-17 00:15:56 -0500

    #550 Create initial Csla.Xaml project for Xamarin Forms

---
+++ Source/Csla.Xaml.Xamarin/Csla.Xaml.Xamarin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Csla.Xaml.Xamarin
+{
+  public class CslaXamlXamarin : ContentPage
+  {
+    public CslaXamlXamarin()
+    {
+      var button = new Button
+      {
+        Text = "Click Me!",
+        VerticalOptions = LayoutOptions.CenterAndExpand,
+        HorizontalOptions = LayoutOptions.CenterAndExpand,
+      };
+
+      int clicked = 0;
+      button.Clicked += (s, e) => button.Text = "Clicked: " + clicked++;
+
+      Content = button;
+    }
+  }
+}

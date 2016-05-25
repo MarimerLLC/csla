103f6c01d64056418e621de8ac727111eb538c63
Rockford Lhotka <rocky@lhotka.net>
2016-05-25 10:47:55 -0500

    #505 Implement ProjectEdit XAML

--- Samples/ProjectTracker/ProjectTracker.Ui.Xamarin/XamarinFormsUi/ViewModels/ProjectEdit.cs
+++ Samples/ProjectTracker/ProjectTracker.Ui.Xamarin/XamarinFormsUi/ViewModels/ProjectEdit.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ProjectTracker.Library;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace XamarinFormsUi.ViewModels
 {
   public class ProjectEdit : ViewModel<ProjectTracker.Library.ProjectEdit>
   {
+    public ICommand SaveItemCommand { get; private set; }
+    public ICommand AssignResourceCommand { get; private set; }
+
     public int ProjectId { get; set; }
 
-    public ProjectEdit() { }
-
-    public ProjectEdit(int id)
+    public ProjectEdit()
     {
-      ProjectId = id;
+      SaveItemCommand = new Command(async () => await SaveAsync());
+      AssignResourceCommand = new Command(() => { });
     }
 
     protected override async Task<ProjectTracker.Library.ProjectEdit> DoInitAsync()

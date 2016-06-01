2360f86fd238ea10f9437ca7236537745d313ce3
Rockford Lhotka <rocky@lhotka.net>
2016-06-01 16:01:29 -0500

    #588 Update version numbers for 4.6.500

--- Source/Csla.Axml.Android/Resources/Resource.Designer.cs
+++ Source/Csla.Axml.Android/Resources/Resource.Designer.cs
@@ -40,11 +40,8 @@ namespace Csla.Axml
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f020001
-			public static int ApplicationName = 2130837505;
-			
 			// aapt resource value: 0x7f020000
-			public static int Hello = 2130837504;
+			public static int ApplicationName = 2130837504;
 			
 			static String()
 			{

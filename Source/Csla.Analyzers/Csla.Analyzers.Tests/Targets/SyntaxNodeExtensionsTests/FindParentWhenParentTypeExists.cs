64721c81a85e1ecfe162176157ad79ba957035bd
Jason Bock <jason@jasonbock.net>
2016-11-10 10:35:50 -0800

    Fixing567 (#663)
    * Getting closer....I'm no longer crashing after I type something after ".SaveAsync()"
    
    * This is fixed!

---
+++ Source/Csla.Analyzers/Csla.Analyzers.Tests/Targets/SyntaxNodeExtensionsTests/FindParentWhenParentTypeExists.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csla.Analyzers.Tests.Targets.SyntaxNodeExtensionsTests
+{
+  public class FindParentWhenParentTypeExists
+  {
+    public Guid NewGuid()
+    {
+      return Guid.NewGuid();
+    }
+  }
+}

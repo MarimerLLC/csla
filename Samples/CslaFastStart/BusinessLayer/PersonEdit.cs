2580f2a810ad3a88c21c8b8196145d055aeab17d
Rockford Lhotka <rocky@lhotka.net>
2016-12-10 22:59:59 -0600

    Add fast track solution

---
+++ Samples/CslaFastStart/BusinessLayer/PersonEdit.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Csla;
+using DataAccessLayer;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLayer
+{
+  [Serializable]
+  public class PersonEdit : BusinessBase<PersonEdit>
+  {
+    public static readonly PropertyInfo<int> IdProperty = RegisterProperty<int>(c => c.Id);
+    public int Id
+    {
+      get { return GetProperty(IdProperty); }
+      private set { LoadProperty(IdProperty, value); }
+    }
+
+    public static readonly PropertyInfo<string> FirstNameProperty = RegisterProperty<string>(c => c.FirstName);
+    [Required]
+    public string FirstName
+    {
+      get { return GetProperty(FirstNameProperty); }
+      set { SetProperty(FirstNameProperty, value); }
+    }
+
+    public static readonly PropertyInfo<string> LastNameProperty = RegisterProperty<string>(c => c.LastName);
+    [Required]
+    public string LastName
+    {
+      get { return GetProperty(LastNameProperty); }
+      set { SetProperty(LastNameProperty, value); }
+    }
+
+    protected override void DataPortal_Create()
+    {
+      var dal = new PersonDal();
+      var dto = dal.Create();
+      using (BypassPropertyChecks)
+      {
+        Id = dto.Id;
+        FirstName = dto.FirstName;
+        LastName = dto.LastName;
+      }
+      BusinessRules.CheckRules();
+    }
+
+    private void DataPortal_Fetch(int id)
+    {
+      var dal = new PersonDal();
+      var dto = dal.GetPerson(id);
+      using (BypassPropertyChecks)
+      {
+        Id = dto.Id;
+        FirstName = dto.FirstName;
+        LastName = dto.LastName;
+      }
+    }
+
+    protected override void DataPortal_Insert()
+    {
+      var dal = new PersonDal();
+      using (BypassPropertyChecks)
+      {
+        var dto = new PersonDto
+        {
+          FirstName = this.FirstName,
+          LastName = this.LastName
+        };
+        Id = dal.InsertPerson(dto);
+      }
+    }
+
+    protected override void DataPortal_Update()
+    {
+      var dal = new PersonDal();
+      using (BypassPropertyChecks)
+      {
+        var dto = new PersonDto
+        {
+          Id = this.Id,
+          FirstName = this.FirstName,
+          LastName = this.LastName
+        };
+        dal.UpdatePerson(dto);
+      }
+    }
+
+    private void DataPortal_Delete(int id)
+    {
+      var dal = new PersonDal();
+      using (BypassPropertyChecks)
+      {
+        dal.DeletePerson(id);
+      }
+    }
+
+    protected override void DataPortal_DeleteSelf()
+    {
+      DataPortal_Delete(Id);
+    }
+  }
+}

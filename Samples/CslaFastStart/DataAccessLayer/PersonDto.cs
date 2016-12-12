849ac8a60f8645c670377298cadbdaddd2912f0d
Rockford Lhotka <rocky@lhotka.net>
2016-12-12 14:59:58 -0500

    Add fast start sample and readme (#670)
    * Add fast track solution
    
    * Add readme content for sample.
    
    * Fix image paths.

---
+++ Samples/CslaFastStart/DataAccessLayer/PersonDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccessLayer
+{
+  public class PersonDto
+  {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+  }
+}

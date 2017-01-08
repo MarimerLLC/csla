0ef3ad3d4c76f2362907d9dfd75ac63322da614a
tfreitasleal <tfreitasleal@gmail.com>
2017-01-08 13:22:22 +0000

    Import of missing files.

---
+++ Samples/NET/cs/ExtendableWcfPortalForDotNet/Rolodex.WPF/Events/EditCompanyEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Rolodex.Silverlight.Events
+{
+    public class EditCompanyEventArgs : EventArgs
+    {
+        public int CompanyID { get; private set; }
+        private EditCompanyEventArgs() { }
+        public EditCompanyEventArgs(int companyID)
+        {
+            CompanyID = companyID;
+        }
+    }
+}

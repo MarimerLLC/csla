a7ef54786bc113459212700c5218781769eb23fc
Rockford Lhotka <rocky@lhotka.net>
2017-03-29 15:57:23 -0500

    Update version numbers to 4.6.600

--- Source/Csla.Analyzers/Csla.Analyzers/Properties/AssemblyInfo.cs
+++ Source/Csla.Analyzers/Csla.Analyzers/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Csla.Analyzers.Tests")]
 
-[assembly: AssemblyVersion("4.6.500.0")]
-[assembly: AssemblyFileVersion("4.6.500.0")]
+[assembly: AssemblyVersion("4.6.600.0")]
+[assembly: AssemblyFileVersion("4.6.600.0")]

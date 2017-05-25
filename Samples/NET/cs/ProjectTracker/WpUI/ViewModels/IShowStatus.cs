1af2e3516acef89916558da7648adbf0c6455b2d
Rockford Lhotka <rocky@lhotka.net>
2017-05-25 13:28:31 -0500

    Remove a lot of old or duplicate samples

--- Samples/NET/cs/ProjectTracker/WpUI/ViewModels/IShowStatus.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WpUI.ViewModels
-{
-  public interface IShowStatus
-  {
-    void ShowStatus(Bxf.Status status);
-  }
-}
--- Samples/Silverlight/cs/PrismDemo/Rolodex.Silverlight/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rolodex.Silverlight
-{
-    public static class Constants
-    {
-        public const string MainRegion = "MainRegion";
-        public const string MenuRegion = "MenuRegion";
-    }
-}

3f628e9363c4e2b37289f29b47d1dbb78adcc0b8
Rockford Lhotka <rocky@lhotka.net>
2017-07-19 16:22:07 -0500

    Update version numbers

--- Source/Csla.Axml.Android/Properties/AssemblyInfo.cs
+++ Source/Csla.Axml.Android/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.6.602.0")]
-[assembly: AssemblyFileVersion("4.6.602.0")]
+[assembly: AssemblyVersion("4.6.603.0")]
+[assembly: AssemblyFileVersion("4.6.603.0")]
--- Source/Csla.Windows.Net4/Properties/AssemblyInfo.cs
+++ Source/Csla.Windows.Net4/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.6.602.0")]
-[assembly: AssemblyFileVersion("4.6.602.0")]
+[assembly: AssemblyVersion("4.6.603.0")]
+[assembly: AssemblyFileVersion("4.6.603.0")]

0a65408fa1fdd152519e5ff9c0cdb102bc7b28e8
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-09-15 08:47:06 +0100

    ActionExtenderSample: fix copyright and instructions.

--- Samples/NET/cs/ActionExtenderSample/ActionExtenderSample.DataAccess/Properties/AssemblyInfo.cs
+++ Samples/NET/cs/ActionExtenderSample/ActionExtenderSample.DataAccess/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ActionExtenderSample.DataAccess")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Marimer LLC 2008-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

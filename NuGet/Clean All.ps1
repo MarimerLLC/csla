75140181306557b4588e91c6e7547ddf97c03ecf
Rockford Lhotka <rocky@lhotka.net>
2017-10-09 17:13:25 -0500

    #759 Add support for EntityFrameworkCore.

--- NuGet/Clean All.ps1
+++ NuGet/Clean All.ps1
@@ -17,7 +17,7 @@ try
     $originalBackground = $host.UI.RawUI.BackgroundColor
     $originalForeground = $host.UI.RawUI.ForegroundColor
     $originalLocation = Get-Location
-    $packages = @("Core", "XamarinForms", "AspNetCore MVC", "ASP.NET", "ASP.NET MVC 5", "ASP.NET MVC 4", "UWP", "Silverlight", "Windows Forms", "WPF", "MonoAndroid", "Data EF4", "Data EF5", "Data EF6", "Data EF7", "Templates", "UpdateValidation")
+    $packages = @("Core", "XamarinForms", "AspNetCore MVC", "ASP.NET", "ASP.NET MVC 5", "ASP.NET MVC 4", "Windows Forms", "UWP", "WPF", "MonoAndroid", "XamarinIos", "Data EF4", "Data EF5", "Data EF6",  "Data EF7", "Templates", "UpdateValidation", "Data EntityFrameworkCore")  
     
     $host.UI.RawUI.BackgroundColor = [System.ConsoleColor]::Black
     $host.UI.RawUI.ForegroundColor = [System.ConsoleColor]::White

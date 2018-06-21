aa43987933197a599cd8f2e454874b44bba36573
Rockford Lhotka <rocky@lhotka.net>
2018-06-21 17:34:14 -0500

    #900 Add CslaModelBinder for IBusinessBase types

--- Source/Csla.Web.Mvc.Shared/CslaModelBinder.cs
+++ Source/Csla.Web.Mvc.Shared/CslaModelBinder.cs
@@ -1,4 +1,3 @@
-#if !NETSTANDARD && !NETSTANDARD2_0
 //-----------------------------------------------------------------------
 // <copyright file="CslaModelBinder.cs" company="Marimer LLC">
 //     Copyright (c) Marimer LLC. All rights reserved.
@@ -6,6 +5,80 @@
 // </copyright>
 // <summary>Model binder for use with CSLA .NET editable business objects.</summary>
 //-----------------------------------------------------------------------
+#if NETSTANDARD2_0
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace mcmreporting.Pages.School
+{
+  /// <summary>
+  /// Model binder for use with CSLA .NET editable business
+  /// objects.
+  /// </summary>
+  public class CslaModelBinder : Csla.Server.ObjectFactory, IModelBinder
+  {
+    /// <summary>
+    /// Bind the form data to a new instance of an IBusinessBase object.
+    /// </summary>
+    /// <param name="bindingContext">Binding context</param>
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+      if (bindingContext == null)
+      {
+        throw new ArgumentNullException(nameof(bindingContext));
+      }
+
+      var result = Csla.Reflection.MethodCaller.CreateInstance(bindingContext.ModelType);
+      if (result == null)
+        return Task.CompletedTask;
+      var properties = Csla.Core.FieldManager.PropertyInfoManager.GetRegisteredProperties(bindingContext.ModelType);
+      foreach (var item in properties)
+      {
+        var index = $"{bindingContext.ModelName}.{item.Name}";
+        try
+        {
+          var value = bindingContext.ActionContext.HttpContext.Request.Form[index].FirstOrDefault();
+          if (!string.IsNullOrWhiteSpace(value))
+          {
+            if (item.Type.Equals(typeof(string)))
+              LoadProperty(result, item, value);
+            else
+              LoadProperty(result, item, Csla.Utilities.CoerceValue(item.Type, value.GetType(), null, value));
+          }
+        }
+        catch (Exception ex)
+        {
+          throw new Exception($"Could not map {index} to model", ex);
+        }
+      }
+
+      bindingContext.Result = ModelBindingResult.Success(result);
+      return Task.CompletedTask;
+    }
+  }
+
+  /// <summary>
+  /// Model binder provider that will use the CslaModelBinder for
+  /// any type that implements the IBusinessBase interface.
+  /// </summary>
+  public class CslaModelBinderProvider : IModelBinderProvider
+  {
+    /// <summary>
+    /// Gets the CslaModelBinder provider.
+    /// </summary>
+    /// <param name="context">Model binder provider context.</param>
+    public IModelBinder GetBinder(ModelBinderProviderContext context)
+    {
+      if (typeof(Csla.IBusinessBase).IsAssignableFrom(context.Metadata.ModelType))
+        return new CslaModelBinder();
+
+      return null;
+    }
+  }
+}
+#elif !NETSTANDARD
 using System;
 using System.Collections.Generic;
 using System.Linq;

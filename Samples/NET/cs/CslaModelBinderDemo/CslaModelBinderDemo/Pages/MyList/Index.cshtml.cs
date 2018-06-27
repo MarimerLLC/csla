8d126f002d7af4ce02c0c8df23ab1485cdcda314
Rockford Lhotka <rocky@lhotka.net>
2018-06-27 16:56:00 -0500

    Add AspNetCore CslaModelBinder sample app.

---
+++ Samples/NET/cs/CslaModelBinderDemo/CslaModelBinderDemo/Pages/MyList/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication3.Pages.MyList
+{
+  public class IndexModel : PageModel
+  {
+    [BindProperty]
+    public MyList DataList { get; set; }
+
+    public async Task OnGetAsync()
+    {
+      DataList = await Csla.DataPortal.FetchAsync<MyList>();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+      foreach (var item in DataList.Where(r => r.Remove).ToList())
+        DataList.Remove(item);
+      DataList = await DataList.SaveAsync();
+      return RedirectToPage("Index");
+    }
+  }
+}

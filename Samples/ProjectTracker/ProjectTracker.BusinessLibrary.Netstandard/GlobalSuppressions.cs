ee1bcd5b839946c5079a5dcc35c814699a7aea30
Rockford Lhotka <rocky@lhotka.net>
2018-09-28 12:31:52 -0500

    Update to use CSLA 4.8.1

--- Samples/ProjectTracker/ProjectTracker.BusinessLibrary.Netstandard/GlobalSuppressions.cs
+++ Samples/ProjectTracker/ProjectTracker.BusinessLibrary.Netstandard/GlobalSuppressions.cs
@@ -5,4 +5,11 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CSLA0010:Find Operation Arguments That Are Not Serializable", Justification = "<Pending>", Scope = "member", Target = "~M:ProjectTracker.Library.ProjectResourceEditCreator.DataPortal_Fetch(System.Int32)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CSLA0010:Find Operation Arguments That Are Not Serializable", Justification = "<Pending>", Scope = "member", Target = "~M:ProjectTracker.Library.ProjectEdit.DataPortal_Fetch(System.Int32)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CSLA0010:Find Operation Arguments That Are Not Serializable", Justification = "<Pending>", Scope = "member", Target = "~M:ProjectTracker.Library.ProjectEdit.DataPortal_Delete(System.Int32)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CSLA0010:Find Operation Arguments That Are Not Serializable", Justification = "<Pending>", Scope = "member", Target = "~M:ProjectTracker.Library.ProjectList.DataPortal_Fetch(System.String)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CSLA0010:Find Operation Arguments That Are Not Serializable", Justification = "<Pending>", Scope = "member", Target = "~M:ProjectTracker.Library.ResourceAssignmentEditCreator.DataPortal_Fetch(System.Int32)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CSLA0010:Find Operation Arguments That Are Not Serializable", Justification = "<Pending>", Scope = "member", Target = "~M:ProjectTracker.Library.ResourceEdit.DataPortal_Fetch(System.Int32)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CSLA0010:Find Operation Arguments That Are Not Serializable", Justification = "<Pending>", Scope = "member", Target = "~M:ProjectTracker.Library.ResourceEdit.DataPortal_Delete(System.Int32)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CSLA0010:Find Operation Arguments That Are Not Serializable", Justification = "<Pending>", Scope = "member", Target = "~M:ProjectTracker.Library.Security.PTIdentity.DataPortal_Fetch(System.String)")]
 

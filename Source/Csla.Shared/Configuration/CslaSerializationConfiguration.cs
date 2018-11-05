309fd58dc81feef3fc0278ea3ca56264071f5f17
Rockford Lhotka <rocky@lhotka.net>
2018-11-04 20:28:48 -0600

    #966 Add fluent configuration types.

---
+++ Source/Csla.Shared/Configuration/CslaSerializationConfiguration.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csla.Configuration
+{
+  /// <summary>
+  /// Use this type to configure the settings for serialization.
+  /// </summary>
+  public class CslaSerializationConfiguration
+  {
+    private CslaConfiguration RootConfiguration { get; set; }
+
+    internal CslaSerializationConfiguration(CslaConfiguration root)
+    {
+      RootConfiguration = root;
+    }
+
+    /// <summary>
+    /// Sets the serialization formatter type used by CSLA .NET
+    /// for all explicit object serialization (such as cloning,
+    /// n-level undo, etc).
+    /// </summary>
+    /// <param name="formatterName">Formatter name (one of MobileFormatter, 
+    /// BinaryFormatter, CustomFormatter, NetDataContractSerializer)</param>
+    public CslaConfiguration SerializationFormatter(string formatterName)
+    {
+      ConfigurationManager.AppSettings["CslaSerializationFormatter"] = formatterName;
+      return RootConfiguration;
+    }
+
+    /// <summary>
+    /// Sets the type name of a custom formatter for use when the
+    /// SerializationFormatter is set to CustomFormatter.
+    /// </summary>
+    /// <param name="typeName">Assembly qualified type name</param>
+    /// <returns></returns>
+    public CslaConfiguration CustomSerializationFormatter(string typeName)
+    {
+      ConfigurationManager.AppSettings["CslaCustomSerializationFormatter"] = typeName;
+      return RootConfiguration;
+    }
+
+    /// <summary>
+    /// Sets type of the writer that is used to write data to 
+    /// serialization stream in MobileFormatter.
+    /// </summary>
+    /// <param name="typeName">Assembly qualified type name</param>
+    public CslaConfiguration MobileWriter(string typeName)
+    {
+      ConfigurationManager.AppSettings["CslaWriter"] = typeName;
+      return RootConfiguration;
+    }
+
+    /// <summary>
+    /// Sets type of the writer that is used to read data to 
+    /// serialization stream in MobileFormatter.
+    /// </summary>
+    /// <param name="typeName">Assembly qualified type name</param>
+    public CslaConfiguration MobileReader(string typeName)
+    {
+      ConfigurationManager.AppSettings["CslaReader"] = typeName;
+      return RootConfiguration;
+    }
+  }
+}

721369296a8e58ea8b882c6dd5392a5241d3b57b
Rockford Lhotka <rocky@lhotka.net>
2018-11-22 00:04:50 -0600

    #972 Add RoutedDataPortal sample solution

---
+++ Samples/RoutedDataPortal/BusinessLibrary/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using Csla;
+using System;
+
+namespace BusinessLibrary
+{
+  [Serializable]
+  public class TestClass : ReadOnlyBase<TestClass>
+  {
+    public static readonly PropertyInfo<string> CreatedFromProperty = RegisterProperty<string>(c => c.CreatedFrom);
+    public string CreatedFrom
+    {
+      get { return GetProperty(CreatedFromProperty); }
+      private set { LoadProperty(CreatedFromProperty, value); }
+    }
+
+    private void DataPortal_Fetch()
+    {
+      CreatedFrom = $"{Environment.MachineName} - {Csla.ApplicationContext.LocalContext["dpv"].ToString()}";
+    }
+  }
+}

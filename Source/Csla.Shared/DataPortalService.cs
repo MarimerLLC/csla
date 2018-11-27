9e7f58bcabbdf16e699283eec9156b8a208891aa
Rockford Lhotka <rocky@lhotka.net>
2018-11-27 17:10:56 -0600

    #982 #1012 Add DataPortalService and IDataPortalService for use in .NET Core DI scenarios

---
+++ Source/Csla.Shared/DataPortalService.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DataPortalService.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: http://www.lhotka.net/cslanet/
+// </copyright>
+// <summary>Implements a data portal service</summary>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csla
+{
+  /// <summary>
+  /// Implements a data portal service
+  /// used to get an access to a client-side data portal
+  /// instance.
+  /// </summary>
+  public class DataPortalService : IDataPortalService
+  {
+    /// <summary>
+    /// Get a client-side data portal instance.
+    /// </summary>
+    /// <typeparam name="T">Root business object type</typeparam>
+    public IDataPortal<T> GetDataPortal<T>()
+    {
+      return new DataPortal<T>();
+    }
+  }
+}
---
+++ Source/Csla.Shared/IDataPortalService.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IDataPortalService.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: http://www.lhotka.net/cslanet/
+// </copyright>
+// <summary>Implements a data portal service</summary>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csla
+{
+  /// <summary>
+  /// Defines a data portal service
+  /// used to get an access to a client-side data portal
+  /// instance.
+  /// </summary>
+  public interface IDataPortalService
+  {
+    /// <summary>
+    /// Get a client-side data portal instance.
+    /// </summary>
+    /// <typeparam name="T">Root business object type</typeparam>
+    IDataPortal<T> GetDataPortal<T>();
+  }
+}

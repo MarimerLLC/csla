85f0b2bfb2d0fc275fdcf3ea344c2bd62d36adae
Rockford Lhotka <rocky@lhotka.net>
2019-03-28 14:33:07 -0500

    #1084 Update version numbers to 4.10.0

--- Source/Csla.Xaml.Net4.5/Properties/AssemblyInfo.cs
+++ Source/Csla.Xaml.Net4.5/Properties/AssemblyInfo.cs
@@ -44,8 +44,8 @@ using System.Windows.Markup;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.9.0.0")]
-[assembly: AssemblyFileVersion("4.9.0.0")]
+[assembly: AssemblyVersion("4.10.0.0")]
+[assembly: AssemblyFileVersion("4.10.0.0")]
 
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.SourceAssembly, ResourceDictionaryLocation.SourceAssembly)]

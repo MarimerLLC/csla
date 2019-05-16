e3f59d6c94d4d030732606295487e86350161e15
Rockford Lhotka <rocky@lhotka.net>
2019-05-16 14:48:58 -0500

    #981 Update project metadata; create/update nuget definitions

---
+++ Source/Csla.Data.SqlClient.Fx/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSLA .NET System.Data.SqlClient support")]
+[assembly: AssemblyDescription("CSLA .NET Framework (NetStandard 2.0)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marimer LLC")]
+[assembly: AssemblyProduct("CSLA .NET")]
+[assembly: AssemblyCopyright("Copyright © 2010-18 Marimer LLC")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]

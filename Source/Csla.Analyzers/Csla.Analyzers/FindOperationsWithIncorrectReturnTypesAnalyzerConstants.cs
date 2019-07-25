d57dda6c64fa2ff4eaaecaf063f560b53e7e8253
Jason Bock <jason@jasonbock.net>
2019-07-24 21:50:24 -0500

    #1100 Got the analyzer working.

---
+++ Source/Csla.Analyzers/Csla.Analyzers/FindOperationsWithIncorrectReturnTypesAnalyzerConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Csla.Analyzers
+{
+  public static class FindOperationsWithIncorrectReturnTypesAnalyzerConstants
+  {
+    public const string Title = "Find Operations With Incorrect Return Types";
+    public const string IdentifierText = "FindOperationsWithIncorrectReturnTypes";
+    public const string Message = "The return type from an operation should be either void or Task.";
+  }
+}

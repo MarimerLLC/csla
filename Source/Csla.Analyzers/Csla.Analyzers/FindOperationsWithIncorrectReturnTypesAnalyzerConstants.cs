b77f5ad703d6c1d9db23a3659f1d56e2cd75d556
Jason Bock <jason@jasonbock.net>
2019-07-24 22:18:19 -0500

    #1100 Got the code fix working, still needs unit tests.

--- Source/Csla.Analyzers/Csla.Analyzers/FindOperationsWithIncorrectReturnTypesAnalyzerConstants.cs
+++ Source/Csla.Analyzers/Csla.Analyzers/FindOperationsWithIncorrectReturnTypesAnalyzerConstants.cs
@@ -1,9 +1,18 @@
-﻿namespace Csla.Analyzers
+﻿using System.Threading.Tasks;
+
+namespace Csla.Analyzers
 {
   public static class FindOperationsWithIncorrectReturnTypesAnalyzerConstants
   {
     public const string Title = "Find Operations With Incorrect Return Types";
     public const string IdentifierText = "FindOperationsWithIncorrectReturnTypes";
     public const string Message = "The return type from an operation should be either void or Task.";
+  }
+
+  public static class FindOperationsWithIncorrectReturnTypeResolveCorrectTypeCodeFixConstants
+  {
+    public const string ChangeReturnTypeToTaskDescription = "Change return type to Task";
+    public const string ChangeReturnTypeToVoidDescription = "Change return type to void";
+    public static readonly string SystemThreadingTasksNamespace = typeof(Task).Namespace;
   }
 }

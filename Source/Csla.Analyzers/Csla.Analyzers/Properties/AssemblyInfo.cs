e9c5f644c6b3741f44dafb8555e8f3268244958d
Rockford Lhotka <rocky@lhotka.net>
2019-07-30 19:26:08 -0500

    Update version numbers for v4.11.2

--- Source/Csla.Analyzers/Csla.Analyzers/Properties/AssemblyInfo.cs
+++ Source/Csla.Analyzers/Csla.Analyzers/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Csla.Analyzers.Tests")]
 
-[assembly: AssemblyVersion("4.11.1.0")]
-[assembly: AssemblyFileVersion("4.11.1.0")]
+[assembly: AssemblyVersion("4.11.2.0")]
+[assembly: AssemblyFileVersion("4.11.2.0")]

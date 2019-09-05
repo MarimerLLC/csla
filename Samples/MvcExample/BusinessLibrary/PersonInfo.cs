a012320380fdc0feea173b53a31f7bf1861600b3
Rockford Lhotka <rocky@lhotka.net>
2019-09-04 23:46:36 -0500

    #905 Create basic MVC example

---
+++ Samples/MvcExample/BusinessLibrary/PersonInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Csla;
+
+namespace BusinessLibrary
+{
+  [Serializable]
+  public class PersonInfo : ReadOnlyBase<PersonInfo>
+  {
+    public static readonly PropertyInfo<int> IdProperty = RegisterProperty<int>(nameof(Id));
+    public int Id
+    {
+      get { return GetProperty(IdProperty); }
+      private set { LoadProperty(IdProperty, value); }
+    }
+
+    public static readonly PropertyInfo<string> NameProperty = RegisterProperty<string>(nameof(Name));
+    public string Name
+    {
+      get { return GetProperty(NameProperty); }
+      private set { LoadProperty(NameProperty, value); }
+    }
+
+    [Fetch]
+    private void Fetch(int id, [Inject]DataAccess.IPersonDal dal)
+    {
+      var data = dal.Get(id);
+      Fetch(data);
+    }
+
+    [FetchChild]
+    private void Fetch(DataAccess.PersonEntity data)
+    {
+      Id = data.Id;
+      Name = data.Name;
+    }
+  }
+}

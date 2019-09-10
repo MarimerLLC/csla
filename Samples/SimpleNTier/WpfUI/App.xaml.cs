9ace107eb0fb82add3af90f285580eeb1a43d6a9
Rockford Lhotka <rocky@lhotka.net>
2019-09-10 15:33:57 -0500

    #1288 Update SimpleNTier example for CSLA 5 and .NET Core app server

--- Samples/SimpleNTier/WpfUI/App.xaml.cs
+++ Samples/SimpleNTier/WpfUI/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using Csla.Configuration;
 
 namespace WpfUI
 {
@@ -12,5 +13,12 @@ namespace WpfUI
   /// </summary>
   public partial class App : Application
   {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+
+      CslaConfiguration.Configure()
+        .DataPortal().DefaultProxy(typeof(Csla.DataPortalClient.HttpProxy), "https://localhost:44332/api/dataportal");
+    }
   }
 }

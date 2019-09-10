d0161db59a1132709d52d15eb058f2be4fc9f827
Rockford Lhotka <rocky@lhotka.net>
2019-09-09 22:13:50 -0500

    #1142 Add support for configuration via UseCsla method

---
+++ Source/Csla.Blazor/ConfigurationExtensions.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WebConfiguration.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: https://cslanet.com
+// </copyright>
+// <summary>Implement extension methods for .NET Core configuration</summary>
+//-----------------------------------------------------------------------
+using System;
+using Microsoft.AspNetCore.Components.Builder;
+
+namespace Csla.Configuration
+{
+  /// <summary>
+  /// Implement extension methods for .NET Core configuration
+  /// </summary>
+  public static class WebConfigurationExtensions
+  {
+    /// <summary>
+    /// Configures the application to use CSLA .NET
+    /// </summary>
+    /// <param name="app">ApplicationBuilder object</param>
+    public static IComponentsApplicationBuilder UseCsla(this IComponentsApplicationBuilder app)
+    {
+      CslaConfiguration.Configure().
+       ContextManager(typeof(Csla.Blazor.ApplicationContextManager));
+     return app;
+    }
+
+    /// <summary>
+    /// Configures the application to use CSLA .NET
+    /// </summary>
+    /// <param name="app">ApplicationBuilder object</param>
+    /// <param name="config">Implement to configure CSLA .NET</param>
+    public static IComponentsApplicationBuilder UseCsla(
+      this IComponentsApplicationBuilder app, Action<CslaConfiguration> config)
+    {
+      CslaConfiguration.Configure().
+       ContextManager(typeof(Csla.Blazor.ApplicationContextManager));
+      config?.Invoke(CslaConfiguration.Configure());
+      return app;
+    }
+  }
+}

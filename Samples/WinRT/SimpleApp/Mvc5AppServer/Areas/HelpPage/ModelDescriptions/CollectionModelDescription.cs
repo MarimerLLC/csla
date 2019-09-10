8878cb3806421bdc80b15f1cfc5f66dd333b0697
Rockford Lhotka <rocky@lhotka.net>
2019-09-09 23:18:04 -0500

    Remove old WinRT Samples

--- Samples/WinRT/SimpleApp/Mvc5AppServer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Mvc5AppServer.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}

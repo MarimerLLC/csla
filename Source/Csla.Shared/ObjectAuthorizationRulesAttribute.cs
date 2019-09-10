0f714346444521a65d7e827d283746f659a758fe
Rockford Lhotka <rocky@lhotka.net>
2019-09-10 01:24:46 -0500

    #1209 Add ObjectAuthorizationRulesAttribute

---
+++ Source/Csla.Shared/ObjectAuthorizationRulesAttribute.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ObjectAuthorizationRulesAttribute.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: https://cslanet.com
+// </copyright>
+// <summary>Authorization method identifiation attribute</summary>
+//-----------------------------------------------------------------------
+using System;
+
+namespace Csla
+{
+  /// <summary>
+  /// Attribute identifying static method invoked
+  /// to add object authorization rules for type.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Method)]
+  public class ObjectAuthorizationRulesAttribute : Attribute 
+  {
+  }
+}

94b2d0cd880dd703d4b918424c3f52667d1d84d4
Rockford Lhotka <rocky@lhotka.net>
2019-10-05 02:35:51 -0500

    #1364 Create draft implementation of RazorPagesExample

---
+++ Samples/RazorPagesExample/RazorPagesExample/Pages/People/Delete.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLibrary;
+using Csla;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesExample.Pages.People
+{
+  public class DeleteModel : PageModel
+  {
+    [BindProperty]
+    public PersonEdit Item { get; set; }
+
+    public async Task OnGet(int id)
+    {
+      Item = await DataPortal.FetchAsync<PersonEdit>(id);
+    }
+
+    public async Task<ActionResult> OnDelete()
+    {
+      await DataPortal.DeleteAsync<PersonEdit>(Item.Id);
+      return RedirectToPage("/People/Index");
+    }
+  }
+}
---
+++ Samples/RazorPagesExample/RazorPagesExample/Pages/People/Edit.cshtml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLibrary;
+using Csla;
+using Csla.Core;
+using Csla.Rules;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesExample.Pages.People
+{
+  public class EditModel : PageModel
+  {
+    [BindProperty]
+    public PersonEdit Item { get; set; }
+
+    public async Task OnGet(int id)
+    {
+      Item = await DataPortal.FetchAsync<PersonEdit>(id);
+    }
+
+    public async Task<ActionResult> OnPost()
+    {
+      try
+      {
+        if (Item is BusinessBase bb && !bb.IsValid)
+          AddBrokenRuleInfo(Item, null);
+        if (ModelState.IsValid)
+        {
+          Item = await Item.SaveAsync(true);
+          return RedirectToPage("/People/Index");
+        }
+      }
+      catch (ValidationException ex)
+      {
+        AddBrokenRuleInfo(Item, ex.Message);
+      }
+      catch (DataPortalException ex)
+      {
+        if (ex.BusinessException != null)
+          ModelState.AddModelError(string.Empty, ex.BusinessException.Message);
+        else
+          ModelState.AddModelError(string.Empty, ex.Message);
+      }
+      catch (Exception ex)
+      {
+        ModelState.AddModelError(string.Empty, ex.Message);
+      }
+      return Page();
+    }
+
+    private void AddBrokenRuleInfo<T>(T item, string defaultText) where T : class, ISavable
+    {
+      if (item is BusinessBase bb)
+      {
+        var errors = bb.BrokenRulesCollection.
+          Where(r => r.Severity == RuleSeverity.Error);
+        foreach (var rule in errors)
+        {
+          if (string.IsNullOrEmpty(rule.Property))
+          {
+            ModelState.AddModelError(string.Empty, rule.Description);
+          }
+          else
+          {
+            var modelItem = ModelState.Where(r => r.Key == rule.Property || r.Key.EndsWith($".{rule.Property}")).FirstOrDefault();
+            ModelState.AddModelError(modelItem.Key, rule.Description);
+          }
+        }
+      }
+      else
+      {
+        ModelState.AddModelError(string.Empty, defaultText);
+      }
+    }
+
+    public string ErrorFor<T>(T source, string propertyName)
+    {
+      var result = string.Empty;
+      if (source is BusinessBase bb)
+        result = bb.BrokenRulesCollection.ToString(",", RuleSeverity.Error, propertyName);
+      return result;
+    }
+
+    public string WarningFor<T>(T source, string propertyName)
+    {
+      var result = string.Empty;
+      if (source is BusinessBase bb)
+        result = bb.BrokenRulesCollection.ToString(",", RuleSeverity.Warning, propertyName);
+      return result;
+    }
+
+    public string InformationFor<T>(T source, string propertyName)
+    {
+      var result = string.Empty;
+      if (source is BusinessBase bb)
+        result = bb.BrokenRulesCollection.ToString(",", RuleSeverity.Information, propertyName);
+      return result;
+    }
+  }
+}

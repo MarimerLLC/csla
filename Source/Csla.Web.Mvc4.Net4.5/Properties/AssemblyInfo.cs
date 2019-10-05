9e635fecbfc7ba3317943064bd8f27fd412a1345
Rockford Lhotka <rocky@lhotka.net>
2019-10-04 23:02:53 -0500

    Fix typo in guid value

--- Source/Csla.Web.Mvc4.Net4.5/Properties/AssemblyInfo.cs
+++ Source/Csla.Web.Mvc4.Net4.5/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a30818d3-c99a-4121-8c43-3797b5.1.05e")]
+[assembly: Guid("a30818d3-c99a-4121-8c43-3797b5d0815e")]
 
 // Version information for an assembly consists of the following four values:
 //

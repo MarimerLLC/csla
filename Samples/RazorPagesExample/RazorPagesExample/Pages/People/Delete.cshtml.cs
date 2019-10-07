182273716a3430bf5a7b9234a67a4928cb157d67
Rockford Lhotka <rocky@lhotka.net>
2019-10-06 23:50:41 -0500

    #1364 Update how navigation is implemented; remove Create page

--- Samples/RazorPagesExample/RazorPagesExample/Pages/People/Delete.cshtml.cs
+++ Samples/RazorPagesExample/RazorPagesExample/Pages/People/Delete.cshtml.cs
@@ -4,22 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using BusinessLibrary;
 using Csla;
+using Csla.Web.Shared.RazorPages;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPagesExample.Pages.People
 {
-  public class DeleteModel : PageModel
+  public class DeleteModel : PageModel<PersonEdit>
   {
-    [BindProperty]
-    public PersonEdit Item { get; set; }
-
     public async Task OnGet(int id)
     {
       Item = await DataPortal.FetchAsync<PersonEdit>(id);
     }
 
-    public async Task<ActionResult> OnDelete()
+    public async Task<ActionResult> OnPost()
     {
       await DataPortal.DeleteAsync<PersonEdit>(Item.Id);
       return RedirectToPage("/People/Index");
--- Samples/RazorPagesExample/RazorPagesExample/Pages/People/Edit.cshtml.cs
+++ Samples/RazorPagesExample/RazorPagesExample/Pages/People/Edit.cshtml.cs
@@ -10,12 +10,15 @@ namespace RazorPagesExample.Pages.People
   {
     public async Task OnGet(int id)
     {
-      Item = await DataPortal.FetchAsync<PersonEdit>(id);
+      if (id == -1)
+        Item = await DataPortal.CreateAsync<PersonEdit>();
+      else
+        Item = await DataPortal.FetchAsync<PersonEdit>(id);
     }
 
     public async Task<ActionResult> OnPost()
     {
-      return await SaveAsync();
+      return await SaveAsync((Item.Id > -1));
     }
   }
 }

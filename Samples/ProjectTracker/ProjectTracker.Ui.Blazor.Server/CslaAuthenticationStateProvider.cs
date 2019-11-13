bd321427daffb4d01b1c4c80ac21a7b8f466c8c2
Rockford Lhotka <rocky@lhotka.net>
2019-11-13 00:25:39 -0600

    #1312 Working version of ProjectEdit page using latest CSLA updates

---
+++ Samples/ProjectTracker/ProjectTracker.Ui.Blazor.Server/CslaAuthenticationStateProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Http;
+
+namespace Csla.Blazor
+{
+  public class CslaAuthenticationStateProvider : AuthenticationStateProvider
+  {
+    HttpContext context;
+
+    public CslaAuthenticationStateProvider(IHttpContextAccessor ctx)
+    {
+      context = ctx.HttpContext;
+    }
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+      //ProjectTracker.Library.Security.PTPrincipal.Load("manager");
+      return Task.FromResult(new AuthenticationState((ClaimsPrincipal)Csla.ApplicationContext.User));
+    }
+  }
+}

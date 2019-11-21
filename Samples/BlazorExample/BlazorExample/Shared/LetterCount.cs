4cf6bea7887a8b73202e9ada4c685f19a94f572d
Rockford Lhotka <rocky@lhotka.net>
2019-11-21 00:00:00 -0500

    #1312 Add LetterCount rule

---
+++ Samples/BlazorExample/BlazorExample/Shared/LetterCount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Csla.Rules;
+
+namespace BlazorExample.Shared
+{
+  public class LetterCount : BusinessRule
+  {
+    public LetterCount(Csla.Core.IPropertyInfo primaryProperty, Csla.Core.IPropertyInfo outputProperty)
+      : base(primaryProperty)
+    {
+      AffectedProperties.Add(outputProperty);
+    }
+
+    protected override void Execute(IRuleContext context)
+    {
+      var text = (string)ReadProperty(context.Target, PrimaryProperty);
+      var count = text.Length;
+      context.AddOutValue(AffectedProperties[1], count);
+    }
+  }
+}

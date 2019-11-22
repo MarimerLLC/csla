2f6c048cb9da222d3c73744f30c35f51e3c68ae8
Jason Bock <jason@jasonbock.net>
2019-11-21 21:10:32 -0600

    #1318 starting to work on code fix for async BusinessRule

--- Source/Csla.Analyzers/Csla.Analyzers/AsynchronousBusinessRuleInheritingFromBusinessRuleAnalyzerConstants.cs
+++ Source/Csla.Analyzers/Csla.Analyzers/AsynchronousBusinessRuleInheritingFromBusinessRuleAnalyzerConstants.cs
@@ -5,4 +5,9 @@
     public const string Title = "Find Asynchronous Business Rules That Do Not Derive From BusinessRuleAsync";
     public const string Message = "Asynchronous business rules should derive from BusinessRuleAsync.";
   }
+
+  public static class AsynchronousBusinessRuleInheritingFromBusinessRuleChangeToBusinessRuleAsyncCodeFixConstants
+  {
+    public const string UpdateToAsyncEquivalentsDescription = "Update base type to asynchronous equivalents";
+  }
 }
---
+++ Source/Csla.Analyzers/Csla.Analyzers/AsynchronousBusinessRuleInheritingFromBusinessRuleChangeToBusinessRuleAsyncCodeFix.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Csla.Analyzers
+{
+  [ExportCodeFixProvider(LanguageNames.CSharp)]
+  [Shared]
+  public sealed class AsynchronousBusinessRuleInheritingFromBusinessRuleChangeToBusinessRuleAsyncCodeFix
+    : CodeFixProvider
+  {
+    public override ImmutableArray<string> FixableDiagnosticIds =>
+      ImmutableArray.Create(Constants.AnalyzerIdentifiers.AsynchronousBusinessRuleInheritance);
+
+    public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+      var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+      context.CancellationToken.ThrowIfCancellationRequested();
+
+      var diagnostic = context.Diagnostics.First();
+      var methodNode = root.FindNode(diagnostic.Location.SourceSpan) as MethodDeclarationSyntax;
+
+      context.CancellationToken.ThrowIfCancellationRequested();
+      await AddCodeFixAsync(context, root, diagnostic, methodNode);
+    }
+
+    private static async Task AddCodeFixAsync(CodeFixContext context, SyntaxNode root,
+      Diagnostic diagnostic, MethodDeclarationSyntax methodNode)
+    {
+      var model = await context.Document.GetSemanticModelAsync(context.CancellationToken);
+      var methodSymbol = model.GetDeclaredSymbol(methodNode);
+      var typeSymbol = methodSymbol.ContainingType;
+
+      var returnType = methodNode.ReturnType;
+      var methodName = methodNode.Identifier;
+
+      var newRoot = root.ReplaceNode(returnType, SyntaxFactory.IdentifierName(nameof(Task)));
+      newRoot = newRoot.ReplaceToken(methodName, SyntaxFactory.Identifier("ExecuteAsync"));
+
+      foreach(var (oldBase, newBase) in GetAllRelevantBaseTypes(typeSymbol, model))
+      {
+        newRoot = newRoot.ReplaceNode(oldBase, newBase);
+      }
+
+      context.RegisterCodeFix(
+        CodeAction.Create(
+          AsynchronousBusinessRuleInheritingFromBusinessRuleChangeToBusinessRuleAsyncCodeFixConstants.UpdateToAsyncEquivalentsDescription,
+          _ => Task.FromResult(context.Document.WithSyntaxRoot(newRoot)),
+          AsynchronousBusinessRuleInheritingFromBusinessRuleChangeToBusinessRuleAsyncCodeFixConstants.UpdateToAsyncEquivalentsDescription), diagnostic);
+    }
+
+    private static IEnumerable<(SimpleBaseTypeSyntax, SimpleBaseTypeSyntax)> GetAllRelevantBaseTypes(
+      INamedTypeSymbol typeSymbol, SemanticModel model)
+    {
+      foreach(var typeSymbolReference in typeSymbol.DeclaringSyntaxReferences)
+      {
+        var typeSymbolReferenceNode = typeSymbolReference.GetSyntax() as TypeDeclarationSyntax;
+
+        foreach(var baseTypeNode in typeSymbolReferenceNode.BaseList.DescendantNodes(_ => true).OfType<SimpleBaseTypeSyntax>())
+        {
+          var baseTypeNodeIdentifier = baseTypeNode.DescendantNodes().OfType<IdentifierNameSyntax>().Single();
+
+          if(baseTypeNodeIdentifier.Identifier.ValueText == "BusinessRule")
+          {
+            yield return (baseTypeNode, SyntaxFactory.SimpleBaseType(SyntaxFactory.IdentifierName("BusinessRuleAsync")));
+          }
+          else if (baseTypeNodeIdentifier.Identifier.ValueText == "IBusinessRule")
+          {
+            yield return (baseTypeNode, SyntaxFactory.SimpleBaseType(SyntaxFactory.IdentifierName("IBusinessRuleAsync")));
+          }
+        }
+      }
+    }
+  }
+}

e079956503809b55836b25bd76800fce4b68a1df
Rockford Lhotka <rocky@lhotka.net>
2019-12-24 23:41:41 -0600

    #1312 Add CORS support for the data portal

--- Samples/ProjectTracker/ProjectTracker.AppServerCore/Startup.cs
+++ Samples/ProjectTracker/ProjectTracker.AppServerCore/Startup.cs
@@ -20,6 +20,7 @@ namespace ProjectTracker.AppServerCore
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddCors();
       services.AddMvc((o) => o.EnableEndpointRouting = false);
 
       // If using Kestrel:
@@ -51,6 +52,8 @@ namespace ProjectTracker.AppServerCore
 
       app.UseHttpsRedirection();
       app.UseMvc();
+
+      app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
       app.UseCsla();
 

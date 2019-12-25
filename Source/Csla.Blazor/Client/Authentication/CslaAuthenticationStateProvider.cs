e5d7d357cbcb28c9ee1bfadef6ff34613fe9da83
Rockford Lhotka <rocky@lhotka.net>
2019-12-25 00:28:37 -0600

    Add Blazor client authentication helpers

---
+++ Source/Csla.Blazor/Client/Authentication/CslaAuthenticationStateProvider.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Csla.Blazor.Client.Authentication
+{
+  /// <summary>
+  /// ASP.NET AuthenticationStateProvider that relies on the
+  /// CslaUserService to manage the current user.
+  /// </summary>
+  public class CslaAuthenticationStateProvider : AuthenticationStateProvider
+  {
+    private readonly CslaUserService _currentUserService;
+
+    /// <summary>
+    /// Creates an instance of the type.
+    /// </summary>
+    /// <param name="currentUserService">CslaUserService instance</param>
+    public CslaAuthenticationStateProvider(CslaUserService currentUserService)
+    {
+      _currentUserService = currentUserService;
+      _currentUserService.CurrentUserChanged += (sender, e) =>
+      {
+        var authState = Task.FromResult(new AuthenticationState(e.NewUser));
+        NotifyAuthenticationStateChanged(authState);
+      };
+    }
+
+    /// <summary>
+    /// Gets the AuthenticationState from the
+    /// CslaUserService service.
+    /// </summary>
+    /// <returns></returns>
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+      return Task.FromResult(new AuthenticationState(_currentUserService.CurrentUser));
+    }
+  }
+}

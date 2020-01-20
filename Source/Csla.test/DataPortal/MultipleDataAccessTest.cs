1fb6e14cf9642f45ca8122eeeb8ae15e61d4ae54
Eduardo.Costa <eduardo@micromust.com.br>
2020-01-20 14:31:44 -0300

    - Correction of issue 1464

---
+++ Source/Csla.test/DataPortal/MultipleDataAccessTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Csla.Test.DataPortal
+{
+  [TestClass]
+  public class MultipleDataAccessTest
+  {
+    [TestMethod]
+    public void TestDpFetch()
+    {
+      var result = MultipleDataAccess.GetMultiple(1);
+      Assert.AreEqual(1, result.Id);
+      Assert.AreEqual("abc", result.Name);
+
+      result = MultipleDataAccess.GetMultiple();
+      Assert.AreEqual(int.MaxValue, result.Id);
+      Assert.AreEqual(string.Empty, result.Name);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(AmbiguousMatchException), "Should throw 'AmbiguousMatchException'")]
+    public void TestDpFetchNullable()
+    {
+      try
+      {
+        var result = MultipleDataAccess.GetMultiple(1, default(bool?));
+      }
+      catch (DataPortalException ex)
+      {
+        throw ex.GetBaseException();
+      }
+    }
+  }
+}

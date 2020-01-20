41e0dbb10b2dbc5090189181b5266e43820d84ca
Eduardo.Costa <eduardo@micromust.com.br>
2020-01-20 15:03:04 -0300

    #1464 Changed the way Data Access methods are searched

---
+++ Source/Csla.test/DataPortal/MultipleDataAccessBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csla.Test.DataPortal
+{
+  [Serializable]
+  public class MultipleDataAccessBase<T>
+    : BusinessBase<T>
+    where T : MultipleDataAccessBase<T>
+  {
+    public static readonly PropertyInfo<int> IdProperty = RegisterProperty<int>(c => c.Id);
+    public int Id
+    {
+      get { return GetProperty(IdProperty); }
+      set { SetProperty(IdProperty, value); }
+    }
+
+    [Fetch]
+    protected void Fetch()
+    {
+      using (BypassPropertyChecks)
+      {
+        Id = int.MaxValue;
+      }
+    }
+
+    [Fetch]
+    protected void Fetch(int id)
+    {
+      using (BypassPropertyChecks)
+      {
+        Id = id;
+      }
+    }
+  }
+}

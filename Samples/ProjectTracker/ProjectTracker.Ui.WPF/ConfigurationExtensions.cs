f5fb6f8743656724ebc12282587524bff19262a6
blago <blago.culjak@galileo.ba>
2020-04-03 09:26:18 +0200

    #1542

---
+++ Samples/ProjectTracker/ProjectTracker.Ui.WPF/ConfigurationExtensions.cs
@@ -0,0 +1,38 @@
+﻿
+//-----------------------------------------------------------------------
+// <copyright file="ConfigurationExtensions.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: https://cslanet.com
+// </copyright>
+// <summary>Implement extension methods for .NET Core configuration</summary>
+//-----------------------------------------------------------------------
+using System;
+using Csla.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfUI
+{
+  /// <summary>
+  /// Implement extension methods for .NET Core configuration
+  /// </summary>
+  public static class ConfigurationExtensions
+  {
+    /// <summary>
+    /// Add CSLA .NET services for use by the application.
+    /// </summary>
+    /// <param name="services">ServiceCollection object</param>
+
+
+    /// <summary>
+    /// Configure CSLA .NET settings from .NET Core configuration
+    /// subsystem.
+    /// </summary>
+    /// <param name="config">Configuration object</param>
+    public static IConfiguration ConfigureCsla(this IConfiguration config)
+    {
+      config.Bind("csla", new CslaConfigurationOptions());
+      return config;
+    }
+  }
+}
--- Samples/ProjectTracker/ProjectTracker.Ui.WPF/Startup.cs
+++ Samples/ProjectTracker/ProjectTracker.Ui.WPF/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Csla.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace WpfUI
 {
@@ -47,9 +48,11 @@ namespace WpfUI
     /// </summary>
     public void Configure()
     {
-      CslaConfiguration.Configure()
-        .DataPortal()
-          .DefaultProxy(typeof(Csla.DataPortalClient.HttpProxy), "https://ptrackerserver.azurewebsites.net/api/dataportal");
+      // CslaConfiguration.Configure()        .DataPortal()          .DefaultProxy(typeof(Csla.DataPortalClient.HttpProxy), "https://ptrackerserver.azurewebsites.net/api/dataportal");
+      var config = new ConfigurationBuilder()
+.AddJsonFile("appsettings.json")
+.Build()
+.ConfigureCsla();
       //CslaConfiguration.Configure().
       //  DataPortal().DefaultProxy(typeof(Csla.DataPortalClient.HttpProxy), "http://localhost:8040/api/dataportal/");
     }

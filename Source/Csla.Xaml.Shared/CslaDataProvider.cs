729bc13c4f84e2229dcb6a2ef3a36b37f5c1007b
Rockford Lhotka <rocky@lhotka.net>
2020-04-16 18:00:34 -0500

    Restore use of WINDOWS_UWP directive

--- Source/Csla.Xaml.Shared/CslaDataProvider.cs
+++ Source/Csla.Xaml.Shared/CslaDataProvider.cs
@@ -1,4 +1,4 @@
-#if !XAMARIN
+#if !XAMARIN && !WINDOWS_UWP
 //-----------------------------------------------------------------------
 // <copyright file="CslaDataProvider.cs" company="Marimer LLC">
 //     Copyright (c) Marimer LLC. All rights reserved.

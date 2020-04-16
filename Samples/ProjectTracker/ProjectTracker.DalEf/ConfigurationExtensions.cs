c35d9dc53358f05521acf25824c3333c297c74fc
Rockford Lhotka <rocky@lhotka.net>
2020-04-16 17:45:45 -0500

    #1567 Start experimenting with DAL updates

---
+++ Samples/ProjectTracker/ProjectTracker.DalEf/ConfigurationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectTracker.DalEf;
+
+namespace ProjectTracker.Dal
+{
+  /// <summary>
+  /// Configuration extension methods
+  /// </summary>
+  public static class ConfigurationExtensions
+  {
+    /// <summary>
+    /// Add the services for ProjectTracker.Dal that
+    /// use Entity Framework
+    /// </summary>
+    /// <param name="services"></param>
+    public static void AddDalEf(this IServiceCollection services)
+    {
+      services.AddScoped<PTrackerEntities>();
+      services.AddTransient<IAssignmentDal, AssignmentDal>();
+      services.AddTransient<IDashboardDal, DashboardDal>();
+      services.AddTransient<IProjectDal, ProjectDal>();
+      services.AddTransient<IResourceDal, ResourceDal>();
+      services.AddTransient<IRoleDal, RoleDal>();
+      services.AddTransient<IUserDal, UserDal>();
+    }
+  }
+}
--- Samples/ProjectTracker/ProjectTracker.DalEf/ProjectDal.cs
+++ Samples/ProjectTracker/ProjectTracker.DalEf/ProjectDal.cs
@@ -1,137 +1,120 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using ProjectTracker.Dal;
-using Csla.Data;
 using System.Data;
 
 namespace ProjectTracker.DalEf
 {
   public class ProjectDal : IProjectDal
   {
+    private readonly PTrackerEntities objectContext;
+
+    public ProjectDal(PTrackerEntities context)
+    {
+      objectContext = context;
+    }
+
     public List<ProjectDto> Fetch()
     {
-      using (var ctx = ObjectContextManager<PTrackerEntities>.GetManager("PTrackerEntities"))
-      {
-        var result = from r in ctx.ObjectContext.Projects
-                     select new ProjectDto
-                     {
-                       Id = r.Id,
-                       Name = r.Name,
-                       Description = r.Description,
-                       Started = r.Started,
-                       Ended = r.Ended,
-                       LastChanged = r.LastChanged
-                     };
-        return result.ToList();
-      }
+      var result = from r in objectContext.Projects
+                   select new ProjectDto
+                   {
+                     Id = r.Id,
+                     Name = r.Name,
+                     Description = r.Description,
+                     Started = r.Started,
+                     Ended = r.Ended,
+                     LastChanged = r.LastChanged
+                   };
+      return result.ToList();
     }
 
     public List<ProjectDto> Fetch(string nameFilter)
     {
-      using (var ctx = ObjectContextManager<PTrackerEntities>.GetManager("PTrackerEntities"))
-      {
-        var result = from r in ctx.ObjectContext.Projects
-                     where r.Name.Contains(nameFilter)
-                     select new ProjectDto
-                     {
-                       Id = r.Id,
-                       Name = r.Name,
-                       Description = r.Description,
-                       Started = r.Started,
-                       Ended = r.Ended,
-                       LastChanged = r.LastChanged
-                     };
-        return result.ToList();
-      }
+      var result = from r in objectContext.Projects
+                   where r.Name.Contains(nameFilter)
+                   select new ProjectDto
+                   {
+                     Id = r.Id,
+                     Name = r.Name,
+                     Description = r.Description,
+                     Started = r.Started,
+                     Ended = r.Ended,
+                     LastChanged = r.LastChanged
+                   };
+      return result.ToList();
     }
 
     public ProjectDto Fetch(int id)
     {
-      using (var ctx = ObjectContextManager<PTrackerEntities>.GetManager("PTrackerEntities"))
-      {
-        var result = (from r in ctx.ObjectContext.Projects
-                      where r.Id == id
-                      select new ProjectDto
-                      {
-                        Id = r.Id,
-                        Name = r.Name,
-                        Description = r.Description,
-                        Started = r.Started,
-                        Ended = r.Ended,
-                        LastChanged = r.LastChanged
-                      }).FirstOrDefault();
-        if (result == null)
-          throw new DataNotFoundException("Project");
-        return result;
-      }
+      var result = (from r in objectContext.Projects
+                    where r.Id == id
+                    select new ProjectDto
+                    {
+                      Id = r.Id,
+                      Name = r.Name,
+                      Description = r.Description,
+                      Started = r.Started,
+                      Ended = r.Ended,
+                      LastChanged = r.LastChanged
+                    }).FirstOrDefault();
+      if (result == null)
+        throw new DataNotFoundException("Project");
+      return result;
     }
 
     public bool Exists(int id)
     {
-      using (var ctx = ObjectContextManager<PTrackerEntities>.GetManager("PTrackerEntities"))
-      {
-        var result = (from r in ctx.ObjectContext.Projects
-                      where r.Id == id
-                      select r.Id).Count() > 0;
-        return result;
-      }
+      var result = (from r in objectContext.Projects
+                    where r.Id == id
+                    select r.Id).Count() > 0;
+      return result;
     }
 
     public void Insert(ProjectDto item)
     {
-      using (var ctx = ObjectContextManager<PTrackerEntities>.GetManager("PTrackerEntities"))
+      var newItem = new Project
       {
-        var newItem = new Project
-        {
-          Name = item.Name,
-          Description = item.Description,
-          Started = item.Started,
-          Ended = item.Ended
-        };
-        ctx.ObjectContext.AddToProjects(newItem);
-        ctx.ObjectContext.SaveChanges();
-        item.Id = newItem.Id;
-        item.LastChanged = newItem.LastChanged;
-      }
+        Name = item.Name,
+        Description = item.Description,
+        Started = item.Started,
+        Ended = item.Ended
+      };
+      objectContext.AddToProjects(newItem);
+      objectContext.SaveChanges();
+      item.Id = newItem.Id;
+      item.LastChanged = newItem.LastChanged;
     }
 
     public void Update(ProjectDto item)
     {
-      using (var ctx = ObjectContextManager<PTrackerEntities>.GetManager("PTrackerEntities"))
-      {
-        var data = (from r in ctx.ObjectContext.Projects
-                    where r.Id == item.Id
-                    select r).FirstOrDefault();
-        if (data == null)
-          throw new DataNotFoundException("Project");
-        if (!data.LastChanged.Matches(item.LastChanged))
-          throw new ConcurrencyException("Project");
+      var data = (from r in objectContext.Projects
+                  where r.Id == item.Id
+                  select r).FirstOrDefault();
+      if (data == null)
+        throw new DataNotFoundException("Project");
+      if (!data.LastChanged.Matches(item.LastChanged))
+        throw new ConcurrencyException("Project");
 
-        data.Name = item.Name;
-        data.Description = item.Description;
-        data.Started = item.Started;
-        data.Ended = item.Ended;
-        var count = ctx.ObjectContext.SaveChanges();
-        if (count == 0)
-          throw new UpdateFailureException("Project");
-        item.LastChanged = data.LastChanged;
-      }
+      data.Name = item.Name;
+      data.Description = item.Description;
+      data.Started = item.Started;
+      data.Ended = item.Ended;
+      var count = objectContext.SaveChanges();
+      if (count == 0)
+        throw new UpdateFailureException("Project");
+      item.LastChanged = data.LastChanged;
     }
 
     public void Delete(int id)
     {
-      using (var ctx = ObjectContextManager<PTrackerEntities>.GetManager("PTrackerEntities"))
+      var data = (from r in objectContext.Projects
+                  where r.Id == id
+                  select r).FirstOrDefault();
+      if (data != null)
       {
-        var data = (from r in ctx.ObjectContext.Projects
-                    where r.Id == id
-                    select r).FirstOrDefault();
-        if (data != null)
-        {
-          ctx.ObjectContext.Projects.DeleteObject(data);
-          ctx.ObjectContext.SaveChanges();
-        }
+        objectContext.Projects.DeleteObject(data);
+        objectContext.SaveChanges();
       }
     }
   }

fc4b61669eb3aefd69e656e27fba5e5effe8b185
Rockford Lhotka <rocky@lhotka.net>
2020-10-22 14:14:35 -0500

    #1849 Update version numbers to 5.3.2

--- Source/Csla.Xaml.Net4.5/Properties/AssemblyInfo.cs
+++ Source/Csla.Xaml.Net4.5/Properties/AssemblyInfo.cs
@@ -44,8 +44,8 @@ using System.Windows.Markup;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.3.1.0")]
-[assembly: AssemblyFileVersion("5.3.1.0")]
+[assembly: AssemblyVersion("5.3.2.0")]
+[assembly: AssemblyFileVersion("5.3.2.0")]
 
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.SourceAssembly, ResourceDictionaryLocation.SourceAssembly)]

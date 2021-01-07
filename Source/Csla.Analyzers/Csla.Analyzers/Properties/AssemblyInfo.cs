c3be8a403f866ba55143a23541a46e626801ed32
Rockford Lhotka <rocky@lhotka.net>
2021-01-06 21:01:28 -0600

    Update version numbers for v6.0.0

--- Source/Csla.Analyzers/Csla.Analyzers/Properties/AssemblyInfo.cs
+++ Source/Csla.Analyzers/Csla.Analyzers/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Csla.Analyzers.Tests")]
 
-[assembly: AssemblyVersion("5.4.0.0")]
-[assembly: AssemblyFileVersion("5.4.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
--- Source/Csla.Windows.NetCoreApp3.0/Properties/AssemblyInfo.cs
+++ Source/Csla.Windows.NetCoreApp3.0/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("5.4.0.0")]
-[assembly: AssemblyFileVersion("5.4.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]

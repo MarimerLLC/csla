67a23c77aaa738e98cd2e53d6908e27521653488
Rockford Lhotka <rocky@lhotka.net>
2021-01-19 11:19:04 -0600

    Update version numbers to 5.4.1

--- Source/Csla.Net5.0/Properties/AssemblyInfo.cs
+++ Source/Csla.Net5.0/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("5.4.0.0")]
-[assembly: AssemblyFileVersion("5.4.0.0")]
+[assembly: AssemblyVersion("5.4.1.0")]
+[assembly: AssemblyFileVersion("5.4.1.0")]

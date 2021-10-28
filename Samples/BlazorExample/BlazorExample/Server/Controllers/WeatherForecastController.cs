2062ae47ae05b91a1a0d05d711657517c118739b
Rockford Lhotka <rocky@lhotka.net>
2021-10-27 22:04:21 -0500

    #2552 Initial updates for CSLA and .NET 6

--- Samples/BlazorExample/BlazorExample/Server/Controllers/WeatherForecastController.cs
+++ Samples/BlazorExample/BlazorExample/Server/Controllers/WeatherForecastController.cs
@@ -1,10 +1,5 @@
-﻿using BlazorExample.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlazorExample.Shared;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace BlazorExample.Server.Controllers
 {
@@ -14,25 +9,24 @@ namespace BlazorExample.Server.Controllers
   {
     private static readonly string[] Summaries = new[]
     {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
 
-    private readonly ILogger<WeatherForecastController> logger;
+    private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
-      this.logger = logger;
+      _logger = logger;
     }
 
     [HttpGet]
     public IEnumerable<WeatherForecast> Get()
     {
-      var rng = new Random();
       return Enumerable.Range(1, 5).Select(index => new WeatherForecast
       {
         Date = DateTime.Now.AddDays(index),
-        TemperatureC = rng.Next(-20, 55),
-        Summary = Summaries[rng.Next(Summaries.Length)]
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
       })
       .ToArray();
     }
--- Samples/BlazorExample/BlazorExample/Server/Program.cs
+++ Samples/BlazorExample/BlazorExample/Server/Program.cs
@@ -1,26 +1,36 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.ResponseCompression;
 
-namespace BlazorExample.Server
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      BuildWebHost(args).Run();
-    }
-
-    public static IWebHost BuildWebHost(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .ConfigureKestrel(o =>
-            {
-              o.AllowSynchronousIO = true;
-            })
-            .UseConfiguration(new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .Build())
-            .UseStartup<Startup>()
-            .Build();
-  }
+  app.UseWebAssemblyDebugging();
 }
+else
+{
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();

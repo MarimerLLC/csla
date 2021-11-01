335df5f65020245e1a031982b07c9d909c046d43
Rockford Lhotka <rocky@lhotka.net>
2021-11-01 18:09:19 -0500

    #2025 HttpProxy now relies on DI to get the HttpClient and options values

---
+++ Source/Csla/DataPortalClient/HttpProxyOptions.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="HttpProxyOptions.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: https://cslanet.com
+// </copyright>
+// <summary>Options for HttpProxy</summary>
+//-----------------------------------------------------------------------
+
+namespace Csla.Channels.Http
+{
+  /// <summary>
+  /// Options for HttpProxy
+  /// </summary>
+  public class HttpProxyOptions
+  {
+    /// <summary>
+    /// Data portal server endpoint URL
+    /// </summary>
+    public string DataPortalUrl { get; set; }
+  }
+}

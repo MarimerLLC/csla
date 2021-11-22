15f7b3f30d29950b7162f596d2a14cde13c47974
Rockford Lhotka <rocky@lhotka.net>
2021-11-22 10:57:14 -0600

    #2026 Make LocalProxy default; auto-load server-side data portal

--- Source/Csla/Configuration/ConfigurationExtensions.cs
+++ Source/Csla/Configuration/ConfigurationExtensions.cs
@@ -7,6 +7,8 @@
 // <summary>Implement extension methods for base .NET configuration</summary>
 //-----------------------------------------------------------------------
 using System;
+using System.Linq;
+using Csla.DataPortalClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -34,30 +36,24 @@ namespace Csla.Configuration
     /// <param name="options">Options for configuring CSLA .NET</param>
     public static ICslaBuilder AddCsla(this IServiceCollection services, Action<CslaConfiguration> options)
     {
-      var builder = new CslaBuilder(services);
+      ICslaBuilder builder = new CslaBuilder(services);
 
-      // Configuration
+      // Custom configuration
       var cslaOptions = new CslaConfiguration(services);
       options?.Invoke(cslaOptions);
 
-      // ApplicationContext
+      // ApplicationContext defaults
       services.TryAddScoped<ApplicationContext>();
       services.TryAddScoped(typeof(Core.IContextManager), typeof(Core.ApplicationContextManager));
 
-      //// Data portal services
-      //if (cslaOptions.DataPortal().UseDataPortalServer)
-      //{
-      //  services.TryAddTransient(typeof(Server.IDataPortalServer), typeof(Csla.Server.DataPortal));
-      //  services.TryAddTransient<Server.DataPortalSelector>();
-      //  services.TryAddTransient<Server.SimpleDataPortal>();
-      //  services.TryAddTransient<Server.FactoryDataPortal>();
-      //  services.TryAddTransient<Server.DataPortalBroker>();
-      //  services.TryAddSingleton(typeof(Server.Dashboard.IDashboard), typeof(Csla.Server.Dashboard.NullDashboard));
-      //}
-
-      // Data portal API
+      // Data portal API defaults
       services.TryAddTransient(typeof(IDataPortal<>), typeof(DataPortal<>));
       services.TryAddTransient(typeof(IChildDataPortal<>), typeof(DataPortal<>));
+
+      // Default to using LocalProxy and local data portal
+      var proxyInit = services.Where(i => i.ServiceType.Equals(typeof(IDataPortalProxy))).Any();
+      if (!proxyInit)
+        AddServerSideDataPortal(cslaOptions.DataPortal());
 
       return builder;
     }
@@ -81,6 +77,7 @@ namespace Csla.Configuration
     public static CslaDataPortalConfiguration AddServerSideDataPortal(this CslaDataPortalConfiguration builder)
     {
       var services = builder.Services;
+      services.TryAddTransient(typeof(IDataPortalProxy), typeof(Channels.Local.LocalProxy));
       services.TryAddTransient(typeof(Server.IDataPortalServer), typeof(Csla.Server.DataPortal));
       services.TryAddTransient<Server.DataPortalSelector>();
       services.TryAddTransient<Server.SimpleDataPortal>();

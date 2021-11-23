b5a0e1d536b14a3f06726769cc6c78deccf10430
Rockford Lhotka <rocky@lhotka.net>
2021-11-23 15:08:12 -0600

    #2540 Provide ApplicationContext to static helper types for authn

--- Source/Csla.Xaml.Shared/CslaDataProvider.cs
+++ Source/Csla.Xaml.Shared/CslaDataProvider.cs
@@ -1,4 +1,4 @@
-#if false
+#if !XAMARIN
 //-----------------------------------------------------------------------
 // <copyright file="CslaDataProvider.cs" company="Marimer LLC">
 //     Copyright (c) Marimer LLC. All rights reserved.

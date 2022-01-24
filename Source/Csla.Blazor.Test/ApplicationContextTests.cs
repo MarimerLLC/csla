030aba078d9af3db85deb0b7e92e70bd65744a80
Rockford Lhotka <rocky@lhotka.net>
2022-01-24 12:43:59 -0800

    #2706 Test to ensure Blazor context manager is loaded

---
+++ Source/Csla.Blazor.Test/ApplicationContextTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Csla.Configuration;
+using Csla.Core;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
+
+namespace Csla.Blazor.Test
+{
+  [TestClass]
+  public class ApplicationContextTests
+  {
+    [TestMethod]
+    public void CorrectManagerChosen()
+    {
+      IServiceCollection services = new ServiceCollection();
+      services.AddScoped(typeof(AuthenticationStateProvider), typeof(TestAuthenticationStateProvider));
+      services.AddCsla(c => c
+        .AddServerSideBlazor());
+      IServiceProvider provider = services.BuildServiceProvider();
+      var manager = provider.GetRequiredService<IContextManager>();
+      Assert.IsInstanceOfType(manager, typeof(Csla.Blazor.ApplicationContextManager));
+    }
+  }
+
+  public class TestAuthenticationStateProvider : AuthenticationStateProvider
+  {
+    public override Task<AuthenticationState> GetAuthenticationStateAsync() 
+      => Task.FromResult<AuthenticationState>(null);
+  }
+}

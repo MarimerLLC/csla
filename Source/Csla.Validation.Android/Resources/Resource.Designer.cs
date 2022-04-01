2b9d1ec1f622c2581e1487b0885528f2fdc4d0d0
Rockford Lhotka <rocky@lhotka.net>
2022-04-01 14:22:55 -0500

    #2862 Update version to 5.5.3, update for latest VS 2019

--- Source/Csla.Validation.Android/Resources/Resource.Designer.cs
+++ Source/Csla.Validation.Android/Resources/Resource.Designer.cs
@@ -14,7 +14,7 @@ namespace Csla.Validation
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.155")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
 	public partial class Resource
 	{
 		

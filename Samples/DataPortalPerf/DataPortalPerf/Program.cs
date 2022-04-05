fc3957250ee0fd4b7734875dfab11a327246ff94
Rockford Lhotka <rocky@lhotka.net>
2022-04-04 22:59:21 -0500

    #2552 Update for CSLA 6

--- Samples/DataPortalPerf/DataPortalPerf/Program.cs
+++ Samples/DataPortalPerf/DataPortalPerf/Program.cs
@@ -1,9 +1,22 @@
 ﻿using BenchmarkDotNet.Running;
+using Csla;
+using Csla.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DataPortalPerf
 {
   class Program
   {
-    static void Main() => BenchmarkRunner.Run<RetrieveEntities>();
+    static void Main()
+    {
+      var services = new ServiceCollection();
+      services.AddCsla();
+      var provider = services.BuildServiceProvider();
+      ApplicationContext = provider.GetService<ApplicationContext>();
+
+      BenchmarkRunner.Run<RetrieveEntities>();
+    }
+
+    public static ApplicationContext ApplicationContext { get; set; }
   }
 }

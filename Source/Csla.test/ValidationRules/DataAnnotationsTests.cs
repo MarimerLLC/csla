ea9893c817f08d05edd406b2a9ee24311fe69556
TheCakeMonster <46025270+TheCakeMonster@users.noreply.github.com>
2022-05-25 20:52:31 +0100

    Provide ServiceProvider to ValidationContext to support DI-based DataAnnotations validation attributes

---
+++ Source/Csla.test/ValidationRules/DataAnnotationsTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csla.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Csla.Test.ValidationRules
+{
+  [TestClass]
+  public class DataAnnotationsTests
+  {
+    private static TestDIContext _testDIContext;
+
+    [ClassInitialize]
+    public static void Initialize(TestContext testcontext)
+    {
+      _testDIContext = TestDIContextFactory.CreateDefaultContext();
+    }
+
+    [TestMethod]
+    public void BrokenRulesCollection_ValidTestObject_ReturnsZeroBrokenRules()
+    {
+      // Arrange
+      IDataPortal<TestObject> dataPortal = _testDIContext.CreateDataPortal<TestObject>();
+      TestObject testObject = dataPortal.Create();
+      int expected = 0;
+      int actual;
+
+      // Act
+      actual = testObject.BrokenRulesCollection.Count;
+
+      // Assert
+      Assert.AreEqual(expected, actual);
+
+    }
+
+    #region Private Helper Classes
+
+    private class TestObject : BusinessBase<TestObject>
+    {
+      public static PropertyInfo<string> TestPropertyProperty = RegisterProperty<string>(nameof(TestProperty));
+
+      [Required]
+      [DIBasedTest]
+      public string TestProperty 
+      { 
+        get { return GetProperty(TestPropertyProperty); } 
+        set { SetProperty(TestPropertyProperty, value); }
+      }
+
+      [Create]
+      private void Create() 
+      {
+        using (BypassPropertyChecks)
+        {
+          LoadProperty(TestPropertyProperty, "Test");
+        }
+        BusinessRules.CheckRules();
+      }
+    }
+
+    #endregion
+  }
+}

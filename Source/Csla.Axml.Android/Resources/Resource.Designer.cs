d04b263442af05253b21d8e5841ff13158586d3f
Rockford Lhotka <rocky@lhotka.net>
2022-08-03 13:41:08 -0500

    Update version

--- Source/Csla.Axml.Android/Resources/Resource.Designer.cs
+++ Source/Csla.Axml.Android/Resources/Resource.Designer.cs
@@ -14,7 +14,7 @@ namespace Csla.Axml
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.99.16")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.0.0.73")]
 	public partial class Resource
 	{
 		

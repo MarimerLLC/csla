5419162db439b789df4fe812a702cac66da0230d
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2024-03-17 22:38:39 +0100

    Fixes #1376 * Avoid sync IO through asyn reading into a memory stream which will be used by the (de)serializer * Add integration test to verify that it works as expected

---
+++ Source/Csla.TestHelpers/Http/TestDataPortalController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Csla.TestHelpers.Http;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestDataPortalController(ApplicationContext applicationContext)
+    : Csla.Server.Hosts.HttpPortalController(applicationContext) {
+  [HttpGet]
+  public string Get() {
+    return "DataPortal running...";
+  }
+}

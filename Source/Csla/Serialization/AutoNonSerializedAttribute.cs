f913f63963a994334bcba31f745dc217363955be
Simon Cropp <simon.cropp@gmail.com>
2024-04-18 08:25:24 +1000

    .

--- Source/Csla/Serialization/AutoNonSerializedAttribute.cs
+++ Source/Csla/Serialization/AutoNonSerializedAttribute.cs
@@ -9,13 +9,9 @@ using System;
 
 namespace Csla.Serialization
 {
-
   /// <summary>
   /// Indicate that a public field or property should be excluded from auto serialization
   /// </summary>
   [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-  public class AutoNonSerializedAttribute : Attribute
-  {
-  }
-
+  public class AutoNonSerializedAttribute : Attribute;
 }

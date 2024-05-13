c203b20cd58585d70d0035e2e6efc25b7592e975
Luiz Fernando Bicalho <luiz.bicalho@aglsolutions.com.br>
2024-05-12 23:05:29 -0300

    `Update SaveState method in StateManager.cs`
    Refactored the `SaveState` method in `StateManager.cs`. Removed the calls to `SendSession` method of `_sessionManager` that were previously using `timeout` and `ct` parameters. The `SaveState` method no longer depends on `SendSession` method of `_sessionManager`.
    
    Overload InitializeAsync and refactor StateManager.cs
    
    Overloaded the `InitializeAsync()` method in `StateManager.cs` to accept a `CancellationToken` parameter and added a new private method `GetState(CancellationToken ct)`. This new method checks if the operating system is a browser and retrieves the session using the provided cancellation token. Removed the previous versions of `GetState(CancellationToken ct)` and `InitializeAsync(CancellationToken ct)` methods that were located in a different part of the code. Also, modified the `SaveState(CancellationToken ct)` method by removing the code block that sends the session using the provided cancellation token when the operating system is a browser.

--- Source/Csla.Blazor/State/StateManager.cs
+++ Source/Csla.Blazor/State/StateManager.cs
@@ -38,41 +38,21 @@ namespace Csla.Blazor.State
     /// <summary>
     /// Get state from cache.
     /// </summary>
+    /// <param name="ct">Cancellation token</param>
+    public Task InitializeAsync(CancellationToken ct)
+    {
+      return GetState(ct);
+    }
+
+    /// <summary>
+    /// Get state from cache.
+    /// </summary>
     /// <param name="timeout">Time to wait before timing out</param>
     private async Task GetState(TimeSpan timeout)
     {
       var isBrowser = OperatingSystem.IsBrowser();
       if (isBrowser)
         _ = await _sessionManager.RetrieveSession(timeout);
-    }
-
-    /// <summary>
-    /// Saves state from Blazor wasm to web server. Must call
-    /// as user navigates to any server-side Blazor page.
-    /// </summary>
-    /// <remarks>
-    /// Normally this method is called from the Dispose method
-    /// of a Blazor page, which is the only reliable point
-    /// at which you know the user is navigating to another
-    /// page.
-    /// </remarks>
-    /// <param name="timeout">Time to wait before timing out</param>
-    public async Task SaveState(TimeSpan timeout)
-    {
-      var isBrowser = OperatingSystem.IsBrowser();
-      if (isBrowser)
-      {
-        await _sessionManager.SendSession(timeout);
-      }
-    }
-
-    /// <summary>
-    /// Get state from cache.
-    /// </summary>
-    /// <param name="ct">Cancellation token</param>
-    public Task InitializeAsync(CancellationToken ct)
-    {
-      return GetState(ct);
     }
 
     /// <summary>
@@ -96,14 +76,30 @@ namespace Csla.Blazor.State
     /// at which you know the user is navigating to another
     /// page.
     /// </remarks>
+    /// <param name="timeout">Time to wait before timing out</param>
+    public async Task SaveState(TimeSpan timeout)
+    {
+      var isBrowser = OperatingSystem.IsBrowser();
+      if (isBrowser)
+        await _sessionManager.SendSession(timeout);
+    }
+
+    /// <summary>
+    /// Saves state from Blazor wasm to web server. Must call
+    /// as user navigates to any server-side Blazor page.
+    /// </summary>
+    /// <remarks>
+    /// Normally this method is called from the Dispose method
+    /// of a Blazor page, which is the only reliable point
+    /// at which you know the user is navigating to another
+    /// page.
+    /// </remarks>
     /// <param name="ct">Cancellation token</param>
     public async Task SaveState(CancellationToken ct)
     {
       var isBrowser = OperatingSystem.IsBrowser();
       if (isBrowser)
-      {
         await _sessionManager.SendSession(ct);
-      }
     }
   }
 }

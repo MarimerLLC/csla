1ff39a0e4cd0a23d0b9be6880fce68fa456b219d
Will Facey <wfacey@carus.ca>
2024-05-29 12:53:53 -0400

    Changes required to replicate VM save then cancel issue

--- Source/Csla.Blazor/ViewModel.cs
+++ Source/Csla.Blazor/ViewModel.cs
@@ -307,6 +307,11 @@ namespace Csla.Blazor
       }
       finally
       {
+        //if (ManageObjectLifetime && Model is Core.ISupportUndo undo)
+        //  undo.BeginEdit();
+
+        _propertyInfoCache.Clear();
+
         HookChangedEvents(Model);
         IsBusy = false;
         if (Exception != null)

f35deb709a3450f9de59683106ec806041647a4e
Will Facey <wfacey@carus.ca>
2024-06-14 16:29:38 -0400

    Remove debugging catch logic in SaveAsync

--- Source/Csla/BusinessBase.cs
+++ Source/Csla/BusinessBase.cs
@@ -196,10 +196,6 @@ namespace Csla
           {
             result = await dp.UpdateAsync((T)this);
           }
-          catch(Exception ex)
-          {
-            var msg = ex.Message;
-          }
           finally
           {
             if (dataPortalOptions.DataPortalClientOptions.AutoCloneOnUpdate)

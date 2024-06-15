01a1dac1041fe48f58a32ac958b2beaee5323545
Rockford lhotka <rocky@lhotka.net>
2024-06-15 16:35:08 -0500

    #1877 Add implementation of ClaimsPrincipal serialization for .NET Framework

--- Source/Csla/Serialization/Mobile/CustomSerializers/ClaimsPrincipalSerializer.cs
+++ Source/Csla/Serialization/Mobile/CustomSerializers/ClaimsPrincipalSerializer.cs
@@ -10,6 +10,7 @@ using System.Security.Claims;
 
 namespace Csla.Serialization.Mobile.CustomSerializers;
 
+#if NET8_0_OR_GREATER || NETSTANDARD2_0_OR_GREATER
 /// <summary>
 /// A formatter that can serialize and deserialize a ClaimsPrincipal object.
 /// </summary>
@@ -37,3 +38,74 @@ public class ClaimsPrincipalSerializer : IMobileSerializer
     info.AddValue("s", buffer.ToArray());
   }
 }
+#else
+using System.Text.Json;
+
+/// <summary>
+/// A formatter that can serialize and deserialize a ClaimsPrincipal object.
+/// </summary>
+public class ClaimsPrincipalSerializer : IMobileSerializer
+{
+  /// <inheritdoc />
+  public object Deserialize(SerializationInfo info)
+  {
+    var json = info.GetValue<string>("s");
+    var dto = JsonSerializer.Deserialize<PrincipalDto>(json);
+    var identities = new List<ClaimsIdentity>();
+    foreach (var i in dto.Identities)
+    {
+      var claims = i.Claims.Select(c => new Claim(c.Type, c.Value, c.ValueType)).ToList();
+      var identity = new ClaimsIdentity(claims, i.AuthenticationType)
+      {
+        Label = i.Label
+      };
+      identities.Add(identity);
+    }
+    var result = new ClaimsPrincipal(identities);
+    return result;
+  }
+
+  /// <inheritdoc />
+  public void Serialize(object obj, SerializationInfo info)
+  {
+    if (obj is not ClaimsPrincipal principal)
+      throw new ArgumentException("obj.GetType() != ClaimsPrincipal", nameof(obj));
+
+    var dto = new PrincipalDto();
+    foreach (var i in principal.Identities)
+    {
+      dto.Identities.Add(new IdentityDto
+      {
+        AuthenticationType = i.AuthenticationType,
+        Label = i.Label,
+        Claims = i.Claims.Select(c => new ClaimDto
+        {
+          Type = c.Type,
+          Value = c.Value,
+          ValueType = c.ValueType
+        }).ToList()
+      });
+    }
+    info.AddValue("s", JsonSerializer.Serialize(dto));
+  }
+
+  private class PrincipalDto
+  {
+    public List<IdentityDto> Identities { get; set; } = [];
+  }
+
+  private class IdentityDto
+  {
+    public string AuthenticationType { get; set; }
+    public string Label { get; set; }
+    public List<ClaimDto> Claims { get; set; }
+  }
+
+  private class ClaimDto
+  {
+    public string Type { get; set; }
+    public string Value { get; set; }
+    public string ValueType { get; set; }
+  }
+}
+#endif

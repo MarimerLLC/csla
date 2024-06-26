994ec42a4034d623af52259431ff147cd162781d
Rockford Lhotka <rocky@lhotka.net>
2024-06-25 21:46:44 -0500

    #3928 Initial creation of RabbitMq channel example solution

---
+++ Samples/RabbitMqExample/RabbitMqService/Program.cs
@@ -0,0 +1,19 @@
+﻿using Csla.Channels.RabbitMq;
+using Csla.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddCsla(o => o.
+DataPortal(o => o.
+  AddServerSideDataPortal(o => o.
+    UseRabbitMqPortal(o => o.
+      DataPortalUri = new Uri("rabbitmq://rabbithost/myservice")))));
+var serviceProvider = services.BuildServiceProvider();
+
+Console.WriteLine("RabbitMq Service starting");
+
+var rabbitMqService = serviceProvider.GetRequiredService<RabbitMqPortal>();
+rabbitMqService.StartListening();
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();

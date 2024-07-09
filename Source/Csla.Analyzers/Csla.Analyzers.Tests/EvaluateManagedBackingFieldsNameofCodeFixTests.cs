5a895056e1f412d438b23bc6fd3330cb1bdd5744
Luiz Fernando Bicalho <luizfbicalho@gmail.com>
2024-07-09 17:14:54 -0300

    Add nameof analyzer to CSLA framework
    Introduced a new analyzer and code fix provider to the CSLA framework to promote the use of `nameof` for referencing managed backing fields, enhancing code quality and maintainability. Key additions include a new rule (`CSLA0023`), diagnostic analyzer, code fix provider, and relevant unit tests. This update aims to reduce errors from typos and improve refactorability by ensuring property names are compile-time checked.

---
+++ Source/Csla.Analyzers/Csla.Analyzers.Tests/EvaluateManagedBackingFieldsNameofCodeFixTests.cs
@@ -0,0 +1,110 @@
+﻿using Csla.Analyzers.ManagedBackingFieldUsesNameof;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Csla.Analyzers.Tests
+{
+  [TestClass]
+  public sealed class EvaluateManagedBackingFieldsNameofCodeFixTests
+  {
+
+    [TestMethod]
+    public void VerifyGetFixableDiagnosticIds()
+    {
+      var fix = new EvaluateManagedBackingFieldsNameofCodeFix();
+      var ids = fix.FixableDiagnosticIds.ToList();
+
+      Assert.AreEqual(1, ids.Count, nameof(ids.Count));
+      Assert.AreEqual(ids[0], Constants.AnalyzerIdentifiers.EvaluateManagedBackingFieldsNameof,
+        nameof(Constants.AnalyzerIdentifiers.EvaluateManagedBackingFieldsNameof));
+    }
+    [TestMethod]
+    public async Task VerifyCodeFixReplacesLambdaWithNameof()
+    {
+      var testCode = """
+
+        using Csla;
+
+        public class TestClass : BusinessBase<TestClass>
+        {
+          private static readonly PropertyInfo<string> TestProperty = RegisterProperty<string>(c => c.TestName, "Test Name");
+          public string TestName
+          {
+            get { return GetProperty(TestProperty); }
+            set { SetProperty(TestProperty, value); }
+          }
+        }
+        """;
+
+      var document = TestHelpers.Create(testCode);
+      var diagnostics = await TestHelpers.GetDiagnosticsAsync(testCode, new EvaluateManagedBackingFieldsNameofAnalyzer());
+
+      var actions = new List<CodeAction>();
+      var codeActionRegistration = new Action<CodeAction, ImmutableArray<Diagnostic>>(
+        (action, _) => actions.Add(action));
+
+      var fix = new EvaluateManagedBackingFieldsNameofCodeFix();
+      var codeFixContext = new CodeFixContext(document, diagnostics[0], codeActionRegistration, new CancellationToken(false));
+      await fix.RegisterCodeFixesAsync(codeFixContext);
+
+      Assert.AreEqual(1, actions.Count, nameof(actions.Count));
+
+      await TestHelpers.VerifyChangesAsync(actions,
+        "Use nameof", document,
+        (model, newRoot) =>
+        {
+          var fieldNode = newRoot.DescendantNodes().OfType<FieldDeclarationSyntax>().First();
+          var fieldInitializer = fieldNode.Declaration.Variables.First().Initializer.Value.ToString();
+          Assert.IsTrue(fieldInitializer.Contains("nameof(TestName)"), "Field initializer should contain nameof expression.");
+        });
+    }
+    [TestMethod]
+    public async Task VerifyCodeFixReplacesConstantWithNameof()
+    {
+      var testCode = """
+
+        using Csla;
+
+        public class TestClass : BusinessBase<TestClass>
+        {
+          public static string TestPropertyName = "TypeName";
+          private static readonly PropertyInfo<string> TestProperty = RegisterProperty<string>(TestPropertyName, "Test Name");
+          public string TestName
+          {
+            get { return GetProperty(TestProperty); }
+            set { SetProperty(TestProperty, value); }
+          }
+        }
+        """;
+
+      var document = TestHelpers.Create(testCode);
+      var diagnostics = await TestHelpers.GetDiagnosticsAsync(testCode, new EvaluateManagedBackingFieldsNameofAnalyzer());
+
+      var actions = new List<CodeAction>();
+      var codeActionRegistration = new Action<CodeAction, ImmutableArray<Diagnostic>>(
+        (action, _) => actions.Add(action));
+
+      var fix = new EvaluateManagedBackingFieldsNameofCodeFix();
+      var codeFixContext = new CodeFixContext(document, diagnostics[0], codeActionRegistration, new CancellationToken(false));
+      await fix.RegisterCodeFixesAsync(codeFixContext);
+
+      Assert.AreEqual(1, actions.Count, nameof(actions.Count));
+
+      await TestHelpers.VerifyChangesAsync(actions,
+        "Use nameof", document,
+        (model, newRoot) =>
+        {
+          var fieldNode = newRoot.DescendantNodes().OfType<FieldDeclarationSyntax>().Last();
+          var fieldInitializer = fieldNode.Declaration.Variables.First().Initializer.Value.ToString();
+          Assert.IsTrue(fieldInitializer.Contains("nameof(TestPropertyName)"), "Field initializer should contain nameof expression.");
+        });
+    }
+  }
+}
---
+++ Source/Csla.Analyzers/Csla.Analyzers/ManagedBackingFieldUsesNameof/EvaluateManagedBackingFieldsNameofCodeFix.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Csla.Analyzers.ManagedBackingFieldUsesNameof
+{
+  [ExportCodeFixProvider(LanguageNames.CSharp)]
+  [Shared]
+  public sealed class EvaluateManagedBackingFieldsNameofCodeFix
+    : CodeFixProvider
+  {
+    public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(Constants.AnalyzerIdentifiers.EvaluateManagedBackingFieldsNameof);
+
+
+    public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+      var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+      var diagnostic = context.Diagnostics.First();
+      var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+      var argumentSyntax = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ArgumentSyntax>().First();
+
+      context.RegisterCodeFix(
+        CodeAction.Create(
+          title: "Use nameof",
+          createChangedDocument: c => UseNameofAsync(context.Document, argumentSyntax, c),
+          equivalenceKey: "Use nameof"),
+        diagnostic);
+    }
+
+    private async Task<Document> UseNameofAsync(Document document, ArgumentSyntax argumentSyntax, CancellationToken cancellationToken)
+    {
+      var propertyName = "";
+      if (argumentSyntax.Expression is SimpleLambdaExpressionSyntax lambdaExpression)
+      {
+        var memberAccessExpression = (MemberAccessExpressionSyntax)lambdaExpression.Body;
+        propertyName = memberAccessExpression.Name.Identifier.ValueText;
+      }
+      else if (argumentSyntax.Expression is IdentifierNameSyntax identifier)
+      {
+        propertyName = identifier.Identifier.ValueText;
+      }
+
+      var nameofExpression = SyntaxFactory.ParseExpression($"nameof({propertyName})");
+
+      var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+      var newRoot = root.ReplaceNode(argumentSyntax.Expression, nameofExpression);
+
+      return document.WithSyntaxRoot(newRoot);
+    }
+  }
+}

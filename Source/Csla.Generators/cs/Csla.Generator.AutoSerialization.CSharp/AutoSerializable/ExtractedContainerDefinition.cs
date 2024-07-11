35beef75888584c8338215f0ac02e70a770b0e31
Luiz Fernando Bicalho <luizfbicalho@gmail.com>
2024-07-11 00:55:53 -0300

    rename Csla.Generators.CSharp to Csla.Generator.AutoSerialization.CSharp (#4094)

---
+++ Source/Csla.Generators/cs/Csla.Generator.AutoSerialization.CSharp/AutoSerializable/ExtractedContainerDefinition.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ExtractedContainerDefinition.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: https://cslanet.com
+// </copyright>
+// <summary>The definition of a container of a type, extracted from the syntax tree provided by Roslyn</summary>
+//-----------------------------------------------------------------------
+
+namespace Csla.Generator.AutoSerialization.CSharp.AutoSerialization
+{
+
+  /// <summary>
+  /// The definition of a container of a type, extracted from the syntax tree provided by Roslyn
+  /// </summary>
+  public class ExtractedContainerDefinition
+  {
+
+    /// <summary>
+    /// The name of the container, such as the class name or namespace name
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The full definition of the container for use in source generation
+    /// </summary>
+    public string FullDefinition { get; set; }
+
+  }
+}

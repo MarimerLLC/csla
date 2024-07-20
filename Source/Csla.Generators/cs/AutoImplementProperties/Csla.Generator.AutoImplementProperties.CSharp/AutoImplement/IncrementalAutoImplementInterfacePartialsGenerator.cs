e9d9065cd28953aa8d2ac5bb63cc89d3071ec0c6
Luiz Fernando Bicalho <luizfbicalho@gmail.com>
2024-07-20 13:14:39 -0300

    Add IncrementalAutoImplementInterfacePartialsGenerator
    - Introduced a new source generator `IncrementalAutoImplementInterfacePartialsGenerator` for automatic partial class implementations, marked with the `[Generator]` attribute.
    - Added `using` directives in `IncrementalAutoImplementInterfacePartialsGenerator.cs` for `System.Text` and several Roslyn API namespaces (`Microsoft.CodeAnalysis`, `Microsoft.CodeAnalysis.CSharp`, `Microsoft.CodeAnalysis.CSharp.Syntax`, `Microsoft.CodeAnalysis.Text`) to facilitate syntax tree and semantic model manipulation.
    - Implemented the `Initialize` method in `IncrementalAutoImplementInterfacePartialsGenerator` to set up syntax receiver for class or struct declarations marked with `Csla.Serialization.AutoImplementPropertiesInterfaceAttribute`1`, preparing them for source generation.
    - The generator's output action combines syntax nodes with semantic model information to generate source code for each identified type definition using a `SerializationPartialBuilder`.
    - Renamed `IncrementalSerializationPartialsGenerator` to `IncrementalAutoImplementPropertiesPartialsGenerator` for clarity and alignment with the new generator's functionality.
    - Added `AutoImplementPropertiesInterfaceAttribute.cs` defining `AutoImplementPropertiesInterfaceAttribute<T>`, a new attribute for indicating classes or structs that should have properties automatically implemented.

--- Source/Csla.Generators/cs/AutoImplementProperties/Csla.Generator.AutoImplementProperties.CSharp/AutoImplement/IncrementalAutoImplementInterfacePartialsGenerator.cs
+++ Source/Csla.Generators/cs/AutoImplementProperties/Csla.Generator.AutoImplementProperties.CSharp/AutoImplement/IncrementalAutoImplementInterfacePartialsGenerator.cs
@@ -11,7 +11,7 @@ namespace Csla.Generator.AutoImplementProperties.CSharp.AutoImplement
   /// Generates partial classes for incremental serialization based on the presence of the <see cref="Csla.Serialization.AutoImplementPropertiesAttribute"/> attribute.
   /// </summary>
   [Generator]
-  public class IncrementalSerializationPartialsGenerator : IIncrementalGenerator
+  public class IncrementalAutoImplementInterfacePartialsGenerator : IIncrementalGenerator
   {
     /// <summary>
     /// Initializes the incremental serialization partials generator.
@@ -21,14 +21,14 @@ namespace Csla.Generator.AutoImplementProperties.CSharp.AutoImplement
     {
       // Register a syntax receiver to collect information during the initial parsing phase
       var classDeclarations = context.SyntaxProvider.ForAttributeWithMetadataName(
-              "Csla.Serialization.AutoImplementPropertiesAttribute",
+              "Csla.Serialization.AutoImplementPropertiesInterfaceAttribute`1",
 
               predicate: static (s, _) => (s is ClassDeclarationSyntax || s is StructDeclarationSyntax) && s is TypeDeclarationSyntax type && type.Modifiers.Any(SyntaxKind.PartialKeyword),
               transform: static (ctx, _) =>
               {
                 if (ctx.TargetNode is TypeDeclarationSyntax typeDeclarationSyntax)
                 {
-                  return TypeDefinitionExtractor.ExtractTypeDefinition(new DefinitionExtractionContext(ctx.SemanticModel, false, true), typeDeclarationSyntax);
+                  return TypeDefinitionExtractor.ExtractTypeDefinition(new DefinitionExtractionContext(ctx.SemanticModel, true, false), typeDeclarationSyntax);
                 }
                 return null;
               })
---
+++ Source/Csla.Generators/cs/AutoImplementProperties/Csla.Generator.AutoImplementProperties.CSharp/AutoImplement/IncrementalAutoImplementPropertiesPartialsGenerator.cs
@@ -0,0 +1,58 @@
+using System.Text;
+using Csla.Generator.AutoImplementProperties.CSharp.AutoImplement.Discovery;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Csla.Generator.AutoImplementProperties.CSharp.AutoImplement
+{
+  /// <summary>
+  /// Generates partial classes for incremental serialization based on the presence of the <see cref="Csla.Serialization.AutoImplementPropertiesAttribute"/> attribute.
+  /// </summary>
+  [Generator]
+  public class IncrementalAutoImplementPropertiesPartialsGenerator : IIncrementalGenerator
+  {
+    /// <summary>
+    /// Initializes the incremental serialization partials generator.
+    /// </summary>
+    /// <param name="context">The initialization context.</param>
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+      // Register a syntax receiver to collect information during the initial parsing phase
+      var classDeclarations = context.SyntaxProvider.ForAttributeWithMetadataName(
+              "Csla.Serialization.AutoImplementPropertiesAttribute",
+
+              predicate: static (s, _) => (s is ClassDeclarationSyntax || s is StructDeclarationSyntax) && s is TypeDeclarationSyntax type && type.Modifiers.Any(SyntaxKind.PartialKeyword),
+              transform: static (ctx, _) =>
+              {
+                if (ctx.TargetNode is TypeDeclarationSyntax typeDeclarationSyntax)
+                {
+                  return TypeDefinitionExtractor.ExtractTypeDefinition(new DefinitionExtractionContext(ctx.SemanticModel, false, true), typeDeclarationSyntax);
+                }
+                return null;
+              })
+          .Where(static m => m is not null);
+
+      var compilation = context.CompilationProvider.Select((c, _) => new { ((CSharpCompilation)c).LanguageVersion, Nullable = c.Options.NullableContextOptions.AnnotationsEnabled() });
+      // Combine the collected syntax nodes with the semantic model
+      var compilationAndClasses = compilation.Combine(classDeclarations.Collect());
+
+      // Set up the generation phase
+      context.RegisterSourceOutput(compilationAndClasses, static (spc, source) =>
+      {
+        var (nullable, classes) = source;
+        SerializationPartialBuilder builder = new SerializationPartialBuilder(nullable.Nullable);
+        foreach (var typeDefinition in classes)
+        {
+          // Build the text for the generated type using the builder
+          var generationResults = builder.BuildPartialTypeDefinition(typeDefinition);
+
+          // Add the generated source to the output
+          spc.AddSource($"{generationResults.FullyQualifiedName}.g.cs",
+              SourceText.From(generationResults.GeneratedSource, Encoding.UTF8));
+        }
+      });
+    }
+  }
+}

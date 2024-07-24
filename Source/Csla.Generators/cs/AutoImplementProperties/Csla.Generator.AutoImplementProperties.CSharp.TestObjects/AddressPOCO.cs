3fc76139f3880681c229d7544dfcc52a021de86d
Luiz Fernando Bicalho <luizfbicalho@gmail.com>
2024-07-23 23:25:13 -0300

    Simplify AddressPOCO by removing unused properties
    The AddressPOCO class has been simplified by removing the following properties: AddressLine2, Town, County, and Postcode. Additionally, the IgnoredProperty, which was marked with the [CslaIgnoreProperty] attribute, has been removed from both the main class and its separate partial class definition. These changes reduce the complexity and number of properties managed by the class.

--- Source/Csla.Generators/cs/AutoImplementProperties/Csla.Generator.AutoImplementProperties.CSharp.TestObjects/AddressPOCO.cs
+++ Source/Csla.Generators/cs/AutoImplementProperties/Csla.Generator.AutoImplementProperties.CSharp.TestObjects/AddressPOCO.cs
@@ -15,23 +15,16 @@ namespace Csla.Generator.AutoImplementProperties.CSharp.TestObjects
   {
     [Display(Name = "Address Line 1")]
     public partial string? AddressLine1 { get; private set; }
-
     public partial string AddressLine2 { get; set; }
-
     public partial string Town { get; set; }
-
     public partial string County { get; set; }
-
     public partial string Postcode { get; set; }
-
     [CslaIgnoreProperty]
     public partial string IgnoredProperty { get; set; }
-
   }
 
   public partial class AddressPOCO
   {
     public partial string IgnoredProperty { get => ""; set { } }
-
   }
 }

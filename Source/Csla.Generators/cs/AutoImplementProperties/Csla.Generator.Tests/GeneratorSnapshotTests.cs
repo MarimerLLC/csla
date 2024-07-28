3a8b61b613632c86fbc172931e55056894fb51e2
Luiz Fernando Bicalho <luizfbicalho@gmail.com>
2024-07-28 18:20:01 -0300

    Add tests and generated classes for CommandPOCO and ReadOnlyPOCO
    Added new test methods in GeneratorSnapshotTests.cs to verify source
    generation for CommandPOCO and ReadOnlyPOCO classes. Generated partial
    classes CommandPOCO and ReadOnlyPOCO with properties AddressLine1,
    AddressLine2, Town, County, and Postcode, each with registered getter
    and setter methods.

--- Source/Csla.Generators/cs/AutoImplementProperties/Csla.Generator.Tests/GeneratorSnapshotTests.cs
+++ Source/Csla.Generators/cs/AutoImplementProperties/Csla.Generator.Tests/GeneratorSnapshotTests.cs
@@ -48,6 +48,73 @@ namespace TestObjects
   }
 
   [Fact]
+  public Task CommandBase()
+  {
+    var cslaSource = @$"
+using System.ComponentModel.DataAnnotations;
+using Csla.Serialization;
+using Csla;
+
+namespace TestObjects
+{{
+  [CslaImplementProperties]
+  public partial class CommandPOCO : CommandBase<CommandPOCO>
+  {{
+    [Display(Name = ""Address Line 1"")]
+    public partial string? AddressLine1 {{ get; private set; }}
+    public partial string AddressLine2 {{ get; set; }}
+    public partial string Town {{ get; set; }}
+    public partial string County {{ get; set; }}
+    public partial string Postcode {{ get; set; }}
+    [CslaIgnoreProperty]
+    public partial string IgnoredProperty {{ get; set; }}
+  }}
+
+  public partial class CommandPOCO
+  {{
+    public partial string IgnoredProperty {{ get => """"; set {{ }} }}
+  }}
+}}
+
+";
+
+    return SourceGenTester<IncrementalAutoImplementPropertiesPartialsGenerator>.Verify(cslaSource, true);
+  }
+
+  [Fact]
+  public Task ReadOnlyBase()
+  {
+    var cslaSource = @$"
+using System.ComponentModel.DataAnnotations;
+using Csla.Serialization;
+using Csla;
+
+namespace TestObjects
+{{
+  [CslaImplementProperties]
+  public partial class ReadOnlyPOCO : ReadOnlyBase<ReadOnlyPOCO>
+  {{
+    [Display(Name = ""Address Line 1"")]
+    public partial string? AddressLine1 {{ get; private set; }}
+    public partial string AddressLine2 {{ get; set; }}
+    public partial string Town {{ get; set; }}
+    public partial string County {{ get; set; }}
+    public partial string Postcode {{ get; set; }}
+    [CslaIgnoreProperty]
+    public partial string IgnoredProperty {{ get; set; }}
+  }}
+  public partial class ReadOnlyPOCO
+  {{
+    public partial string IgnoredProperty {{ get => """"; set {{ }} }}
+  }}
+}}
+
+";
+
+    return SourceGenTester<IncrementalAutoImplementPropertiesPartialsGenerator>.Verify(cslaSource, true);
+  }
+
+  [Fact]
   public Task BusinessBaseInterface()
   {
     var cslaSource = @$"

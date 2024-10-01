5748bafb191dc229af5e616abf6d6515c4260f40
Rockford Lhotka <rocky@lhotka.net>
2024-10-01 15:39:40 -0500

    #2551 Updating ProjectTracker code

--- Samples/ProjectTracker/ProjectTracker.Blazor/ProjectTracker.Blazor/Controllers/CslaStateController.cs
+++ Samples/ProjectTracker/ProjectTracker.Blazor/ProjectTracker.Blazor/Controllers/CslaStateController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Csla;
 using Csla.State;
+using Csla.Blazor.State.Messages;
 
 namespace ProjectTracker.Blazor.Controllers
 {
@@ -12,7 +13,21 @@ namespace ProjectTracker.Blazor.Controllers
   /// <param name="sessionManager"></param>
   [ApiController]
   [Route("[controller]")]
-  public class CslaStateController(ApplicationContext applicationContext, ISessionManager sessionManager) :
-      Csla.AspNetCore.Blazor.State.StateController(applicationContext, sessionManager)
-  {  }
+  public class CslaStateController : Csla.AspNetCore.Blazor.State.StateController
+  {
+    public CslaStateController(ApplicationContext applicationContext, ISessionManager sessionManager) :
+      base(applicationContext, sessionManager)
+    {
+      ApplicationContext = applicationContext;
+    }
+
+    private ApplicationContext ApplicationContext { get; set; }
+
+    public override StateResult Get(long lastTouched)
+    {
+      var user = ApplicationContext.User;
+      var result = base.Get(lastTouched);
+      return result;
+    }
+  }
 }

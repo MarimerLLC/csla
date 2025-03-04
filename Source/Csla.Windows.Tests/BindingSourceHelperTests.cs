fb3b28b56b04c2700369ec25ab7df50dfaa7efe5
Simon Cropp <simon.cropp@gmail.com>
2025-03-04 19:47:29 +1100

    add winforms tests

---
+++ Source/Csla.Windows.Tests/BindingSourceHelperTests.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Csla.Windows;
+
+[TestClass]
+public class BindingSourceHelperTests
+{
+    [TestMethod]
+    [ExpectedException(typeof(ApplicationException))]
+    public void InitializeBindingSourceTree_NullRootSource_ThrowsException()
+    {
+        // Arrange
+        using IContainer container = new Container();
+
+        // Act
+        BindingSourceHelper.InitializeBindingSourceTree(container, null);
+    }
+
+    [TestMethod]
+    public void InitializeBindingSourceTree_ValidRootSource_ReturnsRootNode()
+    {
+        // Arrange
+        using var container = new Container();
+        using var rootSource = new BindingSource();
+        container.Add(rootSource);
+
+        // Act
+        var result = BindingSourceHelper.InitializeBindingSourceTree(container, rootSource);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(rootSource, result.Source);
+    }
+
+    [TestMethod]
+    public void InitializeBindingSourceTree_WithChildSources_SetsUpTreeCorrectly()
+    {
+        // Arrange
+        using var container = new Container();
+        using var rootSource = new BindingSource();
+        using var childSource = new BindingSource
+        {
+            DataSource = rootSource
+        };
+        container.Add(rootSource);
+        container.Add(childSource);
+
+        // Act
+        var result = BindingSourceHelper.InitializeBindingSourceTree(container, rootSource);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Children.Count);
+        Assert.AreEqual(childSource, result.Children[0].Source);
+    }
+}

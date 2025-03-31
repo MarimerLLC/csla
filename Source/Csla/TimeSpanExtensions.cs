71a4ee116de814ef8556dd6b2bbe6e59ea74c96f
Stefan Ossendorf <StefanOssendorf@users.noreply.github.com>
2025-03-31 09:14:45 +0200

    Make ToCancellationToken and containing class obsolete  (#4618)
    * Make ToCancellationToken and containing class obsolete (so we can internalize them in v10)
    Add new method ToCancellationTokenSource and dispose the source correctly.
    
    Fixes #4611
    
    * Revert unnecessary await

--- Source/Csla/TimeSpanExtensions.cs
+++ Source/Csla/TimeSpanExtensions.cs
@@ -3,6 +3,7 @@ namespace System;
 /// <summary>
 /// Provides extension methods for <see cref="TimeSpan"/>.
 /// </summary>
+[Obsolete("The extensions type will become internal with CSLA 10.")]
 public static class TimeSpanExtensions
 {
   /// <summary>
@@ -10,9 +11,17 @@ public static class TimeSpanExtensions
   /// </summary>
   /// <param name="timeout">The timeout duration.</param>
   /// <returns>The <see cref="CancellationToken"/> with the specified timeout.</returns>
+  [Obsolete($"This extension method will be removed with CSLA 10. You should write your own extension with proper disposal handling for the underlying {nameof(CancellationTokenSource)}.")]
   public static CancellationToken ToCancellationToken(this TimeSpan timeout)
   {
     var cts = new CancellationTokenSource(timeout);
     return cts.Token;
   }
+
+  /// <summary>
+  /// Creates a <see cref="CancellationTokenSource"/> with the given <paramref name="timeout"/>.
+  /// </summary>
+  /// <param name="timeout">The timeout duration.</param>
+  /// <returns>The <see cref="CancellationTokenSource"/> with the specified timeout.</returns>
+  public static CancellationTokenSource ToCancellationTokenSource(this TimeSpan timeout) => new(timeout);
 }

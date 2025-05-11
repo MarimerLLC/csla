ab7ebf30b8358fa51e0ffca355c86ff464d18276
Stefan Ossendorf <StefanOssendorf@users.noreply.github.com>
2025-05-11 21:56:48 +0200

    Fix missing generic support for auto implement properties generator (#4645)
    * Fix missing generic support for auto implement properties generator
    Add basic tests to verify the code is correctly generated
    
    * Add missing test for nested generics
    
    * Unnecessary code removed
    Test name fixed

---
+++ Source/Csla.Generators/cs/AutoImplementProperties/Csla.Generator.AutoImplementProperties.CSharp.Tests/ModuleInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using VerifyTests;
+
+
+namespace Csla.Generator.Tests
+{
+  public static class ModuleInitializer
+  {
+    [ModuleInitializer]
+    public static void Init()
+    {
+      VerifySourceGenerators.Initialize();
+    }
+  }
+}

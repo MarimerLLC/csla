5d32e253ba176790ba7f2767109eb5d626d8d7b9
Rockford Lhotka <rocky@lhotka.net>
2025-05-19 15:00:21 -0500

    #4612 Stop caching current principal

--- Source/Csla.Blazor.WebAssembly/ApplicationContextManager.cs
+++ Source/Csla.Blazor.WebAssembly/ApplicationContextManager.cs
@@ -11,180 +11,177 @@ using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
 using System.Security.Principal;
 
-namespace Csla.Blazor.WebAssembly
+namespace Csla.Blazor.WebAssembly;
+
+/// <summary>
+/// Application context manager that uses HttpContextAccessor when 
+/// resolving HttpContext to store context values.
+/// </summary>
+public class ApplicationContextManager : IContextManager, IDisposable
 {
   /// <summary>
-  /// Application context manager that uses HttpContextAccessor when 
-  /// resolving HttpContext to store context values.
+  /// Creates an instance of the object, initializing it
+  /// with the required IServiceProvider.
   /// </summary>
-  public class ApplicationContextManager : IContextManager, IDisposable
+  /// <param name="authenticationStateProvider">AuthenticationStateProvider service</param>
+  public ApplicationContextManager(AuthenticationStateProvider authenticationStateProvider)
   {
-    /// <summary>
-    /// Creates an instance of the object, initializing it
-    /// with the required IServiceProvider.
-    /// </summary>
-    /// <param name="authenticationStateProvider">AuthenticationStateProvider service</param>
-    public ApplicationContextManager(AuthenticationStateProvider authenticationStateProvider)
+    AuthenticationStateProvider = authenticationStateProvider;
+    AuthenticationStateProvider.AuthenticationStateChanged += AuthenticationStateProvider_AuthenticationStateChanged;
+    InitializeUser();
+  }
+
+  private Task<AuthenticationState> AuthenticationState { get; set; }
+  private bool disposedValue;
+
+  /// <summary>
+  /// Gets the current AuthenticationStateProvider instance.
+  /// </summary>
+  protected AuthenticationStateProvider AuthenticationStateProvider { get; }
+
+  /// <summary>
+  /// Gets or sets a reference to the current ApplicationContext.
+  /// </summary>
+  public ApplicationContext ApplicationContext { get; set; }
+
+  private void InitializeUser()
+  {
+    AuthenticationStateProvider_AuthenticationStateChanged(AuthenticationStateProvider.GetAuthenticationStateAsync());
+  }
+
+  private void AuthenticationStateProvider_AuthenticationStateChanged(Task<AuthenticationState> task)
+  {
+    AuthenticationState = task;
+  }
+
+  /// <summary>
+  /// Gets a value indicating whether this
+  /// context manager is valid for use in
+  /// the current environment.
+  /// </summary>
+  public bool IsValid => true;
+
+  /// <summary>
+  /// Gets a value indicating whether the current runtime
+  /// is stateful (e.g. WPF, Blazor, etc.)
+  /// </summary>
+  public bool IsStatefulContext => true;
+
+  private static readonly IPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+  /// <summary>
+  /// Gets the current principal.
+  /// </summary>
+  public IPrincipal GetUser()
+  {
+    IPrincipal result;
+    if (AuthenticationState.IsCompleted && AuthenticationState.Result != null)
+      result = AuthenticationState.Result.User;
+    else
+      result = _anonymous;
+    return result;
+  }
+
+  /// <summary>
+  /// Sets the current principal ONLY IN APPLICATIONCONTEXT.
+  /// To set the value correctly, use your specific
+  /// ApplicationContextStateProvider implementation.
+  /// </summary>
+  /// <param name="principal">Principal object.</param>
+  public virtual void SetUser(IPrincipal principal)
+  {
+    throw new NotSupportedException(nameof(SetUser));
+  }
+
+  /// <summary>
+  /// Gets the local context.
+  /// </summary>
+  public IContextDictionary GetLocalContext()
+  {
+    IContextDictionary localContext;
+    var sessionManager = ApplicationContext.GetRequiredService<ISessionManager>();
+    var session = sessionManager.GetCachedSession();
+    session.TryGetValue("localContext", out var result);
+    if (result is IContextDictionary context)
     {
-      AuthenticationStateProvider = authenticationStateProvider;
-      AuthenticationStateProvider.AuthenticationStateChanged += AuthenticationStateProvider_AuthenticationStateChanged;
-      InitializeUser();
+      localContext = context;
     }
-
-    private Task<AuthenticationState> AuthenticationState { get; set; }
-    private ClaimsPrincipal _currentPrincipal;
-    private bool disposedValue;
-
-    /// <summary>
-    /// Gets the current AuthenticationStateProvider instance.
-    /// </summary>
-    protected AuthenticationStateProvider AuthenticationStateProvider { get; }
-
-    /// <summary>
-    /// Gets or sets a reference to the current ApplicationContext.
-    /// </summary>
-    public ApplicationContext ApplicationContext { get; set; }
-
-    private void InitializeUser()
+    else
     {
-      AuthenticationStateProvider_AuthenticationStateChanged(AuthenticationStateProvider.GetAuthenticationStateAsync());
+      localContext = new ContextDictionary();
+      SetLocalContext(localContext);
     }
+    return localContext;
+  }
 
-    private void AuthenticationStateProvider_AuthenticationStateChanged(Task<AuthenticationState> task)
+  /// <summary>
+  /// Sets the local context.
+  /// </summary>
+  /// <param name="localContext">Local context.</param>
+  public void SetLocalContext(IContextDictionary localContext)
+  {
+    var sessionManager = ApplicationContext.GetRequiredService<ISessionManager>();
+    var session = sessionManager.GetCachedSession();
+    session["localContext"] = localContext;
+  }
+
+  /// <summary>
+  /// Gets the client context.
+  /// </summary>
+  /// <param name="executionLocation"></param>
+  public IContextDictionary GetClientContext(ApplicationContext.ExecutionLocations executionLocation)
+  {
+    IContextDictionary clientContext;
+    var sessionManager = ApplicationContext.GetRequiredService<ISessionManager>();
+    var session = sessionManager.GetCachedSession();
+    session.TryGetValue("clientContext", out var result);
+    if (result is IContextDictionary context)
     {
-      _currentPrincipal = null;
-      AuthenticationState = task;
+      clientContext = context;
     }
-
-    /// <summary>
-    /// Gets a value indicating whether this
-    /// context manager is valid for use in
-    /// the current environment.
-    /// </summary>
-    public bool IsValid => true;
-
-    /// <summary>
-    /// Gets a value indicating whether the current runtime
-    /// is stateful (e.g. WPF, Blazor, etc.)
-    /// </summary>
-    public bool IsStatefulContext => true;
-
-    /// <summary>
-    /// Gets the current principal.
-    /// </summary>
-    public IPrincipal GetUser()
+    else
     {
-      if (_currentPrincipal == null)
+      clientContext = new ContextDictionary();
+      SetClientContext(clientContext, ApplicationContext.ExecutionLocation);
+    }
+    return clientContext;
+  }
+
+  /// <summary>
+  /// Sets the client context.
+  /// </summary>
+  /// <param name="clientContext">Client context.</param>
+  /// <param name="executionLocation"></param>
+  public void SetClientContext(IContextDictionary clientContext, ApplicationContext.ExecutionLocations executionLocation)
+  {
+    var sessionManager = ApplicationContext.GetRequiredService<ISessionManager>();
+    var session = sessionManager.GetCachedSession();
+    session["clientContext"] = clientContext;
+  }
+
+  /// <summary>
+  /// Dispose this object's resources.
+  /// </summary>
+  /// <param name="disposing"></param>
+  protected virtual void Dispose(bool disposing)
+  {
+    if (!disposedValue)
+    {
+      if (disposing)
       {
-        if (AuthenticationState.IsCompleted && AuthenticationState.Result != null)
-          _currentPrincipal = AuthenticationState.Result.User;
-        else
-          _currentPrincipal = new ClaimsPrincipal();
+        AuthenticationStateProvider.AuthenticationStateChanged -= AuthenticationStateProvider_AuthenticationStateChanged;
       }
-      return _currentPrincipal;
+      disposedValue = true;
     }
+  }
 
-    /// <summary>
-    /// Sets the current principal ONLY IN APPLICATIONCONTEXT.
-    /// To set the value correctly, use your specific
-    /// ApplicationContextStateProvider implementation.
-    /// </summary>
-    /// <param name="principal">Principal object.</param>
-    public virtual void SetUser(IPrincipal principal)
-    {
-      throw new NotSupportedException(nameof(SetUser));
-    }
-
-    /// <summary>
-    /// Gets the local context.
-    /// </summary>
-    public IContextDictionary GetLocalContext()
-    {
-      IContextDictionary localContext;
-      var sessionManager = ApplicationContext.GetRequiredService<ISessionManager>();
-      var session = sessionManager.GetCachedSession();
-      session.TryGetValue("localContext", out var result);
-      if (result is IContextDictionary context)
-      {
-        localContext = context;
-      }
-      else
-      {
-        localContext = new ContextDictionary();
-        SetLocalContext(localContext);
-      }
-      return localContext;
-    }
-
-    /// <summary>
-    /// Sets the local context.
-    /// </summary>
-    /// <param name="localContext">Local context.</param>
-    public void SetLocalContext(IContextDictionary localContext)
-    {
-      var sessionManager = ApplicationContext.GetRequiredService<ISessionManager>();
-      var session = sessionManager.GetCachedSession();
-      session["localContext"] = localContext;
-    }
-
-    /// <summary>
-    /// Gets the client context.
-    /// </summary>
-    /// <param name="executionLocation"></param>
-    public IContextDictionary GetClientContext(ApplicationContext.ExecutionLocations executionLocation)
-    {
-      IContextDictionary clientContext;
-      var sessionManager = ApplicationContext.GetRequiredService<ISessionManager>();
-      var session = sessionManager.GetCachedSession();
-      session.TryGetValue("clientContext", out var result);
-      if (result is IContextDictionary context)
-      {
-        clientContext = context;
-      }
-      else
-      {
-        clientContext = new ContextDictionary();
-        SetClientContext(clientContext, ApplicationContext.ExecutionLocation);
-      }
-      return clientContext;
-    }
-
-    /// <summary>
-    /// Sets the client context.
-    /// </summary>
-    /// <param name="clientContext">Client context.</param>
-    /// <param name="executionLocation"></param>
-    public void SetClientContext(IContextDictionary clientContext, ApplicationContext.ExecutionLocations executionLocation)
-    {
-      var sessionManager = ApplicationContext.GetRequiredService<ISessionManager>();
-      var session = sessionManager.GetCachedSession();
-      session["clientContext"] = clientContext;
-    }
-
-    /// <summary>
-    /// Dispose this object's resources.
-    /// </summary>
-    /// <param name="disposing"></param>
-    protected virtual void Dispose(bool disposing)
-    {
-      if (!disposedValue)
-      {
-        if (disposing)
-        {
-          AuthenticationStateProvider.AuthenticationStateChanged -= AuthenticationStateProvider_AuthenticationStateChanged;
-        }
-        disposedValue = true;
-      }
-    }
-
-    /// <summary>
-    /// Dispose this object's resources.
-    /// </summary>
-    public void Dispose()
-    {
-      // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-      Dispose(disposing: true);
-      GC.SuppressFinalize(this);
-    }
+  /// <summary>
+  /// Dispose this object's resources.
+  /// </summary>
+  public void Dispose()
+  {
+    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
   }
 }

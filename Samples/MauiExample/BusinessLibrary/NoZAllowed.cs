78a940267616c9a3aef68e064f8c714ff95ed174
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com>
2026-01-11 13:12:56 -0700

    #4788 Update for MAUI examples (#4793)
    Closes #4788

--- Samples/MauiExample/BusinessLibrary/NoZAllowed.cs
+++ Samples/MauiExample/BusinessLibrary/NoZAllowed.cs
@@ -1,18 +1,23 @@
-﻿using Csla.Rules;
+﻿using Csla.Core;
+using Csla.Rules;
 
-namespace BusinessLibrary
+namespace BusinessLibrary;
+
+public class NoZAllowed : BusinessRule
 {
-  public class NoZAllowed : BusinessRule
+  public NoZAllowed(IPropertyInfo primaryProperty)
+    : base(primaryProperty) 
   {
-    public NoZAllowed(Csla.Core.IPropertyInfo primaryProperty)
-      : base(primaryProperty)
-    { }
+    InputProperties.Add(primaryProperty);
+  }
 
-    protected override void Execute(IRuleContext context)
-    {
-      var text = (string)ReadProperty(context.Target, PrimaryProperty);
-      if (text.ToLower().Contains("z"))
-        context.AddErrorResult("No letter Z allowed");
-    }
+  protected override void Execute(IRuleContext context)
+  {
+    var text = (string?)context.InputPropertyValues[PrimaryProperty!];
+    if (string.IsNullOrWhiteSpace(text))
+      return;
+
+    if (text.IndexOf('z', StringComparison.OrdinalIgnoreCase) >= 0)
+      context.AddErrorResult("No letter Z allowed");
   }
 }
